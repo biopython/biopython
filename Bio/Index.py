09389b1c9139cae470b1a3e14725f2f9316da649
jchang <jchang>
2000-04-09 23:32:48 +0000

    added an _InMemoryIndex and made it the default

--- Bio/Index.py
+++ Bio/Index.py
@@ -10,11 +10,15 @@ This module provides a way to create indexes to text files.
 Classes:
 Index     Dictionary-like class used to store index information.
 
-"""
+_ShelveIndex    An Index class based on the shelve module.
+_InMemoryIndex  An in-memory Index class.
 
+"""
 import os
-import shelve
+import array
 import string
+import cPickle
+import shelve
 import UserDict
 
 class _ShelveIndex(UserDict.UserDict):
@@ -25,6 +29,7 @@ class _ShelveIndex(UserDict.UserDict):
     # generates large files.  When generating an index on a FASTA-
     # formatted file with 82000 sequences (37Mb), the 
     # index 'dat' file is 42Mb and 'dir' file is 8Mb.
+
     __version = 2
     __version_key = '__version'
 
@@ -60,37 +65,63 @@ class _ShelveIndex(UserDict.UserDict):
         if self.__dict__.has_key('data'):
             self.data.close()
 
-class _CustomIndex(UserDict.UserDict):
+class _InMemoryIndex(UserDict.UserDict):
     """This creates an in-memory index file.
 
-    # Format:
-    version
-    key value
-
     """
+    # File Format:
+    # version
+    # key value
+    # [...]
+    
     __version = 3
     __version_key = '__version'
 
     def __init__(self, indexname, truncate=None):
-        raise NotImplementedError
+        self._indexname = indexname
         UserDict.UserDict.__init__(self)
+        
+        # Remove the database if truncate is true.
         if truncate and os.path.exists(indexname):
             os.unlink(indexname)
 
+        # Load the database if it exists
         if os.path.exists(indexname):
             handle = open(indexname)
-            version = string.rstrip(handle.readline())
+            version = self._toobj(string.rstrip(handle.readline()))
             if version != self.__version:
                 raise IOError, "Version %s doesn't match my version %s" % \
                       (version, self.__version)
             lines = handle.readlines()
             lines = map(string.split, lines)
             for key, value in lines:
-                pass
+                key, value = self._toobj(key), self._toobj(value)
+                self[key] = value
             
     def __del__(self):
-        # XXX save file
-        if self.__dict__.has_key('data'):
-            self.data.close()
+        handle = open(self._indexname, 'w')
+        handle.write("%s\n" % self._tostr(self.__version))
+        for key, value in self.items():
+            handle.write("%s %s\n" % (self._tostr(key), self._tostr(value)))
+        handle.close()
 
-Index = _ShelveIndex
+    def _tostr(self, obj):
+        # I need a representation of the object that's saveable to
+        # a file that uses whitespace as delimiters.  Thus, I'm
+        # going to pickle the object, and then convert each character of
+        # the string to its ASCII integer value.  Then, I'm going to convert
+        # the integers into strings and join them together with commas. 
+        # It's not the most efficient way of storing things, but it's
+        # relatively fast.
+        s = cPickle.dumps(obj)
+        intlist = array.array('b', s)
+        strlist = map(str, intlist)
+        return string.join(strlist, ',')
+
+    def _toobj(self, str):
+        intlist = map(int, string.split(str, ','))
+        intlist = array.array('b', intlist)
+        strlist = map(chr, intlist)
+        return cPickle.loads(string.join(strlist, ''))
+
+Index = _InMemoryIndex
