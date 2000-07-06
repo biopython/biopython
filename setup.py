c4f03b84241f4a09280e3afd4de74861e12d2d12
jchang <jchang>
2000-07-06 21:50:42 +0000

    updated README MANIFEST.in now reflects Cayte's new directories

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     sys.exit(0)
 
 setup(name='biopython', 
-      version='0.1-d1',
+      version='0.90-d01',
       author='The Biopython Consortium',
       author_email='biopython@biopython.org',
       url='http://www.bipoython.org/',
