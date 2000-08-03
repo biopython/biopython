60b53acab50fd23b3cca490ea8c74baf367674d4
jchang <jchang>
2000-08-03 05:04:52 +0000

    changed copyright notice to Cayte Lindner, since she wrote the files

--- Tests/test_prodoc.py
+++ Tests/test_prodoc.py
@@ -1,4 +1,4 @@
-# Copyright 1999 by Jeffrey Chang.  All rights reserved.
+# Copyright 1999 by Cayte Lindner.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
