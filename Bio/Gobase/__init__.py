065477d96263625e759e249bdb4bf9d3708afaeb
katel <katel>
2000-09-05 02:54:38 +0000

    *** empty log message ***

---
+++ Bio/Gobase/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 1999 by Katharine Lindner.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+__all__ = ['Rebase']
