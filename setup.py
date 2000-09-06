8535499ea124d7dedb28a047925621de5f288e61
chapmanb <chapmanb>
2000-09-06 17:26:06 +0000

    Support new and old style formats for the extensions

--- setup.py
+++ setup.py
@@ -9,6 +9,44 @@ except ImportError:
     print "Requires distutils"
     sys.exit(0)
 
+# check if the distutils has the new extension class stuff
+# this is to support old distutils which do extensions differently
+try:
+    from distutils.extension import Extension
+    new_extension = 1
+except ImportError:
+    new_extension = 0
+
+if new_extension:
+    extensions = [Extension('Bio.Tools.Classification.cSVM',
+                            ['Bio/Tools/Classification/cSVMmodule.c']
+                            ),
+                  Extension('Bio.Tools.clistfns',
+                            ['Bio/Tools/clistfnsmodule.c']
+                            ),
+                  Extension('Bio.Tools.cmathfns',
+                            ['Bio/Tools/cmathfnsmodule.c']
+                            ),
+                  Extension('Bio.Tools.cstringfns',
+                            ['Bio/Tools/cstringfnsmodule.c']
+                            )
+                  ]
+else:
+    extensions = [('Bio.Tools.Classification.cSVM',
+                   { 'sources' : ['Bio/Tools/Classification/cSVMmodule.c'] }
+                   ),
+                  ('Bio.Tools.clistfns',
+                   { 'sources' : ['Bio/Tools/clistfnsmodule.c'] }
+                   ),
+                  ('Bio.Tools.cmathfns',
+                   { 'sources' : ['Bio/Tools/cmathfnsmodule.c'] }
+                   ),
+                  ('Bio.Tools.cstringfns',
+                   { 'sources' : ['Bio/Tools/cstringfnsmodule.c'] }
+                   )
+                  ]
+
+
 setup(name='biopython', 
       version='0.90-d02',
       author='The Biopython Consortium',
@@ -31,18 +69,7 @@ setup(name='biopython',
                 'Bio/Encodings',
                 'Bio/SeqIO'
                 ],
-      ext_modules = [('Bio.Tools.Classification.cSVM',
-                      { 'sources' : ['Bio/Tools/Classification/cSVMmodule.c'] }
-                      ),
-                     ('Bio.Tools.clistfns',
-                      { 'sources' : ['Bio/Tools/clistfnsmodule.c'] }
-                      ),
-                     ('Bio.Tools.cmathfns',
-                      { 'sources' : ['Bio/Tools/cmathfnsmodule.c'] }
-                      ),
-                     ('Bio.Tools.cstringfns',
-                      { 'sources' : ['Bio/Tools/cstringfnsmodule.c'] }
-                      )
-                     ]
+
+      ext_modules = extensions
       )
 
