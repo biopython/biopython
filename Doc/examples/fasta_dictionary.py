4361ec5a18b1c56ded58f0cf6b65387010c67a3c
chapmanb <chapmanb>
2000-09-10 12:33:54 +0000

    Some example scripts to go along with the documentation

---
+++ Doc/examples/fasta_dictionary.py
@@ -0,0 +1,26 @@
+import string
+from Bio.Fasta import Fasta
+from Bio.Alphabet import IUPAC
+
+def get_accession_num(fasta_record):
+    title_atoms = string.split(fasta_record.title)
+
+    accession_atoms = string.split(title_atoms[0], '|')
+
+    gb_name = accession_atoms[3]
+
+    # strip the version info before returning
+    return gb_name[:-2]
+
+
+Fasta.index_file("ls_orchid.fasta", "my_orchid_dict.idx",
+                 get_accession_num)
+
+dna_parser = Fasta.SequenceParser(IUPAC.ambiguous_dna)
+
+orchid_dict = Fasta.Dictionary("my_orchid_dict.idx", dna_parser)
+
+for id_num in orchid_dict.keys():
+    print 'id number:', id_num
+    print 'description:', orchid_dict[id_num].description
+    print 'sequence:', orchid_dict[id_num].seq
---
+++ Doc/examples/fasta_iterator.py
@@ -0,0 +1,31 @@
+import string
+from Bio.Fasta import Fasta
+
+def extract_organisms(file_to_parse):
+    # set up the parser and iterator
+    parser = Fasta.RecordParser()
+    file = open(file_to_parse, 'r')
+    iterator = Fasta.Iterator(file, parser)
+
+    all_species = []
+    cur_record = iterator.next()
+
+    while cur_record:
+        # extract the info from the title
+        title_atoms = string.split(cur_record.title)
+        new_species = title_atoms[1]
+
+        # append the new species to the list if it isn't there
+        if new_species not in all_species:
+            all_species.append(new_species)
+
+        # advance to the next record
+        cur_record = iterator.next()
+
+    return all_species
+
+if __name__ == "__main__":
+    all_species = extract_organisms("ls_orchid.fasta")
+    print "number of species:", len(all_species)
+    print 'species names:', all_species
+    
