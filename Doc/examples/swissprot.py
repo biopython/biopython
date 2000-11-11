1aa27b89af917f1a656bf1d0276fbbb709f2595e
chapmanb <chapmanb>
2000-11-11 22:42:46 +0000

    11 Nov 00 Brad * Added examples to go along with the Tutorial additions.

---
+++ Doc/examples/swissprot.py
@@ -0,0 +1,34 @@
+"""Example of connecting with exPASy and parsing SwissProt records."""
+# standard library
+import string
+
+# biopython
+from Bio.WWW import ExPASy
+from Bio.SwissProt import SProt
+from Bio import File
+
+# 'O23729', 'O23730', 'O23731', Chalcone synthases from Orchid
+
+ids = ['O23729', 'O23730', 'O23731']
+
+all_results = ''
+for id in ids:
+    results = ExPASy.get_sprot_raw(id)
+    all_results = all_results + results.read()
+
+s_parser = SProt.RecordParser()
+s_iterator = SProt.Iterator(File.StringHandle(all_results), s_parser)
+
+cur_record = s_iterator.next()
+
+while cur_record:
+    print "description:", cur_record.description
+    for ref in cur_record.references:
+        print "authors:", ref.authors
+        print "title:", ref.title
+
+    print "classification:", cur_record.organism_classification
+    print
+
+    cur_record = s_iterator.next()
+
