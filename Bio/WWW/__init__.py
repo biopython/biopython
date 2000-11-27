472a4820e3332694bb7bd519447cb2c195395343
jchang <jchang>
2000-11-27 05:24:26 +0000

    added a RequestLimiter class to artificially slow down WWW requests

--- Bio/WWW/__init__.py
+++ Bio/WWW/__init__.py
@@ -1,1 +1,14 @@
 # This is a Python module
+
+class RequestLimiter:
+    # This class implements a simple countdown timer for delaying WWW
+    # requests.
+    def __init__(self, delay):
+        self.last_time = 0.0
+        self.delay = delay
+    def wait(self):
+        how_long = self.last_time + self.delay - time.time()
+        if how_long > 0:
+            time.sleep(how_long)
+        self.last_time = time.time()
+
