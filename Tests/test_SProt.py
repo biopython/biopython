be4391a9e857262d19816461fda9b43c42641cf0
chapmanb <chapmanb>
2001-01-28 15:47:06 +0000

    Added test for SProt.py, and added test case with the optional OX line

---
+++ Tests/test_SProt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Test for the SProt parser on SwissProt files.
+"""
+import os
+from Bio.SwissProt import SProt
+
+test_files = ['sp001', 'sp002', 'sp003', 'sp004', 'sp005',
+              'sp006', 'sp007', 'sp008', 'sp009']
+
+record_parser = SProt.RecordParser()
+sequence_parser = SProt.SequenceParser()
+
+# test the record parser
+for test_file in test_files:
+    print "\ntesting %s..." % test_file
+    datafile = os.path.join('SwissProt', test_file)
+    test_handle = open(datafile)
+
+    record = record_parser.parse(test_handle)
+
+    # test a couple of things on the record -- this is not exhaustive
+    print record.entry_name
+    print record.accessions
+    print record.organism_classification
+    print record.features
+    print record.seqinfo
+
+    test_handle.close()
+
+# test the sequence parser
+for test_file in test_files:
+    print "\ntesting %s..." % test_file
+    datafile = os.path.join('SwissProt', test_file)
+    test_handle = open(datafile)
+
+    seq_record = sequence_parser.parse(test_handle)
+    print seq_record.id
+    print seq_record.name
+    print seq_record.description
+    print seq_record.seq
+
+    test_handle.close()
+    
+
+    
