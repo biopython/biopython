c19a2368cb899e1efde840098a2993a7d943566f
thomas <thomas>
2001-02-18 10:14:32 +0000

    /tmp/cvsPoPlAl

--- Scripts/xbbtools/nextorf.py
+++ Scripts/xbbtools/nextorf.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # Created: Thu Feb 15 14:22:12 2001
-# Last changed: Time-stamp: <01/02/15 17:09:02 thomas>
+# Last changed: Time-stamp: <01/02/18 11:16:42 thomas>
+# Copyright 2000 by Thomas Sicheritz-Ponten.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+# Authors: Thomas Sicheritz-Ponten and Jan O. Andersson
 # thomas@cbs.dtu.dk, http://www.cbs.dtu.dk/thomas
+# Jan.O.Andersson@home.se
 # File: nextorf.py
 
 import string, re
@@ -110,7 +116,7 @@ class NextOrf:
 
        gcall = 100.0*gcall/nall
        res = '%.1f%%, %.1f%%, %.1f%%, %.1f%%' % (gcall, gc[0], gc[1], gc[2])
-       print 'GC:', res
+#       print 'GC:', res
        return res
           
 
@@ -122,40 +128,39 @@ class NextOrf:
         n = len(seq)
         start_codons = self.table.start_codons
         stop_codons = self.table.stop_codons
+#        print 'Start codons', start_codons
+#        print 'Stop codons', stop_codons
         frame_coordinates = []
         for frame in range(0,3):
             coordinates = []
             for i in range(0+frame, n-n%3, 3):
                 codon = s[i:i+3]
-                if codon in start_codons: coordinates.append((i,1,codon))
-                elif codon in stop_codons: coordinates.append((i,0,codon))
-
-
+                if codon in start_codons: coordinates.append((i+1,1,codon))
+                elif codon in stop_codons: coordinates.append((i+1,0,codon))
             frame_coordinates.append(coordinates)
-
         return frame_coordinates
+     
 
     def HandleRecord(self, rec):
         frame_coordinates = ''
         dir = self.options['strand']
         plus = dir in ['both', 'plus']
         minus = dir in ['both', 'minus']
-
         start, stop = int(self.options['start']), int(self.options['stop'])
-        s = string.upper(rec.sequence[start:stop])
+        if stop > 0:
+           s = string.upper(rec.sequence[start:stop])
+        else:
+           s = string.upper(rec.sequence[start:])
+
         self.seq = Seq(s,IUPAC.ambiguous_dna)
         self.length = len(self.seq)
         self.rseq = None
-
-        
         CDS = []
         if plus: CDS.extend(self.GetCDS(self.seq))
         if minus:
             self.rseq = Seq(antiparallel(s),IUPAC.ambiguous_dna)
             CDS.extend(self.GetCDS(self.rseq, strand = -1))
-
         self.Output(CDS)
-            
         
 
     def GetCDS(self, seq, strand = 1):
@@ -168,34 +173,42 @@ class NextOrf:
         f = 0
         for frame in frame_coordinates:
             f+=1
-            start_site = -1
-            if nostart: start_site = 0
+            start_site = 0
+            if nostart == '1': start_site = 1
             frame.append((self.length, 0, 'XXX'))
             for pos, codon_type, codon in frame:
                 if codon_type == START:
-                    if start_site == -1:start_site = pos
+                    if start_site == 0: start_site = pos
                 elif codon_type == STOP:
-                    if start_site == -1: continue
-                    stop = pos
+                    if start_site == 0: continue
+#                    if codon == 'XXX': print 'do something'
+                    stop = pos + 2
+#                    print stop
                     length = stop - start_site +1
                     if length >= minlength and length <= maxlength:
-                        s = seq[start_site:stop +1]
+                        if nostart == '1' and start_site == 1:
+                           start_site = start_site + f - 1
+                        if codon == 'XXX': stop = start_site + 3*((int((stop-1)-start_site)/3))
+                        s = seq[start_site -1 : stop]
                         CDS.append((start_site, stop, length, s, strand*f))
-                        start_site = -1
-                        if nostart: start_site = pos + 3
-                        del stop
-
+                        start_site = 0
+                        if nostart == '1': start_site = stop + 1
+                    elif length < minlength or length > maxlength:
+                       start_site = 0
+                       if nostart == '1': start_site = stop + 1
+                    del stop   
         return CDS
+
     
 
     def Output(self, CDS):
         out = self.options['output']
         seqs = (self.seq, self.rseq)
-        
+        n = len(self.seq)
         for start, stop, length, subs, strand in CDS:
             self.counter += 1
-            head = 'orf_%s:%s:%d:%d:%d' % (self.counter, self.header, strand, start,stop+1)
-
+            if strand > 0: head = 'orf_%s:%s:%d:%d:%d' % (self.counter, self.header, strand, start,stop)
+            if strand < 0: head = 'orf_%s:%s:%d:%d:%d' % (self.counter, self.header, strand, n-stop+1,n-start+1)
             if self.options['gc']:
                 head = '%s:%s' % (head, self.Gc2(subs.data))
                 
@@ -211,22 +224,32 @@ class NextOrf:
     
 def help():
     global options
-    print 'Usage:', sys.argv[0], '<FASTA file> (<options>)'
+    print 'Usage:', sys.argv[0], '(<options>) <FASTA file>'
     
-    print 'Internal Options:'
-    for a,b in options.items(): print '\t', a,b
-    print ''
-    print "NCBI's Codon Tables:"
+    print 'Options:                                                       default'
+    print '--start       Start position in sequence                             0'
+    print '--stop        Stop position in sequence            (end of seqence)'
+    print '--minlength   Minimum length of orf in bp                          100'
+    print '--maxlength   Maximum length of orf in bp, default           100000000'
+    print '--strand      Strand to analyse [both, plus, minus]               both'
+    print '--frame       Frame to analyse [1 2 3]                             all'
+    print '--noframe     Ignore start codons [0 1]                              0'  
+    print '--output      Output to generate [aa nt pos]                        aa'
+    print '--gc          Creates GC statistics of ORF [0 1]                     0'
+    print '--table       Genetic code to use (see below)                        1'
+    
+#    for a,b in options.items(): print '\t', a,b
+#    print ''
+    print "\nNCBI's Codon Tables:"
     for key, table in CodonTable.ambiguous_dna_by_id.items():
         print '\t',key, table._codon_table.names[0]
-
-    print 'e.g.\n./nextorf.py --minlength 5 --strand plus --output nt --gc 1 testjan.fas'
+    print '\ne.g.\n./nextorf.py --minlength 5 --strand plus --output nt --gc 1 testjan.fas'
     sys.exit(0)
     
 
 options = {
     'start': 0,
-    'stop': -1,
+    'stop': 0,
     'minlength': 100,
     'maxlength': 100000000,
     'strand': 'both',
