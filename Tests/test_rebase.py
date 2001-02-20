bf947b431928a5cd883432318dc355fce1ca3d46
katel <katel>
2001-02-20 01:55:26 +0000

    *** empty log message ***

--- Tests/test_rebase.py
+++ Tests/test_rebase.py
@@ -4,38 +4,23 @@
 # as part of this package.
 
 import os
+import types
+import string
 from types import *
 from Bio import File
 from Bio import ParserSupport
-from Bio import Rebase
+from Bio.Rebase import Rebase
 
 
-tests = [ 'cac81.htm', 'bamii.htm', 'pvuii.htm', 'taqi.htm', 'foki.htm' ]
-#    'ps00488.txt', 'ps00546.txt' ]
-record_parser = Rebase.RecordParser()
+tests = [ 'bamii.htm', 'cac81.htm', 'crei.htm', 'foki.htm', 'pvuii.htm', \
+          'taqi.htm' ]
+record_parser = Rebase.RebaseParser()
 
-def print_list( list ):
-    for item in list:
-        print( '    ' + str( item ) )
 
 for test in tests:
     print "testing %s" % test
     datafile = os.path.join( 'Rebase', test )
     src_handle = open( datafile )
     data = record_parser.parse( src_handle )
-    print 'sequence'
-    print data.seq_5_to_3
-    print data.seq_3_to_5
-    print 'Enzyme number %s ' % data.enzyme_num
-    print ' prototype %s' % data.prototype
-    print data.source
-    print data.microorganism
-    print data.temperature
-    print data.date_entered
-#    print data.date_modified
-    print data.num_Adeno2
-    print data.num_Lambda
-    print data.num_pBR322
-    print data.num_PhiX174
-    print data.num_SV40
-
+    record_parser.print_tags()
+    print '\n'
