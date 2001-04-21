0676eeb9db720093a9c68710da67c14dcaf9e39e
dalke <dalke>
2001-04-21 07:20:58 +0000

    Upped version number to 0.6

--- Martel/__init__.py
+++ Martel/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2000-2001, Dalke Scientific Software, LLC
 # Distributed under the Biopython License Agreement (see the LICENSE file).
 
-__version__ = "0.5"
+__version__ = "0.6"
 
 import Expression
 import convert_re
