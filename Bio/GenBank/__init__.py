edf92b4500be21c27e898af2b01d3bce7fbea2c1
chapmanb <chapmanb>
2001-05-06 18:26:25 +0000

    Added additional check for retrieving GenBank records, if a sequence has been intentionally withdrawn, we'll now raise a KeyError.

--- Bio/GenBank/__init__.py
+++ Bio/GenBank/__init__.py
@@ -1319,6 +1319,10 @@ class NCBIDictionary:
         # Please try again later. Server error  for GI "7212005"
         elif line.find("Please try again later.") >= 0:
             raise KeyError, line
+        # if a sequuence has been withdrawn, we'll get a line like:
+        # The sequence has been intentionally withdrawn : GI "9993999"
+        elif line.find("intentionally withdrawn") >= 0:
+            raise KeyError, line
         elif line.lower().find('html') >= 0:
             raise KeyError, "I unexpectedly got back html-formatted data."
         # Parse the record if a parser was passed in.
