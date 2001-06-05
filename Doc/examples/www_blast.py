4b26c7caed6d6debe770ee986c3d100737e59e7a
chapmanb <chapmanb>
2001-06-05 02:06:17 +0000

    Fixed dumb bug of deep-copying a handle. Now things are handled in the right way.

--- Doc/examples/www_blast.py
+++ Doc/examples/www_blast.py
@@ -18,18 +18,17 @@ f_iterator = Fasta.Iterator(file_for_blast)
 f_record = f_iterator.next()
 
 print 'Doing the BLAST and retrieving the results...'
-b_results = NCBIWWW.blast('blastn', 'nr', f_record)
+result_handle = NCBIWWW.blast('blastn', 'nr', f_record)
 
-# save the results for later
+# save the results for later, in case we want to look at it
 save_file = open('m_cold_blast.out', 'w')
-# copy the handle so we can write it out and still have something for parsing
-save_handle = copy.deepcopy(b_results)
-save_file.write(save_handle.read())
+blast_results = result_handle.read()
+save_file.write(blast_results)
 save_file.close()
 
 print 'Parsing the results and extracting info...'
 b_parser = NCBIWWW.BlastParser()
-b_record = b_parser.parse(b_results)
+b_record = b_parser.parse_str(blast_results)
 
 # now get the alignment info for all e values greater than some threshold
 E_VALUE_THRESH = 0.1
