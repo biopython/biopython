2ddf8b0c495280ec807c682919f13dc39884bd0c
jchang <jchang>
2001-06-28 22:02:37 +0000

    added an optional parameter to RequestLimiter.wait to override default delay

--- Bio/WWW/__init__.py
+++ Bio/WWW/__init__.py
@@ -8,9 +8,10 @@ class RequestLimiter:
     def __init__(self, delay):
         self.last_time = 0.0
         self.delay = delay
-    def wait(self):
-        how_long = self.last_time + self.delay - time.time()
+    def wait(self, delay=None):
+        if delay is None:
+            delay = self.delay
+        how_long = self.last_time + delay - time.time()
         if how_long > 0:
             time.sleep(how_long)
         self.last_time = time.time()
-
