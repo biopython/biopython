8467343d1b6842ff8a72df4ec96666d10022453a
jchang <jchang>
2001-10-05 05:00:55 +0000

    added packages to do logistic regression and maximum entropy classification

---
+++ Bio/Tools/Classification/LogisticRegression.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""LogisticRegression.py
+
+This module provides code for doing logistic regressions.
+
+
+Classes:
+LogisticRegression    Holds information for a LogisticRegression classifier.
+
+
+Functions:
+train        Train a new classifier.
+calculate    Calculate the probabilities of each class, given an observation.
+classify     Classify an observation into a class.
+
+"""
+try:
+    from Numeric import *
+    from LinearAlgebra import *  # inverse
+except ImportError, x:
+    raise ImportError, "This module requires NumPy with the LinearAlgebra lib"
+
+from Bio.Tools import listfns
+
+class LogisticRegression:
+    """Holds information necessary to do logistic regression
+    classification.
+
+    Members:
+    beta    List of the weights for each dimension.
+
+    """
+    def __init__(self):
+        """LogisticRegression()"""
+        beta = []
+
+def train(xs, ys, update_fn=None, typecode=None):
+    """train(xs, ys[, update_fn]) -> LogisticRegression
+    
+    Train a logistic regression classifier on a training set.  xs is a
+    list of observations and ys is a list of the class assignments,
+    which should be 0 or 1.  xs and ys should contain the same number
+    of elements.  update_fn is an optional callback function that
+    takes as parameters that iteration number and log likelihood.
+    
+    """
+    if len(xs) != len(ys):
+        raise ValueError, "xs and ys should be the same length."
+    if not xs or not xs[0]:
+        raise ValueError, "No observations or observation of 0 dimension."
+    classes = listfns.items(ys)
+    classes.sort()
+    if classes != [0, 1]:
+        raise ValueError, "Classes should be 0's and 1's"
+    if typecode is None:
+        typecode = Float
+
+    # Dimensionality of the data is the dimensionality of the
+    # observations plus a constant dimension.
+    N, ndims = len(xs), len(xs[0]) + 1
+
+    # Make an X array, with a constant first dimension.
+    X = ones((N, ndims), typecode)
+    X[:, 1:] = xs
+    Xt = transpose(X)
+    y = asarray(ys, typecode)
+
+    # Initialize the beta parameter to 0.
+    beta = zeros(ndims, typecode)
+
+    MAX_ITERATIONS = 100
+    CONVERGE_THRESHOLD = 0.01
+    stepsize = 1.0
+    # Now iterate using Newton-Raphson until the log-likelihoods
+    # converge.
+    iter = 0
+    old_beta = old_llik = None
+    while iter < MAX_ITERATIONS:
+        # Calculate the probabilities.  p = e^(beta X) / (1+e^(beta X))
+        ebetaX = exp(dot(beta, Xt))
+        p = ebetaX / (1+ebetaX)
+        
+        # Find the log likelihood score and see if I've converged.
+        logp = y*log(p) + (1-y)*log(1-p)
+        llik = sum(logp)
+        if update_fn is not None:
+            update_fn(iter, llik)
+        # Check to see if the likelihood decreased.  If it did, then
+        # restore the old beta parameters and half the step size.
+        if llik < old_llik:
+            stepsize = stepsize / 2.0
+            beta = old_beta
+        # If I've converged, then stop.
+        if old_llik is not None and fabs(llik-old_llik) <= CONVERGE_THRESHOLD:
+            break
+        old_llik, old_beta = llik, beta
+        iter += 1
+
+        W = identity(N) * p
+        Xtyp = dot(Xt, y-p)         # Calculate the first derivative.
+        XtWX = dot(dot(Xt, W), X)   # Calculate the second derivative.
+        #u, s, vt = singular_value_decomposition(XtWX)
+        #print "U", u
+        #print "S", s
+        delta = dot(inverse(XtWX), Xtyp)
+        if fabs(stepsize-1.0) > 0.001:
+            delta = delta * stepsize
+        beta = beta + delta                 # Update beta.
+    else:
+        raise AssertionError, "Didn't converge."
+
+    lr = LogisticRegression()
+    lr.beta = map(float, beta)   # Convert back to regular array.
+    return lr
+
+def calculate(lr, x):
+    """calculate(lr, x) -> list of probabilities
+
+    Calculate the probability for each class.  lr is a
+    LogisticRegression object.  x is the observed data.  Returns a
+    list of the probability that it fits each class.
+
+    """
+    # Insert a constant term for x.
+    x = asarray([1.0] + x)
+    # Calculate the probability.  p = e^(beta X) / (1+e^(beta X))
+    ebetaX = exp(dot(lr.beta, x))
+    p = ebetaX / (1+ebetaX)
+    return [1-p, p]
+
+def classify(lr, x):
+    """classify(lr, x) -> 1 or 0
+
+    Classify an observation into a class.
+
+    """
+    probs = calculate(lr, x)
+    if probs[0] > probs[1]:
+        return 0
+    return 1
---
+++ Bio/Tools/Classification/MaxEntropy.py
@@ -0,0 +1,267 @@
+# Copyright 2001 by Jeffrey Chang.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""MaxEnt.py
+
+Maximum Entropy code.
+
+Uses Improved Iterative Scaling:
+XXX ref
+
+# XXX need to define terminology
+
+"""
+import math
+from Numeric import *
+from Bio.Tools import listfns
+
+# XXX typecodes for Numeric
+# XXX multiprocessor
+
+MAX_IIS_ITERATIONS = 10000    # Maximum iterations for IIS.
+IIS_CONVERGE = 1E-5           # Convergence criteria for IIS.
+MAX_NEWTON_ITERATIONS = 100   # Maximum iterations on Newton's method.
+NEWTON_CONVERGE = 1E-10       # Convergence criteria for Newton's method.
+
+class MaxEntropy:
+    """Holds information for a Maximum Entropy classifier.
+
+    Members:
+    classes      List of the possible classes of data.
+    alphas       List of the weights for each feature.
+    feature_fns  List of the feature functions.
+
+    """
+    def __init__(self):
+        self.classes = []
+        self.alphas = []
+        self.feature_fns = []
+
+def calculate(me, observation):
+    """calculate(me, observation) -> list of log probs
+
+    Calculate the log of the probability for each class.  me is a
+    MaxEntropy object that has been trained.  observation is a vector
+    representing the observed data.  The return value is a list of
+    unnormalized log probabilities for each class.
+
+    """
+    scores = []
+    for klass in range(len(me.classes)):
+        lprob = 0.0
+        for fn, alpha in map(None, me.feature_fns, me.alphas):
+            lprob += fn(observation, klass) * alpha
+        scores.append(lprob)
+    return scores
+
+def classify(me, observation):
+    """classify(me, observation) -> class
+
+    Classify an observation into a class.
+
+    """
+    scores = calculate(me, observation)
+    max_score, klass = scores[0], self.classes[0]
+    for i in range(1, len(scores)):
+        if scores[i] > max_score:
+            max_score, klass = scores[i], self.classes[i]
+    return klass
+
+def _eval_feature_fn(fn, xs, classes):
+    """_eval_feature_fn(fn, xs, classes) -> dict of values
+
+    Evaluate a feature function on every instance of the training set
+    and class.  fn is a callback function that takes two parameters: a
+    training instance and a class.  Return a dictionary of (training
+    set index, class index) -> non-zero value.  Values of 0 are not
+    stored in the dictionary.
+
+    """
+    values = {}
+    for i in range(len(xs)):
+        for j in range(len(classes)):
+            f = fn(xs[i], classes[j])
+            if f != 0:
+                values[(i, j)] = f
+    return values
+
+def _calc_empirical_expects(xs, ys, classes, features):
+    """_calc_empirical_expects(xs, ys, classes, features) -> list of expectations
+
+    Calculate the expectation of each function from the data.  This is
+    the constraint for the maximum entropy distribution.  Return a
+    list of expectations, parallel to the list of features.
+
+    """
+    # E[f_i] = SUM_x,y P(x, y) f(x, y)
+    #        = 1/N f(x, y)
+    class2index = listfns.itemindex(classes)
+    ys_i = [class2index[y] for y in ys]
+    
+    expect = []
+    N = len(xs)
+    for feature in features:
+        s = 0
+        for i in range(N):
+            s += feature.get((i, ys_i[i]), 0)
+        expect.append(float(s) / N)
+    return expect
+
+def _calc_model_expects(xs, classes, features, alphas):
+    """_calc_model_expects(xs, classes, features, alphas) -> list of expectations.
+
+    Calculate the expectation of each feature from the model.  This is
+    not used in maximum entropy training, but provides a good function
+    for debugging.
+
+    """
+    # SUM_X P(x) SUM_Y P(Y|X) F(X, Y)
+    # = 1/N SUM_X SUM_Y P(Y|X) F(X, Y)
+    p_yx = _calc_p_class_given_x(xs, classes, features, alphas)
+
+    expects = []
+    for feature in features:
+        sum = 0.0
+        for (i, j), f in feature.items():
+            sum += p_yx[i][j] * f
+        expects.append(sum/len(xs))
+    return expects
+
+def _calc_p_class_given_x(xs, classes, features, alphas):
+    """_calc_p_class_given_x(xs, classes, features, alphas) -> matrix
+
+    Calculate P(y|x), where y is the class and x is an instance from
+    the training set.  Return a XSxCLASSES matrix of probabilities.
+
+    """
+    prob_yx = zeros((len(xs), len(classes)), Float32)
+
+    # Calculate log P(y, x).
+    for feature, alpha in map(None, features, alphas):
+        for (x, y), f in feature.items():
+            prob_yx[x][y] += alpha * f
+    # Take an exponent to get P(y, x)
+    prob_yx = exp(prob_yx)
+    # Divide out the probability over each class, so we get P(y|x).
+    for i in range(len(xs)):
+        z = sum(prob_yx[i])
+        prob_yx[i] = prob_yx[i] / z
+
+    #prob_yx = []
+    #for i in range(len(xs)):
+    #    z = 0.0   # Normalization factor for this x, over all classes.
+    #    probs = [0.0] * len(classes)
+    #    for j in range(len(classes)):
+    #        log_p = 0.0   # log of the probability of f(x, y)
+    #        for k in range(len(features)):
+    #            log_p += alphas[k] * features[k].get((i, j), 0.0)
+    #        probs[j] = math.exp(log_p)
+    #        z += probs[j]
+    #    # Normalize the probabilities for this x.
+    #    probs = map(lambda x, z=z: x/z, probs)
+    #    prob_yx.append(probs)
+    return prob_yx
+
+def _calc_f_sharp(N, nclasses, features):
+    """_calc_f_sharp(N, nclasses, features) -> matrix of f sharp values."""
+    # f#(x, y) = SUM_i feature(x, y)
+    f_sharp = zeros((N, nclasses))
+    for feature in features:
+        for (i, j), f in feature.items():
+            f_sharp[i][j] += f
+    return f_sharp
+
+def _iis_solve_delta(N, feature, f_sharp, empirical, prob_yx):
+    # Solve delta using Newton's method for:
+    # SUM_x P(x) * SUM_c P(c|x) f_i(x, c) e^[delta_i * f#(x, c)] = 0
+    delta = 0.0
+    iters = 0
+    while iters < MAX_NEWTON_ITERATIONS: # iterate for Newton's method
+        f_newton = df_newton = 0.0       # evaluate the function and derivative
+        for (i, j), f in feature.items():
+            prod = prob_yx[i][j] * f * math.exp(delta * f_sharp[i][j])
+            f_newton += prod
+            df_newton += prod * f_sharp[i][j]
+        f_newton, df_newton = empirical - f_newton / N, -df_newton / N
+
+        ratio = f_newton / df_newton
+        delta -= ratio
+        if math.fabs(ratio) < NEWTON_CONVERGE:  # converged
+            break
+        iters = iters + 1
+    else:
+        raise "Newton's method did not converge"
+    return delta
+
+def _train_iis(xs, classes, features, f_sharp, alphas, e_empirical):
+    # Do one iteration of hill climbing to find better alphas.
+    # This is a good function to parallelize.
+
+    # Pre-calculate P(y|x)
+    p_yx = _calc_p_class_given_x(xs, classes, features, alphas)
+
+    N = len(xs)
+    newalphas = alphas[:]
+    for i in range(len(alphas)):
+        delta = _iis_solve_delta(N, features[i], f_sharp, e_empirical[i], p_yx)
+        newalphas[i] += delta
+    return newalphas
+
+
+def train(training_set, results, feature_fns, update_fn=None):
+    """train(training_set, results, feature_fns[, update_fn]) -> MaxEntropy object
+
+    Train a maximum entropy classifier on a training set.
+    training_set is a list of observations.  results is a list of the
+    class assignments for each observation.  feature_fns is a list of
+    the features.  These are callback functions that take an
+    observation and class and return a 1 or 0.  update_fn is a
+    callback function that's called at each training iteration.  It is
+    passed a MaxEntropy object that encapsulates the current state of
+    the training.
+    
+    """
+    if not len(training_set):
+        raise ValueError, "No data in the training set."
+    if len(training_set) != len(results):
+        raise ValueError, "training_set and results should be parallel lists."
+
+    # Rename variables for convenience.
+    xs, ys = training_set, results
+
+    # Get a list of all the classes that need to be trained.
+    classes = listfns.items(results)
+    classes.sort()
+
+    # Cache values for all features.
+    features = [_eval_feature_fn(fn, training_set, classes)
+                for fn in feature_fns]
+    # Cache values for f#.
+    f_sharp = _calc_f_sharp(len(training_set), len(classes), features)
+
+    # Pre-calculate the empirical expectations of the features.
+    e_empirical = _calc_empirical_expects(xs, ys, classes, features)
+
+    # Now train the alpha parameters to weigh each feature.
+    alphas = [0.0] * len(features)
+    iters = 0
+    while iters < MAX_IIS_ITERATIONS:
+        nalphas = _train_iis(xs, classes, features, f_sharp,
+                             alphas, e_empirical)
+        diff = map(lambda x, y: math.fabs(x-y), alphas, nalphas)
+        diff = reduce(lambda x, y: x+y, diff, 0)
+        alphas = nalphas
+        
+        me = MaxEntropy()
+        me.alphas, me.classes, me.feature_fns = alphas, classes, feature_fns
+        if update_fn is not None:
+            update_fn(me)
+    
+        if diff < IIS_CONVERGE:   # converged
+            break
+    else:
+        raise "IIS did not converge"
+
+    return me
