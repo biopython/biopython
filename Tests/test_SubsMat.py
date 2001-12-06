6fbec2aebe9ae95ac36beae62a5e2d417b7bfe91
idoerg <idoerg>
2001-12-06 15:44:39 +0000

    ---------------------------------------------------------------------- I hope this takes care of the "different float representations on different platforms" bug. Iddo. ----------------------------------------------------------------------

--- Tests/test_SubsMat.py
+++ Tests/test_SubsMat.py
@@ -41,9 +41,11 @@ lo_mat_prot = \
 SubsMat.make_log_odds_matrix(acc_rep_mat=acc_rep_mat,round_digit=1) #,ftab_prot
 f.write("\nLog odds matrix\n")
 f.write("\nLog odds half matrix\n")
-lo_mat_prot.print_mat(f=f,format=" %.1f",alphabet='AVILMCFWYHSTNQKRDEGP')
+# Was %.1f. Let us see if this is OK
+lo_mat_prot.print_mat(f=f,format=" %d",alphabet='AVILMCFWYHSTNQKRDEGP')
 f.write("\nLog odds full matrix\n")
-lo_mat_prot.print_full_mat(f=f,format=" %.1f",alphabet='AVILMCFWYHSTNQKRDEGP')
+# Was %.1f. Let us see if this is OK
+lo_mat_prot.print_full_mat(f=f,format=" %d",alphabet='AVILMCFWYHSTNQKRDEGP')
 
 f.write("\nTesting MatrixInfo\n")
 for i in MatrixInfo.available_matrices:
