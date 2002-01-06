c694c8ccb3f13e28283c9ae3c994473ac781bcde
chapmanb <chapmanb>
2002-01-06 21:15:32 +0000

    Prevent excessive errors on non-python2.2 with the new IO stuff

--- Bio/SeqRecord.py
+++ Bio/SeqRecord.py
@@ -9,12 +9,16 @@
 # annotations, and one of them will be a "vocabulary" to distinguish
 # between differences in vocabulary. :)
 
-from Bio import FormatIO
+try:
+    from Bio import FormatIO
 
-# Should this be in the module namespace or the record namespace?
-io = FormatIO.FormatIO("SeqRecord",
-                       default_input_format = "sequence",
-                       default_output_format = "fasta")
+    # Should this be in the module namespace or the record namespace?
+    io = FormatIO.FormatIO("SeqRecord",
+                           default_input_format = "sequence",
+                           default_output_format = "fasta")
+except SyntaxError: # we will get an error right now without python2.2
+    import warnings # requires python 2.1, so we aren't doing much better :-)
+    warnings.warn("No IO support because your python is too old.")
 
 
 class SeqRecord:                           
