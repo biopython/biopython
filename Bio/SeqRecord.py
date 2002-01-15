039d1d4e7820290bde18808fdf1fc53803cfd686
dalke <dalke>
2002-01-15 12:59:06 +0000

    Removed need for warnings for Python 2.2. Added support for features and database cross references.

--- Bio/SeqRecord.py
+++ Bio/SeqRecord.py
@@ -9,28 +9,31 @@
 # annotations, and one of them will be a "vocabulary" to distinguish
 # between differences in vocabulary. :)
 
-try:
-    from Bio import FormatIO
+from Bio import FormatIO
 
-    # Should this be in the module namespace or the record namespace?
-    io = FormatIO.FormatIO("SeqRecord",
-                           default_input_format = "sequence",
-                           default_output_format = "fasta")
-except SyntaxError: # we will get an error right now without python2.2
-    import warnings # requires python 2.1, so we aren't doing much better :-)
-    warnings.warn("No IO support because your python is too old.")
+# Should this be in the module namespace or the record namespace?
+io = FormatIO.FormatIO("SeqRecord",
+                       default_input_format = "sequence",
+                       default_output_format = "fasta")
 
 
 class SeqRecord:                           
     def __init__(self, seq, id = "<unknown id>", name = "<unknown name>",
-                 description = "<unknown description>"):
+                 description = "<unknown description>", dbxrefs = None,
+                 features = None):
         self.seq = seq
         self.id = id
         self.name = name
         self.description = description
+        if dbxrefs is None:
+            dbxrefs = []
+        self.dbxrefs = dbxrefs
         # annotations about the whole sequence
         self.annotations = {}
         
+        
         # annotations about parts of the sequence
-        self.features = []
+        if features is None:
+            features = []
+        self.features = features
         
