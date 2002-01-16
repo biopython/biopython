328d84d157eb106c62a3fcedb8f46b15e9f20094
dalke <dalke>
2002-01-16 16:12:40 +0000

    Fixed problem in version checking.

--- Bio/SeqRecord.py
+++ Bio/SeqRecord.py
@@ -2,17 +2,13 @@
 
 # NEEDS TO BE SYNCH WITH THE REST OF BIOPYTHON AND BIOPERL
 
-try:
+import sys
+if getattr(sys, "version_info", (1, 5))[:2] >= (2,1):
   from Bio import FormatIO
   # Should this be in the module namespace or the record namespace?
   io = FormatIO.FormatIO("SeqRecord",
                          default_input_format = "sequence",
                          default_output_format = "fasta")
-except ImportError:
-  import sys
-  if getattr(sys, "version_info", (1, 5))[:2] >= (2, 1):
-    raise
-
 
 
 class SeqRecord:                           
