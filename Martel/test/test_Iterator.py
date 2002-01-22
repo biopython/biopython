1338fa37698048ce2bd354d355cecaf22429ee28
dalke <dalke>
2002-01-22 20:33:01 +0000

    Added more test cases. Removed print statements.

--- Martel/test/test_Iterator.py
+++ Martel/test/test_Iterator.py
@@ -3,6 +3,7 @@
 import os
 from testformats import swissprot38
 import Martel
+from Martel import RecordReader, Parser
 from xml.sax import handler
 
 import test_swissprot38
@@ -16,7 +17,10 @@ def test1():
     stream = iterator.iterateString(text, handler.ContentHandler())
     i = 0
     while 1:
-        x = stream.next()
+        try:
+            x = stream.next()
+        except StopIteration:  # for IterParser objects
+            x = None
         if x is None:
             break
         i = i + 1
@@ -91,18 +95,203 @@ def test5():
     except StopIteration:
         pass
 
+def test_header_footer1():
+    exp = Martel.HeaderFooter("dataset", {},
+                              Martel.Re("header\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("end\R"),
+                              RecordReader.CountLines, (1,),)
+    lines = [
+        "header",
+        "aa",
+        "aba",
+        "abba",
+        "end"
+        ]
+    text = "\n".join(lines) + "\n"
+
+    i = 0
+    for info in exp.make_iterator("b").iterateString(text):
+        assert len(info["b"]) == 1
+        assert len(info["b"][0]) == i, (info["b"][0], i)
+        i = i +1
+    assert i == 3, i
+                        
+
+def test_header_footer2():
+    exp = Martel.HeaderFooter("dataset", {},
+                              None,
+                              None, None,
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("end\R"),
+                              RecordReader.CountLines, (1,),)
+    lines = [
+        "aa",
+        "aba",
+        "abba",
+        "end"
+        ]
+    text = "\n".join(lines) + "\n"
+
+    i = 0
+    for info in exp.make_iterator("b").iterateString(text):
+        assert len(info["b"]) == 1
+        assert len(info["b"][0]) == i, (info["b"][0], i)
+        i = i +1
+    assert i == 3, i
+                        
+
+def test_header_footer3():
+    exp = Martel.HeaderFooter("dataset", {},
+                              None,
+                              None, None,
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              None,
+                              None, None)
+    lines = [
+        "aa",
+        "aba",
+        "abba",
+        ]
+    text = "\n".join(lines) + "\n"
+
+    i = 0
+    for info in exp.make_iterator("b").iterateString(text):
+        assert len(info["b"]) == 1
+        assert len(info["b"][0]) == i, (info["b"][0], i)
+        i = i +1
+    assert i == 3, i
+                        
+
+def test_header_footer4():
+    # Test that the errors are correct
+    exp = Martel.HeaderFooter("dataset", {},
+                              Martel.Re("header\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("end\R"),
+                              RecordReader.CountLines, (1,),)
+    lines = [
+        "HEADER",
+        "aa",
+        "aba",
+        "abba",
+        "end"
+        ]
+    text = "\n".join(lines) + "\n"
+
+    try:
+        for info in exp.make_iterator("b").iterateString(text):
+            pass
+    except Parser.ParserPositionException, exc:
+        assert exc.pos == 0
+                        
+
+def test_header_footer5():
+    exp = Martel.HeaderFooter("dataset", {},
+                              None,
+                              None, None,
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              Martel.Re("end\R"),
+                              RecordReader.CountLines, (1,),)
+    lines = [
+        "aa",
+        "aba",
+        "abba",
+        "END"
+        ]
+    text = "\n".join(lines) + "\n"
+
+    try:
+        for info in exp.make_iterator("b").iterateString(text):
+            assert len(info["b"]) == 1
+    except Parser.ParserPositionException, exc:
+        assert exc.pos == 12, exc.pos
+                        
+
+def test_header_footer6():
+    exp = Martel.HeaderFooter("dataset", {},
+                              None,
+                              None, None,
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              None,
+                              None, None)
+    lines = [
+        "aA",
+        "aBbbba",
+        "abba",
+        ]
+    text = "\n".join(lines) + "\n"
+
+    try:
+        for info in exp.make_iterator("b").iterateString(text):
+            pass
+    except Parser.ParserPositionException, exc:
+        assert exc.pos == 1, exc.pos
+
+def test_header_footer7():
+    exp = Martel.HeaderFooter("dataset", {},
+                              None,
+                              None, None,
+                              
+                              Martel.Re("a(?P<b>b*)a\R"),
+                              RecordReader.CountLines, (1,),
+                              
+                              None,
+                              None, None)
+    lines = [
+        "aa",
+        "aBbbba",
+        "abba",
+        ]
+    text = "\n".join(lines) + "\n"
+
+    try:
+        for info in exp.make_iterator("b").iterateString(text):
+            pass
+    except Parser.ParserPositionException, exc:
+        assert exc.pos == 4, exc.pos
+                        
+
     
 def test():
-    print "Iteration through all records"
+    # print "Iteration through all records"
     test1()
-    print "Iteration a record at a time"
+    # print "Iteration a record at a time"
     test2()
-    print "Test non-record-based parsers"
+    # print "Test non-record-based parsers"
     test3()
-    print "Test return of LAX objects"
+    # print "Test return of LAX objects"
     test4()
-    print "Test Python 2.2 iterators"
+    # print "Test Python 2.2 iterators"
     test5()
+
+    test_header_footer1()
+    test_header_footer2()
+    test_header_footer3()
+    test_header_footer4()
+    test_header_footer5()
+    test_header_footer6()
+    test_header_footer7()
 
 if __name__ == "__main__":
     test()
