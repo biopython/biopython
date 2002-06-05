0c9e154cde7fe5e65fcd79b2388b9c36cdf376c9
nisse <nisse>
2002-06-05 15:01:08 +0000

    A class to save a Structure object in a PDB file.

---
+++ Bio/PDB/PDBIO.py
@@ -0,0 +1,75 @@
+ATOM_FORMAT_STRING="%s%5i %-4s%c%3s %c%4i%c   %8.3f%8.3f%8.3f%6.2f%6.2f      %4s%2s%2s\n"
+
+
+class PDBIO:
+	def __init__(self):
+		pass
+
+	# Private methods
+
+	def _get_atom_line(self, atom, hetfield, segid, serial, resname, 
+				resseq, icode, chain_id, element="  ", charge="  "):
+		if hetfield!=" ":
+			record_type="HETATM"
+		else:
+			record_type="ATOM  "
+		name=atom.get_fullname()
+		altloc=atom.get_altloc()
+		x, y, z=atom.get_coord()
+		bfactor=atom.get_bfactor()
+		occupancy=atom.get_occupancy()
+		args=(record_type, serial, name, altloc, resname, chain_id,
+			resseq, icode, x, y, z, occupancy, bfactor, segid,
+			element, charge)
+		return ATOM_FORMAT_STRING % args
+		
+	# Public methods
+
+	def set_structure(self, structure):
+		self.structure=structure
+
+	def save(self, filename):
+		fp=open(filename, "w")
+		get_atom_line=self._get_atom_line
+		# multiple models?
+		if len(self.structure)>1:
+			model_flag=1
+		else:
+			model_flag=0
+		for model in self.structure.get_list():
+			serial=0
+			if model_flag:
+				fp.write("MODEL\n")
+			for chain in model.get_list():
+				chain_id=chain.get_id()
+				for residue in chain.get_unpacked_list():
+					hetfield, resseq, icode=residue.get_id()
+					resname=residue.get_resname()  
+					segid=residue.get_segid()
+					for atom in residue.get_unpacked_list():
+						s=get_atom_line(atom, hetfield, segid, serial, resname,
+								resseq, icode, chain_id)
+						fp.write(s)
+						serial=serial+1
+			if model_flag:
+				fp.write("ENDMDL\n")
+		fp.close()
+
+if __name__=="__main__":
+	
+	from Bio.PDB.PDBParser import PDBParser
+
+	p=PDBParser(PERMISSIVE=1)
+
+	s=p.get_structure("test", "../../Tests/PDB/a_structure.pdb")
+
+	io=PDBIO()
+
+	io.set_structure(s)
+
+	io.save("scr.pdb")
+
+
+
+
+
