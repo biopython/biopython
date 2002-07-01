ab091912f10dbfffaaea9ffb2806eaff745293c7
chapmanb <chapmanb>
2002-07-01 14:59:09 +0000

    Updated happydoc script to work with changes to happydoc

--- Doc/make_api_docs.py
+++ Doc/make_api_docs.py
@@ -6,7 +6,7 @@ This uses HappyDoc (http://sourceforge.net/projects/happydoc) to do the
 documentation.
 
 Usage:
-* Set your PYTHONPATH to include the HappyDoc directory.
+* Set your PYTHONPATH to include the HappyDoc directory. 
 * Run this file from the Doc directory as './make_api_docs.py'
 
 Right now this is kludgy and rough and needs work."""
@@ -23,25 +23,13 @@ import sys
 if os.path.basename(os.getcwd()) == 'Doc':
     os.chdir(os.pardir)
 
-# Kludge: manipulate sys.argv so it will look like happydoc is being called
-# via the happydoc.py program.
+# Kludge: manipulate sys.argv to add the Bio directory to be happydoc'ed
+sys.argv.append('Bio')
+sys.argv.append('Martel')
+sys.argv.append('BioSQL')
 
 try:
-    # this is a HappyDoc file. We can't import happydoc_class or anything
-    # that imports it until after we do this sys.argv hack.
-    import StructuredText
-except ImportError:
-    print IMPORT_MESSAGE
-    sys.exit(1)
-
-# find where happydoc is via the imported StructuredText file
-happydoc_location = os.path.dirname(StructuredText.__file__)
-print 'happydoc_location:', happydoc_location
-
-sys.argv = [os.path.join(happydoc_location, 'happydoc.py'), 'Bio']
-
-try:
-    from happydoc_class import HappyDoc
+    from happydoclib import HappyDoc
 except ImportError:
     print IMPORT_MESSAGE
     sys.exit()
@@ -53,7 +41,8 @@ if __name__ == '__main__':
         doc_gen = HappyDoc()
 
         # set parameters for Biopython
-        doc_gen.author_name = 'The Biopython folks (biopython@biopython.org)'
+        doc_gen.author_name = 'The Biopython Consortium ' + \
+                              '(biopython@biopython.org)'
         doc_gen.output_directory = 'Doc/api'
         doc_gen.app_home = 'http://www.biopython.org'
         doc_gen.docset_title = 'Biopython API documentation'
