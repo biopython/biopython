de3b60f854900e470fb83f7e7c7eef075ea31c09
nisse <nisse>
2002-07-02 11:15:35 +0000

    KDTree regression test initial checkin.

---
+++ Tests/test_KDTree.py
@@ -0,0 +1,10 @@
+from Bio.Tools.KDTree import _neighbor_test, _test
+
+nr_points=5000
+dim=3
+bucket_size=5
+radius=0.01
+
+for i in range(0, 10):
+	_neighbor_test(nr_points, dim, bucket_size, radius)
+	_test(nr_points, dim, bucket_size, radius)
