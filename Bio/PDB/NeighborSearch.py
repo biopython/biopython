ea36f0c95a21ed046b22e63c827e02adbd541239
nisse <nisse>
2002-07-17 07:38:07 +0000

    Added comments.

--- Bio/PDB/NeighborSearch.py
+++ Bio/PDB/NeighborSearch.py
@@ -5,13 +5,22 @@ from Numeric import array
 
 class NeighborSearch:
 	"""
-	Find all atoms/residues/chains/models/structures within radius 
-	of a given vector.
+	This module can be used in two ways:
+
+	1. To find all atoms/residues/chains/models/structures within radius 
+	of a given query vector. 
+
+	2. To find all atoms/residues/chains/models/structures that are within 
+	a fixed radius of each other.
+
+	NeighborSearch makes use of the KD tree C++ module.
 	"""
 	def __init__(self, atom_list, bucket_size=10):
 		"""
-		o atom_list - list of atoms :-)
-		o bucket_size - bucket size of KD tree
+		o atom_list - list of atoms. This list is used in the queries.
+		It can contain atoms from different structures.
+		o bucket_size - bucket size of KD tree. You can play around 
+		with this to optimize speed if you feel like it.
 		"""
 		self.atom_list=atom_list
 		# get the coordinates
@@ -22,8 +31,15 @@ class NeighborSearch:
 		assert(self.coords.typecode()=="f")
 		self.kdt=KDTree(3, bucket_size)
 		self.kdt.set_coords(self.coords)
+	
+	# Private
 
 	def _get_unique_parent_pairs(self, pair_list):
+		# translate a list of (entity, entity) tuples to 
+		# a list of (parent entity, parent entity) tuples,
+		# thereby removing duplicate (parent entity, parent entity)
+		# pairs.
+		# o pair_list - a list of (entity, entity) tuples
 		parent_pair_list=[]
 		for (e1, e2) in pair_list:
 			p1=e1.get_parent()
@@ -36,9 +52,20 @@ class NeighborSearch:
 				parent_pair_list.append((p2, p1))
 		return uniqueify(parent_pair_list)
 
+	# Public
+
 	def search(self, center, radius, level="A"):
-		"""
-		Neighbor search.
+		"""Neighbor search.
+
+		Return all atoms/residues/chains/models/structures
+		that have at least one atom within radius of center.
+		What entitity level is returned (e.g. atoms or residues)
+		is determined by level (A=atoms, R=residues, C=chains,
+		M=models, S=structures).
+
+		o center - Numpy array 
+		o radius - float
+		o level - char (A, R, C, M, S)
 		"""
 		if not level in entity_levels:
 			raise Exception, "%s: Unknown level" % level
@@ -55,8 +82,13 @@ class NeighborSearch:
 			return unfold_entities(n_atom_list, level)
 			
 	def search_all(self, radius, level="A"):
-		"""
-		All neighbor search.
+		"""All neighbor search.
+
+		Search all entities that have atoms pairs within
+		radius. 
+
+		o radius - float
+		o level - char (A, R, C, M, S)
 		"""
 		if not level in entity_levels:
 			raise Exception, "%s: Unknown level" % level
