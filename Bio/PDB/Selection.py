3db84750fc4570cd9d1f7328e1f126e031fb123f
nisse <nisse>
2002-07-17 07:44:26 +0000

    Added comments.

--- Bio/PDB/Selection.py
+++ Bio/PDB/Selection.py
@@ -12,6 +12,10 @@ def uniqueify(l):
 	return d.keys()
 
 def get_unique_parents(entity_list):
+	"""
+	Translate a list of entities to a list of their
+	(unique) parents.
+	"""	
 	l=[]
 	for entity in entity_list:
 		parent=entity.get_parent()
@@ -19,10 +23,23 @@ def get_unique_parents(entity_list):
 	return uniqueify(l)
 
 def unfold_entities(entity_list, target_level):
+	"""
+	Unfold a list of entities to a list of entities of another 
+	level.  E.g.:
+
+	list of atoms -> list of residues
+	list of modules -> list of atoms
+	list of residues -> list of chains
+
+	o entity_list - list of entities or a single entity
+	o target_level - char (A, R, C, M, S)
+	"""
 	if not target_level in entity_levels:
 		raise Exception, "%s: Not an entity level." % target_level
 	if type(entity_list)!=ListType:
+		# single entity
 		entity_list=[entity_list]
+	# level of entity list
 	level=entity_list[0].get_level()
 	for entity in entity_list:
 		if not (entity.get_level()==level):
@@ -30,19 +47,23 @@ def unfold_entities(entity_list, target_level):
 	target_index=entity_levels.index(target_level)
 	level_index=entity_levels.index(level)
 	if level_index==target_index:
+		# already right level
 		return entity_list
 	if level_index>target_index:
+		# we're going down, e.g. S->A
 		for i in range(target_index, level_index):
 			new_entity_list=[]
 			for entity in entity_list:
 				new_entity_list=new_entity_list+entity.get_list()
 			entity_list=new_entity_list
 	else:
+		# we're going up, e.g. A->S
 		for i in range(level_index, target_index):
 			new_entity_list=[]	
 			for entity in entity_list:
 				parent=entity.get_parent()
 				new_entity_list.append(parent)
+			# find unique parents
 			entity_list=uniqueify(new_entity_list)
 	return entity_list
 
