bc0689e68259137f13e51ba611e5f7205357bef1
nisse <nisse>
2002-12-10 12:53:08 +0000

    Changed assert to raise PDBConstructionException for duplicated entities.

--- Bio/PDB/Entity.py
+++ Bio/PDB/Entity.py
@@ -108,7 +108,8 @@ class Entity:
 	def add(self, entity):
 		"Add a child to the Entity."
 		entity_id=entity.get_id()
-		assert(not self.has_id(entity_id))
+		if self.has_id(entity_id):
+			raise PDBConstructionException, "%s defined twice" % entity.get_full_id()
 		entity.set_parent(self)
 		self.child_list.append(entity)
 		#self.child_list.sort(self._sort)
