82fcbd6f036f85b97ba7576c83035cce2b5e0127
chapmanb <chapmanb>
2003-06-14 19:35:41 +0000

    Raise import error on GFF test if not configured

--- Tests/test_GFF.py
+++ Tests/test_GFF.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Test the Bio.GFF module and dependencies
 """
-
+import os
 import MySQLdb
 import Bio.GFF
 import Bio.GFF.GenericTools
@@ -16,5 +16,10 @@ Bio.GFF.easy._test()
 print "Bio.GFF.easy doctests complete."
 
 print "Running Bio.GFF doctests..."
-Bio.GFF._test()
+# only do the test if we are set up to do it. We need to have MYSQLPASS
+# set and have a GFF wormbase installed (see the code in Bio/GFF/__init_.py
+if os.environ.has_key("MYSQLPASS"):
+    Bio.GFF._test()
+else:
+    raise ImportError("Environment not configured for GFF test")
 print "Bio.GFF doctests complete."
