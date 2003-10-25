dc3dcb82c40b3462d1afe05fe77021e2f9d9f62b
idoerg <idoerg>
2003-10-25 00:04:37 +0000

    Regression tests for yair Benita's additions to SeqUtils

---
+++ Tests/test_CodonUsage.py
@@ -0,0 +1,24 @@
+from Bio import Fasta
+from Bio.SeqUtils import CodonUsage
+import os
+import sys
+
+# first make a CAI object
+X = CodonUsage.CodonAdaptationIndex()
+# now generate an index from a file
+if os.path.exists("./CodonUsage/HighlyExpressedGenes.txt"):
+	X.generate_index("./CodonUsage/HighlyExpressedGenes.txt")
+elif os.path.exists("./Tests/CodonUsage/HighlyExpressedGenes.txt"):
+	X.generate_index("./Tests/CodonUsage/HighlyExpressedGenes.txt")
+else:
+	print "Cannot find the file HighlyExpressedGene.txt\nMake sure you run the tests from within the Tests folder"
+	sys.exit()
+# alternatively you could use any predefined dictionary like this:
+# from CaiIndices import SharpIndex # you can save your dictionary in this file.
+# X.SetCaiIndex(SharpIndex)
+
+print "The current index used:"
+X.print_index()
+
+print "-" * 60
+print "codon adaptation index for test gene: %.2f" % X.cai_for_gene("ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA")
---
+++ Tests/test_ProtParam.py
@@ -0,0 +1,24 @@
+from Bio.SeqUtils import ProtParam, ProtParamData
+
+def PrintDictionary(MyDict):
+	for i in MyDict.keys():
+		print "%s\t%.2f" %(i, MyDict[i])
+	print ""
+
+X = ProtParam.ProteinAnalysis("MAEGEITTFTALTEKFNLPPGNYKKPKLLYCSNGGHFLRILPDGTVDGTRDRSDQHIQLQLSAESVGEVYIKSTETGQYLAMDTSGLLYGSQTPSEECLFLERLEENHYNTYTSKKHAEKNWFVGLKKNGSCKRGPRTHYGQKAILFLPLPV")
+print "Amino acid\tCount\n",
+PrintDictionary(X.count_amino_acids())
+print "Amino acid\tFraction\n",
+PrintDictionary(X.get_amino_acids_percent())
+print "Molecular weight of test protein:", X.molecular_weight()
+print "Aromaticity of test protein: %.2f" % X.aromaticity()
+print "Instability index of test protein: %.2f" % X.instability_index()
+print "length of flexibility list:", len(X.flexibility())
+print "The isoelectric point of the test protein is: %.2f" % X.isoelectric_point()
+Helix, Turn, Sheet = X.secondary_structure_fraction()
+print "Fraction of amino acids in Helix: %.2f" % Helix
+print "Fraction of amino acids in Turn: %.2f" % Turn
+print "Fraction of amino acids in Sheet: %.2f" % Sheet
+print "\nKyde and Doolittle protein scale:"
+for i in X.protein_scale(ProtParamData.kd, 9, 0.4):
+	print "%.3f" %i
