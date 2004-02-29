9dfeb9b52a6a744631213d010351cdb4cda33fee
chapmanb <chapmanb>
2004-02-29 22:19:59 +0000

    New Fasta parser which uses Martel instead of the old text based approach. API compatible with the old version. New expanded test suite to make sure it works.

--- Bio/Fasta/__init__.py
+++ Bio/Fasta/__init__.py
@@ -1,14 +1,6 @@
-# Copyright 1999 by Jeffrey Chang.  All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+"""Utilities for working with FASTA-formatted sequences.
 
-# 2/2003: Iddo: replaced '\n' with os.linesep for compatibility along
-# platforms using different EOL characters
-"""Fasta package
-
-This module provides code to work with FASTA-formatted sequences.
-
+This module uses Martel-based parsing to speed up the parsing process.
 
 Classes:
 Record             Holds FASTA sequence data.
@@ -17,30 +9,21 @@ Dictionary         Accesses a FASTA file using a dictionary interface.
 RecordParser       Parses FASTA sequence data into a Record object.
 SequenceParser     Parses FASTA sequence data into a Sequence object.
 
-_Scanner           Scans a FASTA-format stream.
-_RecordConsumer    Consumes FASTA data to a Record object.
-_SequenceConsumer  Consumes FASTA data to a Sequence object.
-
-
 Functions:
 index_file         Index a FASTA file for a Dictionary.
-
 """
-from types import *
-import string
 import os
+import cStringIO
 
-__all__ = [
-    'FastaAlign',
-    ]
-
-
-from Bio import File
-from Bio import Index
 from Bio import Seq
 from Bio import SeqRecord
-from Bio.ParserSupport import *
 from Bio import Alphabet
+
+from Bio import Mindy
+from Bio.Mindy import SimpleSeqRecord
+from Bio.expressions import fasta
+
+from Martel.LAX import LAX
 
 class Record:
     """Holds information from a FASTA record.
@@ -68,139 +51,76 @@ class Record:
         while i < len(self.sequence):
             s.append(self.sequence[i:i+self._colwidth])
             i = i + self._colwidth
-        return string.join(s, os.linesep)
+        return os.linesep.join(s)
 
 class Iterator:
     """Returns one record at a time from a FASTA file.
-
-    Methods:
-    next   Return the next record from the stream, or None.
-
     """
-    def __init__(self, handle, parser=None):
-        """__init__(self, handle, parser=None)
-
-        Create a new iterator.  handle is a file-like object.  parser
-        is an optional Parser object to change the results into another form.
-        If set to None, then the raw contents of the file will be returned.
-
+    def __init__(self, handle, parser = None, debug = 0):
+        """Initialize a new iterator.
         """
-        if type(handle) is not FileType and type(handle) is not InstanceType:
-            raise ValueError, "I expected a file handle or file-like object"
-        self._uhandle = File.UndoHandle(handle)
+        it_builder = fasta.format.make_iterator("record", debug_level = debug)
         self._parser = parser
-
-    def next(self):
-        """next(self) -> object
-
-        Return the next FASTA record from the file.  If no more records,
-        return None.
-
-        """
-        lines = []
-        while 1:
-            line = self._uhandle.readline()
-            if not line:
-                break
-            if not line.strip():   # ignore blank lines
-                continue
-            if line[0] == '>' and lines:
-                self._uhandle.saveline(line)
-                break
-            lines.append(line)
-            
-        if not lines:
-            return None
-            
-        data = string.join(lines, '')
-        if self._parser is not None:
-            return self._parser.parse(File.StringHandle(data))
-        return data
+        self._iterator = it_builder.iterateFile(handle,
+                LAX(fields = ['bioformat:sequence', 'bioformat:description']))
 
     def __iter__(self):
         return iter(self.next, None)
 
-class Dictionary:
-    """Accesses a FASTA file using a dictionary interface.
+    def next(self):
+        try:
+            result = self._iterator.next()
+        except StopIteration:
+            return None
 
-    """
-    __filename_key = '__filename'
-    
-    def __init__(self, indexname, parser=None, filename = None):
-        """__init__(self, indexname, parser=None)
-
-        Open a Fasta Dictionary.  indexname is the name of the
-        index for the dictionary.  The index should have been created
-        using the index_file function.  parser is an optional Parser
-        object to change the results into another form.  If set to None,
-        then the raw contents of the file will be returned.
-        
-        filename specifies the name of the file that this index references. 
-        This is useful in cases where the file has been moved since indexing.
-        If no filename is supplied (the default) the filename stored in the
-        index will be used.
-        """
-        self._index = Index.Index(indexname)
-        if filename:
-            self._handle = open(filename)
+        if self._parser is None:
+            # return a string formatted result comparable to the original
+            parser = RecordParser()
+            rec = parser.convert_lax(result)
+            return str(rec) + "\n"
         else:
-            self._handle = open(self._index[Dictionary.__filename_key])
-        self._parser = parser
+            return self._parser.convert_lax(result)
 
-    def __len__(self):
-        return len(self._index)
-
-    def __getitem__(self, key):
-        start, len = self._index[key]
-        self._handle.seek(start)
-        data = self._handle.read(len)
-        if self._parser is not None:
-            return self._parser.parse(File.StringHandle(data))
-        return data
-
-    def __getattr__(self, name):
-        return getattr(self._index, name)
-
-    def keys(self):
-        """Override keys to return only valid keys.
-
-        The keys which is called normally on this dictionary will return
-        internal values such as '__filename'. This just strips those out
-        before returning.
-        """
-        all_keys = self._index.keys()
-        to_remove = []
-        for key in all_keys:
-            # if the key is an internal element with a '__', strip it
-            if key[:2] == '__':
-                to_remove.append(key)
-
-        for key in to_remove:
-            all_keys.remove(key)
-
-        return all_keys
-
-class RecordParser(AbstractParser):
-    """Parses FASTA sequence data into a Record object.
-
+class _MartelBaseFastaParser:
+    """Base class to provide a old-Biopython style to Martel parsing.
     """
-    def __init__(self):
-        self._scanner = _Scanner()
-        self._consumer = _RecordConsumer()
+    def __init__(self, debug):
+        self._it_builder = fasta.format.make_iterator("record",
+                debug_level = debug)
 
     def parse(self, handle):
-        if isinstance(handle, File.UndoHandle):
-            uhandle = handle
-        else:
-            uhandle = File.UndoHandle(handle)
-        self._scanner.feed(uhandle, self._consumer)
-        return self._consumer.data
+        """Parse a single FASTA record in an file handle.
 
-class SequenceParser:
-    """Parses FASTA sequence data into a Sequence object.
+        Internally, this parses the record into a dictionary
+        and then passes that on to the convert_lax function defined
+        in a derived class.
+        """
+        iterator = self._it_builder.iterateFile(handle,
+                LAX(fields = ['bioformat:sequence', 'bioformat:description']))
+        return self.convert_lax(iterator.next())
 
+    def convert_lax(self, result):
+        raise NotImplementedError("Derived class must implement")
+
+class RecordParser(_MartelBaseFastaParser):
+    """Parses FASTA sequence data into a Record object.
     """
-    def __init__(self, alphabet = Alphabet.generic_alphabet, title2ids = None):
+    def __init__(self, debug = 0):
+        _MartelBaseFastaParser.__init__(self, debug)
+
+    def convert_lax(self, result):
+        """Convert a dictionary LAX parsing result into a Record object.
+        """
+        rec = Record()
+        rec.title = "".join(result['bioformat:description'])
+        rec.sequence = "".join(result['bioformat:sequence'])
+        return rec
+
+class SequenceParser(_MartelBaseFastaParser):
+    """Parses FASTA sequence data into a Sequence object.
+    """
+    def __init__(self, alphabet = Alphabet.generic_alphabet, title2ids = None,
+            debug = 0):
         """Initialize a Scanner and Sequence Consumer.
 
         Arguments:
@@ -211,155 +131,167 @@ class SequenceParser:
         description (in that order) for the record. If this is not given,
         then the entire title line will be used as the description.
         """
-        self._scanner = _Scanner()
-        self._consumer = _SequenceConsumer(alphabet, title2ids)
-
-    def parse(self, handle):
-        if isinstance(handle, File.UndoHandle):
-            uhandle = handle
-        else:
-            uhandle = File.UndoHandle(handle)
-        self._scanner.feed(uhandle, self._consumer)
-        return self._consumer.data
-
-
-class _Scanner:
-    """Scans a FASTA-formatted file.
-
-    Methods:
-    feed   Feed in one FASTA record.
-
-    """
-    def feed(self, handle, consumer):
-        """feed(self, handle, consumer)
-
-        Feed in FASTA data for scanning.  handle is a file-like object
-        containing FASTA data.  consumer is a Consumer object that will
-        receive events as the FASTA data is scanned.
-
-        """
-        assert isinstance(handle, File.UndoHandle), \
-               "handle must be an UndoHandle"
-        if handle.peekline():
-            self._scan_record(handle, consumer)
-
-    def _scan_record(self, uhandle, consumer):
-        consumer.start_sequence()
-        self._scan_title(uhandle, consumer)
-        self._scan_sequence(uhandle, consumer)
-        consumer.end_sequence()
-
-    def _scan_title(self, uhandle, consumer):
-        read_and_call(uhandle, consumer.title, start='>')
-
-    def _scan_sequence(self, uhandle, consumer):
-        while 1:
-            line = uhandle.readline()
-            if is_blank_line(line):
-                break
-            elif line[0] == '>':
-                uhandle.saveline(line)
-                break
-            consumer.sequence(line)
-
-class _RecordConsumer(AbstractConsumer):
-    """Consumer that converts a FASTA record to a Record object.
-
-    Members:
-    data    Record with FASTA data.
-    
-    """
-    def __init__(self):
-        self.data = None
-    
-    def start_sequence(self):
-        self.data = Record()
-
-    def end_sequence(self):
-        pass
-    
-    def title(self, line):
-        assert line[0] == '>'
-        self.data.title = string.rstrip(line[1:])
-
-    def sequence(self, line):
-        # This can be optimized
-        seq = string.rstrip(line)
-        self.data.sequence = self.data.sequence + seq
-
-class _SequenceConsumer(AbstractConsumer):
-    """Consumer that converts a FASTA record to a Sequence object.
-
-    Members:
-    data    Sequence with FASTA data.
-
-    """
-    def __init__(self, alphabet = Alphabet.generic_alphabet, title2ids = None):
-        """Initialize the Consumer.
-
-        Arguments:
-        o alphabet - The alphabet of the sequences we will be creating.
-        o title2ids - A function that will convert the title of a FASTA
-        record into the id, name and description information.
-        """
-        self.data = None
+        _MartelBaseFastaParser.__init__(self, debug)
         self.alphabet = alphabet
         self.title2ids = title2ids
     
-    def start_sequence(self):
-        seq = Seq.Seq('', self.alphabet)
-        self.data = SeqRecord.SeqRecord(seq)
-
-    def end_sequence(self):
-        pass
-    
-    def title(self, line):
-        assert line[0] == '>'
-        if self.title2ids:
-            id, name, descr = self.title2ids(string.rstrip(line[1:]))
-            self.data.id = id
-            self.data.name = name
-            self.data.description = descr
-        else:
-            self.data.description = string.rstrip(line[1:])
-
-    def sequence(self, line):
-        # This can be optimized
-        seq = Seq.Seq(string.rstrip(line), self.alphabet)
-        self.data.seq = self.data.seq + seq
-
-def index_file(filename, indexname, rec2key=None):
-    """index_file(filename, indexname, rec2key=None)
-
-    Index a FASTA file.  filename is the name of the file.
-    indexname is the name of the dictionary.  rec2key is an
-    optional callback that takes a Record and generates a unique key
-    (e.g. the accession number) for the record.  If not specified,
-    the sequence title will be used.
-
-    """
-    if not os.path.exists(filename):
-        raise ValueError, "%s does not exist" % filename
-
-    index = Index.Index(indexname, truncate=1)
-    index[Dictionary._Dictionary__filename_key] = filename
-    
-    iter = Iterator(open(filename), parser=RecordParser())
-    while 1:
-        start = iter._uhandle.tell()
-        rec = iter.next()
-        length = iter._uhandle.tell() - start
+    def convert_lax(self, result):
+        """Convert a dictionary LAX parsing result into a Sequence object.
+        """
+        seq = Seq.Seq("".join(result['bioformat:sequence']), self.alphabet)
+        rec = SeqRecord.SeqRecord(seq)
         
-        if rec is None:
-            break
-        if rec2key is not None:
-            key = rec2key(rec)
+        title = "".join(result['bioformat:description'])
+        if self.title2ids:
+            seq_id, name, descr = self.title2ids(title)
+            rec.id = seq_id
+            rec.name = name
+            rec.description = descr
         else:
-            key = rec.title
-            
-        if not key:
-            raise KeyError, "empty sequence key was produced"
-        elif index.has_key(key):
-            raise KeyError, "duplicate key %s found" % key
+            rec.description = title
 
-        index[key] = start, length
+        return rec
+
+class Dictionary:
+    """Accesses an indexed FASTA file using a dictionary interface.
+    """
+    def __init__(self, indexname, parser=None, filename = None):
+        """Open a Fasta Dictionary.  
+        
+        indexname is the name of the index for the dictionary.  The index should 
+        have been created using the index_file function.  
+        
+        parser is an optional Parser object to change the results into another 
+        form.  If set to None, then the raw contents of the file will be returned.
+        
+        filename specifies the name of the file that this index references. 
+        This is useful in cases where the file has been moved since indexing.
+        If no filename is supplied (the default) the filename stored in the
+        index will be used. XXX This is no longer supported -- use symbolic
+        links in the filesystem.
+        """
+        # we can't support finding the index file name if we want to follow
+        # standard open-bio fetching protocols.
+        if filename is not None:
+            raise AttributeError("Specifying filenames is no longer supported")
+        
+        self._index = Mindy.open(indexname)
+        self._parser = parser
+
+    def __len__(self):
+        return len(self.keys())
+
+    def __getitem__(self, key):
+        # first try to retrieve by the base id
+        try:
+            seqs = self._index.lookup(id = key)
+        # if we can't do that, we have to try and fetch by alias
+        except KeyError:
+            seqs = self._index.lookup(aliases = key)
+
+        if len(seqs) == 1:
+            seq = seqs[0]
+        else:
+            raise KeyError("Multiple sequences found for %s" % key)
+
+        if self._parser:
+            handle = cStringIO.StringIO(seq.text)
+            return self._parser.parse(handle)
+        else:
+            return seq.text
+
+    def keys(self):
+        primary_key_retriever = self._index['id']
+        return primary_key_retriever.keys()
+
+def index_file(filename, indexname, rec2key = None):
+    """Index a FASTA file.
+
+    filename is the name of the file to index.
+
+    indexname is the name of the dictionary to be created. This can be
+    just the name of the index, in which case the index information will
+    be created in a directory of the given index name in the current
+    directory, or a full pathname to a directory to save the indexing
+    information.
+
+    rec2key is an optional callback fuction that takes a Fasta Record and 
+    generates a unique key (e.g. the accession number) for the record.
+    Optionally, it can also return 3 items, to be used as the id (unique key)
+    name, and aliases for the index. If not specified, the sequence title 
+    will be used.
+    """
+    if rec2key:
+        indexer = _FastaFunctionIndexer(rec2key)
+    else:
+        indexer = _FastaTitleIndexer()
+
+    SimpleSeqRecord.create_berkeleydb([filename], indexname, indexer)
+
+class _FastaTitleIndexer(SimpleSeqRecord.BaseSeqRecordIndexer):
+    """Simple indexer to index by the title of a FASTA record.
+
+    This doesn't do anything fancy, just gets the title and uses that as the
+    identifier.
+    """
+    def __init__(self):
+        SimpleSeqRecord.BaseSeqRecordIndexer.__init__(self)
+
+    def primary_key_name(self):
+        return "id"
+
+    def secondary_key_names(self):
+        return ["name", "aliases"]
+
+    def get_id_dictionary(self, seq_record):
+        sequence_id = seq_record.description
+            
+        id_info = {"id" : [sequence_id],
+                   "name" : [],
+                   "aliases" : []}
+        return id_info
+
+
+class _FastaFunctionIndexer(SimpleSeqRecord.BaseSeqRecordIndexer):
+    """Indexer to index based on values returned by a function.
+    
+    This class is passed a function to parse description titles from a Fasta
+    title. It needs to return either one item, which is an id from the title,
+    or three items which are (in order), the id, a list of names, and a list
+    of aliases.
+
+    This indexer allows indexing to be completely flexible based on passed
+    functions.
+    """
+    def __init__(self, index_function):
+        SimpleSeqRecord.BaseSeqRecordIndexer.__init__(self)
+        self.index_function = index_function
+
+    def primary_key_name(self):
+        return "id"
+
+    def secondary_key_names(self):
+        return ["name", "aliases"]
+
+    def get_id_dictionary(self, seq_record):
+        # make a FASTA record to make this compatible with previous Biopython
+        # code
+        tmp_rec = Record()
+        tmp_rec.title = seq_record.description
+        tmp_rec.sequence = seq_record.seq.data
+        items = self.index_function(tmp_rec)
+        if type(items) is not type([]) and type(items) is not type(()):
+            items = [items]
+        if len(items) == 1:
+            seq_id = items[0]
+            name = []
+            aliases = []
+        elif len(items) == 3:
+            seq_id, name, aliases = items
+        else:
+            raise ValueError("Unexpected items from index function: %s" %
+                    (items))
+        
+        return {"id" : [seq_id],
+                "name" : name,
+                "aliases" : aliases}
--- Tests/test_Fasta.py
+++ Tests/test_Fasta.py
@@ -4,86 +4,186 @@
 # as part of this package.
 
 import os
+import sys
+import unittest
 from types import *
-from Bio import File
-from Bio import ParserSupport
 from Bio import Fasta
 
-def pbool(b):
-    if b:
-        return 1
-    return 0
+from Bio import SeqRecord
+from Bio import Seq
+from Bio import Alphabet
+from Bio.Alphabet import IUPAC
 
-### Record
+def run_tests(argv):
+    test_suite = testing_suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity = 2)
+    runner.run(test_suite)
 
-print "Running test on Record"
+def testing_suite():
+    """Generate the suite of tests.
+    """
+    test_suite = unittest.TestSuite()
 
-r = Fasta.Record()
-print pbool(type(r.title) is StringType)    # StringType
-print pbool(type(r.sequence) is StringType) # StringType
+    test_loader = unittest.TestLoader()
+    test_loader.testMethodPrefix = 't_'
+    tests = [RecordTest, ParserTest, IteratorTest, DictionaryTest]
     
+    for test in tests:
+        cur_suite = test_loader.loadTestsFromTestCase(test)
+        test_suite.addTest(cur_suite)
 
-### _Scanner
+    return test_suite
 
-print "Running tests on _Scanner"
+class RecordTest(unittest.TestCase):
+    def t_record_basic(self):
+        """Basic test on Record
+        """
+        def pbool(b):
+            if b:
+                return 1
+            return 0
 
-tests = ['f001', 'f002']
+        r = Fasta.Record()
+        assert pbool(type(r.title) is StringType)    # StringType
+        assert pbool(type(r.sequence) is StringType) # StringType
 
-scanner = Fasta._Scanner()
-for test in tests:
-    print "testing %s" % test
-    datafile = os.path.join("Fasta", test)
-    tc = ParserSupport.TaggingConsumer()
-    handle = File.UndoHandle(open(datafile))
-    scanner.feed(handle, tc)
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        files = ["f001", "f002"]
+        self.handles = []
+        for filename in files:
+            self.handles.append(open(os.path.join("Fasta", filename)))
 
+        self.lengths = {0 : (96, 79),
+                        1 : (100, 633)}
 
-### _RecordConsumer
+    def tearDown(self):
+        for handle in self.handles:
+            handle.close()
 
-print "Running tests on _RecordConsumer"
+    def t_record_parser(self):
+        """Basic operation of the Record Parser.
+        """
+        parser = Fasta.RecordParser()
+        for index, handle in enumerate(self.handles):
+            rec = parser.parse(handle)
+            assert isinstance(rec, Fasta.Record)
+            assert len(rec.title) == self.lengths[index][0]
+            assert len(rec.sequence) == self.lengths[index][1]
 
-c = Fasta._RecordConsumer()
-c.start_sequence()
-c.title('>This is a title\n')
-c.sequence('ABCD\n')
-c.sequence('EFG\n')
-c.end_sequence()
+    def t_sequence_parser(self):
+        """Basic operation of the Sequence Parser.
+        """
+        parser = Fasta.SequenceParser()
+        for index, handle in enumerate(self.handles):
+            rec = parser.parse(handle)
+            assert isinstance(rec, SeqRecord.SeqRecord)
+            assert isinstance(rec.seq, Seq.Seq)
+            assert rec.seq.alphabet == Alphabet.generic_alphabet
+            assert len(rec.seq) == self.lengths[index][1]
+            assert len(rec.description) == self.lengths[index][0]
 
-print c.data.title        # "This is a title"
-print c.data.sequence     # "ABCDEFG"
-# clear the record
-c.start_sequence()   
-c.end_sequence()
-print repr(c.data.title)  # ''
+    def t_sequence_alphabet(self):
+        """Setting the alphabet for the Sequence Parser.
+        """
+        parser = Fasta.SequenceParser(alphabet =
+                IUPAC.unambiguous_dna)
+        rec = parser.parse(self.handles[0])
+        assert rec.seq.alphabet == IUPAC.unambiguous_dna
 
+    def t_sequence_title_convert(self):
+        """Test title conversion for the Sequence Parser.
+        """
+        def test_title2ids(title):
+            return "id", "name", "description"
+        parser = Fasta.SequenceParser(title2ids = test_title2ids)
+        rec = parser.parse(self.handles[0])
+        assert rec.id == "id"
+        assert rec.name == "name"
+        assert rec.description == "description"
 
+class IteratorTest(unittest.TestCase):
+    def setUp(self):
+        self.test_handle = open(os.path.join('Fasta', 'f002'))
 
-### _SequenceConsumer
+    def tearDown(self):
+        self.test_handle.close()
 
-print "Running tests on _SequenceConsumer"
+    def t_basic_iterator(self):
+        """Ensure the Fasta iterator works returning text.
+        """
+        i = Fasta.Iterator(self.test_handle)
+        rec_info = {0 : ">gi|1348912|gb|G26680|G26680",
+                    1 : ">gi|1348917|gb|G26685|G26685",
+                    2 : ">gi|1592936|gb|G29385|G29385"}
+        for rec_num in range(3):
+            rec = i.next()
+            lines = rec.split("\n")
+            title_part = lines[0].split()
+            assert title_part[0] == rec_info[rec_num]
 
-c = Fasta._SequenceConsumer()
-c.start_sequence()
-c.title('>This is a title\n')
-c.sequence('ABCD\n')
-c.sequence('EFG\n')
-c.end_sequence()
+        # make sure we keep getting None when the iterator is done
+        assert i.next() is None
+        assert i.next() is None
 
-print c.data.description  # "This is a title"
-print c.data.seq          # "Seq('ABCDEFG', Alphabet())"
-# clear the record
-c.start_sequence()   
-c.end_sequence()
-print repr(c.data.name)   # '<unknown name>'
+    def t_new_iterator(self):
+        """Ensure the Fasta iterator works like a Python 2.2 iterator.
+        """
+        n = 0
+        iterator = Fasta.Iterator(self.test_handle)
+        for rec in iter(iterator):
+            n += 1
+        assert n == 3
 
+    def t_record_iterator(self):
+        """Test the iterator with a Record Parser.
+        """
+        parser = Fasta.RecordParser()
+        iterator = Fasta.Iterator(self.test_handle, parser)
+        for rec in iter(iterator):
+            assert isinstance(rec, Fasta.Record)
 
+    def t_sequence_iterator(self):
+        """Test the iterator with a Sequence Parser.
+        """
+        parser = Fasta.SequenceParser()
+        iterator = Fasta.Iterator(self.test_handle, parser)
+        for rec in iter(iterator):
+            assert isinstance(rec, SeqRecord.SeqRecord)
 
-### Iterator
+class DictionaryTest(unittest.TestCase):
+    def setUp(self):
+        self.filename = os.path.join("Fasta", "f002")
+        self.indexname = self.filename + ".idx"
 
-print "Running tests on Iterator"
+    def tearDown(self):
+        try:
+            os.remove(self.indexname) # remove files -- old Biopython
+        except OSError: # is a directory -- new
+            for filename in os.listdir(self.indexname):
+                os.remove(os.path.join(self.indexname, filename))
+            os.removedirs(self.indexname)
 
-i = Fasta.Iterator(open(os.path.join('Fasta', 'f002')))
-print i.next()   # '>gi|1348912' [...]
-print i.next()   # '>gi|1348917'
-print i.next()   # '>gi|1592936'
-print repr(i.next())   # None
+    def t_index_file(self):
+        """Test indexing a file and retrieving from it.
+        """
+        def rec_to_ids(rec):
+            """Just return the GI number to index by.
+            """
+            parts = rec.title.split()
+            gi_parts = parts[0].split("|")
+            return gi_parts[1]
+
+        Fasta.index_file(self.filename, self.indexname, rec_to_ids)
+        gis = ["1348912", "1348917", "1592936"]
+        fasta_dict = Fasta.Dictionary(self.indexname, Fasta.RecordParser())
+        assert len(fasta_dict) == len(gis), len(fasta_dict)
+        dict_keys = fasta_dict.keys()
+        dict_keys.sort()
+        assert dict_keys == gis
+        for gi in gis:
+            rec = fasta_dict[gi]
+            assert isinstance(rec, Fasta.Record)
+
+if __name__ == "__main__":
+    sys.exit(run_tests(sys.argv))
