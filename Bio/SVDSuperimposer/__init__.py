a13271ff00e51b2779050831b64b998faabd761e
nisse <nisse>
2004-03-21 15:26:13 +0000

    *** empty log message ***

--- Bio/SVDSuperimposer/__init__.py
+++ Bio/SVDSuperimposer/__init__.py
@@ -1,7 +1,8 @@
 """
 SVDSuperimposer finds the best rotation and translation to put
 two point sets on top of each other (minimizing the RMSD). This is 
-eg. useful to superimpose crystal structures. 
+eg. useful to superimpose crystal structures. SVD stands for singular 
+value decomposition, which is used in the algorithm.
 """
 
 from SVDSuperimposer import *
