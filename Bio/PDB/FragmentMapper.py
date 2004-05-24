62f9b26b679a62d8b2fc187a0e5cb1acaeaf3b22
nisse <nisse>
2004-05-24 15:34:46 +0000

    *** empty log message ***

--- Bio/PDB/FragmentMapper.py
+++ Bio/PDB/FragmentMapper.py
@@ -224,13 +224,17 @@ class FragmentMapper:
     """
     def __init__(self, model, lsize=20, flength=5, fdir="."):
         """
+        @param model: the model that will be mapped
+        @type model: L{Model}
+
         @param lsize: number of fragments in the library
         @type lsize: int
 
         @param flength: length of fragments in the library
         @type flength: int
 
-        @param fdir: directory where teh definition files are
+        @param fdir: directory where the definition files are
+            found (default=".")
         @type fdir: string
         """
         if flength==5:
