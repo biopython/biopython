7f1df8656c0fda39b5e1a762fe838e27d927f116
jchang <jchang>
2004-08-26 14:21:29 +0000

    committed Fred Sohm's patch so triemodule can compile under Python 2.4. The PyMarshal_WriteObjectToString function has changed.

--- Bio/triemodule.c
+++ Bio/triemodule.c
@@ -477,8 +477,14 @@ int _write_value_to_handle(const void *value, void *handle)
     int length;
     int success = 0;
 
-    if(!(py_marshalled = PyMarshal_WriteObjectToString(py_value)))
-	goto _write_value_to_handle_cleanup;
+#ifdef Py_MARSHAL_VERSION  
+    if(!(py_marshalled =   
+	 PyMarshal_WriteObjectToString(py_value, Py_MARSHAL_VERSION)))  
+        goto _write_value_to_handle_cleanup;  
+#else  
+    if(!(py_marshalled = PyMarshal_WriteObjectToString(py_value)))  
+        goto _write_value_to_handle_cleanup;  
+#endif  
     if(PyString_AsStringAndSize(py_marshalled, &marshalled, &length) == -1)
 	goto _write_value_to_handle_cleanup;
     if(!_write_to_handle(&length, sizeof(length), handle))
