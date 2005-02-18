ace0714e03a1235e9edddc50df0b0fe19ce95bdb
mdehoon <mdehoon>
2005-02-18 09:27:23 +0000

    Replaced C++-style comments (//) with ANSI compliant C-style comments (/* */ ). Included the <string.h> header file, containing the strlen function.

--- Bio/Nexus/cnexus.c
+++ Bio/Nexus/cnexus.c
@@ -1,18 +1,19 @@
-// Copyright 2005 by Frank Kauff.  All rights reserved.
-// This code is part of the Biopython distribution and governed by its
-// license. Please see the LICENSE file that should have been included
-// as part of this package.
-//
-// cnexus.c
-//
-// Parse input strings, cut out (nested) comments, deal with quoted text.
-// Input lines terminated with ; are separated by ASCII code 7 (something
-// that naturally doesn't occur in plain NEXUS files).
-//
-// Used by Nexus.py
-//
+/* Copyright 2005 by Frank Kauff.  All rights reserved.
+ * This code is part of the Biopython distribution and governed by its
+ * license. Please see the LICENSE file that should have been included
+ * as part of this package.
+ *
+ * cnexus.c
+ *
+ * Parse input strings, cut out (nested) comments, deal with quoted text.
+ * Input lines terminated with ; are separated by ASCII code 7 (something
+ * that naturally doesn't occur in plain NEXUS files).
+ *
+ * Used by Nexus.py
+ */
 
 #include <Python.h>
+#include <string.h>
 
 static PyObject * cnexus_scanfile(PyObject *self, PyObject *args)
 {
@@ -30,34 +31,34 @@ static PyObject * cnexus_scanfile(PyObject *self, PyObject *args)
     if (!(scanned=malloc(strlen(input)+1)))
         PyErr_NoMemory();
     scanned_start=scanned;
-    for(t=*input;t=*input;input++) 
+    for(t=*input;(t=*input);input++) 
     {
-        // end of standard quote
+        /* end of standard quote */
         if (!(commlevel || speciallevel) && t==quotelevel)
             quotelevel=0;
-        // start of standard quote
+        /* start of standard quote */
         else if (!quotelevel && !(commlevel || speciallevel) && (t=='\'' || t=='"'))
             quotelevel=t;
-        // start of comment outside quote
+        /* start of comment outside quote */
         else if (!quotelevel  && t=='[')
         {
-            // check for special comments
+            /* check for special comments */
             if ((*(input+1)=='!' || *(input+1)=='&' || *(input+1)=='%' || 
                     *(input+1)=='/' || *(input+1)=='\\' || *(input+1)=='@')
                     && !(commlevel || speciallevel))
                 speciallevel=1;
-            else // standard comment
+            else /* standard comment */
                 commlevel++;
         }
         else if (!quotelevel && t==']')
         {
-            // does it end a special comment?
+            /* does it end a special comment? */
             if (speciallevel)
                 speciallevel=0;
             else
             {
                 commlevel--;
-                if (commlevel<0) // error: unmatched ]
+                if (commlevel<0) /* error: unmatched ] */
                 {
                     free(scanned);
                     return Py_BuildValue("s","]");
@@ -67,21 +68,23 @@ static PyObject * cnexus_scanfile(PyObject *self, PyObject *args)
         }
         if (!commlevel)
         {
-            // we replace the ; at the end of command lines with special character
-            // to make subsequent parsing of blocks easier
+            /* we replace the ; at the end of command lines with special
+             * character to make subsequent parsing of blocks easier */
             if (t==';' && !(quotelevel || speciallevel))
-                //need an ascii code thats not part of a nexus file, used as separator
+                /* need an ascii code thats not part of a nexus file, used as
+                 * separator */
                 *(scanned++)=7;
             else
                 *(scanned++)=t;
         }
-        //printf("t %c, commlevel %d, speciallevel %d, quotelevel '%c', scanned %d\n",
-        //t,commlevel,speciallevel,quotelevel,scanned);
+        /* printf("t %c, commlevel %d, speciallevel %d, quotelevel '%c', scanned %d\n",
+         * t,commlevel,speciallevel,quotelevel,scanned);
+         */
     }               
     if (commlevel>0)
-        // error: unmatched [
+        /* error: unmatched [ */
         return Py_BuildValue("s","[");
-    *scanned=0; //end of string
+    *scanned=0; /* end of string */
     return Py_BuildValue("s",scanned_start);
 }
 
