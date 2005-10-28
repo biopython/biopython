87a37d9f332dee4d8ce78882acdbca1635680317
mdehoon <mdehoon>
2005-10-28 19:41:02 +0000

    Updated version number in setup.py for 1.41 release.

--- setup.py
+++ setup.py
@@ -508,7 +508,7 @@ PACKAGES.append("BioSQL")
 
 setup(
     name='biopython',
-    version='1.40b',
+    version='1.41',
     author='The Biopython Consortium',
     author_email='biopython@biopython.org',
     url='http://www.biopython.org/',
