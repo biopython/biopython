75a6aecd4cb7d4647c2c739a7ca02e352734990f
peterc <peterc>
2006-01-05 16:06:17 +0000

    Modified the __str__ function of the GEO record object to only show the first 20 rows in any data table.  Without this, attempting to print the object for a large record causes an lockup as several MB of text could be returned.

--- Bio/Geo/Record.py
+++ Bio/Geo/Record.py
@@ -1,4 +1,5 @@
 # Copyright 2001 by Katharine Lindner.  All rights reserved.
+# Copyright 2006 by PeterC.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
@@ -27,8 +28,6 @@ import string
 from Bio.Seq import Seq
 from Bio.Align.Generic import Alignment
 import Bio.Alphabet
-
-
 
 """Hold GEO data in a straightforward format.
 
@@ -86,11 +85,22 @@ class Record:
             val = self.col_defs[ key ]
             output = output + '    %s: %s\n' % ( key, val[ :40 ] )
             output = output + out_block( val[ 40: ], '    ' )
-        for row in self.table_rows:
+        #May have to display VERY large tables,
+        #so only show the first 20 lines of data
+        MAX_ROWS = 20+1 # include header in count
+        for row in self.table_rows[0:MAX_ROWS]:
             output = output + '%s: ' % self.table_rows.index( row )
             for col in row:
                 output = output + '%s\t' % col
             output = output + '\n'
+        if len(self.table_rows) > MAX_ROWS :
+            output = output + '...\n'
+            row = self.table_rows[-1]
+            output = output + '%s: ' % self.table_rows.index( row )
+            for col in row:
+                output = output + '%s\t' % col
+            output = output + '\n'
+            
         return output
 
 def out_block( text, prefix = '' ):
@@ -99,9 +109,6 @@ def out_block( text, prefix = '' ):
         output = output + '%s%s\n'  % ( prefix, text[ j: j + 80 ] )
     output = output + '\n'
     return output
-
-
-
 
 
 
