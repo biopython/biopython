8c170c03647349c25096eb8ee6352d4d63e1d876
peterc <peterc>
2006-03-28 11:33:35 +0000

    Also test the five new examples from the NCBI to document the 2005 file format changes.

--- Tests/test_geo.py
+++ Tests/test_geo.py
@@ -8,12 +8,19 @@ from Bio.File import SGMLHandle
 
 testfiles   = [  'GSE16.txt', 'GSM645.txt', 'GSM691.txt', 'GSM700.txt', 'GSM804.txt'  ]
 
+#Five additional files from the NCBI to document the GEO SOFT file format
+#changes made in 2005.  Note the new table_begin and table_end lines.
+testfiles.extend(['soft_ex_affy.txt',
+                  'soft_ex_dual.txt',
+                  'soft_ex_family.txt',
+                  'soft_ex_platform.txt',
+                  'soft_ex_series.txt'])
+
 for file in testfiles:
     fh = open(os.path.join("Geo", file))
     print "Testing Bio.Geo on " + file + "\n\n"
     records = Bio.Geo.Iterator( fh, Bio.Geo.RecordParser(debug_level=0))
     while 1:
-#    for j in range( 0, 1 ):
         record = records.next()
         if record is not None:
             print record
