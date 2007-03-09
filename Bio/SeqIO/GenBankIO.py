880f97e44c026a598f4f8d823ac7f6aced6abccb
peterc <peterc>
2007-03-09 23:24:40 +0000

    The file GenBankIO.py was replaced with InsdcIO.py (and I meant to delete it at the time)

--- Bio/SeqIO/GenBankIO.py
+++
@@ -1,6 +0,0 @@
-#This file should have beed deleted a month ago.
-#
-#The code is now in InsdcIO.py (together with EMBL support)
-#
-#Making it empty for now, and then I'll go back to trying
-#to get my CVS client to comply...
