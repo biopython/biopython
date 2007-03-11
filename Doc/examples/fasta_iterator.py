49ac254ffbf02ea6ce7ab4ca0a424eff2d2c9782
peterc <peterc>
2007-03-11 20:23:52 +0000

    Added example doing same thing using Bio.SeqIO

--- Doc/examples/fasta_iterator.py
+++ Doc/examples/fasta_iterator.py
@@ -1,3 +1,32 @@
+# The New Way
+# ===========
+# This next bit of code use Bio.SeqIO to parse a FASTA file
+
+from Bio import SeqIO
+
+def extract_organisms(file_to_parse, format):
+    all_species = []
+    for cur_record in SeqIO.parse(open(file_to_parse), format) :
+        # extract the info from the description
+        new_species = cur_record.description.split()[1]
+
+        # append the new species to the list if it isn't there
+        if new_species not in all_species:
+            all_species.append(new_species)
+
+    return all_species
+
+if __name__ == "__main__":
+    print "Using Bio.SeqIO on a FASTA file"
+    all_species = extract_organisms("ls_orchid.fasta", "fasta")
+    print "number of species:", len(all_species)
+    print 'species names:', all_species
+
+
+# The Old Way
+# ===========
+# This next bit of code still works fine, it uses Bio.Fasta instead
+
 from Bio import Fasta
 
 def extract_organisms(file_to_parse):
@@ -24,6 +53,7 @@ def extract_organisms(file_to_parse):
     return all_species
 
 if __name__ == "__main__":
+    print "Using Bio.Fasta"
     all_species = extract_organisms("ls_orchid.fasta")
     print "number of species:", len(all_species)
     print 'species names:', all_species
