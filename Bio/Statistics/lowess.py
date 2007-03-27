b3bbfdb5b42479df74dcd44d409c0241a76f599d
mdehoon <mdehoon>
2007-03-27 14:56:32 +0000

    Added another literature reference.

--- Bio/Statistics/lowess.py
+++ Bio/Statistics/lowess.py
@@ -5,9 +5,14 @@ Functions:
 lowess        Fit a smooth nonparametric regression curve to a scatterplot.
 
 For more information, see
+
 William S. Cleveland: "Robust locally weighted regression and smoothing
 scatterplots", Journal of the American Statistical Association, December 1979,
 volume 74, number 368, pp. 829-836.
+
+William S. Cleveland and Susan J. Devlin: "Locally weighted regression: An
+approach to regression analysis by local fitting", Journal of the American
+Statistical Association, September 1988, volume 83, number 403, pp. 596-610.
 """
 
 try:
