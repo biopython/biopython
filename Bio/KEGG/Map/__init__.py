9430b7cf201accca78801374196eeb5a42c65441
mdehoon <mdehoon>
2007-09-12 12:05:23 +0000

    Replaced the Martel-based parser with a pure-Python parser to avoid problems with the new version (3.0) of mxTextTools.

--- Bio/KEGG/Map/__init__.py
+++ Bio/KEGG/Map/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2001 by Tarjei Mikkelsen. All rights reserved.
+# Copyright 2007 by Michiel de Hoon. All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
@@ -19,27 +20,10 @@ EC number of the enzyme that catalyze the reaction.
 
 Classes:
 Iterator             -- Iterates through a file of map file.
-Parser               -- Parses KEGG map lines into Pathway.Reaction objects.
-
-_Scanner
-_Consumer
 
 """
 
-# XML from python
-from xml.sax import handler
-
-# Martel
-import Martel
-from Martel import RecordReader
-
-# other Biopython stuff
-from Bio import File
 from Bio.Pathway import Reaction
-from Bio.ParserSupport import AbstractConsumer
-from Bio.ParserSupport import EventGenerator
-
-import map_format
 
 class Iterator:
     """Iterator to read a file of KEGG reactions one at a time.
@@ -49,130 +33,45 @@ class Iterator:
 
         Arguments:
         o handle - A handle to a map file to iterate through.
-        o parser - An optional parser to pass the entries through before
-        returning them. If None, then the raw entry will be returned.
         """
-        self._reader = RecordReader.CountLines(handle, 1)          
-        self._parser = parser
+        import warnings
+        warnings.warn("Bio.KEGG.Map.Iterator(handle, parser) is deprecated. Please use Bio.KEGG.Map.parse(handle) instead. It also returns an iterator.", DeprecationWarning)
+        self.records = parse(handle)
+
 
     def next(self):
         """Return the next Pathway.Reaction object from the handle.
 
         Will return None if we ran out of records.
         """
-        data = self._reader.next()
-        if self._parser is not None:
-            if data:
-                return self._parser.parse(File.StringHandle(data))
-        return data
+        return self.records.next()
     
     def __iter__(self):
         return iter(self.next, None)
 
 
-class Parser:
-    """Parse KEGG Map files into Pathway.Reaction objects
-    """
-    def __init__(self, debug_level = 0):
-        """Initialize the parser.
-
-        Arguments:
-        o debug_level - An optional argument that species the amount of
-        debugging information Martel should spit out. By default we have
-        no debugging info (the fastest way to do things), but if you want
-        you can set this as high as two and see exactly where a parse fails.
-        """
-        self._scanner = _Scanner(debug_level)
-
-    def parse(self, handle):
-        """Parse the specified handle into a Pathway.Reaction object.
-        """
-        self._consumer = _Consumer()
-        self._scanner.feed(handle, self._consumer)
-        return self._consumer.data
-
-
-class _Consumer(AbstractConsumer):
-    """Create a Pathway.Reaction object from scanner events.
-
-    """
-    def __init__(self):
-        self.data = None
-        self._reaction_id  = ""
-        self._ec_id        = ""
-        self._stoch_in     = []
-        self._substrates   = []
-        self._stoch_out    = []
-        self._products     = []
-        self._arrow        = 0
-        self._stoch        = 0
-    def stoch(self, stoch):
-        if not self._arrow:
-            self._stoch_in.append(int(stoch[0]))
-        else:
-            self._stoch_out.append(int(stoch[0]))
-        self._stoch = 1
-    def reactant(self, reactants):
-        if not self._arrow:
-            self._substrates.extend(reactants)
-            self._stoch_in.extend([1] * (len(reactants) - self._stoch))
-        else:
-            self._products.extend(reactants)
-            self._stoch_out.extend([1] * (len(reactants) - self._stoch))
-        self._stoch = 0
-    def arrows(self, arrows):
-        self._arrow = 1
-    def reaction_id(self, reaction_id):
-        self._reaction_id = reaction_id[0]
-    def ec_id(self, ec_id):
-        self._ec_id = ec_id[0]
-    def end(self, end):
+def parse(handle):
+    for line in handle:
+        data, catalysts, reaction = line.split(":")
+        catalysts = [(catalysts,)]
         reactants = {}
-        for i in range(len(self._products)):
-            reactants[self._products[i]] = self._stoch_out[i]
-        for i in range(len(self._substrates)):
-            reactants[self._substrates[i]] = - self._stoch_in[i]
-        self.data  = Reaction(reactants, [(self._ec_id,)], 1, self._reaction_id)
-
-
-class _Scanner:
-    """Start up Martel to do the scanning of the file.
-
-    This initialzes the Martel based parser and connects it to a handler
-    that will generate events for a Consumer.
-    """
-    def __init__(self, debug = 0):
-        """Initialize the scanner by setting up our caches.
-
-        Arguments:
-        o debug - The level of debugging that the parser should display.
-                  Level 0 is no debugging, Level 2 displays the most
-                  debugging info (but is much slower).
-                  See Martel documentation for more info on this.
-        """
-        # a listing of all tags we are interested in scanning for
-        # in the Martel parser
-        self.interest_tags = ["stoch", "reactant", "arrows", "reaction_id",
-                              "ec_id", "end"]
-
-        # make a parser that returns only the tags we are interested in
-        expression = Martel.select_names(map_format.reaction,
-                                         self.interest_tags)
-        self._parser = expression.make_parser(debug_level = debug)
-
-    def feed(self, handle, consumer):
-        """Feed a set of data into the scanner.
-
-        Arguments:
-        o handle - A handle with the information to parse.
-        o consumer - The consumer that should be informed of events.
-        """
-        self._parser.setContentHandler(EventGenerator(consumer,
-                                                      self.interest_tags))
-        self._parser.setErrorHandler(handler.ErrorHandler())
-        self._parser.parseFile(handle)
-
-
-
-
-
+        before, after = reaction.split("<=>")
+        compounds = before.split(" + ")
+        for compound in compounds:
+            compound = compound.strip()
+            try:
+               number, compound = compound.split()
+               number = -int(number)
+            except ValueError:
+               number = -1
+            reactants[compound] = number
+        compounds = after.split(" + ")
+        for compound in compounds:
+            compound = compound.strip()
+            try:
+               number, compound = compound.split()
+               number = int(number)
+            except ValueError:
+               number = +1
+            reactants[compound] = number
+        yield Reaction(reactants, catalysts, True, data)
