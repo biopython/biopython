607ab1837ea1a55d2726aab469d7032693a51ad7
peterc <peterc>
2007-11-26 20:45:12 +0000

    Comment about SeqRecord and the DBSeqRecord class needing to be in sync

--- Bio/SeqRecord.py
+++ Bio/SeqRecord.py
@@ -1,6 +1,9 @@
 # Stores data about the sequence
 
 # NEEDS TO BE SYNCH WITH THE REST OF BIOPYTHON AND BIOPERL
+# In particular, the SeqRecord and BioSQL.BioSeq.DBSeqRecord classes
+# need to be in sync (this is the BioSQL "Database SeqRecord", see
+# also BioSQL.BioSeq.DBSeq which is the "Database Seq" class)
 
 class SeqRecord:
     """A SeqRecord object holds a sequence and information about it.
