dc5f29c7d5673a8f4398a37d3b529a09612c4c92
mdehoon <mdehoon>
2007-12-11 10:25:07 +0000

    Using the new read() function in Bio.Prosite.Prodoc.

--- Tests/test_prodoc.py
+++ Tests/test_prodoc.py
@@ -10,13 +10,9 @@ from Bio import ParserSupport
 from Bio.Prosite import Prodoc
 
 
-#tests = [ 'ps00107.txt','ps00159.txt', 'ps00165.txt', 'ps00213.txt', 'ps00432.txt', \
-#    'ps00488.txt', 'ps00546.txt' ]
-
 tests = [ 'pdoc00100.txt', 'pdoc00113.txt', 'pdoc00144.txt', 'pdoc00149.txt', \
     'pdoc00340.txt', 'pdoc00424.txt', 'pdoc00472.txt', 'pdoc00640.txt', \
      'pdoc00787.txt', 'pdoc00933.txt' ]
-record_parser = Prodoc.RecordParser()
 
 def print_list( list ):
     for item in list:
@@ -32,11 +28,11 @@ for test in tests:
 #    print "testing %s" % test
     datafile = os.path.join( 'Prosite', 'Doc', test )
     src_handle = open( datafile )
-    data = record_parser.parse( src_handle )
+    data = Prodoc.read(src_handle)
     print data.accession
     print 'prosite_refs'
-    print_list( data.prosite_refs )
+    print_list(data.prosite_refs)
     print data.text
     print 'references'
-    print_references( data.references )
+    print_references(data.references)
     src_handle.close()
