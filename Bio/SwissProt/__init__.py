18c587eab3b0f702afd7d779ceee1118c5b1cddc
mdehoon <mdehoon>
2007-12-12 07:05:16 +0000

    Adding read(), parse() functions.

--- Bio/SwissProt/__init__.py
+++ Bio/SwissProt/__init__.py
@@ -1,6 +1,37 @@
-# Copyright 1999 by Jeffrey Chang.  All rights reserved.
+# Copyright 2007 by Michiel de Hoon.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 """Parsers for file formats from the SwissProt database.
 """
+
+# The parse(), read() functions can probably be simplified if we don't
+# use the "parser = RecordParser(); parser.parse(handle)" approach.
+def parse(handle):
+    from SProt import RecordParser
+    import cStringIO
+    parser = RecordParser()
+    text = ""
+    for line in handle:
+        text += line
+        if line[:2]=='//':
+            handle = cStringIO.StringIO(text)
+            record = parser.parse(handle)
+            text = ""
+            yield record
+
+def read(handle):
+    from SProt import RecordParser
+    parser = RecordParser()
+    try:
+        record = parser.parse(handle)
+    except ValueError, error:
+        if error.message.startswith("Line does not start with 'ID':"): 
+            raise ValueError, "No SwissProt record found"
+        else:
+            raise error
+    # We should have reached the end of the record by now
+    remainder = handle.read()
+    if remainder:
+        raise ValueError, "More than one SwissProt record found"
+    return record
