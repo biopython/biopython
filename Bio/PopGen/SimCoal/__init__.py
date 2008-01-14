236105060cecc100e359a04fbd6c93715b0d5bea
tiagoantao <tiagoantao>
2008-01-14 10:38:05 +0000

    2375 Workaround for build/test loop

--- Bio/PopGen/SimCoal/__init__.py
+++ Bio/PopGen/SimCoal/__init__.py
@@ -7,6 +7,13 @@
 SimCoal2 execution module and support functions.
 '''
 
-from os import sep
+from os import sep, access, F_OK
+from sys import path
 
-builtin_tpl_dir = sep.join(__file__.split(sep)[:-1] + ['data'])
+#This is a workaround to work with the test system
+#In any case the problem is with the test system
+for instance in path:
+    test_path = instance + sep + sep.join(['Bio', 'PopGen', 'SimCoal', 'data'])
+    if access(test_path, F_OK):
+        builtin_tpl_dir = test_path
+        break
