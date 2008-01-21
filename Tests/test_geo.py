ed5c3987285849e4ec62c10a126dd80facac364b
mdehoon <mdehoon>
2008-01-21 12:00:23 +0000

    Adding Tests/geo/soft_ex_affy_chp.txt.

--- Tests/test_geo.py
+++ Tests/test_geo.py
@@ -10,6 +10,7 @@ testfiles   = [  'GSE16.txt', 'GSM645.txt', 'GSM691.txt', 'GSM700.txt', 'GSM804.
 #Five additional files from the NCBI to document the GEO SOFT file format
 #changes made in 2005.  Note the new table_begin and table_end lines.
 testfiles.extend(['soft_ex_affy.txt',
+                  'soft_ex_affy_chp.txt',
                   'soft_ex_dual.txt',
                   'soft_ex_family.txt',
                   'soft_ex_platform.txt',
