c7c2ef3f0d9171e80a07789f17df12796b490059
mdehoon <mdehoon>
2008-04-20 15:28:58 +0000

    Fixing some bugs discovered by running the ELink tests.

--- Bio/Entrez/ELink.py
+++ Bio/Entrez/ELink.py
@@ -23,13 +23,32 @@ def startElement(self, name, attrs):
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList"]:
         self.record[-1]["IdUrlList"] = []
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet"]:
-        self.record[-1]["IdUrlList"].append({})
+        self.record[-1]["IdUrlList"].append({"ObjUrl": []})
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"] = {"SubjectType": [], "Attribute": []}
+        d = {"SubjectType": [], "Attribute": []}
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"].append(d)
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"] = {}
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"] = {}
     elif self.element==["eLinkResult", "LinkSet", "IdCheckList"]:
         self.record[-1]["IdCheckList"] = []
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "Id"]:
+        row = [None, None, None]
+        if "HasLinkOut" in attrs:
+            if attrs["HasLinkOut"]=='Y': row[1] = True
+            elif attrs["HasLinkOut"]=='N': row[1] = False
+        if "HasNeighbor" in attrs:
+            if attrs["HasNeighbor"]=='Y': row[2] = True
+            elif attrs["HasNeighbor"]=='N': row[2] = False
+        self.record[-1]["IdCheckList"].append(row)
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet"]:
+        # This is not in the DTD, but this is what I found when using the
+        # following query:
+        # >>> Bio.Entrez.elink(dbfrom="pubmed", id="12169658,11748140",
+        #                      cmd="acheck")
+        d = {"LinkInfo": []}
+        self.record[-1]["IdCheckList"].append(d)
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"].append({})
 
 def endElement(self, name):
     if name=="Error":
@@ -50,22 +69,38 @@ def endElement(self, name):
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "Id"]:
         self.record[-1]["IdUrlList"][-1]["Id"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Url"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Url"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Url"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "IconUrl"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["IconUrl"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["IconUrl"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "LinkName"]:
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["LinkName"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "SubjectType"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["SubjectType"].append(self.content)
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["SubjectType"].append(self.content)
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Attribute"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Attribute"].append(self.content)
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Attribute"].append(self.content)
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider", "Name"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"]["Name"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"]["Name"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider", "NameAbbr"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"]["NameAbbr"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"]["NameAbbr"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider", "Id"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"]["Id"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"]["Id"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider", "Url"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"]["Url"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"]["Url"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdUrlList", "IdUrlSet", "ObjUrl", "Provider", "IconUrl"]:
-        self.record[-1]["IdUrlList"][-1]["ObjUrl"]["Provider"]["IconUrl"] = self.content
+        self.record[-1]["IdUrlList"][-1]["ObjUrl"][-1]["Provider"]["IconUrl"] = self.content
     elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "Id"]:
-        self.record[-1]["IdCheckList"].append(self.content)
+        self.record[-1]["IdCheckList"][-1][0] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "Id"]:
+        self.record[-1]["IdCheckList"][-1]["Id"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "DbTo"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["DbTo"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "LinkName"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["LinkName"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "MenuTag"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["MenuTag"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "HtmlTag"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["HtmlTag"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "Url"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["Url"] = self.content
+    elif self.element==["eLinkResult", "LinkSet", "IdCheckList", "IdLinkSet", "LinkInfo", "Priority"]:
+        self.record[-1]["IdCheckList"][-1]["LinkInfo"][-1]["Priority"] = int(self.content)
