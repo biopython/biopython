b7a2a7ac63c5be5b426f6f70dfaf5cd9ed3190d6
peterc <peterc>
2008-06-17 11:18:52 +0000

    Added a minimal comment to better suit epydoc for our online API documentation.

--- Bio/Nexus/__init__.py
+++ Bio/Nexus/__init__.py
@@ -1,3 +1,5 @@
-#
-#
-#
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""The Bio.Nexus contains a NEXUS file parser and objects to model this data."""
