16287837d5493e9565848226a5b2e9109c2b8d61
peterc <peterc>
2008-06-26 22:01:43 +0000

    Adding a blast XML file with no hits, and modifying the unit test output to print out much less (22K versus 107K output file)

--- Tests/test_NCBIXML.py
+++ Tests/test_NCBIXML.py
@@ -15,12 +15,13 @@ all_tests = [
     'xbt005.xml', # TBLASTX 2.2.12, gi|1348853|gb|G26621.1|G26621, BLOSUM80
     'xbt006.xml', # BLASTP 2.2.18+, gi|160837788|ref|NP_075631.2|
                   # NOTE - no date in version field, downloaded 2008/05/08
+    'xbt007.xml', # BLASTP 2.2.18+, SwissProt Q08386 and P07175, no hits
     ]
 
 detailed_tests = [
     'xbt001.xml', # BLASTP 2.2.12, gi|49176427|ref|NP_418280.3|
     'xbt002.xml', # BLASTN 2.2.12, gi|1348916|gb|G26684.1|G26684
-    'xbt003.xml', # BLASTX 2.2.12, gi|1347369|gb|G25137.1|G25137
+    'xbt006.xml',
     ]
 
 for test in detailed_tests :
@@ -37,37 +38,37 @@ for test in all_tests:
 
     records = NCBIXML.parse(input)
 
-    if not test in detailed_tests:
-        # Pull out the record, but don't print anything
-        for record in records:
-            pass
-        continue
-
     for record in records:
         alignments = record.alignments
-        print 'Blast of', record.query
-        print 'Found %s alignments with a total of %s HSPs' % (len(alignments),
-                  reduce(lambda a,b: a+b, [len(a.hsps) for a in alignments]))
+        if not alignments :
+            print '%s - no hits' % record.query_id
+            continue
+        print '%s - %i alignments with a total of %i HSPs' \
+              % (record.query_id,
+                 len(alignments),
+                 reduce(lambda a,b: a+b, [len(a.hsps) for a in alignments]))
+
+        if not test in detailed_tests:
+            continue
+        
+        E_VALUE_THRESH = 10**-10
         for alignment in alignments:
             print alignment.title[:50], alignment.length, 'bp', len(alignment.hsps), 'HSPs'
 
-        # Cookbook example
-        E_VALUE_THRESH = 0.04
-        for alignment in alignments:
+            # Cookbook example
+            assert len(alignment.hsps) > 0
             for hsp in alignment.hsps:
                 if hsp.expect < E_VALUE_THRESH:
-                    print '*****'
-                    print 'sequence', alignment.title
-                    print 'length', alignment.length
+                    print 'HSP alignment length', alignment.length,
                     # The following is a quick and dirty hack to get the
                     # number of digits in the exponent to match that on record
                     # as the expected output.
                     f = str(hsp.expect)
                     if f.find("e-") <> -1 :
                         matissa, exponent = str(f).split("e-")
-                        print 'e value %se-%02i' % (matissa, int(exponent))
+                        print 'e-value %se-%02i' % (matissa, int(exponent))
                     else :
-                        print 'e value', hsp.expect
+                        print 'e-value', hsp.expect
                     print hsp.query[:75] + '...'
                     print hsp.match[:75] + '...'
                     print hsp.sbjct[:75] + '...'
