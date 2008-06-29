09793eb63f5c9b33268f761e37c89b16221eb174
mdehoon <mdehoon>
2008-06-29 11:48:08 +0000

    Setting version to 1.46 for Biopython release 1.46.

--- Martel/__init__.py
+++ Martel/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2000-2001, Dalke Scientific Software, LLC
 # Distributed under the Biopython License Agreement (see the LICENSE file).
 
-__version__ = "1.45"
+__version__ = "1.46"
 
 import Expression
 import convert_re
