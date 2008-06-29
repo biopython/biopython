4d1c9df619cdfe1e83fdcb5a44571654e32cd69c
peterc <peterc>
2008-06-29 15:24:20 +0000

    Removing stub file that's been here for three releases just to help anyone with old code which used Bio.FormatIO

--- Bio/FormatIO.py
+++
@@ -1,4 +0,0 @@
-#Would like to have just issued a deprecation warning, and removed this
-#module later.  However, due to the FormatIO code in Bio/SeqRecord.py the
-#deprecation warning would be triggered whenever someone used the SeqRecord.
-raise ImportError, "Bio.FormatIO has been removed.  Please try Bio.SeqIO instead"
