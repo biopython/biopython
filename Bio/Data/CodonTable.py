5e9a7248bc063fbf37422537a4ca95df292cb2ef
peterc <peterc>
2008-06-29 01:38:18 +0000

    Only create one shared version of each ambiguous table, rather than one copy for each different name of the table

--- Bio/Data/CodonTable.py
+++ Bio/Data/CodonTable.py
@@ -673,14 +673,8 @@ class AmbiguousForwardTable:
         self._cache[codon] = x
         return x
 
-
+#Prepare the ambiguous tables for DNA, RNA and Generic (DNA or RNA)
 ambiguous_dna_by_name = {}
-for key, val in unambiguous_dna_by_name.items():
-    ambiguous_dna_by_name[key] = AmbiguousCodonTable(val,
-                                     IUPAC.ambiguous_dna,
-                                     IUPACData.ambiguous_dna_values,
-                                     IUPAC.extended_protein,
-                                     IUPACData.extended_protein_values)
 ambiguous_dna_by_id = {}
 for key, val in unambiguous_dna_by_id.items():
     ambiguous_dna_by_id[key] = AmbiguousCodonTable(val,
@@ -688,14 +682,10 @@ for key, val in unambiguous_dna_by_id.items():
                                      IUPACData.ambiguous_dna_values,
                                      IUPAC.extended_protein,
                                      IUPACData.extended_protein_values)
+    for name in unambiguous_dna_by_name:
+        ambiguous_dna_by_name[name] = ambiguous_dna_by_id[key]
 
 ambiguous_rna_by_name = {}
-for key, val in unambiguous_rna_by_name.items():
-    ambiguous_rna_by_name[key] = AmbiguousCodonTable(val,
-                                     IUPAC.ambiguous_rna,
-                                     IUPACData.ambiguous_rna_values,
-                                     IUPAC.extended_protein,
-                                     IUPACData.extended_protein_values)
 ambiguous_rna_by_id = {}
 for key, val in unambiguous_rna_by_id.items():
     ambiguous_rna_by_id[key] = AmbiguousCodonTable(val,
@@ -703,17 +693,12 @@ for key, val in unambiguous_rna_by_id.items():
                                      IUPACData.ambiguous_rna_values,
                                      IUPAC.extended_protein,
                                      IUPACData.extended_protein_values)
+    for name in unambiguous_rna_by_name:
+        ambiguous_rna_by_name[name] = ambiguous_rna_by_id[key]
 
 #The following isn't very elegant, but seems to work nicely.
 _merged_values = dict(IUPACData.ambiguous_rna_values.iteritems())
 _merged_values["T"] = "U"
-
-for key, val in generic_by_name.items():
-    ambiguous_generic_by_name[key] = AmbiguousCodonTable(val,
-                                     Alphabet.NucleotideAlphabet(),
-                                     _merged_values,
-                                     IUPAC.extended_protein,
-                                     IUPACData.extended_protein_values)
 
 for key, val in generic_by_id.items():
     ambiguous_generic_by_id[key] = AmbiguousCodonTable(val,
@@ -721,8 +706,14 @@ for key, val in generic_by_id.items():
                                      _merged_values,
                                      IUPAC.extended_protein,
                                      IUPACData.extended_protein_values)
+    for name in generic_by_name:
+        ambiguous_generic_by_name[name] = ambiguous_generic_by_id[key]
 
-#Basic sanity test,
+#Basic sanity tests,
+assert len(ambiguous_dna_by_name.keys()) == len(unambiguous_dna_by_name.keys())
+assert len(ambiguous_rna_by_name.keys()) == len(unambiguous_rna_by_name.keys())
+assert len(ambiguous_generic_by_name.keys()) == len(generic_by_name.keys())
+
 for id in ambiguous_generic_by_id.keys() :
     assert ambiguous_rna_by_id[id].forward_table["GUU"] == "V"
     assert ambiguous_rna_by_id[id].forward_table["GUN"] == "V"
