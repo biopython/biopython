e5d197933887316cad2ccfc290c815c459bc9993
peterc <peterc>
2008-07-15 15:50:20 +0000

    Adding fasta-m10 output file using DNA sequences and FASTA 35.4.1 (which added an alignment consensus sequence)

--- Tests/test_AlignIO.py
+++ Tests/test_AlignIO.py
@@ -45,6 +45,7 @@ test_files = [ \
     ("fasta-m10", 2, 4, 'Fasta/output001.m10'),
     ("fasta-m10", 2, 6, 'Fasta/output002.m10'),
     ("fasta-m10", 2, 3, 'Fasta/output003.m10'),
+    ("fasta-m10", 2, 1, 'Fasta/output004.m10'),
     ("ig", 16, 1, 'IntelliGenetics/VIF_mase-pro.txt'),
     ]
 
