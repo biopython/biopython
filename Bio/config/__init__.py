9ea3d1d1029eb3ba2a1ff5cff897f12a68cb93e9
peterc <peterc>
2008-07-22 15:56:38 +0000

    Explicitly deprecating Bio.formatdefs, Bio.config and Bio.dbrefs which all seem to depend on the already deprecated Bio.expressions

--- Bio/config/__init__.py
+++ Bio/config/__init__.py
@@ -2,3 +2,9 @@
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
+import warnings
+warnings.warn("Bio.config, Bio.formatdefs and the underlying " \
+              +"Bio.expressions module are deprecated. If you want to " \
+              +"continue to use these modules, please get in contact on " \
+              +"the Biopython mailinglist.",
+              DeprecationWarning)
--- Bio/formatdefs/__init__.py
+++ Bio/formatdefs/__init__.py
@@ -1,1 +1,6 @@
 # This is a Python module.
+import warnings
+warnings.warn("Bio.formatdefs and the underlying Bio.expressions module " \
+              +"are deprecated. If you want to continue to use these " \
+              +"modules, please get in contact on the Biopython mailinglist.",
+              DeprecationWarning)
