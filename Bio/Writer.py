e5b0b171da2f4506af2073715990a4d1cdcfd4c3
peterc <peterc>
2008-07-24 11:55:20 +0000

    Deprecating Bio.Writer as none of Biopython uses it anymore

--- Bio/Writer.py
+++ Bio/Writer.py
@@ -1,3 +1,8 @@
+import warnings
+warnings.warn("Bio.Writer was deprecated, as any Biopython modules that used "\
+               +"it have been deprecated. If you do use this module, please "\
+               +"get in touch via the Biopython mailing list or bugzilla.")
+
 class Writer:
     def __init__(self, outfile):
         self.outfile = outfile
