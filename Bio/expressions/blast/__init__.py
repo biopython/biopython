6a3b9e4cedb350d5619722da444bc5a353400cc3
peterc <peterc>
2008-09-20 11:30:39 +0000

    Removing deprecated modules config, formatdefs, dbdefs and expressions

--- Bio/expressions/blast/__init__.py
+++
@@ -1,5 +0,0 @@
-# This is a Python module
-import warnings
-warnings.warn("Bio.expressions was deprecated, as it does not work with recent versions of mxTextTools. If you want to continue to use this module, please get in contact with the Biopython developers at biopython-dev@biopython.org to avoid permanent removal of this module from Biopython", DeprecationWarning)
-
-
--- Bio/formatdefs/__init__.py
+++
@@ -1,8 +0,0 @@
-# This is a Python module.
-"""Martel based file format defintions (DEPRECATED)."""
-
-import warnings
-warnings.warn("Bio.formatdefs and the underlying Bio.expressions module " \
-              +"are deprecated. If you want to continue to use these " \
-              +"modules, please get in contact on the Biopython mailinglist.",
-              DeprecationWarning)
