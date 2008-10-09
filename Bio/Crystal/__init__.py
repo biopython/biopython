d6539b6d21014fae28688ba3bdfcc40f48f95c27
peterc <peterc>
2008-10-09 20:17:17 +0000

    Tweaked the exception for python 2.6 compatibility, and some trivial formatting changes (removing extra brackets and white space - see PEP8)

--- Bio/Crystal/__init__.py
+++ Bio/Crystal/__init__.py
@@ -4,64 +4,54 @@
 # as part of this package.
 
 """
-Hetero, Crystal and Chain exist to represent the NDB Atlas structure.  Atlas is a minimal
-subset of the PDB format.  Heteo supports a 3 alphameric code.
+Module to represent the NDB Atlas structure (a minimal subset of PDB format).
+
+Hetero, Crystal and Chain exist to represent the NDB Atlas structure.  Atlas
+is a minimal subset of the PDB format.  Heteo supports a 3 alphameric code.
 The NDB web interface is located at http://ndbserver.rutgers.edu/NDB/index.html
 """
-
 
 import string, array, copy
 from Bio.Seq import Seq
 from Bio.Seq import MutableSeq
 
-def wrap_line( line ):
+class CrystalError(Exception):
+    pass
+
+def wrap_line(line):
     output = ''
-    for i in range( 0, len( line ), 80 ):
+    for i in range(0, len(line), 80):
         output = output + '%s\n' % line[ i: i + 80 ]
     return output
 
-def validate_key( key ):
-    if( type( key ) != type( '' ) ):
-        raise CrystalError( 'chain requires a string label' )
-    if( len( key ) != 1 ):
-        raise CrystalError( 'chain label should contain one letter' )
-
-class Error( Exception ):
-    """
-    """
-    def __init__( self ):
-        pass
-
-class CrystalError( Error ):
-
-    """
-        message - description of error
-    """
-
-    def __init__( self, message ):
-        self.message = message
+def validate_key(key):
+    if type(key) != type(''):
+        raise CrystalError('chain requires a string label')
+    if len(key) != 1:
+        raise CrystalError('chain label should contain one letter')
 
 class Hetero:
     """
-    This class exists to support the PDB hetero codes.  Supports only the 3 alphameric code.
+    This class exists to support the PDB hetero codes.
+
+    Supports only the 3 alphameric code.
     The annotation is available from http://alpha2.bmc.uu.se/hicup/
     """
     def __init__(self, data):
         # Enforce string storage
-        if( type(data) != type("") ):
-            raise CrystalError( 'Hetero data must be an alphameric string' )
-        if( data.isalnum() == 0 ):
-            raise CrystalError( 'Hetero data must be an alphameric string' )
-        if( len( data ) > 3 ):
-            raise CrystalError( 'Hetero data may contain up to 3 characters' )
-        if( len( data ) < 1 ):
-            raise CrystalError( 'Hetero data must not be empty' )
+        if type(data) != type(""):
+            raise CrystalError('Hetero data must be an alphameric string')
+        if data.isalnum() == 0:
+            raise CrystalError('Hetero data must be an alphameric string')
+        if len(data) > 3:
+            raise CrystalError('Hetero data may contain up to 3 characters')
+        if len(data) < 1:
+            raise CrystalError('Hetero data must not be empty')
 
         self.data = data[:].lower()
 
     def __eq__(self, other):
-        return (self.data == other.data )
-
+        return self.data == other.data
 
     def __ne__(self, other):
         """Returns true iff self is not equal to other."""
@@ -73,51 +63,50 @@ class Hetero:
     def __str__(self):
         return "%s" % self.data
 
-
     def __len__(self): return len(self.data)
 
 class Chain:
-    def __init__(self, residues = '' ):
+    def __init__(self, residues = ''):
         self.data = []
-        if( type( residues ) == type( '' ) ):
-            residues = residues.replace( '*', ' ' )
+        if type(residues) == type(''):
+            residues = residues.replace('*', ' ')
             residues = residues.strip()
             elements = residues.split()
-            self.data = map( Hetero, elements )
-        elif( type( residues ) == type( [] ) ):
+            self.data = map(Hetero, elements)
+        elif type(residues) == type([]):
             for element in residues:
-                if( not isinstance( element, Hetero ) ):
-                    raise CrystalError( 'Text must be a string' )
+                if not isinstance(element, Hetero):
+                    raise CrystalError('Text must be a string')
             for residue in residues:
-                self.data.append( residue )
-        elif( isinstance( residues, Chain ) ):
+                self.data.append(residue)
+        elif isinstance(residues, Chain):
             for residue in residues:
-                self.data.append( residue )
+                self.data.append(residue)
         self.validate()
 
-    def validate( self ):
+    def validate(self):
         data = self.data
         for element in data:
-            self.validate_element( element )
+            self.validate_element(element)
 
-    def validate_element( self, element ):
-        if(  not isinstance( element, Hetero ) ):
+    def validate_element(self, element):
+        if not isinstance(element, Hetero):
             raise TypeError
 
-    def __str__( self ):
+    def __str__(self):
         output = ''
         i = 0
         for element in self.data:
             output = output + '%s ' % element
         output = output.strip()
-        output = wrap_line( output )
+        output = wrap_line(output)
         return output
 
 
     def __eq__(self, other):
-        if( len( self.data ) != len( other.data ) ):
+        if len(self.data) != len(other.data):
             return 0
-        ok = reduce( lambda x, y: x and y, map( lambda x, y: x == y, self.data, other.data ) )
+        ok = reduce(lambda x, y: x and y, map(lambda x, y: x == y, self.data, other.data))
         return ok
 
     def __ne__(self, other):
@@ -129,9 +118,9 @@ class Chain:
 
     def __setitem__(self, i, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         self.data[i] = item
 
     def __delitem__(self, i):
@@ -147,8 +136,8 @@ class Chain:
             self.data[i:j] = other.data
         elif isinstance(other, type(self.data)):
             self.data[i:j] = other
-        elif type( other ) == type( '' ):
-            self.data[ i:j ] = Chain( other ).data
+        elif type(other) == type(''):
+            self.data[ i:j ] = Chain(other).data
         else:
             raise TypeError
 
@@ -158,95 +147,93 @@ class Chain:
 
     def __contains__(self, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         return item in self.data
 
     def append(self, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         self.data.append(item)
 
     def insert(self, i, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         self.data.insert(i, item)
 
     def remove(self, item):
-        item = Hetero( item.lower() )
+        item = Hetero(item.lower())
         self.data.remove(item)
 
     def count(self, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         return self.data.count(item)
 
     def index(self, item):
         try:
-            self.validate_element( item )
+            self.validate_element(item)
         except TypeError:
-            item = Hetero( item.lower() )
+            item = Hetero(item.lower())
         return self.data.index(item)
 
     def __add__(self, other):
         if isinstance(other, Chain):
             return self.__class__(self.data + other.data)
-        elif type( other ) == type( '' ):
-            return self.__class__(self.data + Chain( other).data )
+        elif type(other) == type(''):
+            return self.__class__(self.data + Chain(other).data)
         else:
             raise TypeError
 
     def __radd__(self, other):
         if isinstance(other, Chain):
-            return self.__class__( other.data + self.data )
-        elif type( other ) == type( '' ):
-            return self.__class__( Chain( other ).data + self.data )
+            return self.__class__(other.data + self.data)
+        elif type(other) == type(''):
+            return self.__class__(Chain(other).data + self.data)
         else:
             raise TypeError
 
     def __iadd__(self, other):
-        if isinstance(other, Chain ):
+        if isinstance(other, Chain):
             self.data += other.data
-        elif type( other ) == type( '' ):
-            self.data += Chain( other ).data
+        elif type(other) == type(''):
+            self.data += Chain(other).data
         else:
             raise TypeError
         return self
 
 class Crystal:
-    def __init__(self, data = {} ):
+    def __init__(self, data = {}):
         # Enforcestorage
-        if( type( data ) != type( {} ) ):
-            raise CrystalError( 'Crystal must be a dictionary' )
+        if type(data) != type({}):
+            raise CrystalError('Crystal must be a dictionary')
         self.data = data
         self.fix()
 
-    def fix( self ):
+    def fix(self):
         data = self.data
         for key in data.keys():
             element = data[ key ]
-            if( isinstance( element, Chain ) ):
+            if isinstance(element, Chain):
                 pass
-            elif type( element ) == type( '' ):
-                data[ key ] = Chain( element )
+            elif type(element) == type(''):
+                data[ key ] = Chain(element)
             else:
                 raise TypeError
-
-
 
     def __repr__(self):
         output = ''
         keys = self.data.keys()
         keys.sort()
         for key in keys:
-            output = output +  '%s : %s\n' % ( key, self.data[ key ] )
+            output = output +  '%s : %s\n' % (key, self.data[ key ])
         return output
 
     def __str__(self):
@@ -254,7 +241,7 @@ class Crystal:
         keys = self.data.keys()
         keys.sort()
         for key in keys:
-            output = output +  '%s : %s\n' % ( key, self.data[ key ] )
+            output = output +  '%s : %s\n' % (key, self.data[ key ])
         return output
 
     def tostring(self):
@@ -263,10 +250,10 @@ class Crystal:
     def __len__(self): return len(self.data)
     def __getitem__(self, key): return self.data[key]
     def __setitem__(self, key, item):
-        if isinstance( item, Chain ):
+        if isinstance(item, Chain):
             self.data[key] = item
-        elif type( item ) == type( '' ):
-            self.data[ key ] = Chain( item )
+        elif type(item) == type(''):
+            self.data[ key ] = Chain(item)
         else:
             raise TypeError
 
