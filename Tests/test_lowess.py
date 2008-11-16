c39899f5f32100cd46a5016565a9364c4df98888
mdehoon <mdehoon>
2008-11-16 06:37:05 +0000

    Adding a test case for Bio.Statistics.lowess; see bug #2657.

---
+++ Tests/test_lowess.py
@@ -0,0 +1,28 @@
+from Bio.Statistics.lowess import lowess
+from numpy import array
+import unittest, sys
+
+class test_lowess(unittest.TestCase):
+
+    def test_Precomputed(self):
+        x = array([0.0, 1.0, 2.0, 3.0, 5.0, 9.0, 11.0])
+        y = x**2
+        # Precalculated smooth output
+        ys = array([-2.96219015, 1.72680044, 6.58686813,
+                    11.62986671, 28.18598762, 86.85271581, 116.83893423 ])
+        # Smooth output calculated by the lowess function
+        output = lowess(x, y, f=2./3., iter = 3)
+        for precomputed, calculated in zip(ys, output):
+            self.assertAlmostEqual(precomputed, calculated, 4)
+
+def testing_suite():
+    s = unittest.makeSuite(test_lowess)
+    return unittest.TestSuite([s])
+
+def run_tests(argv):
+    test_suite = testing_suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity = 2)
+    runner.run(test_suite)
+
+if __name__ == "__main__":
+    sys.exit(run_tests(sys.argv))
