f7917277353e6660450fa596bf1518524fc05d57
peterc <peterc>
2009-03-22 22:48:47 +0000

    Removing PNG output (renderPM isn't always present) and testing of an experimental feature, accidentally included in the last check-in.

--- Tests/test_GenomeDiagram.py
+++ Tests/test_GenomeDiagram.py
@@ -270,14 +270,6 @@ class DiagramTest(unittest.TestCase):
                 #Out of frame (too far right)
                 continue
 
-            #This URL should work in SVG output from recent versions
-            #of ReportLab.  You need something newer than ReportLab 2.3
-            try :
-                url = "http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi"+\
-                      "?db=protein&id=%s" % feature.qualifiers["protein_id"][0]
-            except KeyError :
-                url = None
-                
             #Note that I am using strings for color names, instead
             #of passing in color objects.  This should also work!
             if len(gds_features) % 2 == 0 :
@@ -287,7 +279,6 @@ class DiagramTest(unittest.TestCase):
             #Checking it can cope with the old UK spelling colour.
             #Also show the labels perpendicular to the track.
             gds_features.add_feature(feature, colour=color,
-                                     url = url,
                                      sigil="ARROW",
                                      label_position = "start",
                                      label_size = 8,
@@ -308,8 +299,6 @@ class DiagramTest(unittest.TestCase):
 
         output_filename = os.path.join('Graphics', 'GD_region_linear.svg')
         gdd.write(output_filename, 'SVG')
-        output_filename = os.path.join('Graphics', 'GD_region_linear.png')
-        gdd.write(output_filename, 'PNG')
 
         #Circular with a particular start/end is a bit odd, but by setting
         #circular=False (above) a sweep of 90% is used (a wedge is left out)
@@ -320,8 +309,6 @@ class DiagramTest(unittest.TestCase):
         gdd.write(output_filename, 'PDF')
         output_filename = os.path.join('Graphics', 'GD_region_circular.svg')
         gdd.write(output_filename, 'SVG')
-        output_filename = os.path.join('Graphics', 'GD_region_circular.png')
-        gdd.write(output_filename, 'PNG')
 
     def test_diagram_via_methods_pdf(self) :
         """Construct and draw PDF using method approach."""
