2cf7a37c5e2850330c76da23a6c00589718b8793
peterc <peterc>
2009-04-22 11:31:03 +0000

    Include EMBL version of NC_005816 for cross checking with GenBank parser

--- Tests/test_SeqIO_features.py
+++ Tests/test_SeqIO_features.py
@@ -15,10 +15,10 @@ from Bio.Seq import Seq, UnknownSeq, MutableSeq
 from Bio.SeqRecord import SeqRecord
 
 #Top level function as this makes it easier to use for debugging:
-def compare_record(old, new) :
-    #Note the name matching is a bit fuzzy, e.g. truncation and
-    #no spaces in PHYLIP files.
-    if old.id != new.id and old.name != new.name \
+def compare_record(old, new, ignore_name=False) :
+    #Note the name matching is a bit fuzzy
+    if not ignore_name \
+    and old.id != new.id and old.name != new.name \
     and (old.id not in new.id) and (new.id not in old.id) \
     and (old.id.replace(" ","_") != new.id.replace(" ","_")) :
         raise ValueError("'%s' or '%s' vs '%s' or '%s' records" \
@@ -30,10 +30,11 @@ def compare_record(old, new) :
             raise ValueError("'%s' vs '%s'" % (old.seq, new.seq))
         else :
             raise ValueError("'%s...' vs '%s...'" % (old.seq[:100], new.seq[:100]))
-    if old.features and new.features \
-    and len(old.features) != len(new.features) :
-        raise ValueError("%i vs %i features" \
-                         % (len(old.features, len(new.features))))
+    if old.features and new.features :
+        return compare_features(old.features, new.features)
+    #Just insist on at least one word in common:
+    if not set(old.description.split()).intersection(new.description.split()):
+        raise ValueError("%s versus %s" % (old.description, new.description))
     #TODO - check annotation
 
 def compare_records(old_list, new_list) :
@@ -45,13 +46,46 @@ def compare_records(old_list, new_list) :
             return False
     return True
 
-class GenBankTests(unittest.TestCase):
+def compare_feature(old, new) :
+    """Check two SeqFeatures agree."""
+    if old.type != new.type :
+        raise ValueError("Type %s versus %s" % (old.type, new.type))
+    if old.location.nofuzzy_start != new.location.nofuzzy_start \
+    or old.location.nofuzzy_end != new.location.nofuzzy_end :
+        raise ValueError("%s versus %s" % (old.location, new.location))
+    if old.strand != new.strand :
+        raise ValueError("Different strand")
+    if old.location.start != new.location.start :
+        raise ValueError("%s versus %s" % (old.location.start, new.location.start))
+    if old.location.end != new.location.end :
+        raise ValueError("%s versus %s" % (old.location.end, new.location.end))
+    if len(old.sub_features) != len(new.sub_features) :
+        raise ValueError("Different sub features")
+    for a,b in zip(old.sub_features, new.sub_features) :
+        if not compare_feature(a,b) :
+            return False
+    #TODO - Check qualifiers?
+    return True
+
+def compare_features(old_list, new_list) :
+    """Check two lists of SeqFeatures agree, raises a ValueError if mismatch."""
+    if len(old_list) != len(new_list) :
+        raise ValueError("%i vs %i features" % (len(old_list), len(new_list)))
+    for old, new in zip(old_list, new_list) :
+        #This assumes they are in the same order
+        if not compare_feature(old,new) :
+            return False
+    return True
+
+class NC_005816(unittest.TestCase):
     basename = "NC_005816"
+    emblname = "AE017046"
     table = 11
     skip_trans_test = []
+    __doc__ = "Tests using %s GenBank and FASTA files from the NCBI" % basename
     
     def test_CDS(self) :
-        """Checking GenBank CDS translations vs FASTA faa file."""
+        #"""Checking GenBank CDS translations vs FASTA faa file."""
         filename = os.path.join("GenBank",self.basename+".gb")
         gb_cds = list(SeqIO.parse(open(filename),"genbank-cds"))
         filename = os.path.join("GenBank",self.basename+".faa")
@@ -59,7 +93,7 @@ class GenBankTests(unittest.TestCase):
         compare_records(gb_cds, fasta)
 
     def test_Translations(self):
-        """Checking translation of FASTA features (faa vs ffn)."""
+        #"""Checking translation of FASTA features (faa vs ffn)."""
         filename = os.path.join("GenBank",self.basename+".faa")
         faa_records = list(SeqIO.parse(open(filename),"fasta"))
         filename = os.path.join("GenBank",self.basename+".ffn")
@@ -82,15 +116,20 @@ class GenBankTests(unittest.TestCase):
                                     faa.format("fasta")))
 
     def test_Genome(self) :
-        """Checking GenBank sequence vs FASTA fna file."""
+        #"""Checking GenBank sequence vs FASTA fna file."""
         filename = os.path.join("GenBank",self.basename+".gb")
         gb_record = SeqIO.read(open(filename),"genbank")
         filename = os.path.join("GenBank",self.basename+".fna")
         fa_record = SeqIO.read(open(filename),"fasta")
         compare_record(gb_record, fa_record)
+        if self.emblname is None :
+            return
+        filename = os.path.join("EMBL",self.emblname+".embl")
+        embl_record = SeqIO.read(open(filename),"embl")
+        compare_record(gb_record, embl_record, ignore_name=True)
 
     def test_Features(self) :
-        """Checking GenBank features sequences vs FASTA ffn file."""
+        #"""Checking GenBank features sequences vs FASTA ffn file."""
         filename = os.path.join("GenBank",self.basename+".gb")
         gb_record = SeqIO.read(open(filename),"genbank")
         features = [f for f in gb_record.features if f.type=="CDS"]
@@ -114,12 +153,15 @@ class GenBankTests(unittest.TestCase):
             self.assertEqual(str(fa_record.seq),
                              str(f_seq))
 
-class GenBankTests2(GenBankTests):
+class NC_006980(NC_005816):
     #This includes several joins and fuzzy joins :)
     basename = "NC_006980"
+    emblname = None
     table = 1
     skip_trans_test = ["gi|126643907|ref|XP_001388140.1|"]
+    __doc__ = "Tests using %s GenBank and FASTA files from the NCBI" % basename
     #TODO - neat way to change the docstrings...
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity = 2)
