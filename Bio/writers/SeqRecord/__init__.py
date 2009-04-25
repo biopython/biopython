a0cf047a355cfbc5bf959935b98d9070e40134ff
peterc <peterc>
2009-04-25 20:49:05 +0000

    Removing deprectaed module Bio.writers

--- Bio/writers/SeqRecord/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Part of an old unused and undocumented sequence writing framework (DEPRECATED)."""
-# This is a Python module.
--- Bio/writers/SeqRecord/empty.py
+++
@@ -1,7 +0,0 @@
-"""Part of an old unused and undocumented sequence writing framework (DEPRECATED)."""
-from Bio import Writer
-
-class WriteEmpty(Writer.Writer):
-    pass
-
-make_writer = WriteEmpty
