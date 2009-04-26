32ac6046266e16c712b37328edf047b49511a9c2
peterc <peterc>
2009-04-26 12:39:11 +0000

    Removing invalid paramater names aliases (should be single words, see Bio.Application)

--- Bio/AlignAce/Applications.py
+++ Bio/AlignAce/Applications.py
@@ -15,28 +15,28 @@ class AlignAceCommandline(Application.AbstractCommandline):
 
         self.parameters = \
           [
-            _Option(["-i","input","Sequence File"],["input"],lambda x : x.__class__== str,1,
+            _Option(["-i","input"],["input"],lambda x : x.__class__== str,1,
                     "Input Sequence file in FASTA format."),
             
-            _Option(["-numcols","numcols","number of columns to align"],["input"],lambda x : x.__class__== int,0,
+            _Option(["-numcols","numcols"],["input"],lambda x : x.__class__== int,0,
                     "Number of columns to align"),
 
-            _Option(["-expect","expect","number of sites expected in model "],["input"],lambda x : x.__class__== int,0,
+            _Option(["-expect","expect"],["input"],lambda x : x.__class__== int,0,
                     "number of sites expected in model "),
             
-            _Option(["-gcback","gcback","background fractional GC content of input sequence"],["input"],lambda x : x.__class__== float,0,
+            _Option(["-gcback","gcback"],["input"],lambda x : x.__class__== float,0,
                     "background fractional GC content of input sequence"),
             
-            _Option(["-minpass","minpass","minimum number of non-improved passes in phase 1"],["input"],lambda x : x.__class__== int,0,
+            _Option(["-minpass","minpass"],["input"],lambda x : x.__class__== int,0,
                     "minimum number of non-improved passes in phase 1"),
             
-            _Option(["-seed","seed","set seed for random number generator (time)"],["input"],lambda x : x.__class__== int,0,
+            _Option(["-seed","seed"],["input"],lambda x : x.__class__== int,0,
                     "set seed for random number generator (time)"),
             
-            _Option(["-undersample","undersample","possible sites / (expect * numcols * seedings)"],["input"],lambda x : x.__class__== int,0,
+            _Option(["-undersample","undersample"],["input"],lambda x : x.__class__== int,0,
                     "possible sites / (expect * numcols * seedings)"),
 
-            _Option(["-oversample","oversample","1/undersample"],["input"],lambda x : x.__class__== int,0,
+            _Option(["-oversample","oversample"],["input"],lambda x : x.__class__== int,0,
                     "1/undersample"),
           ]
 
