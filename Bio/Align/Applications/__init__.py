ec270f3a555e73b239ce34702ec8bc60f500e29c
peterc <peterc>
2009-04-28 15:37:15 +0000

    Adding Cymon's Clustalw wrapper under Bio.Align.Applications, with basic testing.

--- Bio/Align/Applications/__init__.py
+++ Bio/Align/Applications/__init__.py
@@ -1,6 +1,7 @@
-# Copyright 2009 by Peter Cock.  All rights reserved.
+# Copyright 2009 by Peter Cock & Cymon J. Cox.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-"""Alignment coomand line tool wrappers."""
+"""Alignment command line tool wrappers."""
 from _Muscle import MuscleCommandline
+from _Clustalw import ClustalwCommandline
