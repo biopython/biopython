cb423e39943bae6b117edf76e6d360f468211e35
peterc <peterc>
2009-04-28 10:56:21 +0000

    Reorganising Motif command line wrappers as per mailing list

---
+++ Bio/Motif/Applications/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2009 by Bartek Wilczynski.  All rights reserved.
+# Revisions copyright 2009 by Peter Cock.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+"""Motif command line tool wrappers."""
+from _AlignAce import AlignAceCommandline
