2246181ca56780b6191915e98fd077b81275692b
peterc <peterc>
2009-04-28 14:33:44 +0000

    Should have been part of previous checkin...

---
+++ Bio/Align/Applications/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2009 by Peter Cock.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+"""Alignment coomand line tool wrappers."""
+from _Muscle import MuscleCommandline
