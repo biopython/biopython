21a916e6da675b6b712864d056e96da37cba1a30
tiagoantao <tiagoantao>
2009-09-17 03:09:01 +0000

    Support for frequentist statistics via GenePop

--- Tests/test_PopGen_GenePop.py
+++ Tests/test_PopGen_GenePop.py
@@ -1,99 +1,92 @@
-# Copyright 2007 by Tiago Antao <tiagoantao@gmail.com>.  All rights reserved.
+# Copyright 2009 by Tiago Antao <tiagoantao@gmail.com>.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-
 import os
 import unittest
-from Bio.PopGen import GenePop
+
+from Bio import MissingExternalDependencyError
+from Bio.PopGen.GenePop.Controller import GenePopController
+
+#Tests genepop related code. Note: this case requires genepop
+#test_PopGen_GenePop_nodepend tests code that does not require genepop
+
+found = False
+for path in os.environ['PATH'].split(os.pathsep):
+    try:
+        for filename in os.listdir(path):
+            if filename.startswith('Genepop'):
+                found = True
+    except os.error:
+        pass #Path doesn't exist - correct to pass
+if not found:
+    raise MissingExternalDependencyError(\
+        "Install GenePop if you want to use Bio.PopGen.GenePop.")
 
 
-class RecordTest(unittest.TestCase):
-    def test_record_basic(self):
-        """Basic test on Record
+class AppTest(unittest.TestCase):
+    """Tests genepop execution via biopython.
+    """
+
+    def test_allele_genotype_frequencies(self):
+        """Test genepop execution on basic allele and genotype frequencies.
         """
+        ctrl = GenePopController()
+        pop_iter, locus_iter = ctrl.calc_allele_genotype_freqs("PopGen" + os.sep + "controller1.gen")
+        #print pop, loci
+        #for popc in pop_iter:
+        #    pop_name, loci_content = popc
+        #    print pop_name
+        #    for locus in loci_content.keys():
+        #        geno_list, hets, freq_fis = loci_content[locus]
+        #        print locus
+        #        print hets
+        #        print freq_fis
+        #        print geno_list
+        #    print
 
-        r = GenePop.Record()
-        assert type(r.marker_len)   == int
-        assert type(r.comment_line) == str
-        assert type(r.loci_list)    == list
-        assert type(r.populations)  == list
-
-class ParserTest(unittest.TestCase):
-    def setUp(self):
-        files = ["c2line.gen", "c3line.gen", "c2space.gen", "c3space.gen"]
-        self.handles = []
-        for filename in files:
-            self.handles.append(open(os.path.join("PopGen", filename)))
-
-        self.pops_indivs = [
-            (3, [4, 3, 5]),
-            (3, [4, 3, 5]),
-            (3, [4, 3, 5]),
-            (3, [4, 3, 5])
-        ]
-        self.num_loci = [3, 3, 3, 3]
-        self.marker_len = [2, 3, 2, 3]
-
-    def tearDown(self):
-        for handle in self.handles:
-            handle.close()
-
-    def test_record_parser(self):
-        """Basic operation of the Record Parser.
+    def test_calc_diversities_fis_with_identity(self):
+        """Test calculations of diversities ...
         """
-        for index in range(len(self.handles)):
-            handle = self.handles[index]
-            rec = GenePop.parse(handle)
-            assert isinstance(rec, GenePop.Record)
-            assert len(rec.loci_list) == self.num_loci[index]
-            assert rec.marker_len == self.marker_len[index]
-            assert len(rec.populations) == self.pops_indivs[index][0]
-            for i in range(self.pops_indivs[index][0]):
-                assert len(rec.populations[i]) == \
-                           self.pops_indivs[index][1][i]
+        ctrl = GenePopController()
+        iter, avg_fis, avg_Qintra = ctrl.calc_diversities_fis_with_identity(
+            "PopGen" + os.sep + "big.gen")
+        liter = list(iter)
+        assert len(liter) == 37
+        assert liter[0][0] == "Locus1"
+        assert len(avg_fis)==10
+        assert len(avg_Qintra)==10
 
-    def test_wrong_file_parser(self):
-        """Testing the ability to deal with wrongly formatted files
+    def test_estimate_nm(self):
+        """Test Nm estimation.
         """
-        f = open(os.path.join("PopGen", "fdist1"))
-        try:
-            rec = GenePop.parse(f)
-            raise Error("Should have raised exception")
-        except ValueError:
-            pass
-        f.close()
-
-class UtilsTest(unittest.TestCase):
-    def setUp(self):
-        #All files have to have at least 3 loci and 2 pops
-        files = ["c2line.gen"]
-        self.handles = []
-        for filename in files:
-            self.handles.append(open(os.path.join("PopGen", filename)))
+        ctrl = GenePopController()
+        mean_sample_size, mean_priv_alleles, mig10, mig25, mig50, mig_corrected =\
+            ctrl.estimate_nm("PopGen" + os.sep + "big.gen")
+        assert (mean_sample_size, mean_priv_alleles, mig10, mig25, mig50, mig_corrected) == \
+               (28.0, 0.016129, 52.5578, 15.3006, 8.94583, 13.6612)
 
 
-    def tearDown(self):
-        for handle in self.handles:
-            handle.close()
-
-    def test_utils(self):
-        """Basic operation of GenePop Utils.
+    def test_fst_all(self):
+        """Test genepop execution on all fst.
         """
-        for index in range(len(self.handles)):
-            handle = self.handles[index]
-            rec = GenePop.parse(handle)
-        initial_pops = len(rec.populations)
-        initial_loci = len(rec.loci_list)
-        first_loci = rec.loci_list[0]
-        rec.remove_population(0)
-        assert len(rec.populations) == initial_pops - 1
-        rec.remove_locus_by_name(first_loci)
-        assert len(rec.loci_list) == initial_loci - 1
-        assert rec.loci_list[0] != first_loci
-        rec.remove_locus_by_position(0)
-        assert len(rec.loci_list) == initial_loci - 2
+        ctrl = GenePopController()
+        (allFis, allFst, allFit), itr = ctrl.calc_fst_all("PopGen" + os.sep + "c2line.gen")
+        results = list(itr)
+        assert (len(results) == 3)
+        assert (results[0][0] == "136255903")
+        assert (results[1][3] - 0.33 < 0.01)
+
+    def test_haploidy(self):
+        """Test haploidy.
+        """
+        ctrl = GenePopController()
+        (allFis, allFst, allFit), itr = ctrl.calc_fst_all("PopGen" + os.sep + "haplo.gen")
+        litr = list(itr)
+        assert not type(allFst) == int
+        assert len(litr) == 37
+        assert litr[36][0] == "Locus37" 
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity = 2)
