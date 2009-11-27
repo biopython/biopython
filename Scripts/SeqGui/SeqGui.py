07eb383f6ec5e9998a4924a77c690240acedc5eb
Peter <p.j.a.cock@googlemail.com>
2009-11-27 13:08:59 +0000

    Using wx instead of deprecated wxPython namespace, see http://www.wxpython.org/MigrationGuide.html for details

--- Scripts/SeqGui/SeqGui.py
+++ Scripts/SeqGui/SeqGui.py
@@ -1,7 +1,6 @@
 from Bio.Seq import translate, transcribe, back_transcribe
-from wxPython import wx
+import wx
 
-#TODO - import these from the wx library:
 ID_APPLY = 101
 ID_CLEAR  = 102
 ID_EXIT = 103
@@ -11,24 +10,24 @@ ID_CODON = 106
 ID_TRANSFORM = 107
 
 
-class ParamsPanel(wx.wxPanel):
+class ParamsPanel(wx.Panel):
     def __init__(self, parent, log):
-        wx.wxPanel.__init__(self, parent, -1)
-        codon_table_static = wx.wxStaticText(self, -1, 'Codon Tables',
-                                          style=wx.wxALIGN_CENTRE)
-        lc = wx.wxLayoutConstraints()
-        lc.top.SameAs(self, wx.wxTop, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
+        wx.Panel.__init__(self, parent, -1)
+        codon_table_static = wx.StaticText(self, -1, 'Codon Tables',
+                                           style=wx.ALIGN_CENTER)
+        lc = wx.LayoutConstraints()
+        lc.top.SameAs(self, wx.Top, 5)
+        lc.left.SameAs(self, wx.Left, 5)
         lc.height.AsIs()
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.right.SameAs(self, wx.Right, 5)
         codon_table_static.SetConstraints(lc)
 
-        codon_table_lb = wx.wxListBox(self, ID_CODON)
-        lc = wx.wxLayoutConstraints()
+        codon_table_lb = wx.ListBox(self, ID_CODON)
+        lc = wx.LayoutConstraints()
         lc.top.Below(codon_table_static, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
-        lc.height.PercentOf(self, wx.wxHeight, 30)
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.left.SameAs(self, wx.Left, 5)
+        lc.height.PercentOf(self, wx.Height, 30)
+        lc.right.SameAs(self, wx.Right, 5)
         codon_table_lb.SetConstraints(lc)
         self.codon_table_lb = codon_table_lb
 
@@ -46,21 +45,21 @@ class ParamsPanel(wx.wxPanel):
         codon_table_lb.Append('Blepharisma Macronuclear')
         codon_table_lb.SetSelection(0)
 
-        transform_static = wx.wxStaticText(self, -1, 'Transformation',
-                                        style=wx.wxALIGN_CENTRE)
-        lc = wx.wxLayoutConstraints()
+        transform_static = wx.StaticText(self, -1, 'Transformation',
+                                         style=wx.ALIGN_CENTER)
+        lc = wx.LayoutConstraints()
         lc.top.Below(codon_table_lb, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
+        lc.left.SameAs(self, wx.Left, 5)
         lc.height.AsIs()
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.right.SameAs(self, wx.Right, 5)
         transform_static.SetConstraints(lc)
 
-        transform_lb = wx.wxListBox(self, ID_TRANSFORM)
-        lc = wx.wxLayoutConstraints()
+        transform_lb = wx.ListBox(self, ID_TRANSFORM)
+        lc = wx.LayoutConstraints()
         lc.top.Below(transform_static, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
-        lc.height.PercentOf(self, wx.wxHeight, 30)
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.left.SameAs(self, wx.Left, 5)
+        lc.height.PercentOf(self, wx.Height, 30)
+        lc.right.SameAs(self, wx.Right, 5)
         transform_lb.SetConstraints(lc)
 
         transform_lb.Append('Transcribe')
@@ -70,74 +69,73 @@ class ParamsPanel(wx.wxPanel):
         self.transform_lb = transform_lb
 
 
-class SeqPanel(wx.wxPanel):
+class SeqPanel(wx.Panel):
     def __init__(self, parent, log):
         self.parent = parent
-        wx.wxPanel.__init__(self, parent, -1)
-        apply_button = wx.wxButton(self, ID_APPLY, "Apply")
-        clear_button = wx.wxButton(self, ID_CLEAR, "Clear")
-        close_button = wx.wxButton(self, ID_CLOSE, "Close")
+        wx.Panel.__init__(self, parent, -1)
+        apply_button = wx.Button(self, ID_APPLY, "Apply")
+        clear_button = wx.Button(self, ID_CLEAR, "Clear")
+        close_button = wx.Button(self, ID_CLOSE, "Close")
         wx.EVT_BUTTON(self, ID_CLOSE, self.OnClose)
         wx.EVT_BUTTON(self, ID_APPLY, self.OnApply)
         wx.EVT_BUTTON(self, ID_CLEAR, self.OnClear)
 
-        lc = wx.wxLayoutConstraints()
-        lc.bottom.SameAs(self, wx.wxBottom, 10)
-        lc.left.SameAs(self, wx.wxLeft, 10)
+        lc = wx.LayoutConstraints()
+        lc.bottom.SameAs(self, wx.Bottom, 10)
+        lc.left.SameAs(self, wx.Left, 10)
         lc.height.AsIs()
-        lc.width.PercentOf(self, wx.wxWidth, 25)
+        lc.width.PercentOf(self, wx.Width, 25)
         apply_button.SetConstraints(lc)
 
-        lc = wx.wxLayoutConstraints()
-        lc.bottom.SameAs(self, wx.wxBottom, 10)
+        lc = wx.LayoutConstraints()
+        lc.bottom.SameAs(self, wx.Bottom, 10)
         lc.left.RightOf(apply_button, 5)
         lc.height.AsIs()
-        lc.width.PercentOf(self, wx.wxWidth, 25)
+        lc.width.PercentOf(self, wx.Width, 25)
         clear_button.SetConstraints(lc)
 
-        lc = wx.wxLayoutConstraints()
-        lc.bottom.SameAs(self, wx.wxBottom, 10)
+        lc = wx.LayoutConstraints()
+        lc.bottom.SameAs(self, wx.Bottom, 10)
         lc.left.RightOf(clear_button, 5)
         lc.height.AsIs()
-        lc.width.PercentOf(self, wx.wxWidth, 25)
+        lc.width.PercentOf(self, wx.Width, 25)
         close_button.SetConstraints(lc)
 
-        src_static = wx.wxStaticText(self, -1, 'Original Sequence',
-                                  style=wx.wxALIGN_CENTRE)
-        lc = wx.wxLayoutConstraints()
-        lc.top.SameAs(self, wx.wxTop, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
+        src_static = wx.StaticText(self, -1, 'Original Sequence',
+                                   style=wx.ALIGN_CENTER)
+        lc = wx.LayoutConstraints()
+        lc.top.SameAs(self, wx.Top, 5)
+        lc.left.SameAs(self, wx.Left, 5)
         lc.height.AsIs()
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.right.SameAs(self, wx.Right, 5)
         src_static.SetConstraints(lc)
 
-        src_text = wx.wxTextCtrl(self, -1, '', style = wx.wxTE_MULTILINE)
-        lc = wx.wxLayoutConstraints()
+        src_text = wx.TextCtrl(self, -1, '', style=wx.TE_MULTILINE)
+        lc = wx.LayoutConstraints()
         lc.top.Below(src_static, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
-        lc.height.PercentOf(self, wx.wxHeight, 30)
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.left.SameAs(self, wx.Left, 5)
+        lc.height.PercentOf(self, wx.Height, 30)
+        lc.right.SameAs(self, wx.Right, 5)
         src_text.SetConstraints(lc)
         self.src_text = src_text
 
-        dest_static = wx.wxStaticText(self, -1, 'Transformed Sequence',
-                                   style=wx.wxALIGN_CENTRE)
-        lc = wx.wxLayoutConstraints()
+        dest_static = wx.StaticText(self, -1, 'Transformed Sequence',
+                                    style=wx.ALIGN_CENTER)
+        lc = wx.LayoutConstraints()
         lc.top.Below(src_text, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
+        lc.left.SameAs(self, wx.Left, 5)
         lc.height.AsIs()
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.right.SameAs(self, wx.Right, 5)
         dest_static.SetConstraints(lc)
 
-        dest_text = wx.wxTextCtrl(self, -1, '', style = wx.wxTE_MULTILINE)
-        lc = wx.wxLayoutConstraints()
+        dest_text = wx.TextCtrl(self, -1, '', style=wx.TE_MULTILINE)
+        lc = wx.LayoutConstraints()
         lc.top.Below(dest_static, 5)
-        lc.left.SameAs(self, wx.wxLeft, 5)
-        lc.height.PercentOf(self, wx.wxHeight, 30)
-        lc.right.SameAs(self, wx.wxRight, 5)
+        lc.left.SameAs(self, wx.Left, 5)
+        lc.height.PercentOf(self, wx.Height, 30)
+        lc.right.SameAs(self, wx.Right, 5)
         dest_text.SetConstraints(lc)
         self.dest_text = dest_text
-
 
     def OnClose(self, event):
         parent = self.GetParent()
@@ -183,37 +181,37 @@ class SeqPanel(wx.wxPanel):
         self.dest_text.SetValue(back_transcribe(seq))
 
 
-class SeqFrame(wx.wxFrame):
+class SeqFrame(wx.Frame):
     def __init__(self, parent, ID, title):
-        wx.wxFrame.__init__(self, parent, ID, title,
-                              wx.wxDefaultPosition, wx.wxSize(500, 400))
-        self.SetAutoLayout(wx.true)
+        wx.Frame.__init__(self, parent, ID, title,
+                          wx.DefaultPosition, wx.Size(500, 400))
+        self.SetAutoLayout(True)
         self.CreateStatusBar()
         self.SetStatusText("This is the statusbar")
-        menu = wx.wxMenu()
+        menu = wx.Menu()
         menu.Append(ID_ABOUT, "&About",
                     "More information about this program")
         menu.AppendSeparator()
         menu.Append(ID_EXIT, "E&xit", "Terminate the program")
 
-        menuBar = wx.wxMenuBar()
+        menuBar = wx.MenuBar()
         menuBar.Append(menu, "&File");
         self.SetMenuBar(menuBar)
 
         params_panel = ParamsPanel(self, -1)
-        lc = wx.wxLayoutConstraints()
-        lc.top.SameAs(self, wx.wxTop, 10)
-        lc.left.SameAs(self, wx.wxLeft, 5)
-        lc.bottom.SameAs(self, wx.wxBottom, 5)
-        lc.width.PercentOf(self, wx.wxWidth, 40)
+        lc = wx.LayoutConstraints()
+        lc.top.SameAs(self, wx.Top, 10)
+        lc.left.SameAs(self, wx.Left, 5)
+        lc.bottom.SameAs(self, wx.Bottom, 5)
+        lc.width.PercentOf(self, wx.Width, 40)
         params_panel.SetConstraints(lc)
 
         seq_panel = SeqPanel(self, -1)
-        lc = wx.wxLayoutConstraints()
-        lc.top.SameAs(self, wx.wxTop, 10)
+        lc = wx.LayoutConstraints()
+        lc.top.SameAs(self, wx.Top, 10)
         lc.left.RightOf(params_panel, 5)
-        lc.bottom.SameAs(self, wx.wxBottom, 5)
-        lc.right.SameAs(self, wx.wxRight)
+        lc.bottom.SameAs(self, wx.Bottom, 5)
+        lc.right.SameAs(self, wx.Right)
         seq_panel.SetConstraints(lc)
 
         self.seq_panel = seq_panel
@@ -222,15 +220,15 @@ class SeqFrame(wx.wxFrame):
         wx.EVT_MENU(self, ID_EXIT, self.exit)
 
     def exit(self, event):
-        self.Close(wx.true)
+        self.Close(True)
 
 
-class MyApp(wx.wxApp):
+class MyApp(wx.App):
     def OnInit(self):
-        frame = SeqFrame(wx.NULL, -1, "Greetings from Biopython")
-        frame.Show(wx.true)
+        frame = SeqFrame(None, -1, "Greetings from Biopython")
+        frame.Show(True)
         self.SetTopWindow(frame)
-        return wx.true
+        return True
 
 app = MyApp(0)
 app.MainLoop()
