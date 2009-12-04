4f77d1d8d3c7c95e6a94ca49a28a6f1d19997fc3
Eric Talevich <eric.talevich@gmail.com>
2009-12-04 13:08:14 -0500

    BaseTree: removed PhyloDB stuff

--- Bio/Tree/BaseTree.py
+++ Bio/Tree/BaseTree.py
@@ -8,12 +8,6 @@
 All object representations for phylogenetic trees should derive from these base
 classes in order to use the common methods defined on them.
 
-The attributes on these classes map directly to the schema for PhyloDB, a BioSQL
-extension for representing phylogenetic trees or networks. (Hopefully, this will
-let us support database import/export through this module eventually.)
-
-See: U{ http://biosql.org/wiki/Extensions }
-
 """
 __docformat__ = "epytext en"
 
@@ -21,6 +15,9 @@ import re
 from collections import deque
 from itertools import izip
 
+# TODO:
+#   - Traversal options: preorder = DFS, postorder, breadth_first
+#   - Fix the docstrings!
 
 def trim_str(text, maxlen=60):
     assert isinstance(text, basestring), \
@@ -143,6 +140,10 @@ class TreeMixin(object):
     """
     # Plumbing
 
+    # TODO: split traversal into multiple methods:
+    #   preorder (aka depth_first)
+    #   postorder
+    #   breadth_first
     def filter_search(self, filterfunc, breadth_first):
         """Perform a BFS or DFS through all elements in this tree.
 
@@ -377,49 +378,26 @@ class TreeMixin(object):
 
 
 class Tree(TreeElement, TreeMixin):
-    """A phylogenetic tree.
+    """A phylogenetic tree, containing global info for the phylogeny.
 
-    From PhyloDB:
-    =============
-
-        A tree basically is a namespace for nodes, and thereby implicitly for
-        their relationships (edges). In this model, tree is also bit of misnomer
-        because we try to support reticulating trees, i.e., networks, too, so
-        arguably it should be called graph. Typically, this will be used for
-        storing phylogenetic trees, sequence trees (a.k.a. gene trees) as much
-        as species trees.
+    The structure and node-specific data is accessible through the 'root'
+    subtree attached to the Tree instance.
 
     @param root:
         The starting node of the tree. If the tree is rooted, this will usually
-        be the root node. Note that the root node(s) of a rooted tree must be
-        stored in tree_root, too.
-    @type root: list
+        be the root node.
+    @type root: Subtree
 
     @param rooted:
         Whether or not the tree is rooted. By default, a tree is assumed to be
         rooted.
     @type rooted: bool
 
-    @param id:
-        The identifier of the tree, if there is one.
+    @param id: The identifier of the tree, if there is one.
     @type id: str
 
-    @param name:
-        The name of the tree, in essence a label.
+    @param name: The name of the tree, in essence a label.
     @type name: str
-
-    Not implemented:
-
-    @param biodatabase_id:
-        The namespace of the tree itself. Though trees are in a sense named
-        containers themselves (namely for nodes), they also constitute (possibly
-        identifiable!) data objects in their own right. Some data sources may
-        only provide a single tree, so that assigning a namespace for the tree
-        may seem excessive, but others, such as TreeBASE, contain many trees,
-        just as sequence databanks contain many sequences. The choice of how to
-        name a tree is up to the user; one may assign a default namespace (such
-        as "biosql"), or create one named the same as the tree.
-
     """
     def __init__(self, root=None, rooted=True, id=None, name=None):
         self.root = root or Subtree()
@@ -442,25 +420,20 @@ class Tree(TreeElement, TreeMixin):
 
 
 class Subtree(TreeElement, TreeMixin):
-    """A node in a tree.
+    """A recursively defined subtree.
 
-    From PhyloDB:
-    =============
+    @param branch_length:
+        The length of the branch leading to the root node of this subtree.
+    @type branch_length: str
 
-        Typically, this will be a node in a phylogenetic tree, resembling either
-        a nucleotide or protein sequence, or a taxon, or more generally an
-        "operational taxonomic unit" (OTU).
-
-    @param label:
-         The label of a node. This may the latin binomial of the taxon, the
-         accession number of a sequences, or any other construct that uniquely
-         identifies the node within one tree.
+    @param label: The label of a node.
+    @type label: str
 
     @param clades: Sub-trees rooted directly under this tree's root.
     @type clades: list
     """
     def __init__(self, branch_length=None, label=None, clades=None):
-        self.branch_length = branch_length  # XXX or move this to Edge?
+        self.branch_length = branch_length
         self._label = label
         self.clades = clades or []
 
@@ -472,7 +445,7 @@ class Subtree(TreeElement, TreeMixin):
 
     def _get_label(self): return self._label
     def _set_label(self, x): self._label = x
-    def _del_label(self, x): del self._label
+    def _del_label(self, x): self._label = None
     label = property(_get_label, _set_label, _del_label)
 
     @property
@@ -483,7 +456,7 @@ class Subtree(TreeElement, TreeMixin):
     # Sequence-type behavior methods
 
     def __getitem__(self, index):
-        """Get sub-trees by index (integer or slice)."""
+        """Get subtrees by index (integer or slice)."""
         if isinstance(index, int) or isinstance(index, slice):
             return self.clades[index]
         ref = self
@@ -492,289 +465,10 @@ class Subtree(TreeElement, TreeMixin):
         return ref
 
     def __iter__(self):
-        """Iterate through this tree's direct sub-trees (clades)."""
+        """Iterate through this tree's direct subtrees (clades)."""
         return iter(self.clades)
 
     def __len__(self):
-        """Number of nodes/sub-trees directy under this tree's root."""
+        """Number of subtrees directy under the root."""
         return len(self.clades)
-
-
-# Additional PhyloDB tables
-
-class TreeQualifierValue(TreeElement):
-    """Tree metadata as attribute/value pairs.
-
-    From PhyloDB:
-    =============
-
-        Attribute names are from a controlled vocabulary (or ontology).
-
-    @param tree_id:
-	 The tree with which the metadata is being associated.
-
-    @param term_id:
-         The name of the metadate element as a term from a controlled vocabulary
-         (or ontology).
-
-    @param value:
-         The value of the metadata element.
-
-    @param rank:
-         The index of the metadata value if there is more than one value for
-         the same metadata element. If there is only one value, this may be left
-         at the default of zero.
-    """
-    # value, rank
-    # relations: tree, term
-    pass
-
-
-class TreeDbxref(TreeElement):
-    """Secondary identifiers and other database cross-references for trees.
-
-    From PhyloDB:
-    =============
-
-        There can only be one dbxref of a specific type for a tree.
-
-    @param tree_id:
-	The tree to which the database corss-reference is being assigned.
-
-    @param dbxref_id:
-	The database cross-reference being assigned to the tree.
-
-    @param term_id:
-        The type of the database cross-reference as a controlled vocabulary or
-        ontology term. The type of a tree accession should be ''primary
-        identifier''.
-    """
-    # relations: tree, term, dbxref
-    pass
-
-
-class NodeQualifierValue(TreeElement):
-    """Tree (or network) node metadata as attribute/value pairs.
-
-    From PhyloDB:
-    =============
-
-        Attribute names are from a controlled vocabulary (or ontology).
-
-    @param node_id:
-        The tree (or network) node to which the metadata is being associated.
-
-    @param term_id:
-        The name of the metadate element as a term from a controlled vocabulary
-        (or ontology).
-
-    @param value:
-        The value of the attribute/value pair association of metadata (if
-        applicable).
-
-    @param rank:
-        The index of the metadata value if there is more than one value for the
-        same metadata element. If there is only one value, this may be left at
-        the default of zero.
-
-    """
-    # value, rank
-    # relations: node, term
-
-
-class NodeDbxref(TreeElement):
-    """Identifiers and other database cross-references for nodes. 
-
-    From PhyloDB:
-    =============
-
-        There can only be one dbxref of a specific type for a node.
-
-    @param node_id:
-        The node to which the database cross-reference is being assigned.
-
-    @param dbxref_id:
-        The database cross-reference being assigned to the node.
-
-    @param term_id:
-        The type of the database cross-reference as a controlled vocabulary or
-        ontology term. The type of a node identifier should be ''primary
-        identifier''.
-    """
-    # relations: node, term, dbxref
-    pass
-
-
-class NodeBioentry(TreeElement):
-    """Links tree nodes to sequences (or other bioentries).
-
-    From PhyloDB:
-    =============
-
-        If the alignment is concatenated on molecular data, there will be more
-        than one sequence, and rank can be used to order these appropriately.
-
-    @param node_id:
-        The node to which the bioentry is being linked.
-
-    @param bioentry_id:
-        The bioentry being linked to the node.
-
-    @param rank:
-        The index of this bioentry within the list of bioentries being linked to
-        the node, if the order is significant. Typically, this will be used to
-        represent the position of the respective sequence within the
-        concatenated alignment, or the partition index.
-    """
-    # rank
-    # relations: self, node, bioentry
-    # may belong to a sequence
-    pass
-
-
-class NodeTaxon(TreeElement):
-    """Links tree nodes to taxa.
-
-    From PhyloDB:
-    =============
-
-        If the alignment is concatenated on molecular data, there may be more
-        than one sequence, and these may not necessarily be from the same taxon
-        (e.g., they might be from subspecies). Rank can be used to order these
-        appropriately.';
-
-    @param node_id:
-	The node to which the taxon is being linked.
-
-    @param taxon_id:
-        The taxon being linked to the node.
-
-    @param rank:
-        The index of this taxon within the list of taxa being linked to the
-        node, if the order is significant. Typically, this will be used to
-        represent the position of the respective sequence within the
-        concatenated alignment, or the partition index.
-    """
-    # rank
-    # relations: self, node, taxon
-    # may belong to a sequence
-    pass
-
-
-class TreeRoot(TreeElement):
-    """Root node for a rooted tree.
-
-    From PhyloDB:
-    =============
-
-        A phylogenetic analysis might suggest several alternative root nodes,
-        with possible probabilities.
-
-    @param tree_id:
-        The tree for which the referenced node is a root node.
-
-    @param node_id:
-        The node that is a root for the referenced tree.
-
-    @param is_alternate:
-        True if the root node is the preferential (most likely) root node of the
-        tree, and false otherwise.
-
-    @param significance:
-        The significance (such as likelihood, or posterior probability) with
-        which the node is the root node. This only has meaning if the method
-        used for reconstructing the tree calculates this value.
-    """
-    # is_alternate, significance
-    # relations: self, tree, node
-    # may belong to a sequence
-    pass
-
-
-class Edge(TreeElement):
-    """An edge between two nodes in a tree (or graph).
-
-    From PhyloDB:
-    =============
-
-    @param child_node_id:
-        The endpoint node of the two nodes connected by a directed edge. In a
-        phylogenetic tree, this is the descendant.
-
-    @param parent_node_id:
-        The startpoint node of the two nodes connected by a directed edge. In a
-        phylogenetic tree, this is the ancestor.
-    """
-    # relations: self, child_node, parent_node
-    # (distance/length, qualifier)
-    # may belong to a sequence
-    pass
-
-
-class NodePath(TreeElement):
-    """A path between two nodes in a tree (or graph).
-
-    From PhyloDB:
-    =============
-
-        Two nodes A and B are connected by a (directed) path if B can be reached
-        from A by following nodes that are connected by (directed) edges.
-
-    @param child_node_id:
-        The endpoint node of the two nodes connected by a (directed) path. In a
-        phylogenetic tree, this is the descendant.
-
-    @param parent_node_id:
-        The startpoint node of the two nodes connected by a (directed) path. In
-        a phylogenetic tree, this is the ancestor.
-
-    @param path:
-        The path from startpoint to endpoint as the series of nodes visited
-        along the path. The nodes may be identified by label, or, typically more
-        efficient, by their primary key, or left or right value. The latter or
-        often smaller than the primary key, and hence consume less space. One
-        may increase efficiency further by using a base-34 numeric
-        representation (24 letters of the alphabet, plus 10 digits) instead of
-        decimal (base-10) representation. The actual method used is not
-        important, though it should be used consistently.
-
-    @param distance:
-        The distance (or length) of the path. The path between a node and itself
-        has length zero, and length 1 between two nodes directly connected by an
-        edge. If there is a path of length l between two nodes A and Z and an
-        edge between Z and B, there is a path of length l+1 between nodes A and
-        B.
-    """
-    # path, distance
-    # relations: child_node, parent_node
-    pass
-
-
-class EdgeQualifierValue(TreeElement):
-    """Edge metadata as attribute/value pairs.
-
-    From PhyloDB:
-    =============
-
-        Attribute names are from a controlled vocabulary (or ontology).
-
-    @param edge_id:
-	The tree edge to which the metadata is being associated.
-
-    @param term_id:
-        The name of the metadate element as a term from a controlled vocabulary
-        (or ontology).
-
-    @param value:
-        The value of the attribute/value pair association of metadata (if
-        applicable).
-
-    @param rank:
-        The index of the metadata value if there is more than one value for the
-        same metadata element. If there is only one value, this may be left at
-        the default of zero.
-    """
-    # value, rank
-    # relations: edge, term
-    pass
 
