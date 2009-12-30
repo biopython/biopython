aafc77fc8dedab67791bc216ed145d55a3676f4a
Eric Talevich <eric.talevich@gmail.com>
2009-12-29 17:36:20 -0800

    Tree.Utils: added to_adjacency_matrix
    This completes our coverage of standard graph representations.

--- Bio/Tree/Utils.py
+++ Bio/Tree/Utils.py
@@ -49,6 +49,39 @@ def pretty_print(treeobj, file=sys.stdout, show_all=False, indent=0):
     print_phylo(treeobj, indent)
 
 
+def to_adjacency_matrix(tree):
+    """Create an adjacency matrix (NumPy array) from clades/branches in tree.
+
+    Also returns a list of all clades in tree ("allclades"), where the position
+    of each clade in the list corresponds to a row and column of the numpy
+    array. So, a cell i,j in the array represents the length of the branch from
+    allclades[i] to allclades[j].
+
+    @return: tuple of (allclades, adjacency_matrix) where allclades is a list
+    and adjacency_matrix is a NumPy 2D array.
+    """
+    try:
+        import numpy
+    except ImportError:
+        from Bio import MissingExternalDependencyError
+        raise MissingExternalDependencyError(
+                "The numpy library is not installed.")
+
+    # ENH/future: use OrderedDict for both lookup and allclades
+    allclades = list(tree.find_clades(order='level'))
+    lookup = {}
+    for i, elem in enumerate(allclades):
+        lookup[elem] = i
+    adj = numpy.zeros((len(allclades), len(allclades)))
+    for parent in tree.find_clades(terminal=False, order='level'):
+        for child in parent.clades:
+            if child.branch_length:
+                adj[lookup[parent], lookup[child]] = child.branch_length
+    if not tree.rooted:
+        adj += adj.transpose
+    return (allclades, adj)
+
+
 def to_networkx(tree):
     """Convert a Tree object to a networkx graph.
 
