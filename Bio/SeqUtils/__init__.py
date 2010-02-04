8135f463012e5056614ebfd16082a3a1f89cd795
Peter <p.j.a.cock@googlemail.com>
2010-02-04 15:15:41 +0000

    Deprecating Bio.SeqUtils.makeTableX function, plus ProteinX and MissingTable classes which are all dead code after the removal of the Bio.SeqUtils.translate function

--- Bio/SeqUtils/__init__.py
+++ Bio/SeqUtils/__init__.py
@@ -221,13 +221,18 @@ def nt_search(seq, subseq):
 # should be moved to ???
 
 class ProteinX(Alphabet.ProteinAlphabet):
+    """Variant of the extended IUPAC extended protein alphabet (DEPRECATED)."""
     letters = IUPACData.extended_protein_letters + "X"
 
+#Can't add a deprecation warning to the class due to the following line:
 proteinX = ProteinX()
 
 class MissingTable:
     def __init__(self, table):
         self._table = table
+        import warnings
+        warnings.warn("Function Bio.SeqUtils.makeTableX() and related classes ProteinX "
+                      "and MissingTable are deprecated.", DeprecationWarning)
     def get(self, codon, stop_symbol):
         try:
             return self._table.get(codon, stop_symbol)
