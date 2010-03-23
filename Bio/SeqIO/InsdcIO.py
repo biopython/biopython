67884b48b372d16f47343731df693449bdf392b2
Peter <p.j.a.cock@googlemail.com>
2010-03-23 12:43:31 +0000

    Fixed single letter feature locations in SeqIO EMBL/GenBank output (e.g. 2 rather than 2..2) as pointed out by Chris Fields on the mailing list

--- Bio/SeqIO/InsdcIO.py
+++ Bio/SeqIO/InsdcIO.py
@@ -114,12 +114,19 @@ def _insdc_location_string_ignoring_strand_and_subfeatures(feature):
     else:
         ref = ""
     assert not feature.ref_db
-    if feature.location.start == feature.location.end \
-    and isinstance(feature.location.end, SeqFeature.ExactPosition):
-        #Special case, 12^13 gets mapped to location 12:12
+    if isinstance(feature.location.start, SeqFeature.ExactPosition) \
+    and isinstance(feature.location.end, SeqFeature.ExactPosition) \
+    and feature.location.start.position == feature.location.end.position:
+        #Special case, for 12:12 return 12^13
         #(a zero length slice, meaning the point between two letters)
         return "%s%i^%i" % (ref, feature.location.end.position,
                             feature.location.end.position+1)
+    if isinstance(feature.location.start, SeqFeature.ExactPosition) \
+    and isinstance(feature.location.end, SeqFeature.ExactPosition) \
+    and feature.location.start.position+1 == feature.location.end.position:
+        #Special case, for 11:12 return 12 rather than 12..12
+        #(a length one slice, meaning a single letter)
+        return "%s%i" % (ref, feature.location.end.position)
     else:
         #Typical case, e.g. 12..15 gets mapped to 11:15
         return ref \
