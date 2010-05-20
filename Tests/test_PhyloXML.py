1eb77246bf75cfc18e4f96e2f16f504e8b66785f
Eric Talevich <eric.talevich@gmail.com>
2010-05-20 01:11:06 -0700

    test_PhyloXML: don't leave an open handle for the temp file
    Regarding the tempfile.mktemp docstring, this security issue already existed
    because we're reusing the file name -- but this script doesn't run with special
    privileges anyway, usually, so I don't think we need to worry about it.

--- Tests/test_PhyloXML.py
+++ Tests/test_PhyloXML.py
@@ -33,7 +33,7 @@ EX_PHYLO = 'PhyloXML/phyloxml_examples.xml'
 EX_DOLLO = 'PhyloXML/o_tol_332_d_dollo.xml'
 
 # Temporary file name for Writer tests below
-DUMMY = tempfile.mkstemp()[1]
+DUMMY = tempfile.mktemp()
 
 
 # ---------------------------------------------------------
