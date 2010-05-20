6e3c76d3f50efbd3d57cfe8ee4bbb8faa5cc87db
Peter Cock <pjcock@ppserver.scri.sari.ac.uk>
2010-05-20 17:21:54 +0100

    Bump version number read for Biopython 1.54 release

--- Bio/__init__.py
+++ Bio/__init__.py
@@ -10,7 +10,7 @@ of freely available Python tools for computational molecular biology.
 http://biopython.org
 """
 
-__version__ = "1.54b+"
+__version__ = "1.54"
 
 class MissingExternalDependencyError(Exception):
     pass
