1bff40d10516bc8a8064f38889a7caf5d43b37f5
Peter <p.j.a.cock@googlemail.com>
2010-07-08 12:09:25 +0100

    Another explicit integer division in GenomeDiagram for Py3 compatibility

--- Bio/Graphics/GenomeDiagram/_CircularDrawer.py
+++ Bio/Graphics/GenomeDiagram/_CircularDrawer.py
@@ -887,8 +887,8 @@ class CircularDrawer(AbstractDrawer):
         greytrack_bgs.append(bg)
 
         if track.greytrack_labels:  # Labels are required for this track
-            labelstep = self.length/track.greytrack_labels  # label interval
-            for pos in range(self.start, self.end, int(labelstep)):
+            labelstep = self.length//track.greytrack_labels  # label interval
+            for pos in range(self.start, self.end, labelstep):
                 label = String(0, 0, track.name,            # Add a new label at
                            fontName=track.greytrack_font,   # each interval
                            fontSize=track.greytrack_fontsize,
