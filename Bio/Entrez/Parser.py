2573a06a7d2edf56756ae72c2250dde7fde7fce6
Peter <p.j.a.cock@googlemail.com>
2010-07-09 09:09:11 +0100

    Clarify missing DTD message (suggest bugzilla, and need to sign up to mailing list befor emailing us)

--- Bio/Entrez/Parser.py
+++ Bio/Entrez/Parser.py
@@ -370,9 +370,10 @@ in order for Bio.Entrez to find it.
 Alternatively, you can save %s in the directory
 Bio/Entrez/DTDs in the Biopython distribution, and reinstall Biopython.
 
-Please also inform the Biopython developers by sending an email to
-biopython-dev@biopython.org to inform us about this missing DTD, so that we
-can include it with the next release of Biopython.
+Please also inform the Biopython developers about this missing DTD, by
+reporting a bug on http://bugzilla.open-bio.org/ or sign up to our mailing
+list and emailing us, so that we can include it with the next release of
+Biopython.
 """ % (filename, filename, filename, self.dtd_dir, filename)
             raise RuntimeError(message)
             
