3fdf02f2a2b753ca1eff457131b078f23d0cfcaf
peterjc <p.j.a.cock@googlemail.com>
2010-07-15 20:06:13 +0100

    Don't need to use dict's keys() method for iteration or membership testing (GenomeDiagram)

--- Bio/Graphics/DisplayRepresentation.py
+++ Bio/Graphics/DisplayRepresentation.py
@@ -172,7 +172,7 @@ class ChromosomeCounts:
     def _color_from_count(self, count):
         """Translate the given count into a color using the color scheme.
         """
-        for count_start, count_end in self._color_scheme.keys():
+        for count_start, count_end in self._color_scheme:
             if count >= count_start and count <= count_end:
                 return self._color_scheme[(count_start, count_end)]
 
