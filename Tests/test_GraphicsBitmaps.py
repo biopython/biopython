ccbefdf38e4e44bcf940260d089db2c43112c834
peterjc <p.j.a.cock@googlemail.com>
2010-07-15 12:18:45 +0100

    Refactor test_GraphicsBitmaps.py so it gets skipped by run_tests.py if there are problems with the renderPM or PIL installation

--- Tests/test_GraphicsBitmaps.py
+++ Tests/test_GraphicsBitmaps.py
@@ -37,67 +37,67 @@ from reportlab.graphics.renderPM import RenderPMError
 # the stuff we're testing
 from Bio.Graphics.Comparative import ComparativeScatterPlot
 
+# We're not really using the unittest framework, because we need to
+# raise the dependency error BEFORE the invidual tests in order that
+# this be skipped by run_tests.py
 
+def real_test():
+    min_two_d_lists = 1
+    max_two_d_lists = 7
+
+    min_num_points = 1
+    max_num_points = 500
+
+    min_point_num = 0
+    max_point_num = 200
+
+    plot_info = []
+    num_two_d_lists = random.randrange(min_two_d_lists, max_two_d_lists)
+
+    for two_d_list in range(num_two_d_lists):
+        cur_list = []
+        num_points = random.randrange(min_num_points, max_num_points)
+        for point in range(num_points):
+            x_point = random.randrange(min_point_num, max_point_num)
+            y_point = random.randrange(min_point_num, max_point_num)
+            cur_list.append((x_point, y_point))
+        plot_info.append(cur_list)
+    
+    compare_plot = ComparativeScatterPlot("png")
+    compare_plot.display_info = _make_random_points()
+
+    output_file = os.path.join(os.getcwd(), "Graphics", "scatter_test.png")
+    try:
+        compare_plot.draw_to_file(output_file, "Testing Scatter Plots")
+    # there is a bug in reportlab which occasionally generates an
+    # error here.
+    except IndexError:
+        pass
+    except IOError, err:
+        if "encoder zip not available" in str(err):
+            raise MissingExternalDependencyError(
+                "Check zip encoder installed for PIL and ReportLab renderPM")
+        else:
+            raise err
+    except RenderPMError, err :
+        if str(err).startswith("Can't setFont(") :
+            #TODO - can we raise the error BEFORE the unit test function
+            #is run? That way it can be skipped in run_tests.py
+            raise MissingExternalDependencyError(\
+                "Check the fonts needed by ReportLab if you want "
+                "bitmaps from Bio.Graphics\n" + str(err))
+        else :
+            raise err
+    
+    return True
+
+               
 class ComparativeTest(unittest.TestCase):
     """Do tests for modules involved with comparing data."""
-    def setUp(self):
-        self.min_two_d_lists = 1
-        self.max_two_d_lists = 7
-
-        self.min_num_points = 1
-        self.max_num_points = 500
-
-        self.min_point_num = 0
-        self.max_point_num = 200
-
-    def _make_random_points(self):
-        """Make a bunch of random points for testing plots."""
-        plot_info = []
-        num_two_d_lists = random.randrange(self.min_two_d_lists,
-                                           self.max_two_d_lists)
-
-        for two_d_list in range(num_two_d_lists):
-            cur_list = []
-            num_points = random.randrange(self.min_num_points,
-                                          self.max_num_points)
-            for point in range(num_points):
-                x_point = random.randrange(self.min_point_num,
-                                           self.max_point_num)
-                y_point = random.randrange(self.min_point_num,
-                                           self.max_point_num)
-
-                cur_list.append((x_point, y_point))
-
-            plot_info.append(cur_list)
-        return plot_info
-                
     def test_simple_scatter_plot(self):
         """Test creation of a simple PNG scatter plot."""
-        compare_plot = ComparativeScatterPlot("png")
-        compare_plot.display_info = self._make_random_points()
-
-        output_file = os.path.join(os.getcwd(), "Graphics", "scatter_test.png")
-        try:
-            compare_plot.draw_to_file(output_file, "Testing Scatter Plots")
-        # there is a bug in reportlab which occasionally generates an
-        # error here.
-        except IndexError:
-            pass
-        except IOError, err:
-            if "encoder zip not available" in str(err):
-                raise MissingExternalDependencyError(
-                    "Check zip encoder installed for PIL and ReportLab renderPM")
-            else:
-                raise err
-        except RenderPMError, err :
-            if str(err).startswith("Can't setFont(") :
-                #TODO - can we raise the error BEFORE the unit test function
-                #is run? That way it can be skipped in run_tests.py
-                raise MissingExternalDependencyError(\
-                    "Check the fonts needed by ReportLab if you want "
-                    "bitmaps from Bio.Graphics\n" + str(err))
-            else :
-                raise err
+        #Dummy method to show up via run_tests.py
+        pass
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity = 2)
