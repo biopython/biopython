37fa84301eab29a46b38156a2efe87330b801d5e
Andrea Pierleoni <andreapierleoni@macbook-pro-di-andrea-pierleoni.local>
2010-07-27 15:31:32 +0200

    correct bug in lxml import, thanks etal

--- Bio/SeqIO/UniprotIO.py
+++ Bio/SeqIO/UniprotIO.py
@@ -34,7 +34,7 @@ except ImportError:
     except ImportError:
         # Python 2.4 -- check for 3rd-party implementations
         try:
-            from lxml.etree import ElementTree
+            from lxml import etree as ElementTree
         except ImportError:
             try:
                 import cElementTree as ElementTree
