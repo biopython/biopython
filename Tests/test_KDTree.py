7f9031da304b77ed2af3462ad6571afe29da59f4
peterjc <p.j.a.cock@googlemail.com>
2010-07-28 18:26:51 +0100

    Skip unit tests requiring Bio.KDTree._CKDTree when it is not compiled

--- Tests/test_KDTree.py
+++ Tests/test_KDTree.py
@@ -9,6 +9,13 @@ except ImportError:
     raise MissingExternalDependencyError(\
         "Install NumPy if you want to use Bio.KDTree.")
 
+try:
+    from Bio.KDTree import _CKDTree
+except ImportError:
+    from Bio import MissingExternalDependencyError
+    raise MissingExternalDependencyError(\
+        "C module in Bio.KDTree not compiled")
+
 from Bio.KDTree.KDTree import _neighbor_test, _test
 
 nr_points=5000
--- Tests/test_PDB.py
+++ Tests/test_PDB.py
@@ -21,6 +21,13 @@ except ImportError:
     raise MissingExternalDependencyError(\
         "Install NumPy if you want to use Bio.PDB.")
 
+try:
+    from Bio.KDTree import _CKDTree
+except ImportError:
+    from Bio import MissingExternalDependencyError
+    raise MissingExternalDependencyError(\
+        "C module in Bio.KDTree not compiled")
+
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_protein
 from Bio.PDB import PDBParser, PPBuilder, CaPPBuilder, PDBIO
