adcfd49c77e44f54dcaf0e77d506a65b9c0a720b
peterjc <p.j.a.cock@googlemail.com>
2010-07-30 10:05:00 +0100

    Tweak the setup.py y/n prompt to look sensible on Python 2.7

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_yes_or_no(question, default):
         default_str = 'n'
 
     while True:
-        print ("%s %s " % (question, option_str),)
+        print ("%s %s:" % (question, option_str))
         if sys.version_info[0] == 3:
             response = input().lower()
         else:
