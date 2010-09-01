505585e4fea69f663d744c13f581fe0a4003d5ae
peterjc <p.j.a.cock@googlemail.com>
2010-09-01 11:46:39 +0100

    Bump version to 1.55+ now release 1.55 is out

--- Bio/__init__.py
+++ Bio/__init__.py
@@ -10,7 +10,7 @@ of freely available Python tools for computational molecular biology.
 http://biopython.org
 """
 
-__version__ = "1.55"
+__version__ = "1.55+"
 
 class MissingExternalDependencyError(Exception):
     pass
