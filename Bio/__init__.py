93aee5c9647d9296968b9a8d4458b9d9c4444107
Michiel de Hoon <mdehoon@michiel-de-hoons-computer.local>
2010-09-11 12:39:04 +0900

    Adding a BiopythonDeprecationWarning to Bio/__init__.py

--- Bio/__init__.py
+++ Bio/__init__.py
@@ -15,3 +15,8 @@ __version__ = "1.55+"
 class MissingExternalDependencyError(Exception):
     pass
 
+class BiopythonDeprecationWarning(UserWarning):
+    """Code marked as deprecated is likely to be removed in a future version
+of Biopython. To avoid removal of this code, please contact the Biopython
+developers by sending an email to biopython-dev@biopython.org.
+"""
