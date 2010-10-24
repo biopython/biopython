08b25d50b7df5834032411d63f4ca141005c68eb
Tiago Antao <tiagoantao@gmail.com>
2010-10-24 16:34:15 +0100

    efficient handling of big genepop files

--- Bio/PopGen/FDist/Utils.py
+++ Bio/PopGen/FDist/Utils.py
@@ -13,7 +13,7 @@ import Bio.PopGen.FDist
 # add locus, etc...). The recommended strategy is convert back
 # and forth from/to GenePop and use GenePop Utils
 
-def convert_genepop_to_fdist(gp_rec, report_loci = None):
+def convert_genepop_to_fdist(gp_rec, report_pops = None):
     """Converts a GenePop record to a FDist one.
 
        Parameters:
@@ -25,7 +25,7 @@ def convert_genepop_to_fdist(gp_rec, report_loci = None):
     if hasattr(gp_rec, "populations"):
         return _convert_genepop_to_fdist(gp_rec)
     else:
-        return _convert_genepop_to_fdist_big(gp_rec, report_loci)
+        return _convert_genepop_to_fdist_big(gp_rec, report_pops)
 
 def _convert_genepop_to_fdist(gp_rec):
     """Converts a standard GenePop record to a FDist one.
@@ -68,7 +68,63 @@ def _convert_genepop_to_fdist(gp_rec):
         fd_rec.loci_data.append((len(alleles), pop_data))
     return fd_rec
 
-def _convert_genepop_to_fdist_big(gp_rec, report_loci = None):
+def _convert_genepop_to_fdist_big(gp_rec, report_pops = None):
+    """Converts a big GenePop record to a FDist one.
+
+       Parameters:
+       gp_rec - Genepop Record (Big)
+
+       Returns:
+       FDist record.
+    """
+    fd_rec = Bio.PopGen.FDist.Record()
+
+    fd_rec.data_org = 1
+    fd_rec.num_loci = len(gp_rec.loci_list)
+    num_loci = len(gp_rec.loci_list)
+    loci = []
+    for i in range(num_loci):
+        loci.append(set())
+    pops = []
+    work_rec = FileParser.read(gp_rec.fname)
+    lParser = work_rec.get_individual()
+    def init_pop():
+        my_pop = []
+        for i in range(num_loci):
+            my_pop.append({})
+        return my_pop
+    curr_pop = init_pop()
+    num_pops = 1
+    report_pops(num_pops)
+    while lParser:
+        if lParser != True:
+            for loci_pos in range(num_loci):
+                for al in lParser[1][loci_pos]:
+                    if al is not None:
+                        loci[loci_pos].add(al)
+                        curr_pop[loci_pos][al]= curr_pop[loci_pos].get(al,0)+1
+        else:
+            pops.append(curr_pop)
+            num_pops += 1
+            report_pops(num_pops)
+            curr_pop = init_pop()
+        lParser = work_rec.get_individual()
+    pops.append(curr_pop)
+    fd_rec.num_pops = num_pops
+    for loci_pos in range(num_loci):
+        alleles = list(loci[loci_pos])
+        alleles.sort()
+        loci_rec = [len(alleles), []]
+        for pop in pops:
+            pop_rec = []
+            for allele in alleles:
+                pop_rec.append(pop[loci_pos].get(allele, 0))
+            loci_rec[1].append(pop_rec)
+        fd_rec.loci_data.append(tuple(loci_rec))
+    return fd_rec
+
+
+def _convert_genepop_to_fdist_big_old(gp_rec, report_loci = None):
     """Converts a big GenePop record to a FDist one.
 
        Parameters:
@@ -134,7 +190,6 @@ def _convert_genepop_to_fdist_big(gp_rec, report_loci = None):
         process_pop(pop_data, alleles, allele_counts)
         fd_rec.loci_data.append((len(alleles), pop_data))
     return fd_rec
-
 
 
 def approximate_fst(desired_fst, simulated_fst, parameter_fst,
