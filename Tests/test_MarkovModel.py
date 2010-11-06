ee1befdbc21d55b7878a275cfd0f3c5304b6d446
Michiel de Hoon <mdehoon@michiel-de-hoons-computer.local>
2010-11-06 11:37:53 +0900

    Don't show the numpy warning, and convert to a unittest-styl test

--- Tests/test_MarkovModel.py
+++ Tests/test_MarkovModel.py
@@ -3,190 +3,214 @@
 # as part of this package.
 
 try:
-    from numpy import asarray
+    from numpy import array
 except ImportError:
     from Bio import MissingPythonDependencyError
     raise MissingPythonDependencyError(\
         "Install NumPy if you want to use Bio.MarkovModel.")
 
+import unittest
+
+import warnings
+#Silence this warning:
+#For optimal speed, please update to Numpy version 1.3 or later
+warnings.filterwarnings("ignore", category=UserWarning)
 from Bio import MarkovModel
-
-def print_mm(markov_model):
-    print "STATES: %s" % ' '.join(markov_model.states)
-    print "ALPHABET: %s" % ' '.join(markov_model.alphabet)
-    print "INITIAL:"
-    for i in range(len(markov_model.p_initial)):
-        print "  %s: %.2f" % (
-            markov_model.states[i], markov_model.p_initial[i])
-    print "TRANSITION:"
-    for i in range(len(markov_model.p_transition)):
-        x = ["%.2f" % x for x in markov_model.p_transition[i]]
-        print "  %s: %s" % (markov_model.states[i], ' '.join(x))
-    print "EMISSION:"
-    for i in range(len(markov_model.p_emission)):
-        x = ["%.2f" % x for x in markov_model.p_emission[i]]
-        print "  %s: %s" % (markov_model.states[i], ' '.join(x))
+warnings.filters.pop()
 
 
+class TestMarkovModel(unittest.TestCase):
 
-print "TESTING train_visible"
-states = ["0", "1", "2", "3"]
-alphabet = ["A", "C", "G", "T"]
-training_data = [
-    ("AACCCGGGTTTTTTT", "001112223333333"),
-    ("ACCGTTTTTTT", "01123333333"),
-    ("ACGGGTTTTTT", "01222333333"),
-    ("ACCGTTTTTTTT", "011233333333"),
-    ]
-print "Training HMM"
-mm = MarkovModel.train_visible(states, alphabet, training_data)
-print "Classifying"
+    def test_train_visible(self):
+        states = ["0", "1", "2", "3"]
+        alphabet = ["A", "C", "G", "T"]
+        training_data = [
+            ("AACCCGGGTTTTTTT", "001112223333333"),
+            ("ACCGTTTTTTT", "01123333333"),
+            ("ACGGGTTTTTT", "01222333333"),
+            ("ACCGTTTTTTTT", "011233333333"),
+            ]
+        markov_model = MarkovModel.train_visible(states, alphabet, training_data)
+        states = MarkovModel.find_states(markov_model, "AACGTT")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ['0', '0', '1', '2', '3', '3'])
+        self.assertAlmostEqual(state_float, 0.0082128906)
+        self.assertEqual(markov_model.states, ['0', '1', '2', '3'])
+        self.assertEqual(markov_model.alphabet, ['A', 'C', 'G', 'T'])
+        self.assertEqual(len(markov_model.p_initial), 4)
+        self.assertAlmostEqual(markov_model.p_initial[0], 1.0)
+        self.assertAlmostEqual(markov_model.p_initial[1], 0.0)
+        self.assertAlmostEqual(markov_model.p_initial[2], 0.0)
+        self.assertAlmostEqual(markov_model.p_initial[3], 0.0)
+        self.assertEqual(len(markov_model.p_transition), 4)
+        self.assertEqual(len(markov_model.p_transition[0]), 4)
+        self.assertEqual(len(markov_model.p_transition[1]), 4)
+        self.assertEqual(len(markov_model.p_transition[2]), 4)
+        self.assertEqual(len(markov_model.p_transition[3]), 4)
+        self.assertAlmostEqual(markov_model.p_transition[0][0], 0.2)
+        self.assertAlmostEqual(markov_model.p_transition[0][1], 0.8)
+        self.assertAlmostEqual(markov_model.p_transition[0][2], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[0][3], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[1][0], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[1][1], 0.5)
+        self.assertAlmostEqual(markov_model.p_transition[1][2], 0.5)
+        self.assertAlmostEqual(markov_model.p_transition[1][3], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[2][0], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[2][1], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[2][2], 0.5)
+        self.assertAlmostEqual(markov_model.p_transition[2][3], 0.5)
+        self.assertAlmostEqual(markov_model.p_transition[3][0], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[3][1], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[3][2], 0.0)
+        self.assertAlmostEqual(markov_model.p_transition[3][3], 1.0)
+        self.assertEqual(len(markov_model.p_emission), 4)
+        self.assertEqual(len(markov_model.p_emission[0]), 4)
+        self.assertEqual(len(markov_model.p_emission[1]), 4)
+        self.assertEqual(len(markov_model.p_emission[2]), 4)
+        self.assertEqual(len(markov_model.p_emission[3]), 4)
+        self.assertAlmostEqual(markov_model.p_emission[0][0], 0.666667, 4)
+        self.assertAlmostEqual(markov_model.p_emission[0][1], 0.111111, 4)
+        self.assertAlmostEqual(markov_model.p_emission[0][2], 0.111111, 4)
+        self.assertAlmostEqual(markov_model.p_emission[0][3], 0.111111, 4)
+        self.assertAlmostEqual(markov_model.p_emission[1][0], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[1][1], 0.750000, 4)
+        self.assertAlmostEqual(markov_model.p_emission[1][2], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[1][3], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[2][0], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[2][1], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[2][2], 0.750000, 4)
+        self.assertAlmostEqual(markov_model.p_emission[2][3], 0.083333, 4)
+        self.assertAlmostEqual(markov_model.p_emission[3][0], 0.031250, 4)
+        self.assertAlmostEqual(markov_model.p_emission[3][1], 0.031250, 4)
+        self.assertAlmostEqual(markov_model.p_emission[3][2], 0.031250, 4)
+        self.assertAlmostEqual(markov_model.p_emission[3][3], 0.906250, 4)
 
-#print MarkovModel.find_states(mm, "AACGTT")
-#Don't just print this, as the float may have different
-#precision on different platforms.  This returns a list
-#containing a tuple containing a list (fine), and a float.
-states = MarkovModel.find_states(mm, "AACGTT")
-for state_list, state_float in states:
-    print "State %s, %0.10f" % (repr(state_list), state_float)
-print_mm(mm)
+    def test_baum_welch(self):
+        states = ["CP", "IP"]
+        alphabet = ["cola", "ice_t", "lem"]
+        outputs = [
+            (2, 1, 0)
+            ]
+        p_initial = [1.0, 0.0000001]
+        p_transition = [[0.7, 0.3],
+                        [0.5, 0.5]]
+        p_emission = [[0.6, 0.1, 0.3],
+                      [0.1, 0.7, 0.2]]
+        N, M = len(states), len(alphabet)
+        x = MarkovModel._baum_welch(N, M, outputs,
+                                    p_initial=p_initial,
+                                    p_transition=p_transition,
+                                    p_emission=p_emission
+                                    )
+        p_initial, p_transition, p_emission = x
+        markov_model = MarkovModel.MarkovModel(states, alphabet,
+                                     p_initial, p_transition, p_emission)
+        self.assertEqual(markov_model.states, ['CP', 'IP'])
+        self.assertEqual(markov_model.alphabet, ['cola', 'ice_t', 'lem'])
+        self.assertEqual(len(markov_model.p_initial), 2)
+        self.assertAlmostEqual(markov_model.p_initial[0], 1.0, 4)
+        self.assertAlmostEqual(markov_model.p_initial[1], 0.0, 4)
+        self.assertEqual(len(markov_model.p_transition), 2)
+        self.assertEqual(len(markov_model.p_transition[0]), 2)
+        self.assertEqual(len(markov_model.p_transition[1]), 2)
+        self.assertAlmostEqual(markov_model.p_transition[0][0], 0.02460365, 4)
+        self.assertAlmostEqual(markov_model.p_transition[0][1], 0.97539634, 4)
+        self.assertAlmostEqual(markov_model.p_transition[1][0], 1.0, 4)
+        self.assertAlmostEqual(markov_model.p_transition[1][1], 0.0, 4)
+        self.assertEqual(len(markov_model.p_emission), 2)
+        self.assertEqual(len(markov_model.p_emission[0]), 3)
+        self.assertEqual(len(markov_model.p_emission[1]), 3)
+        self.assertAlmostEqual(markov_model.p_emission[0][0], 0.5)
+        self.assertAlmostEqual(markov_model.p_emission[0][1], 0.0)
+        self.assertAlmostEqual(markov_model.p_emission[0][2], 0.5)
+        self.assertAlmostEqual(markov_model.p_emission[1][0], 0.0)
+        self.assertAlmostEqual(markov_model.p_emission[1][1], 1.0)
+        self.assertAlmostEqual(markov_model.p_emission[1][2], 0.0)
 
+    # Do some tests from the topcoder competition.
 
-
-
-print "TESTING baum welch"
-states = ["CP", "IP"]
-alphabet = ["cola", "ice_t", "lem"]
-outputs = [
-    (2, 1, 0)
-    ]
-print "Training HMM"
-p_initial = [1.0, 0.0000001]
-p_transition = [[0.7, 0.3],
-                [0.5, 0.5]]
-p_emission = [[0.6, 0.1, 0.3],
-              [0.1, 0.7, 0.2]]
-N, M = len(states), len(alphabet)
-x = MarkovModel._baum_welch(N, M, outputs,
-                            p_initial=p_initial,
-                            p_transition=p_transition,
-                            p_emission=p_emission
-                            )
-p_initial, p_transition, p_emission = x
-mm = MarkovModel.MarkovModel(states, alphabet,
-                             p_initial, p_transition, p_emission)
-print_mm(mm)
-
-
-# Test Baum-Welch.  This is hard because it is a non-deterministic
-# algorithm.  Each run will result in different states having to
-# different emissions.  In order to help this, we need to specify some
-# initial probabilities to bias the final results.  This is not
-# implemented yet in the MarkovModel module.
-
-## states = [
-##     "state0",
-##     "state1",
-##     "state2",
-##     "state3",
-##     ]
-## alphabet = ["a", "c", "g", "t"]
-
-## training_data = [
-##     "aacccgggttttttt",
-##     "accgttttttt",
-##     "acgggtttttt",
-##     "accgtttttttt",
-##     "aaccgtttttttt",
-##     "aacggttttt",
-##     "acccggttttt",
-##     "acccggggttt",
-##     "aacccggggtttt",
-##     "aaccgggtttttt"
-##     ]
-## print "TRAINING HMM"
-## ep = ones((len(states), len(alphabet)))
-## hmm = MarkovModel.train_bw(states, alphabet, training_data,
-##                            pseudo_emission=ep)
-## print "CLASSIFYING"
-## states = MarkovModel.find_states(hmm, "aacgtt")
-## print states
-## print "STATES: %s" % ' '.join(hmm.states)
-## print "ALPHABET: %s" % ' '.join(hmm.alphabet)
-## print "INITIAL:"
-## for i in range(len(hmm.p_initial)):
-##     print "  %s: %.2f" % (hmm.states[i], hmm.p_initial[i])
-## print "TRANSITION:"
-## for i in range(len(hmm.p_transition)):
-##     x = ["%.2f" % x for x in hmm.p_transition[i]]
-##     print "  %s: %s" % (hmm.states[i], ' '.join(x))
-## print "EMISSION:"
-## for i in range(len(hmm.p_emission)):
-##     x = ["%.2f" % x for x in hmm.p_emission[i]]
-##     print "  %s: %s" % (hmm.states[i], ' '.join(x))
-
-
-
-
-# Do some tests from the topcoder competition.
-
-class DNAStrand:
-    def mostLikely(self, normal, island, dnastrand):
+    def test_topcoder1(self):
+        # NNNN
         states = "NR"
         alphabet = "AGTC"
-
-        normal = [float(x)/100 for x in normal]
-        island = [float(x)/100 for x in island]
-        
-        p_initial = [1.0, 0.0]
-        p_initial = asarray(p_initial)
-
-        p_transition = []
-        p_transition.append([1.0-normal[-1], normal[-1]])
-        p_transition.append([island[-1], 1.0-island[-1]])
-        p_transition = asarray(p_transition)
-        
-        p_emission = []   # 2x4 matrix
-        p_emission.append(normal[:4])
-        p_emission.append(island[:4])
-        p_emission = asarray(p_emission)
-
-        mm = MarkovModel.MarkovModel(
+        p_initial = array([1.0, 0.0])
+        p_transition = array([[0.90, 0.10],
+                              [0.20, 0.80]])
+        p_emission = array([[0.30, 0.20, 0.30, 0.20],
+                            [0.10, 0.40, 0.10, 0.40]])
+        markov_model = MarkovModel.MarkovModel(
             states, alphabet, p_initial, p_transition, p_emission)
+        states = MarkovModel.find_states(markov_model, "TGCC")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ['N', 'N', 'N', 'N'])
 
-        x = MarkovModel.find_states(mm, dnastrand)
-        states, x = x[0]
-        return ''.join(states)
+    def test_topcoder2(self):
+        # NNNRRRNNRRNRRN
+        states = "NR"
+        alphabet = "AGTC"
+        p_initial = array([1.0, 0.0])
+        p_transition = array([[0.56, 0.44],
+                              [0.25, 0.75]])
+        p_emission = array([[0.04, 0.14, 0.62, 0.20],
+                            [0.39, 0.15, 0.04, 0.42]])
+        markov_model = MarkovModel.MarkovModel(
+            states, alphabet, p_initial, p_transition, p_emission)
+        states = MarkovModel.find_states(markov_model, "CCTGAGTTAGTCGT")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ['N', 'N', 'N', 'R', 'R', 'R', 'N', 'N', 'R', 'R', 'N', 'R', 'R', 'N'])
 
-ds = DNAStrand()
+    def test_topcoder3(self):
+        # NRRRRRRRRRRRNNNNRRRRRRRRR
+        states = "NR"
+        alphabet = "AGTC"
+        p_initial = array([1.0, 0.0])
+        p_transition = array([[0.75, 0.25],
+                              [0.25, 0.75]])
+        p_emission = array([[0.45, 0.36, 0.06, 0.13],
+                            [0.24, 0.18, 0.12, 0.46]])
+        markov_model = MarkovModel.MarkovModel(
+            states, alphabet, p_initial, p_transition, p_emission)
+        states = MarkovModel.find_states(markov_model, "CCGTACTTACCCAGGACCGCAGTCC")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ['N', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'N', 'N', 'N', 'N', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'])
+
+    def test_topcoder4(self):
+        # NRRRRRRRRRR
+        states = "NR"
+        alphabet = "AGTC"
+        p_initial = array([1.0, 0.0])
+        p_transition = array([[0.55, 0.45],
+                              [0.15, 0.85]])
+        p_emission = array([[0.75, 0.03, 0.01, 0.21],
+                            [0.34, 0.11, 0.39, 0.16]])
+        markov_model = MarkovModel.MarkovModel(
+            states, alphabet, p_initial, p_transition, p_emission)
+        states = MarkovModel.find_states(markov_model, "TTAGCAGTGCG")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ['N','R','R','R','R','R','R','R','R','R','R'])
+
+    def test_topcoder5(self):
+        # N
+        states = "NR"
+        alphabet = "AGTC"
+        p_initial = array([1.0, 0.0])
+        p_transition = array([[0.84, 0.16],
+                              [0.25, 0.75]])
+        p_emission = array([[0.26, 0.37, 0.08, 0.29],
+                            [0.31, 0.13, 0.33, 0.23]])
+        markov_model = MarkovModel.MarkovModel(
+            states, alphabet, p_initial, p_transition, p_emission)
+        states = MarkovModel.find_states(markov_model, "T")
+        self.assertEqual(len(states), 1)
+        state_list, state_float = states[0]
+        self.assertEqual(state_list, ["N"])
 
 
-# NNNN
-print ds.mostLikely([30, 20, 30, 20, 10],
-                    [10, 40, 10, 40, 20],
-                    "TGCC"
-                    )
-
-# NNNRRRNNRRNRRN
-print ds.mostLikely([4, 14, 62, 20, 44],
-                    [39, 15, 4, 42, 25],
-                    "CCTGAGTTAGTCGT"
-                    )
-
-# NRRRRRRRRRRRNNNNRRRRRRRRR
-print ds.mostLikely([45, 36, 6, 13, 25],
-                    [24, 18, 12, 46, 25],
-                    "CCGTACTTACCCAGGACCGCAGTCC"
-                    )
-
-# NRRRRRRRRRR
-print ds.mostLikely([75,3,1,21,45],
-                    [34,11,39,16,15],
-                    "TTAGCAGTGCG"
-                    )
-
-# N
-print ds.mostLikely([26,37,8,29,16],
-                    [31,13,33,23,25],
-                    "T"
-                    )
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity = 2)
+    unittest.main(testRunner=runner)
