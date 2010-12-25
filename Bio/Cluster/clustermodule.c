e3227d30f7334986c219a8fb7b13c120a762cfe7
Michiel de Hoon <mdehoon@michiel-de-hoons-computer.local>
2010-12-26 00:23:45 +0900

    Changing to ANSI-C style comments

--- Bio/Cluster/clustermodule.c
+++ Bio/Cluster/clustermodule.c
@@ -12,12 +12,12 @@
 static struct module_state _state;
 #endif
 
-//Must define Py_TYPE for Python 2.5 or older
+/* Must define Py_TYPE for Python 2.5 or older */
 #ifndef Py_TYPE
 #  define Py_TYPE(o) ((o)->ob_type)
 #endif
 
-//Must define PyVarObject_HEAD_INIT for Python 2.5 or older
+/* Must define PyVarObject_HEAD_INIT for Python 2.5 or older */
 #ifndef PyVarObject_HEAD_INIT
 #define PyVarObject_HEAD_INIT(type, size)       \
         PyObject_HEAD_INIT(type) size,
