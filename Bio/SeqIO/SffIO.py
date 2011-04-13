3497eb292f1ffdd6f8dba5dc2eadb47ae151d9f9
Connor McCoy <cmccoy@fhcrc.org>
2011-04-13 16:06:17 -0700

    Trim trailing whitespace

--- Bio/SeqIO/SffIO.py
+++ Bio/SeqIO/SffIO.py
@@ -145,8 +145,8 @@ region (i.e. the sequence after trimming) starts with AAAGA exactly (the non-
 degenerate bit of this pretend primer):
 
     >>> from Bio import SeqIO
-    >>> records = (record for record in 
-    ...            SeqIO.parse("Roche/E3MFGYR02_random_10_reads.sff","sff") 
+    >>> records = (record for record in
+    ...            SeqIO.parse("Roche/E3MFGYR02_random_10_reads.sff","sff")
     ...            if record.seq[record.annotations["clip_qual_left"]:].startswith("AAAGA"))
     >>> count = SeqIO.write(records, "temp_filtered.sff", "sff")
     >>> print "Selected %i records" % count
@@ -366,7 +366,7 @@ def _sff_find_roche_index(handle):
     and size, and the actual read index offset and size.
 
     Raises a ValueError for unsupported or non-Roche index blocks.
-    """    
+    """
     handle.seek(0)
     header_length, index_offset, index_length, number_of_reads, \
     number_of_flows_per_read, flow_chars, key_sequence \
@@ -449,7 +449,7 @@ def ReadRocheXmlManifest(handle):
     took advantage of this to define their own index block wich also embeds
     an XML manifest string. This is not a publically documented extension to
     the SFF file format, this was reverse engineered.
-    
+
     The handle should be to an SFF file opened in binary mode. This function
     will use the handle seek/tell functions and leave the handle in an
     arbitrary location.
@@ -489,7 +489,7 @@ def _sff_read_roche_index(handle):
     """
     number_of_reads, header_length, index_offset, index_length, xml_offset, \
     xml_size, read_index_offset, read_index_size = _sff_find_roche_index(handle)
-    #Now parse the read index...    
+    #Now parse the read index...
     handle.seek(read_index_offset)
     fmt = ">5B"
     for read in range(number_of_reads):
@@ -684,7 +684,7 @@ def SffIterator(handle, alphabet=Alphabet.generic_dna, trim=False):
     >>> handle.close()
 
     Or, with the trim option:
-        
+
     >>> handle = open("Roche/E3MFGYR02_random_10_reads.sff", "rb")
     >>> for record in SffIterator(handle, trim=True):
     ...     print record.id, len(record)
@@ -949,7 +949,7 @@ class SffWriter(SequenceWriter):
                              1, #the only flowgram format code we support
                              self._flow_chars, self._key_sequence)
         self.handle.write(header + _null*padding)
-        
+
     def write_record(self, record):
         """Write a single additional record to the output file.
 
@@ -1004,7 +1004,7 @@ class SffWriter(SequenceWriter):
                 self._index = None
             else:
                 self._index.append((name, self.handle.tell()))
-        
+
         #the read header format (fixed part):
         #read_header_length     H
         #name_length            H
@@ -1074,7 +1074,7 @@ if __name__ == "__main__":
         assert len(index1) == len(list(SffIterator(open(filename))))
         assert len(index1) == len(list(SffIterator(BytesIO(open(filename,"r").read()))))
         assert len(index1) == len(list(SffIterator(BytesIO(open(filename).read()))))
-                    
+
     sff = list(SffIterator(open(filename, "rb")))
 
     sff2 = list(SffIterator(open("../../Tests/Roche/E3MFGYR02_alt_index_at_end.sff", "rb")))
@@ -1106,7 +1106,7 @@ if __name__ == "__main__":
     for old, new in zip(sff, sff2):
         assert old.id == new.id
         assert str(old.seq) == str(new.seq)
-    
+
     sff_trim = list(SffIterator(open(filename, "rb"), trim=True))
 
     print ReadRocheXmlManifest(open(filename, "rb"))
@@ -1128,7 +1128,7 @@ if __name__ == "__main__":
         print s.id
         #print s.seq
         #print s.letter_annotations["phred_quality"]
-        
+
         assert s.id == f.id == q.id
         assert str(s.seq) == str(f.seq)
         assert s.letter_annotations["phred_quality"] == q.letter_annotations["phred_quality"]
