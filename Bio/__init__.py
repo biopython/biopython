183339ad22f526f0f28942ad80198e08f6146da0
peterjc <p.j.a.cock@googlemail.com>
2011-05-26 11:31:14 +0100

    Introduce BiopythonParserWarning and use for GenBank LOCUS lines

--- Bio/__init__.py
+++ Bio/__init__.py
@@ -46,6 +46,21 @@ class BiopythonWarning(Warning):
     """
     pass
 
+class BiopythonParserWarning(BiopythonWarning):
+    """Biopython parser warning.
+    
+    Some in-valid data files cannot be parsed and will trigger an exception.
+    Where a reasonable interpretation is possible, Biopython will issue this
+    warning to indicate a potential problem. To silence these warnings, use:
+
+        >>> import warnings
+        >>> from Bio import BiopythonParserWarning
+        >>> warnings.simplefilter('ignore', BiopythonParserWarning)
+
+    Consult the warnings module documentation for more details.
+    """
+    pass
+
 class BiopythonDeprecationWarning(BiopythonWarning):
     """Biopython deprecation warning.
     
