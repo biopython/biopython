2018c5f0978ab015ddf41f30ffb363f4fbd1d478
Phillip Garland <pgarland@gmail.com>
2011-05-31 21:35:30 -0700

    _calculate_to_transitions, _calculate_from_transitions: use _collection_associations

--- Bio/HMM/MarkovModel.py
+++ Bio/HMM/MarkovModel.py
@@ -61,22 +61,7 @@ def _calculate_from_transitions(trans_probs):
     lists of destination states reachable from the source state via a
     transition.
     """
-    from_transitions = {}
-
-    # loop over all of the transitions, mapping source states to lists
-    # of destination states
-    for trans_key in trans_probs:
-        from_state = trans_key[0]
-        to_state = trans_key[1]
-        # Add to_state to the list of destination states that are reachable
-        # from from_state. Map from_state if we haven't seen it before.
-        if from_state in from_transitions:
-            from_transitions[from_state].append(to_state)
-        # otherwise create the list and add the state
-        else:
-            from_transitions[from_state] = [to_state]
-
-    return from_transitions
+    return _collect_associations(trans_probs.keys(), 0)
 
 def _calculate_to_transitions(trans_probs):
     """Calculate which 'to transitions' are allowed for each state
@@ -87,22 +72,7 @@ def _calculate_to_transitions(trans_probs):
     lists of source states from which the destination is reachable via a
     transition.
     """
-    to_transitions = {}
-
-    # loop over all of the transitions, mapping destination states to lists
-    # of source states
-    for trans_key in trans_probs:
-        from_state = trans_key[0]
-        to_state = trans_key[1]
-        # Add from_state to the list of source states from which to_state
-        # is reachable. Map to_state if we haven't seen it before.
-        if to_state in to_transitions:
-            to_transitions[to_state].append(from_state)
-        # otherwise create the list and add the state
-        else:
-            to_transitions[to_state] = [from_state]
-
-    return to_transitions
+    return _collect_associations(trans_probs.keys(), 1)
 
 class MarkovModelBuilder(object):
     """Interface to build up a Markov Model.
