b223292a1d1df68cc8800481c78bb042b5b119cf
Mikael Trellet <mikael.trellet@gmail.com>
2011-06-14 09:56:58 +0200

    Added unit test script for Interface analysis.

---
+++ Tests/test_Interface.py
@@ -0,0 +1,46 @@
+# Copyright 2011 by Mikael Trellet.  All rights reserved.
+# Revisions copyright 2009-2010 by Peter Cock.  All rights reserved.
+#
+# Converted by Mikael Trellet from an unit test copyright 2009
+# by Eric Talevich.
+# 
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""Unit tests for the Bio.PDB module."""
+import os
+import unittest
+import warnings
+from StringIO import StringIO
+
+from Bio.PDB import PDBParser
+from Bio.PDB import InterfaceBuilder
+
+class buildInterface(unittest.TestCase):
+    " Test to build an interface from a PDB "
+
+    def setUp(self):
+        "Setup"
+    
+        P = PDBParser(QUIET=1)
+        structure = P.get_structure('test', 'PDB/2WFU.pdb')[0]
+        self.structure = structure
+
+    def test_InterfaceBuilder(self):
+        """InterfaceBuilder Test"""
+
+        interfaceBuilder = InterfaceBuilder.InterfaceBuilder(self.structure)
+        interface = interfaceBuilder.get_interface()
+
+        self.assertEqual(interface.level, 'I')
+        self.assertEqual(interface.id, 'Interface_AB')
+        self.assertEqual(len(interface), 9)
+
+        interface_ids = [(' ', 19, ' '), (' ', 19, ' '), (' ', 20, ' '), (' ', 20, ' '), (' ', 21, ' '), (' ', 21, ' '), (' ', 22, ' '), (' ', 22, ' '), (' ', 23, ' ')]
+        # Indirect way of testing because of formatting problems otherwise
+        self.assertEqual(sorted([res.id for res in interface.get_list()]), interface_ids)
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
