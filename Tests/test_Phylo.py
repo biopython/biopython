eebf86f863f1d5bfc9a09ab53abf101a4cc8dc79
Eric Talevich <eric.talevich@gmail.com>
2011-06-18 13:32:41 -0400

    test_Phylo: fixed a platform-specific error in testing float formatting

--- Tests/test_Phylo.py
+++ Tests/test_Phylo.py
@@ -49,7 +49,10 @@ class IOTests(unittest.TestCase):
         tree = Phylo.read(StringIO('A:0.1;'), 'newick')
         mem_file = StringIO()
         Phylo.write(tree, mem_file, 'newick', format_branch_length='%.0e')
-        self.assertEqual(mem_file.getvalue().strip(), 'A:1e-01;')
+        # Py2.5 compat: Windows with Py2.5- represents this as 1e-001;
+        # on all other platforms it's 1e-01
+        self.assertTrue(mem_file.getvalue().strip()
+                        in ['A:1e-01;', 'A:1e-001;'])
 
     def test_convert(self):
         """Convert a tree between all supported formats."""
