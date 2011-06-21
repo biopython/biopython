ee6dc256f7175716c26109ee89f8208bdf3a1e93
peterjc <p.j.a.cock@googlemail.com>
2011-06-21 14:11:22 +0100

    Correct paths in test_Baseml.py

--- Tests/test_Baseml.py
+++ Tests/test_Baseml.py
@@ -13,15 +13,15 @@ from Bio.Phylo.PAML._paml import PamlError
 
 class ModTest(unittest.TestCase):
     
-    align_file = os.path.join("Tests", "PAML", "alignment.phylip")
-    tree_file = os.path.join("Tests", "PAML", "species.tree")
-    bad_tree_file = os.path.join("Tests", "PAML", "bad.tree")
-    out_file = os.path.join("Tests", "PAML", "test.out")
-    working_dir = os.path.join("Tests", "PAML", "baseml_test")
-    results_file = os.path.join("Tests", "PAML", "bad_results.out")
-    bad_ctl_file1 = os.path.join("Tests", "PAML", "bad1.ctl")
-    bad_ctl_file2 = os.path.join("Tests", "PAML", "bad2.ctl")
-    ctl_file = os.path.join("Tests", "PAML", "baseml.ctl")
+    align_file = os.path.join("PAML", "alignment.phylip")
+    tree_file = os.path.join("PAML", "species.tree")
+    bad_tree_file = os.path.join("PAML", "bad.tree")
+    out_file = os.path.join("PAML", "test.out")
+    working_dir = os.path.join("PAML", "baseml_test")
+    results_file = os.path.join("PAML", "bad_results.out")
+    bad_ctl_file1 = os.path.join("PAML", "bad1.ctl")
+    bad_ctl_file2 = os.path.join("PAML", "bad2.ctl")
+    ctl_file = os.path.join("PAML", "baseml.ctl")
        
     def __del__(self):
         """Just in case BASEML creates some junk files, do a clean-up."""
@@ -186,7 +186,7 @@ class ModTest(unittest.TestCase):
         self.assertRaises(ValueError, baseml.read, self.results_file)
         
     def testParseAllVersions(self):
-        folder = os.path.join("Tests", "PAML","Results", "baseml", "versions")
+        folder = os.path.join("PAML","Results", "baseml", "versions")
         for results_file in os.listdir(folder):
             file_path = os.path.join(folder, results_file)
             if os.path.isfile(file_path) and results_file[:6] == "baseml":
@@ -195,8 +195,8 @@ class ModTest(unittest.TestCase):
                 self.assertEqual(len(results["parameters"]), 7)
     
     def testParseSEs(self):
-        SE_results_file = os.path.join("Tests", "PAML", "Results", "baseml",
-            "baseml_SE.out")
+        SE_results_file = os.path.join("PAML", "Results", "baseml",
+                                       "baseml_SE.out")
         SE_results = baseml.read(SE_results_file)
         SE_parameters = SE_results.get("parameters")
         self.assertNotEqual(SE_parameters.get("SEs"), None)
