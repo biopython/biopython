8889a8aa363e6c6196e09d089a0c49fc29aa6152
Phillip Garland <pgarland@gmail.com>
2011-06-24 22:39:31 -0700

    Make set_random_emission_probabilities only use allowed emissions

--- Bio/HMM/MarkovModel.py
+++ Bio/HMM/MarkovModel.py
@@ -257,12 +257,19 @@ class MarkovModelBuilder(object):
         return self.transition_prob
 
     def set_random_emission_probabilities(self):
-        """Set all emission probabilities to a randomly generated distribution.
-        Returns the dictionary containing the emission probabilities.
+        """Set all allowed emission probabilities to a randomly generated
+        distribution.  Returns the dictionary containing the emission
+        probabilities.
         """
-        for state in self._state_alphabet.letters:
-            freqs = _gen_random_array(len(self._emission_alphabet.letters))
-            for symbol in self._emission_alphabet.letters:
+
+        if not self.emission_prob:
+            raise Exception("No emissions have been allowed yet. " +
+                            "Allow some or all emissions.")
+
+        emissions = _calculate_emissions(self.emission_prob)
+        for state in emissions.iterkeys():
+            freqs = _gen_random_array(len(emissions[state]))
+            for symbol in emissions[state]:
                 self.emission_prob[(state, symbol)] = freqs.pop()
 
         return self.emission_prob
