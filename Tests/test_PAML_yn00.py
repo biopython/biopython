147096a091ab31c0ef3e6c721ca8c73785d782de
Brandon Invergo <b.invergo@gmail.com>
2011-08-17 00:47:24 +0200

    PAML: removed binary dependency testing from the test_PAML_xxxx.py tests

--- Tests/test_PAML_yn00.py
+++ Tests/test_PAML_yn00.py
@@ -7,46 +7,8 @@ import unittest
 import os
 import os.path
 import sys
-from Bio import MissingExternalDependencyError
 from Bio.Phylo.PAML import yn00
 from Bio.Phylo.PAML._paml import PamlError
-
-
-def is_exe(filepath):
-        return os.path.exists(filepath) and os.access(filepath, os.X_OK)
-
-
-def which(program):
-    filepath, filename = os.path.split(program)
-    os_path = os.environ["PATH"].split(os.pathsep)
-    if sys.platform == "win32":
-        try:
-            #This can vary depending on the Windows language.
-            prog_files = os.environ["PROGRAMFILES"]
-        except KeyError:
-            prog_files = r"C:\Program Files"
-        #For Windows, the user is instructed to move the programs to a folder
-        #and then to add the folder to the system path. Just in case they didn't
-        #do that, we can check for it in Program Files.
-        likely_dirs = ["", #Current dir
-                       prog_files,
-                       os.path.join(prog_files,"paml41"),
-                       os.path.join(prog_files,"paml43"),
-                       os.path.join(prog_files,"paml44")] + sys.path
-        os_path.extend(likely_dirs)
-    for path in os.environ["PATH"].split(os.pathsep):
-        exe_file = os.path.join(path, program)
-        if is_exe(exe_file):
-            return exe_file
-    return None
-
-if sys.platform == "win32":
-    binary = "yn00.exe"
-else:
-    binary = "yn00"
-if which(binary) is None:
-    raise MissingExternalDependencyError(\
-        "Install YN00 if you want to use the Bio.Phylo.PAML wrapper.")
 
 
 class ModTest(unittest.TestCase):
