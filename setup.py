1975c3ac3ca40ca02595c7920e3659d59a6cd27d
peterjc <p.j.a.cock@googlemail.com>
2011-08-18 18:18:06 +0100

    Officially ending support for Python 2.4

--- setup.py
+++ setup.py
@@ -47,12 +47,10 @@ def get_yes_or_no(question, default):
     return response[0] == 'y'
 
 # Make sure I have the right Python version.
-if sys.version_info[:2] < (2, 4):
-    print ("Biopython requires Python 2.4 or better (but not Python 3 " \
+if sys.version_info[:2] < (2, 5):
+    print ("Biopython requires Python 2.5 or better (but not Python 3 " \
           + "yet).  Python %d.%d detected" % sys.version_info[:2])
     sys.exit(-1)
-elif sys.version_info[:2] == (2,4):
-    print ("WARNING - Biopython no longer officially supports Python 2.4")
 elif sys.version_info[0] == 3:
     print("WARNING - Biopython does not yet officially support Python 3")
     import do2to3
