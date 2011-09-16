1221f59ad0fbea1eb9354120d66f59cd725c9807
peterjc <p.j.a.cock@googlemail.com>
2011-09-16 12:10:54 +0100

    Skip test_MarkovModel.py under PyPy

--- Tests/test_MarkovModel.py
+++ Tests/test_MarkovModel.py
@@ -4,6 +4,7 @@
 
 try:
     from numpy import array
+    from numpy import random #missing in PyPy's micronumpy
 except ImportError:
     from Bio import MissingPythonDependencyError
     raise MissingPythonDependencyError(\
