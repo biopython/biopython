558a91d8245c0a2fc6cb2aa2ac7da4d09af40579
Connor McCoy <cmccoy@fhcrc.org>
2011-10-05 09:40:17 -0700

    Add AsHandleTestCase

--- Tests/test_File.py
+++ Tests/test_File.py
@@ -3,8 +3,15 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-import os
-import string
+from __future__ import with_statement
+
+import os.path
+import unittest
+import shutil
+from StringIO import StringIO
+import tempfile
+
+
 from Bio import File
 
 
@@ -45,10 +52,10 @@ lines = h.readlines()
 print repr(lines[0])   # 'saved again'
 print repr(lines[1])   # 'a multi-line'
 print repr(lines[2])   # 'file'
-    
+
 # should be empty now
 print repr(h.readline())       # ''
-    
+
 h.saveline("save after empty")
 print h.readline()             # 'save after empty'
 print repr(h.readline())       # ''
@@ -57,3 +64,43 @@ print repr(h.readline())       # ''
 h = File.UndoHandle(File.StringHandle("some text"))
 h.saveline("more text")
 print h.read()                 # 'more textsome text'
+
+class AsHandleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # Create a directory to work in
+        self.temp_dir = tempfile.mkdtemp(prefix='biopython-test')
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def _path(self, *args):
+        return os.path.join(self.temp_dir, *args)
+
+    def test_handle(self):
+        "Test as_handle with a file-like object argument"
+        p = self._path('test_file.fasta')
+        with open(p, 'wb') as fp:
+            with File.as_handle(fp) as handle:
+                self.assertEqual(fp, handle, "as_handle should "
+                        "return argument when given a file-like object")
+                self.assertFalse(handle.closed)
+
+            self.assertFalse(handle.closed,
+                    "Exiting as_handle given a file-like object should not "
+                    "close the file")
+
+    def test_path(self):
+        "Test as_handle with a path argument"
+        p = self._path('test_file.fasta')
+        mode = 'wb'
+        with File.as_handle(p, mode=mode) as handle:
+            self.assertEqual(p, handle.name)
+            self.assertEqual(mode, handle.mode)
+            self.assertFalse(handle.closed)
+        self.assertTrue(handle.closed)
+
+    def test_stringio(self):
+        s = StringIO()
+        with File.as_handle(s) as handle:
+            self.assertEqual(s, handle)
