cc2308563cbdf1ef094d9d4648dfc26597b07be8
peterjc <p.j.a.cock@googlemail.com>
2011-10-05 17:31:22 +0100

    Move with_statement import up to please Jython (but still after docstring)

--- Bio/AlignIO/__init__.py
+++ Bio/AlignIO/__init__.py
@@ -117,6 +117,10 @@ You can also use any file format supported by Bio.SeqIO, such as "fasta" or
 "ig" (which are listed above), PROVIDED the sequences in your file are all the
 same length.
 """
+
+# For using with statement in Python 2.5 or Jython
+from __future__ import with_statement
+
 __docformat__ = "epytext en" #not just plaintext
 
 #TODO
@@ -132,9 +136,6 @@ __docformat__ = "epytext en" #not just plaintext
 #
 # - MSF multiple alignment format, aka GCG, aka PileUp format (*.msf)
 #   http://www.bioperl.org/wiki/MSF_multiple_alignment_format
-
-# For with statement in Python 2.5
-from __future__ import with_statement
 
 from Bio.Align import MultipleSeqAlignment
 from Bio.Align.Generic import Alignment
