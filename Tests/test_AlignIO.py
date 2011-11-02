b7ad206f8216c333b84549a0fd7b426edad58d63
Brandon Invergo <b.invergo@gmail.com>
2011-11-02 21:25:09 +0100

    Fixed typo in test_AlignIO.py seq. Phylip test

--- Tests/test_AlignIO.py
+++ Tests/test_AlignIO.py
@@ -47,7 +47,7 @@ test_files = [
     ("phylip", 4, 1, 'Phylip/interlaced2.phy'),
     ("phylip-relaxed", 12, 1, 'ExtendedPhylip/primates.phyx'),
     ("phylip-sequential", 3, 1, 'Phylip/sequential.phy'),
-    ("phylip-sequential", 4, 1, 'Phylip/sequential.phy'),
+    ("phylip-sequential", 4, 1, 'Phylip/sequential2.phy'),
     ("emboss", 4, 1, 'Emboss/alignret.txt'),
     ("emboss", 2, 5, 'Emboss/needle.txt'),
     ("emboss", 2, 1, 'Emboss/needle_asis.txt'),
