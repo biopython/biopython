b60838fe44efe4b66ebeacf7b2f4056de93a0f7a
peterjc <p.j.a.cock@googlemail.com>
2011-11-09 10:29:51 +0000

    Use example.fastq.bgz in test_bgzf.py

--- Tests/test_bgzf.py
+++ Tests/test_bgzf.py
@@ -24,23 +24,18 @@ class BgzfTests(unittest.TestCase):
         if os.path.isfile(self.temp_file):
             os.remove(self.temp_file)
 
-    def test_one(self):
-        """Reproduce BGZF compression for BAM file"""
-        temp_file = self.temp_file
-
-        #Note this example is from an old version of samtools
-        #and all the blocks are full (except the last one)
-        h = gzip.open("SamBam/ex1.bam", "rb")
+    def rewrite(self, compressed_input_file, output_file):
+        h = gzip.open(compressed_input_file, "rb")
         data = h.read()
         h.close()
 
-        h = BgzfWriter(temp_file, "wb")
+        h = BgzfWriter(output_file, "wb")
         h.write(data)
         h.flush()
         h.flush() #Second flush gives empty BGZF block as BAM EOF marker
         h.close()
 
-        h = gzip.open(temp_file)
+        h = gzip.open(output_file)
         new_data = h.read()
         h.close()
 
@@ -49,16 +44,33 @@ class BgzfTests(unittest.TestCase):
         self.assertEqual(len(data), len(new_data))
         self.assertEqual(data, new_data)
 
-        #Now check the blocks agree (using the fact that
-        #this example BAM file has simple block usage)
-        h = open("SamBam/ex1.bam", "rb")
+    def check_blocks(self, old_file, new_file):
+        h = open(old_file, "rb")
         old = list(BgzfBlocks(h))
         h.close()
-        h = open(temp_file, "rb")
+        h = open(new_file, "rb")
         new = list(BgzfBlocks(h))
         h.close()
         self.assertEqual(len(old), len(new))
         self.assertEqual(old, new)
+
+    def test_bam_ex1(self):
+        """Reproduce BGZF compression for BAM file"""
+        temp_file = self.temp_file
+
+        #Note this example is from an old version of samtools
+        #and all the blocks are full (except the last one)
+        self.rewrite("SamBam/ex1.bam", temp_file)
+
+        #Now check the blocks agree (using the fact that
+        #this example BAM file has simple block usage)
+        self.check_blocks("SamBam/ex1.bam", temp_file)
+
+    def test_example_fastq(self):
+        """Reproduce BGZF compression for a FASTQ file"""
+        temp_file = self.temp_file
+        self.rewrite("Quality/example.fastq.gz", temp_file)
+        self.check_blocks("Quality/example.fastq.bgz", temp_file)
 
 
 if __name__ == "__main__":
