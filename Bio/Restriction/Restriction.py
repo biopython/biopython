32fa08974ce7f4c1a9c9e97b95f1872b4b15c864
Eric Talevich <eric.talevich@gmail.com>
2011-11-26 14:11:32 -0500

    Restriction: commented out a line that bothers Pypy (Bug #3236)
    See mailing list thread:
    http://lists.open-bio.org/pipermail/biopython-dev/2011-November/009314.html

--- Bio/Restriction/Restriction.py
+++ Bio/Restriction/Restriction.py
@@ -238,7 +238,9 @@ class RestrictionType(type):
         if "-" in name :
             raise ValueError("Problem with hyphen in %s as enzyme name" \
                              % repr(name))
-        super(RestrictionType, cls).__init__(cls, name, bases, dct)
+        # 2011/11/26 - Nobody knows what this call was supposed to accomplish,
+        # but all unit tests seem to pass without it. 
+        # super(RestrictionType, cls).__init__(cls, name, bases, dct)
         try :
             cls.compsite = re.compile(cls.compsite)
         except Exception, err :
