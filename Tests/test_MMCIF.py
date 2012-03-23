a32bdb95284d7f6ada6ffb06e604273f2f9f9672
Lenna Peterson <lxpeters@purdue.edu>
2012-03-23 19:39:53 -0400

    Minimal unit test for MMCIFParser.

--- Tests/test_MMCIF.py
+++ Tests/test_MMCIF.py
@@ -1,5 +1,11 @@
-# Written 2012 Lenna X. Peterson
-# arklenna@gmail.com
+# Copyright 2012 Lenna X. Peterson (arklenna@gmail.com).
+# All rights reserved.
+#
+# Tests adapted from test_PDB.py
+#
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
 
 """Unit tests for the MMCIF portion of the Bio.PDB module."""
 
@@ -17,24 +23,67 @@ except ImportError:
     raise MissingPythonDependencyError(
         "Install NumPy if you want to use Bio.PDB.")
 
-#from Bio.Seq import Seq
-#from Bio.Alphabet import generic_protein
-#from Bio.PDB import PDBParser, PPBuilder, CaPPBuilder, PDBIO
-#from Bio.PDB import HSExposureCA, HSExposureCB, ExposureCN
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_protein
 from Bio.PDB.PDBExceptions import PDBConstructionException, PDBConstructionWarning
-#from Bio.PDB import rotmat, Vector
 
+from Bio.PDB import PPBuilder, CaPPBuilder
 from Bio.PDB.MMCIFParser import MMCIFParser
-from Bio.PDB import PPBuilder
 
 class ParseReal(unittest.TestCase):
     """Testing with real CIF file(s)."""
 
     def test_parser(self):
+        """Extract polypeptides from 1A80."""
         parser = MMCIFParser()
         structure = parser.get_structure("example", "PDB/1A8O.cif")
         self.assertEqual(len(structure), 1)
-        #polypeptides = 
+        for ppbuild in [PPBuilder(), CaPPBuilder()]:
+            #==========================================================
+            #First try allowing non-standard amino acids,
+            polypeptides = ppbuild.build_peptides(structure[0], False)
+            self.assertEqual(len(polypeptides), 1)
+            pp = polypeptides[0]
+            # Check the start and end positions
+            self.assertEqual(pp[0].get_id()[1], 151)
+            self.assertEqual(pp[-1].get_id()[1], 220)
+            # Check the sequence
+            s = pp.get_sequence()
+            self.assertTrue(isinstance(s, Seq))
+            self.assertEqual(s.alphabet, generic_protein)
+            #Here non-standard MSE are shown as M
+            self.assertEqual("MDIRQGPKEPFRDYVDRFYKTLRAEQASQEVKNWMTETLLVQ"
+                             "NANPDCKTILKALGPGATLEEMMTACQG", str(s))
+            #==========================================================
+            #Now try strict version with only standard amino acids
+            #Should ignore MSE 151 at start, and then break the chain
+            #at MSE 185, and MSE 214,215
+            polypeptides = ppbuild.build_peptides(structure[0], True)
+            self.assertEqual(len(polypeptides), 3)
+            #First fragment
+            pp = polypeptides[0]
+            self.assertEqual(pp[0].get_id()[1], 152)
+            self.assertEqual(pp[-1].get_id()[1], 184)
+            s = pp.get_sequence()
+            self.assertTrue(isinstance(s, Seq))
+            self.assertEqual(s.alphabet, generic_protein)
+            self.assertEqual("DIRQGPKEPFRDYVDRFYKTLRAEQASQEVKNW", str(s))
+            #Second fragment
+            pp = polypeptides[1]
+            self.assertEqual(pp[0].get_id()[1], 186)
+            self.assertEqual(pp[-1].get_id()[1], 213)
+            s = pp.get_sequence()
+            self.assertTrue(isinstance(s, Seq))
+            self.assertEqual(s.alphabet, generic_protein)
+            self.assertEqual("TETLLVQNANPDCKTILKALGPGATLEE", str(s))
+            #Third fragment
+            pp = polypeptides[2]
+            self.assertEqual(pp[0].get_id()[1], 216)
+            self.assertEqual(pp[-1].get_id()[1], 220)
+            s = pp.get_sequence()
+            self.assertTrue(isinstance(s, Seq))
+            self.assertEqual(s.alphabet, generic_protein)
+            self.assertEqual("TACQG", str(s))
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
