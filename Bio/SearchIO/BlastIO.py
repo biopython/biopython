172579a18f50b2367d5d015e25d948fafe048854
bow <bow@bow.web.id>
2012-05-12 23:25:11 +0200

    added: BlastIO boilerplate

--- Bio/SearchIO/BlastIO.py
+++ Bio/SearchIO/BlastIO.py
@@ -5,4 +5,84 @@
 
 """Bio.SearchIO parser for BLAST+ output formats.
 
+This module adds support for parsing BLAST+ outputs. BLAST+ is a rewrite
+of NCBI's legacy BLAST (Basic Local Alignment Search Tool), based on the
+NCBI C++ toolkit. The BLAST+ suite is available as command line programs
+or on NCBI's web page.
+
+Specifically, this module supports the following BLAST+ output formats:
+
+  - XML        - 'blast-xml'
+  - Tabular    - 'blast-tab'
+  - Plain text - 'blast-text'
+
+And the following BLAST+ programs: blastn, blastp, blastx, tblastn, tblastx
+
+More information are available through these links:
+  - Publication: http://www.biomedcentral.com/1471-2105/10/421
+  - Web interface: http://blast.ncbi.nlm.nih.gov/
+  - User guide: http://www.ncbi.nlm.nih.gov/books/NBK1762/
+
+For legacy BLAST outputs, see the Bio.Blast module.
+
 """
+
+from Bio.SearchIO._objects import Result, Hit, HSP
+
+
+def blast_xml_iterator(handle):
+    """Generator function to parse BLAST+ XML output as Result objects.
+
+    handle -- Handle to the file, or the filename as string.
+
+    """
+
+
+
+def blast_tabular_iterator(handle):
+    """Generator function to parse BLAST+ tabular output as Result objects.
+
+    handle -- Handle to the file, or the filename as string.
+
+    This method accepts the tabular output variants with or without headers.
+    If the handle points to the tabular variant file with headers, it can
+    parse arbitrary tabs. However, is the tabular file does not have any
+    headers, then it will raise an Exception if the tab columns are not
+    the default ones.
+
+    """
+
+
+
+def blast_text_iterator(handle):
+    """Generator function to parse BLAST+ plain text output as Result objects.
+
+    handle -- Handle to the file, or the filename as string.
+
+    """
+
+
+
+def _test():
+    """Run the Bio.SearchIO.BlastIO module's doctests.
+
+    This will try and locate the unit tests directory, and run the doctests
+    from there in order that the relative paths used in the examples work.
+    """
+    import doctest
+    import os
+
+    test_dir = 'Tests'
+
+    if os.path.isdir(os.path.join('..', '..', test_dir)):
+        print "Runing doctests..."
+        cur_dir = os.path.abspath(os.curdir)
+        os.chdir(os.path.join('..', '..', test_dir))
+        doctest.testmod()
+        os.chdir(cur_dir)
+        print "Done"
+
+
+# if not used as a module, run the doctest
+if __name__ == "__main__":
+    _test()
