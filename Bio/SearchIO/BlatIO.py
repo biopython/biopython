faa52475cc2c3fc5e898f4ffa4e6cb7d0ea0f589
bow <bow@bow.web.id>
2012-05-13 08:45:24 +0200

    added: BlatIO boilerplate

--- Bio/SearchIO/BlatIO.py
+++ Bio/SearchIO/BlatIO.py
@@ -5,4 +5,55 @@
 
 """Bio.SearchIO parser for BLAT output formats.
 
+This module adds support for parsing BLAT outputs. BLAT (BLAST-Like Alignment
+Tool) is a sequence homology search program initially built for annotating
+the human genome.
+
+Specifically, this module supports the following BLAT output formats:
+
+  - PSL - 'blat-psl'
+
+More information are available through these links:
+  
+  - Publication: http://genome.cshlp.org/content/12/4/656
+  - User guide: http://genome.ucsc.edu/goldenPath/help/blatSpec.html
+  - Source download: http://www.soe.ucsc.edu/~kent/src
+  - Executable download: http://hgdownload.cse.ucsc.edu/admin/exe/
+
 """
+
+from Bio.SearchIO._objects import Result, Hit, HSP
+
+
+def blat_psl_iterator(handle):
+    """Generator function to parse BLAT PSL output as Result objects.
+
+    handle -- Handle to the file, or the filename as string.
+
+    """
+
+
+
+def _test():
+    """Run the Bio.SearchIO.BlatIO module's doctests.
+
+    This will try and locate the unit tests directory, and run the doctests
+    from there in order that the relative paths used in the examples work.
+    """
+    import doctest
+    import os
+
+    test_dir = 'Tests'
+
+    if os.path.isdir(os.path.join('..', '..', test_dir)):
+        print "Runing doctests..."
+        cur_dir = os.path.abspath(os.curdir)
+        os.chdir(os.path.join('..', '..', test_dir))
+        doctest.testmod()
+        os.chdir(cur_dir)
+        print "Done"
+
+
+# if not used as a module, run the doctest
+if __name__ == "__main__":
+    _test()
