b0d69d0023477cd6aaaf0fa0dc015956ed3fbb72
bow <bow@bow.web.id>
2012-06-11 02:17:27 +0200

    Update blast-xml indexer to cope with Blast-generated Query IDs

--- Bio/SearchIO/BlastIO/blastxml.py
+++ Bio/SearchIO/BlastIO/blastxml.py
@@ -357,8 +357,10 @@ class BlastXmlIndexer(SearchIndexer):
         block_size = self.block_size
         handle = self._handle
         handle.seek(0)
-        re_id = re.compile(r'<Iteration_query-ID>(.*?)</Iteration_query-ID>')
-        re_id_part = re.compile(r'</Iteration_query-ID>')
+        re_desc = re.compile(r'<Iteration_query-ID>(.*?)'
+                '</Iteration_query-ID>\s+?<Iteration_query-def>'
+                '(.*?)</Iteration_query-def>')
+        re_desc_end = re.compile(r'</Iteration_query-def>')
         counter = 0
 
         while True:
@@ -367,21 +369,24 @@ class BlastXmlIndexer(SearchIndexer):
             # for each iteration start mark
             for qstart_idx in self.get_offsets(block, qstart_mark):
                 # get the id of the query (re.search gets the 1st result)
-                regx = re.search(re_id, block[qstart_idx:])
+                regx = re.search(re_desc, block[qstart_idx:])
                 try:
-                    qstart_id = regx.group(1).split(' ', 1)[0]
+                    qstart_desc = regx.group(2)
+                    qstart_id = regx.group(1)
                 # handle cases where the iteration query ID tag lies in a block
                 # split
                 except AttributeError:
                     # if we still haven't found the query ID, use the
                     # fallback value
-                    if re.search(re_id_part, block[qstart_idx:]):
+                    if re.search(re_desc_end, block[qstart_idx:]):
+                        qstart_desc = self._fallback['desc']
                         qstart_id = self._fallback['id']
                     else:
                         # extend the cached read
                         block_ext = block + handle.read(block_size)
-                        regx = re.search(re_id, block_ext[qstart_idx:])
-                        qstart_id = regx.group(1).split(' ', 1)[0]
+                        regx = re.search(re_desc, block_ext[qstart_idx:])
+                        qstart_desc = regx.group(2)
+                        qstart_id = regx.group(1)
                         # set file pointer to the position before block_ext
                         handle.seek((counter + 1) * block_size)
 
@@ -394,7 +399,7 @@ class BlastXmlIndexer(SearchIndexer):
                 while qlen < 0:
                     ext = handle.read(block_size)
                     if not ext:
-                        raise ValueError("Query end for %s not found" % qstart_id)
+                        raise ValueError("Query end for %r not found" % qstart_id)
                     block_ext += ext
                     qlen = block_ext[qstart_idx:].find(qend_mark)
 
@@ -403,6 +408,8 @@ class BlastXmlIndexer(SearchIndexer):
                 qlen = len(qend_mark) + qlen
                 # move pointer to original position
                 handle.seek((counter + 1) * block_size)
+                if qstart_id.startswith('Query_'):
+                    qstart_id = qstart_desc.split(' ', 1)[0]
                 # yield key, offset, length
                 yield qstart_id, qstart_idx, qlen
 
