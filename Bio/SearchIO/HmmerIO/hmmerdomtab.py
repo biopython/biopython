fcf07816b839d8ae3d22bc858f969b38ceefc37f
bow <bow@bow.web.id>
2012-06-21 02:00:49 +0200

    Add hmmer-domtab parsing and its tests

--- Bio/SearchIO/HmmerIO/hmmerdomtab.py
+++ Bio/SearchIO/HmmerIO/hmmerdomtab.py
@@ -33,7 +33,7 @@ def hmmer_domtab_hmmquery_iterator(handle):
     coordinates.
 
     """
-    for qresult in HmmerDomtabIterator(handle, hmm_as_query=False):
+    for qresult in HmmerDomtabIterator(handle, hmm_as_hit=False):
         yield qresult
 
 
@@ -52,14 +52,15 @@ def read_forward(handle, strip=True):
             return line
 
 
-class HmmerDomtabHmmhitIterator(object):
+class HmmerDomtabIterator(object):
 
     """Parser for the HMMER domain table format that assumes HMM profile
-    coordinaes are hit coordinates."""
+    coordinates are hit coordinates."""
 
-    def __init__(self, handle):
+    def __init__(self, handle, hmm_as_hit):
         self.handle = handle
         self.line = read_forward(self.handle)
+        self.hmm_as_hit = hmm_as_hit
 
     def __iter__(self):
         # stop iterating if it's an empty file
@@ -82,12 +83,104 @@ class HmmerDomtabHmmhitIterator(object):
 
     def parse_result_row(self):
         """Returns a dictionary of parsed row values."""
+        assert self.line
+        cols = filter(None, self.line.strip().split(' '))
+        # if len(cols) > 22, we have extra description columns
+        # combine them all into one string in the 19th column
+        if len(cols) > 22:
+            cols[22] = ' '.join(cols[22:])
+        else:
+            cols[22] = ''
+
+        # assign parsed column data into qresult, hit, and hsp dicts
+        qresult = {}
+        qresult['id'] = cols[3]                 # query name
+        qresult['acc'] = cols[4]                # query accession
+        qresult['seq_len'] = cols[5]            # qlen
+        hit = {}
+        hit['id'] = cols[0]                     # target name
+        hit['acc'] = cols[1]                    # target accession
+        hit['seq_len'] = cols[2]                # tlen
+        hit['evalue'] = cols[6]                 # evalue
+        hit['bitscore'] = cols[7]               # score
+        hit['bias'] = cols[8]                   # bias
+        hit['desc'] = cols[22]                  # description of target
+        hsp = {}
+        hsp['domain_index'] = cols[9]           # # (domain number)
+        # not parsing cols[10] since it's basically len(hit)
+        hsp['evalue_cond'] = cols[11]           # c-evalue
+        hsp['evalue'] = cols[12]                # i-evalue
+        hsp['bitscore'] = cols[13]              # score
+        hsp['bias'] = cols[14]                  # bias
+        hsp['hit_from'] = int(cols[15]) - 1     # hmm from
+        hsp['hit_to'] = int(cols[16]) - 1       # hmm to
+        hsp['query_from'] = int(cols[17]) - 1   # ali from
+        hsp['query_to'] = int(cols[18]) - 1     # ali to
+        hsp['env_from'] = int(cols[19]) - 1     # env from
+        hsp['env_to'] = int(cols[20]) - 1       # env to
+        hsp['acc_avg'] = cols[21]               # acc
+
+        # switch hmm<-->ali coordinates if hmm is not hit
+        if not self.hmm_as_hit:
+            hsp['hit_to'], hsp['query_to'] = \
+                    hsp['query_to'], hsp['hit_to']
+            hsp['hit_from'], hsp['query_from'] = \
+                    hsp['query_from'], hsp['hit_from']
 
         return {'qresult': qresult, 'hit': hit, 'hsp': hsp}
 
     def parse_qresult(self):
         """Generator function that returns QueryResult objects."""
-        qid_cache = ''
+        qid_cache = None
+        hid_cache = None
+        # flag for denoting whether the query is the same as the previous line
+        # or not
+        same_query = False
+        while True:
+            # only parse the result row if it's not EOF
+            if self.line:
+                parsed = self.parse_result_row()
+                qresult_id = parsed['qresult']['id']
+
+            # a new qresult is created whenever qid_cache != qresult_id
+            if qid_cache != qresult_id:
+                # append the last hit and yield qresult if qid_cache is filled
+                if qid_cache is not None:
+                    qresult.append(hit)
+                    yield qresult
+                    same_query = False
+                qid_cache = qresult_id
+                qresult = QueryResult(qresult_id)
+                for attr, value in parsed['qresult'].items():
+                    setattr(qresult, attr, value)
+            # when we've reached EOF, try yield any remaining qresult and break
+            elif not self.line:
+                qresult.append(hit)
+                yield qresult
+                break
+            # otherwise, we must still be in the same query, so set the flag
+            elif not same_query:
+                same_query = True
+
+            hit_id = parsed['hit']['id']
+            # a new hit is created whenever hid_cache != hit_id
+            if hid_cache != hit_id:
+                # if we're in the same query, append the previous line's hit
+                if same_query:
+                    qresult.append(hit)
+                hid_cache = hit_id
+                hit = Hit(hit_id, qresult_id)
+                for attr, value in parsed['hit'].items():
+                    setattr(hit, attr, value)
+
+            # each line is basically a different HSP, so we always add it to
+            # any hit object we have
+            hsp = HSP(hit_id, qresult_id)
+            for attr, value in parsed['hsp'].items():
+                setattr(hsp, attr, value)
+            hit.append(hsp)
+
+            self.line = read_forward(self.handle)
 
 
 class HmmerDomtabHmmhitIndexer(SearchIndexer):
