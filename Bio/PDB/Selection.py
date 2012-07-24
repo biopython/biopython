af2d44fc2ef950e20da3c12b6f140b87011be1e6
David Cain <davidjosephcain@gmail.com>
2012-07-24 16:46:53 +0800

    Perform PEP8 cleanup and readability improvements
    Break unfold_entities up into logical blocks, add spacing to operators
    per PEP8 specification, and in-line some comments.

--- Bio/PDB/Selection.py
+++ Bio/PDB/Selection.py
@@ -10,7 +10,7 @@ from Bio.PDB.Entity import Entity
 from Bio.PDB.PDBExceptions import PDBException
 
 
-entity_levels=["A", "R", "C", "M", "S"]
+entity_levels = ["A", "R", "C", "M", "S"]
 
 
 def uniqueify(items):
@@ -22,7 +22,7 @@ def uniqueify(items):
 
 
 def get_unique_parents(entity_list):
-    """Translate a list of entities to a list of their (unique) parents.""" 
+    """Translate a list of entities to a list of their (unique) parents."""
     parents = [entity.get_parent() for entity in entity_list]
     return uniqueify(parents)
 
@@ -30,7 +30,7 @@ def get_unique_parents(entity_list):
 def unfold_entities(entity_list, target_level):
     """Unfold entities list to a child level (e.g. residues in chain).
 
-    Unfold a list of entities to a list of entities of another 
+    Unfold a list of entities to a list of entities of another
     level.  E.g.:
 
     list of atoms -> list of residues
@@ -51,33 +51,32 @@ def unfold_entities(entity_list, target_level):
     if entity_list == []:
         return []
     if isinstance(entity_list, Entity) or isinstance(entity_list, Atom):
-        entity_list=[entity_list]
-    # level of entity list
-    level=entity_list[0].get_level()
+        entity_list = [entity_list]
+
+    level = entity_list[0].get_level()  # level of entity list
     for entity in entity_list:
-        if not (entity.get_level()==level):
+        if not (entity.get_level() == level):
             raise PDBException("Entity list is not homogeneous.")
-    target_index=entity_levels.index(target_level)
-    level_index=entity_levels.index(level)
-    if level_index==target_index:
-        # already right level
+    target_index = entity_levels.index(target_level)
+    level_index = entity_levels.index(level)
+
+    if level_index == target_index:  # already right level
         return entity_list
-    if level_index>target_index:
-        # we're going down, e.g. S->A
+
+    if level_index > target_index:  # we're going down, e.g. S->A
         for i in range(target_index, level_index):
-            new_entity_list=[]
+            new_entity_list = []
             for entity in entity_list:
-                new_entity_list=new_entity_list+entity.get_list()
-            entity_list=new_entity_list
-    else:
-        # we're going up, e.g. A->S
+                new_entity_list = new_entity_list + entity.get_list()
+            entity_list = new_entity_list
+    else:  # we're going up, e.g. A->S
         for i in range(level_index, target_index):
-            new_entity_list=[]  
+            new_entity_list = []
             for entity in entity_list:
-                parent=entity.get_parent()
+                parent = entity.get_parent()
                 new_entity_list.append(parent)
             # find unique parents
-            entity_list=uniqueify(new_entity_list)
+            entity_list = uniqueify(new_entity_list)
     return entity_list
 
 
@@ -91,4 +90,3 @@ def _test():
 
 if __name__ == "__main__":
     _test()
-
