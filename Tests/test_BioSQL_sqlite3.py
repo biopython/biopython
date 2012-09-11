5c5797b36ff45f24c6a857612279f0a82d20c2d5
Nick Semenkovich <semenko@alum.mit.edu>
2012-09-10 22:39:18 -0500

    Use /dev/shm for sqlite3 testing (where possible)
    The current sqlite3 tests are incredibly slow since they rely on disk. This
    means that ACID compliance forces lots of fsync() calls, which are slowww.
    
    This takes advantage of the /dev/shm ramdisk for testing. Things should fail
    gracefully (back to disk if /dev/shm is inaccessible). The sqlite3 DB seems
    to take only ~2 MB max, so OOM errors shouldn't be an issue.
    
    On a blazing Ivy Bridge / RAID10 machine, this test's execution goes from:
    
      real    4m3.680s
      user    0m5.032s
      sys     0m1.032s
    
    To:
    
      real    0m4.788s
      user    0m4.112s
      sys     0m0.648s
    
    Bonus: This should fix Travis CI's intermittent timeouts, assuming /dev/shm
    is accessible.
    
    Bonus #2: PEP8 fixes.
    
    Signed-off-by: Nick Semenkovich <semenko@alum.mit.edu>

--- Tests/test_BioSQL_sqlite3.py
+++ Tests/test_BioSQL_sqlite3.py
@@ -5,6 +5,7 @@
 
 """Run BioSQL tests using SQLite"""
 import os
+import tempfile
 from Bio import MissingExternalDependencyError
 from BioSQL import BioSeqDatabase
 
@@ -17,10 +18,22 @@ DBPASSWD = ''
 
 DBDRIVER = 'sqlite3'
 DBTYPE = 'sqlite'
-TESTDB = os.path.join(os.getcwd(), "BioSQL", "temp_sqlite.db")
+
 # In memory SQLite does not work with current test structure since the tests
 # expect databases to be retained between individual tests.
 #TESTDB = ':memory:'
+# Instead, we use (if we can) /dev/shm
+try:
+    test_db_fname = tempfile.mkstemp(dir='/dev/shm')[1]
+except OSError:
+    # We can't use /dev/shm
+    import warnings
+    warnings.warn('/dev/shm not available: '
+                  'Using (MUCH!) slower local file for sqlite3 testing')
+    test_db_fname = os.path.join(os.getcwd(), "BioSQL", "temp_sqlite.db")
+
+TESTDB = test_db_fname
+
 
 #This will abort if driver not installed etc:
 check_config(DBDRIVER, DBTYPE, DBHOST, DBUSER, DBPASSWD, TESTDB)
@@ -29,13 +42,15 @@ check_config(DBDRIVER, DBTYPE, DBHOST, DBUSER, DBPASSWD, TESTDB)
 #so just in case there is no database already:
 create_database()
 
+
 if False:
     #This is how I generated test file Tests/BioSQL/cor6_6.db
     #which is test cross-checked with the latest bindings to
     #catch any regressions in how we map GenBank entries to
     #the database.
     assert not os.path.isfile("BioSQL/cor6_6.db")
-    server = BioSeqDatabase.open_database(driver=DBDRIVER, db="BioSQL/cor6_6.db")
+    server = BioSeqDatabase.open_database(driver=DBDRIVER,
+                                          db="BioSQL/cor6_6.db")
     DBSCHEMA = "biosqldb-" + DBTYPE + ".sql"
     SQL_FILE = os.path.join(os.getcwd(), "BioSQL", DBSCHEMA)
     assert os.path.isfile(SQL_FILE), SQL_FILE
@@ -48,6 +63,7 @@ if False:
     assert len(db) == 6
     server.close()
 
+
 class BackwardsCompatibilityTest(unittest.TestCase):
     def test_backwards_compatibility(self):
         """Check can re-use an old BioSQL SQLite3 database."""
@@ -58,12 +74,14 @@ class BackwardsCompatibilityTest(unittest.TestCase):
         db = server["OLD"]
         self.assertEqual(len(db), len(original_records))
         #Now read them back...
-        biosql_records = [db.lookup(name=rec.name) \
+        biosql_records = [db.lookup(name=rec.name)
                           for rec in original_records]
         #And check they agree
         self.assertTrue(compare_records(original_records, biosql_records))
 
 if __name__ == "__main__":
     #Run the test cases
-    runner = unittest.TextTestRunner(verbosity = 2)
-    unittest.main(testRunner=runner)
+    runner = unittest.TextTestRunner(verbosity=2)
+    # exit=False so we can return to unlink TESTDB, which could be in RAM.
+    unittest.main(testRunner=runner, exit=False)
+    os.unlink(TESTDB)
