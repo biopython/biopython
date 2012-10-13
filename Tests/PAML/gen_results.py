8d1047b912eaef5546f80b99b959be06af9e9a85
Brandon Invergo <brandon@invergo.net>
2012-10-13 20:19:55 +0200

    update PAML gen_results to be more useful and flexible

--- Tests/PAML/gen_results.py
+++ Tests/PAML/gen_results.py
@@ -1,19 +1,30 @@
+#!/usr/bin/env python
+# Copyright (C) 2012 by Brandon Invergo (b.invergo@gmail.com)
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+
+
 import os.path
 import sys
 
 
-VERSIONS = ["4_1", "4_3", "4_4", "4_4c", "4_5"]
-VERBOSE = True
-    
-def codeml():
+VERSIONS = ["4_1", "4_3", "4_4", "4_4c", "4_5", "4_6"]
+
+
+def codeml(vers=None, verbose=False):
     from Bio.Phylo.PAML import codeml
+    if vers is not None:
+        versions = [vers]
+    else:
+        versions = VERSIONS
     tests = [("aa_model0", "aa_alignment.phylip", "species.tree"),
             ("aa_pairwise", "aa_alignment.phylip", "species.tree"),
             ("all_NSsites", "alignment.phylip", "species.tree"),
             ("branchsiteA", "alignment.phylip", "species.tree"),
             ("clademodelC", "alignment.phylip", "species.tree"),
             ("freeratio", "alignment.phylip", "species.tree"),
-            ("ngene2_mgene012", "lysinYangSwanson2002.nuc", "lysin.trees"),
+            ("ngene2_mgene02", "lysinYangSwanson2002.nuc", "lysin.trees"),
             ("ngene2_mgene34", "lysinYangSwanson2002.nuc", "lysin.trees"),
             ("pairwise", "alignment.phylip", "species.tree"),
             ("SE", "alignment.phylip", "species.tree")]
@@ -23,24 +34,27 @@ def codeml():
         cml = codeml.Codeml()
         cml.working_dir = "temp"
         ctl_file = os.path.join("Control_files",
-                                "codeml", 
+                                "codeml",
                                 '.'.join([test[0], "ctl"]))
         alignment = os.path.join("Alignments", test[1])
-        tree = os.path.join("Trees", test[2])                   
+        tree = os.path.join("Trees", test[2])
         cml.read_ctl_file(ctl_file)
         cml.alignment = alignment
         cml.tree = tree
-        for version in VERSIONS:
+        for version in versions:
             print "\t{0}".format(version.replace('_', '.'))
             out_file = '.'.join(['-'.join([test[0], version]), "out"])
             cml.out_file = os.path.join("Results", "codeml", test[0], out_file)
             bin = ''.join(["codeml", version])
-            cml.run(command=bin, verbose=VERBOSE)
-         
+            cml.run(command=bin, verbose=verbose)
 
-def baseml():
+
+def baseml(vers=None, verbose=False):
     from Bio.Phylo.PAML import baseml
-    
+    if vers is not None:
+        versions = [vers]
+    else:
+        versions = VERSIONS
     tests = [("model", range(0, 9)), ("nhomo", [1, 3, 4]),
             ("nparK", range(1, 5)), ("alpha1rho1", None), ("SE", None)]
     alignment = os.path.join("Alignments", "alignment.phylip")
@@ -48,24 +62,24 @@ def baseml():
     for test in tests:
         print test[0]
         bml = baseml.Baseml()
-        for version in VERSIONS:
+        for version in versions:
             print "\t{0}".format(version.replace('_', '.'))
             if test[1] is not None:
                 for n in test[1]:
-                    if (version in ["4_3", "4_4", "4_4c", "4_5"] and 
+                    if (version in ["4_3", "4_4", "4_4c", "4_5"] and
                             test[0] == "nparK" and n in [3, 4]):
                         continue
                     print "\t\tn = {0}".format(n)
-                    ctl_file = os.path.join("Control_files", "baseml", 
-                        "{0}{1}.ctl".format(test[0], n))                   
+                    ctl_file = os.path.join("Control_files", "baseml",
+                        "{0}{1}.ctl".format(test[0], n))
                     bml.read_ctl_file(ctl_file)
                     bml.alignment = alignment
-                    bml.tree = tree                
-                    out_file = "{0}{1}-{2}.out".format(test[0], n, version)  
+                    bml.tree = tree
+                    out_file = "{0}{1}-{2}.out".format(test[0], n, version)
                     bml.out_file = os.path.join("Results", "baseml", test[0],
                         out_file)
                     bin = "baseml{0}".format(version)
-                    bml.run(command=bin, verbose=VERBOSE)               
+                    bml.run(command=bin, verbose=VERBOSE)
             else:
                 if (version in ["4_3", "4_4", "4_4c", "4_5"] and
                         test[0] == "alpha1rho1"):
@@ -74,22 +88,25 @@ def baseml():
                     "{0}.ctl".format(test[0]))
                 bml.read_ctl_file(ctl_file)
                 bml.alignment = alignment
-                bml.tree = tree                                  
+                bml.tree = tree
                 out_file = "{0}-{1}.out".format(test[0], version)
                 bml.out_file = os.path.join("Results", "baseml", test[0],
                     out_file)
                 bin = "baseml{0}".format(version)
-                bml.run(command=bin, verbose=VERBOSE)
+                bml.run(command=bin, verbose=verbose)
 
-def yn00():
+def yn00(vers=None, verbose=False):
     from Bio.Phylo.PAML import yn00
-    
+    if vers is not None:
+        versions = [vers]
+    else:
+        versions = VERSIONS
     tests = ["yn00"]
     alignment = os.path.join("Alignments", "alignment.phylip")
     for test in tests:
         print test[0]
         yn = yn00.Yn00()
-        for version in VERSIONS:
+        for version in versions:
             print "\t{0}".format(version.replace('_', '.'))
             ctl_file = os.path.join("Control_files", "yn00",
                 "{0}.ctl".format(test))
@@ -98,16 +115,54 @@ def yn00():
             out_file = "{0}-{1}.out".format(test, version)
             yn.out_file = os.path.join("Results", "yn00", out_file)
             bin = "yn00{0}".format(version)
-            yn.run(command=bin, verbose=VERBOSE)
+            yn.run(command=bin, verbose=verbose)
+
+
+def print_usage():
+    versions = ", ".join([vers.replace("_", ".") for vers in VERSIONS])
+    usage = \
+'''Usage: gen_results.py [-v] PROGRAM [VERSION]
+Generate result files to be used in Bio.Phylo.PAML unit tests.
+
+  -v         Use verbose output
+  PROGRAM    codeml, baseml or yn00
+  VERSION    %s
+
+To use this, the PAML programs must be in your executable path and
+they must be named programX_Y, where X and Y are the version numbers
+(i.e. baseml4_5 or codeml4_4c). If VERSION is not specified, test
+results will be generated for all versions listed above.
+'''%(versions)
+    sys.exit(usage)
 
 if __name__ == "__main__":
+    programs = ["codeml", "baseml", "yn00"]
+    prog = None
+    verbose = False
+    vers = None
     if len(sys.argv) < 2:
-        sys.exit("specify a paml program")
-    if sys.argv[1] == "codeml":
-        codeml()
-    elif sys.argv[1] == "baseml":
-        baseml()
-    elif sys.argv[1] == "yn00":
-        yn00()
-    else:
-        sys.exit("specify a paml program")
+        print_usage()
+    for arg in sys.argv[1:]:
+        if arg == "-v":
+            verbose = True
+        elif arg in programs:
+            if prog is not None:
+                print "Only one program at a time, please."
+                print_usage()
+            prog = arg
+        elif arg.replace(".", "_") in VERSIONS:
+            if vers is not None:
+                print "Only one version at a time, sorry."
+            vers = arg.replace(".", "_")
+        else:
+            print "Unrecognized argument"
+            print_usage()
+    if prog is None:
+        print "No program specified"
+        print_usage()
+    if prog == "codeml":
+        codeml(vers, verbose)
+    elif prog == "baseml":
+        baseml(vers, verbose)
+    elif prog == "yn00":
+        yn00(vers, verbose)
