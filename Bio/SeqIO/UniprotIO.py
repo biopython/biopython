ca33ec2e5f2b1debf1de40d9415fd2c116599fe6
Peter Cock <p.j.a.cock@googlemail.com>
2012-10-13 13:16:32 +0100

    Fix indentation spotted by pylint

--- Bio/SeqIO/UniprotIO.py
+++ Bio/SeqIO/UniprotIO.py
@@ -303,31 +303,31 @@ class Parser(object):
             # </dbReference>
             if 'type' in element.attrib:
                 if element.attrib['type'] == 'PDB':
-                        method = ""
-                        resolution = ""
-                        for ref_element in element.getchildren():  
-                            if ref_element.tag == NS + 'property':
-                                dat_type = ref_element.attrib['type']
-                                if dat_type == 'method':
-                                    method = ref_element.attrib['value']
-                                if dat_type == 'resolution':
-                                    resolution = ref_element.attrib['value']
-                                if dat_type == 'chains':
-                                    pairs = ref_element.attrib['value'].split(',')
-                                    for elem in pairs:
-                                        pair = elem.strip().split('=')
-                                        if pair[1] != '-':
-                                            #TODO - How best to store these, do SeqFeatures make sense?
-                                            feature = SeqFeature.SeqFeature()
-                                            feature.type = element.attrib['type']
-                                            feature.qualifiers['name'] = element.attrib['id']
-                                            feature.qualifiers['method'] = method
-                                            feature.qualifiers['resolution'] = resolution
-                                            feature.qualifiers['chains'] = pair[0].split('/')
-                                            start = int(pair[1].split('-')[0])-1
-                                            end = int(pair[1].split('-')[1])
-                                            feature.location = SeqFeature.FeatureLocation(start, end)
-                                            #self.ParsedSeqRecord.features.append(feature)
+                    method = ""
+                    resolution = ""
+                    for ref_element in element.getchildren():  
+                        if ref_element.tag == NS + 'property':
+                            dat_type = ref_element.attrib['type']
+                            if dat_type == 'method':
+                                method = ref_element.attrib['value']
+                            if dat_type == 'resolution':
+                                resolution = ref_element.attrib['value']
+                            if dat_type == 'chains':
+                                pairs = ref_element.attrib['value'].split(',')
+                                for elem in pairs:
+                                    pair = elem.strip().split('=')
+                                    if pair[1] != '-':
+                                        #TODO - How best to store these, do SeqFeatures make sense?
+                                        feature = SeqFeature.SeqFeature()
+                                        feature.type = element.attrib['type']
+                                        feature.qualifiers['name'] = element.attrib['id']
+                                        feature.qualifiers['method'] = method
+                                        feature.qualifiers['resolution'] = resolution
+                                        feature.qualifiers['chains'] = pair[0].split('/')
+                                        start = int(pair[1].split('-')[0])-1
+                                        end = int(pair[1].split('-')[1])
+                                        feature.location = SeqFeature.FeatureLocation(start, end)
+                                        #self.ParsedSeqRecord.features.append(feature)
 
             for ref_element in  element.getchildren():  
                 if ref_element.tag == NS + 'property':
