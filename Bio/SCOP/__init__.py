biopython/biopython

commit 3bbeddd96ace79070df9b952485276623c70b10f

Fix comparisons to None (PEP8 E711).

Bio/SCOP/__init__.py
@@ -175,10 +175,11 @@ class Scop(object):
         self._sidDict = {}
         self._sunidDict = {}
 
-        if cla_handle==des_handle==hie_handle==dir_path==db_handle==None: return 
+        if all(h is None for h in [cla_handle, des_handle, hie_handle, dir_handle, db_handle]):
+            return
 
         if dir_path is None and db_handle is None:
-            if cla_handle == None or des_handle==None or hie_handle==None:
+            if cla_handle is None or des_handle is None or hie_handle is None:
                 raise RuntimeError("Need CLA, DES and HIE files to build SCOP")
 
         sunidDict = {}
@@ -337,7 +338,8 @@ class Scop(object):
 
     def getDomainFromSQL(self, sunid=None, sid=None):
         """Load a node from the SQL backend using sunid or sid"""
-        if sunid==sid==None: return None
+        if sunid is None and sid is None:
+            return None
 
         cur = self.db_handle.cursor()
         
@@ -710,7 +712,7 @@ class Astral(object):
                     using writeToSQL.
         """
 
-        if astral_file==dir_path==db_handle==None:
+        if astral_file is None and dir_path is None and db_handle is None:
             raise RuntimeError("Need either file handle, or (dir_path + "\
                        + "version) or database handle to construct Astral")
         if not scop:
@@ -721,7 +723,7 @@ class Astral(object):
 
         
         if not astral_file and not db_handle:
-            if dir_path == None or version == None:
+            if dir_path is None or version is None:
                 raise RuntimeError("must provide dir_path and version")
 
             self.version = version
@@ -777,7 +779,7 @@ class Astral(object):
 
     def getAstralDomainsFromFile(self,filename=None,file_handle=None):
         """Get the scop domains from a file containing a list of sids"""
-        if file_handle == filename == None:
+        if file_handle is None and filename is None:
             raise RuntimeError("You must provide a filename or handle")
         if not file_handle:
             file_handle = open(filename)

Bio/Cluster/__init__.py
@@ -493,15 +493,15 @@ expclusters=None:  For hierarchical clustering results, expclusters
 
 """
         (ngenes,nexps) = numpy.shape(self.data)
-        if self.gorder == None:
+        if self.gorder is None:
             gorder = numpy.arange(ngenes)
         else:
             gorder = self.gorder
-        if self.eorder == None:
+        if self.eorder is None:
             eorder = numpy.arange(nexps)
         else:
             eorder = self.eorder
-        if geneclusters!=None and expclusters!=None and \
+        if geneclusters is not None and expclusters is not None and \
            type(geneclusters) != type(expclusters):
             raise ValueError("found one k-means and one hierarchical "
                            + "clustering solution in geneclusters and "
@@ -514,7 +514,7 @@ expclusters=None:  For hierarchical clustering results, expclusters
             # This is a hierarchical clustering result.
             geneindex = _savetree(jobname, geneclusters, gorder, 0)
             gid = 1
-        elif geneclusters!=None:
+        elif geneclusters is not None:
             # This is a k-means clustering result.
             filename = jobname + "_K"
             k = max(geneclusters+1)
@@ -527,7 +527,7 @@ expclusters=None:  For hierarchical clustering results, expclusters
             # This is a hierarchical clustering result.
             expindex = _savetree(jobname, expclusters, eorder, 1)
             aid = 1
-        elif expclusters!=None:
+        elif expclusters is not None:
             # This is a k-means clustering result.
             filename = jobname + "_K"
             k = max(expclusters+1)
@@ -569,7 +569,7 @@ expclusters=None:  For hierarchical clustering results, expclusters
 
     def _savedata(self, jobname, gid, aid, geneindex, expindex):
         # Save the clustered data.
-        if self.genename == None:
+        if self.genename is None:
             genename = self.geneid
         else:
             genename = self.genename
@@ -578,15 +578,15 @@ expclusters=None:  For hierarchical clustering results, expclusters
             outputfile = open(jobname+'.cdt', 'w')
         except IOError:
             raise IOError("Unable to open output file")
-        if self.mask!=None:
+        if self.mask is not None:
             mask = self.mask
         else:
             mask = numpy.ones((ngenes,nexps), int)
-        if self.gweight!=None:
+        if self.gweight is not None:
             gweight = self.gweight
         else:
             gweight = numpy.ones(ngenes)
-        if self.eweight!=None:
+        if self.eweight is not None:
             eweight = self.eweight
         else:
             eweight = numpy.ones(nexps)

Bio/Compass/__init__.py
@@ -193,12 +193,12 @@ def __read_query_alignment(record, line):
     if m:
         record.query_start = int(m.group(1))
     m = __regex["align"].match(line)
-    assert m!=None, "invalid match"
+    assert m is not None, "invalid match"
     record.query_aln += m.group(1)
 
 def __read_positive_alignment(record, line):
     m = __regex["positive_alignment"].match(line)
-    assert m!=None, "invalid match"
+    assert m is not None, "invalid match"
     record.positives += m.group(1)
 
 def __read_hit_alignment(record, line):
@@ -206,5 +206,5 @@ def __read_hit_alignment(record, line):
     if m:
         record.hit_start = int(m.group(1))
     m = __regex["align"].match(line)
-    assert m!=None, "invalid match"
+    assert m is not None, "invalid match"
     record.hit_aln += m.group(1)

Bio/Entrez/__init__.py
@@ -415,7 +415,7 @@ def _open(cgi, params={}, post=False):
         params["tool"] = tool
     # Tell Entrez who we are
     if not "email" in params:
-        if email!=None:
+        if email is not None:
             params["email"] = email
         else:
             warnings.warn("""

Bio/PopGen/GenePop/__init__.py
@@ -141,7 +141,7 @@ class Record(object):
                 for marker in markers:
                     rep.append(' ')
                     for al in marker:
-                        if al == None:
+                        if al is None:
                             al = '0'
                         aStr = str(al)
                         while len(aStr)<self.marker_len:

Bio/SubsMat/__init__.py
@@ -171,7 +171,7 @@ class SeqMat(dict):
               self.update(data)
           except ValueError:
               raise ValueError("Failed to store data in a dictionary")
-      if alphabet == None:
+      if alphabet is None:
          alphabet = Alphabet.Alphabet()
       assert Alphabet.generic_alphabet.contains(alphabet)
       self.alphabet = alphabet
@@ -251,7 +251,7 @@ class SeqMat(dict):
       f = f or sys.stdout 
       # create a temporary dictionary, which holds the full matrix for
       # printing
-      assert non_sym == None or isinstance(non_sym, float) or \
+      assert non_sym is None or isinstance(non_sym, float) or \
       isinstance(non_sym, int)
       full_mat = copy.copy(self)
       for i in self:

Bio/UniGene/__init__.py
@@ -116,7 +116,7 @@ class SequenceLine(object):
         self.mgc = ''
         self.seqtype = ''
         self.trace = ''
-        if not text==None:
+        if text is not None:
             self.text=text
             self._init_from_text(text)
 
@@ -153,7 +153,7 @@ class ProtsimLine(object):
         self.protid = ''
         self.pct = ''
         self.aln = ''
-        if not text==None:
+        if text is not None:
             self.text=text
             self._init_from_text(text)
 
@@ -182,7 +182,7 @@ class STSLine(object):
     def __init__(self,text=None):
         self.acc = ''
         self.unists = ''
-        if not text==None:
+        if text is not None:
             self.text=text
             self._init_from_text(text)
 

Bio/Blast/NCBIXML.py
@@ -423,21 +423,21 @@ class BlastParser(_XMLparser):
         """raw score of HSP
         """
         self._hsp.score = float(self._value)
-        if self._descr.score == None:
+        if self._descr.score is None:
             self._descr.score = float(self._value)
 
     def _end_Hsp_bit_score(self):
         """bit score of HSP
         """
         self._hsp.bits = float(self._value)
-        if self._descr.bits == None:
+        if self._descr.bits is None:
             self._descr.bits = float(self._value)
 
     def _end_Hsp_evalue(self):
         """expect value value of the HSP
         """
         self._hsp.expect = float(self._value)
-        if self._descr.e == None:
+        if self._descr.e is None:
             self._descr.e = float(self._value)
 
     def _end_Hsp_query_from(self):

commit db8c4b2f52c626ac2c98f4810b2bf44b9e6c3db4

Use correct variable in None check.
Broken in commit 3bbeddd96ace79070df9b952485276623c70b10f.

Bio/SCOP/__init__.py
@@ -175,7 +175,7 @@ class Scop(object):
         self._sidDict = {}
         self._sunidDict = {}
 
-        if all(h is None for h in [cla_handle, des_handle, hie_handle, dir_handle, db_handle]):
+        if all(h is None for h in [cla_handle, des_handle, hie_handle, dir_path, db_handle]):
             return
 
         if dir_path is None and db_handle is None: