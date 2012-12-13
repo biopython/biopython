4a6342258816ed0d24128290cde453b5ed913e33
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2012-12-13 16:44:35 +1100

    SearchIO hmmer3-text: Don't overload the hit_list variable with different datatypes
    No functional change, just a rename of a variable to make the code less confusing
    
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- Bio/SearchIO/HmmerIO/hmmer3_text.py
+++ Bio/SearchIO/HmmerIO/hmmer3_text.py
@@ -155,10 +155,10 @@ class Hmmer3TextParser(object):
         is_included = True
 
         # parse the hit table
-        hit_list = []
+        hit_attr_list = []
         while True:
             if not self.line:
-                return hit_list
+                return []
             elif self.line.startswith('  ------ inclusion'):
                 is_included = False
                 self.line = read_forward(self.handle)
@@ -169,10 +169,10 @@ class Hmmer3TextParser(object):
                 while True:
                     self.line = read_forward(self.handle)
                     if self.line.startswith('Internal pipeline'):
-                        assert len(hit_list) == 0
-                        return hit_list
+                        assert len(hit_attr_list) == 0
+                        return []
             elif self.line.startswith('Domain annotation for each '):
-                hit_list = self._create_hits(hit_list, qid)
+                hit_list = self._create_hits(hit_attr_list, qid)
                 return hit_list
             # entering hit results row
             # parse the columns into a list
@@ -198,7 +198,7 @@ class Hmmer3TextParser(object):
                 'description': row[9],
                 'is_included': is_included,
             }
-            hit_list.append(hit_attrs)
+            hit_attr_list.append(hit_attrs)
 
             self.line = read_forward(self.handle)
 
