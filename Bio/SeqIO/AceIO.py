269927c8697c268ac9963eaa5c5ee55a6dda0717
bow <bow@bow.web.id>
2012-12-20 16:37:05 +0100

    Refactor doctests in SeqIO and AlignIO to use Bio._utils.run_doctest

--- Bio/SeqIO/AceIO.py
+++ Bio/SeqIO/AceIO.py
@@ -110,31 +110,6 @@ def AceIterator(handle):
     #All done
 
 
-def _test():
-    """Run the Bio.SeqIO module's doctests.
-
-    This will try and locate the unit tests directory, and run the doctests
-    from there in order that the relative paths used in the examples work.
-    """
-    import doctest
-    import os
-    if os.path.isdir(os.path.join("..", "..", "Tests", "Ace")):
-        print "Running doctests..."
-        cur_dir = os.path.abspath(os.curdir)
-        os.chdir(os.path.join("..", "..", "Tests"))
-        assert os.path.isfile("Ace/consed_sample.ace")
-        doctest.testmod()
-        os.chdir(cur_dir)
-        del cur_dir
-        print "Done"
-    elif os.path.isdir(os.path.join("Tests", "Ace")):
-        print "Running doctests..."
-        cur_dir = os.path.abspath(os.curdir)
-        os.chdir(os.path.join("Tests"))
-        doctest.testmod()
-        os.chdir(cur_dir)
-        del cur_dir
-        print "Done"
-
 if __name__ == "__main__":
-    _test()
+    from Bio._utils import run_doctest
+    run_doctest()
