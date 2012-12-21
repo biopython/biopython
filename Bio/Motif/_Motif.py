c88c9b09c16d050254ecb236bc174e1bf9885ac9
Peter Cock <p.j.a.cock@googlemail.com>
2012-12-21 13:37:15 +0000

    Even float('inf') fails on Windows Python 2.5

--- Bio/Motif/_Motif.py
+++ Bio/Motif/_Motif.py
@@ -134,7 +134,12 @@ class GenericPositionMatrix(dict):
     def anticonsensus(self):
         sequence = ""
         for i in range(self.length):
-            minimum = float("inf")
+            try:
+                minimum = float("inf")
+            except ValueError:
+                # On Python 2.5 or older that was handled in C code,
+                # and failed on Windows XP 32bit
+                minimum = 1E400
             for letter in self.alphabet.letters:
                 count = self[letter][i]
                 if count < minimum:
