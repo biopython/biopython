bf445a9985ca94f2c15e7817b7fb521dc0662b08
bendmorris <ben@bendmorris.com>
2012-12-28 10:46:02 -0500

    CDAO trees/clades use their own subclass of BaseTree.Tree and BaseTree.Clade instead of Newick.Tree and Newick.Clade.

---
+++ Bio/Phylo/CDAO.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2009 by Eric Talevich (eric.talevich@gmail.com)
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""Classes corresponding to CDAO trees, also used for Nexus trees.
+
+See classes in `Bio.Nexus`: Trees.Tree, Trees.NodeData, and Nodes.Chain.
+"""
+__docformat__ = "restructuredtext en"
+
+from Bio.Phylo import BaseTree
+
+
+class Tree(BaseTree.Tree):
+    """CDAO Tree object."""
+
+    def __init__(self, root=None, rooted=False, id=None, name=None, weight=1.0):
+        BaseTree.Tree.__init__(self, root=root or Clade(),
+                rooted=rooted, id=id, name=name)
+        self.weight = weight
+
+
+class Clade(BaseTree.Clade):
+    """CDAO Clade (sub-tree) object."""
+
+    def __init__(self, branch_length=1.0, name=None, clades=None,
+                 confidence=None, comment=None):
+        BaseTree.Clade.__init__(self, branch_length=branch_length,
+                name=name, clades=clades, confidence=confidence)
+        self.comment = comment
