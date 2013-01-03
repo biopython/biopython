3ad2cc6854a8db2dc6ed5f0dadc7a2aeb1e29726
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-03 23:41:41 +1100

    added with statement import in TAIR test, should pass in python 2.5

--- Bio/Webtools/TAIR.py
+++ Bio/Webtools/TAIR.py
@@ -3,9 +3,6 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-from __future__ import with_statement
-
-
 """Functions to get Arabidopsis sequences by their AGI identifier.
 """
 from Bio.Webtools.MultipartPostHandler import MultipartPostHandler
