4e0a2d1b9434e0a1ac0f260b8a860b5a57a6eaa3
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-04 00:25:55 +1100

    reimplemented everying as module-level functions

--- Bio/Webtools/TAIR.py
+++ Bio/Webtools/TAIR.py
@@ -22,155 +22,124 @@ def _sanitise_agis(agis):
     return clean_agis
 
 
-class TAIRDirect(object):
-    """
-    Class of functions to get sequences directly from arabidopsis.org, by their
-    AGI identifier.
-    """
-    def __init__(self):
-        self.datasets = {
-            "transcript": "At_transcripts",
-            "cds": "ATH1_cds",
-            "gene": "ATH1_seq",
-            "genomic": "ATH1_seq",
-            "protein": "ATH1_pep",
-            "peptide": "ATH1_pep",
-            "coding_sequence": "ATH1_cds",
-            "genomic_locus_sequence": "ATH1_seq",
-            "upstream_500": "At_upstream_500",
-            "upstream_1000": "At_upstream_1000",
-            "upstream_3000": "At_upstream_3000",
-            "downstream_500": "At_downstream_500",
-            "downstream_1000": "At_downstream_1000",
-            "downstream_3000": "At_downstream_3000",
-            "intergenic": "At_intergenic",
-            "intron": "At_intron",
-            "3prime_utr": "ATH1_3_UTR",
-            "5prime_utr": "ATH1_5_UTR"
-            }
-        self.targets = {
-            "representative": "rep_gene",
-            "all": "both",
-            "specified": "genemodel"
-            }
+# functions to get sequences directly from arabidopsis.org
+tair_datasets = {
+    "transcript": "At_transcripts",
+    "cds": "ATH1_cds",
+    "gene": "ATH1_seq",
+    "genomic": "ATH1_seq",
+    "protein": "ATH1_pep",
+    "peptide": "ATH1_pep",
+    "coding_sequence": "ATH1_cds",
+    "genomic_locus_sequence": "ATH1_seq",
+    "upstream_500": "At_upstream_500",
+    "upstream_1000": "At_upstream_1000",
+    "upstream_3000": "At_upstream_3000",
+    "downstream_500": "At_downstream_500",
+    "downstream_1000": "At_downstream_1000",
+    "downstream_3000": "At_downstream_3000",
+    "intergenic": "At_intergenic",
+    "intron": "At_intron",
+    "3prime_utr": "ATH1_3_UTR",
+    "5prime_utr": "ATH1_5_UTR"
+    }
 
-    def _get_fasta_text(self, agis, dataset, target):
-        bad_agi_exception = ValueError(
-            "Must specify AGIs as a iterable, list or tuple"
-            )
-        agis = _sanitise_agis(agis)
-        if agis is None:
-            raise bad_agi_exception
-
-        # Check dataset
-        if dataset in self.datasets:
-            dataset = self.datasets[dataset]
-        elif dataset in self.datasets.values():
-            pass  # dataset is already equal to required value
-        else:
-            raise ValueError("%s is an invalid TAIR dataset" % dataset)
-
-        # Check dataset
-        if target in self.targets:
-            target = self.targets[target]
-        elif target in self.targets.values():
-            pass  # target is already equal to required value
-        else:
-            raise ValueError("%s is an invalid TAIR target" % target)
-
-        # Prepare http request params
-        url = 'http://www.arabidopsis.org/cgi-bin/bulk/sequences/getseq.pl'
-        params = {
-            'loci': "\r\n".join(agis),
-            'file': "",
-            "dataset": dataset,
-            "search_against": target,
-            "outputformat": "outputformat"
-            }
-
-        # Prepare and get request
-        opener = urllib2.build_opener(MultipartPostHandler)
-        raw_seq_string = opener.open(url, params).read()
-
-        # Trim returned fasta file, responce has leading non-fasta text
-        in_fasta_text = False
-        seq_string = ""
-        for line in raw_seq_string.splitlines():
-            if len(line) < 1:
-                continue
-            if line[0] == ">":
-                in_fasta_text = True
-            if in_fasta_text:
-                if line[0] == "-":
-                    break
-                seq_string += line
-                seq_string += "\n"
-        return seq_string
-
-    def get(self, agis, dataset="gene", target="rep_gene"):
-        return SeqIO.parse(
-            StringIO(self._get_fasta_text(agis, dataset, target)),
-            "fasta"
-            )
+tair_targets = {
+    "representative": "rep_gene",
+    "all": "both",
+    "specified": "genemodel"
+    }
 
 
-class TAIRNCBI(object):
-    """
-    Class of functions to get sequences from NCBI using Efetch, by their AGI
-    identifier.
-    """
-    def _agi_to_rna(self, agis):
-        rna_ids = []
-        for agi in agis:
-            try:
-                rna_ids.append(ncbi_rna[agi])
-            except LookupError:
-                next
-        return rna_ids
+def get(agis, dataset, target):
+    bad_agi_exception = ValueError(
+        "Must specify AGIs as a iterable, list or tuple"
+        )
+    agis = _sanitise_agis(agis)
+    if agis is None:
+        raise bad_agi_exception
 
-    def _agi_to_protein(self, agis):
-        protein_ids = []
-        for agi in agis:
-            try:
-                protein_ids.append(ncbi_prot[agi])
-            except LookupError:
-                next
-        return protein_ids
-
-    def get_rna_from_ncbi(self, agis):
-        Entrez.email = ""
-        entrez_handle = Entrez.efetch(
-                db="nucleotide",
-                id=",".join(self._agi_to_rna(agis)),
-                rettype="gb",
-                retmode="text"
-                )
-        return SeqIO.parse(entrez_handle, "gb")
-
-    def get_protein_from_ncbi(self, agis):
-        Entrez.email = ""
-        entrez_handle = Entrez.efetch(
-                db="protein",
-                id=",".join(self._agi_to_protein(agis)),
-                rettype="gb",
-                retmode="text"
-                )
-        return SeqIO.parse(entrez_handle, "gb")
-
-
-def get(agis, dataset="gene", target="rep_gene"):
-    tair = TAIRDirect()
-    return tair.get(_sanitise_agis(agis), dataset, target)
-
-
-def get_from_ncbi(agis, mode):
-    tair = TAIRNCBI()
-    if mode.lower() in ["rna", "mrna", "transcript"]:
-        return tair.get_rna_from_ncbi(agis)
-    elif mode.lower() in ["protein", "peptide", "amino acid", "aa"]:
-        return tair.get_protein_from_ncbi(agis)
+    # Check dataset
+    if dataset in self.datasets:
+        dataset = self.datasets[dataset]
+    elif dataset in self.datasets.values():
+        pass  # dataset is already equal to required value
     else:
-        raise ValueError("Invalid mode '%s' for get_from_ncbi()" % mode)
+        raise ValueError("%s is an invalid TAIR dataset" % dataset)
+
+    # Check dataset
+    if target in self.targets:
+        target = self.targets[target]
+    elif target in self.targets.values():
+        pass  # target is already equal to required value
+    else:
+        raise ValueError("%s is an invalid TAIR target" % target)
+
+    # Prepare http request params
+    url = 'http://www.arabidopsis.org/cgi-bin/bulk/sequences/getseq.pl'
+    params = {
+        'loci': "\r\n".join(agis),
+        'file': "",
+        "dataset": dataset,
+        "search_against": target,
+        "outputformat": "outputformat"
+        }
+
+    # Prepare and get request
+    opener = urllib2.build_opener(MultipartPostHandler)
+    raw_seq_string = opener.open(url, params).read()
+
+    # Trim returned fasta file, responce has leading non-fasta text
+    in_fasta_text = False
+    seq_string = ""
+    for line in raw_seq_string.splitlines():
+        if len(line) < 1:
+            continue
+        if line[0] == ">":
+            in_fasta_text = True
+        if in_fasta_text:
+            if line[0] == "-":
+                break
+            seq_string += line
+            seq_string += "\n"
+    return SeqIO.parse(StringIO(seq_string), "fasta")
+
+
+# Functions to get sequences from NCBI
+def get_rna_from_ncbi(agis):
+    agis = _sanitise_agis(agis)
+    rna_ids = []
+    for agi in agis:
+        try:
+            rna_ids.append(ncbi_rna[agi])
+        except LookupError:
+            next
+    Entrez.email = ""
+    entrez_handle = Entrez.efetch(
+            db="nucleotide",
+            id=",".join(rna_ids),
+            rettype="gb",
+            retmode="text"
+            )
+    return SeqIO.parse(entrez_handle, "gb")
+
+
+def get_protein_from_ncbi(agis):
+    agis = _sanitise_agis(agis)
+    protein_ids = []
+    for agi in agis:
+        try:
+            protein_ids.append(ncbi_prot[agi])
+        except LookupError:
+            next
+    Entrez.email = ""
+    entrez_handle = Entrez.efetch(
+            db="protein",
+            id=",".join(protein_id),
+            rettype="gb",
+            retmode="text"
+            )
+    return SeqIO.parse(entrez_handle, "gb")
 
 
 ncbi_prot = {
