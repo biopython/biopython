453b3bb96945ae013df7c32aaa2adf2d2fe79e3c
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-04 11:31:04 +1100

    made MultipartPoster, not tested yet

--- Bio/Webtools/MultipartPoster.py
+++ Bio/Webtools/MultipartPoster.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-####
-# 02/2006 Will Holcomb <wholcomb@gmail.com>
-# 
+# This borrrows heavily from the MultipartPostHandler module by Will Holcomb
+# <wholcomb@gmail.com>
+
+
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
@@ -13,72 +14,44 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
-"""
-Usage:
-  Enables the use of multipart/form-data for posting forms
-
-Inspirations:
-  Upload files in python:
-    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/146306
-  urllib2_file:
-    Fabien Seisen: <fabien@seisen.org>
-
-Example:
-  import MultipartPostHandler, urllib2, cookielib
-
-  cookies = cookielib.CookieJar()
-  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
-                                MultipartPostHandler.MultipartPostHandler)
-  params = { "username" : "bob", "password" : "riviera",
-             "file" : open("filename", "rb") }
-  opener.open("http://wwww.bobsite.com/upload/", params)
-
-Further Example:
-  The main function of this file is a sample which downloads a page and
-  then uploads it to the W3C validator.
-"""
 
 import urllib
 import urllib2
-import mimetools, mimetypes
-import os, stat
+import mimetools
+import mimetypes
+import os
+import stat
 
 class Callable:
     def __init__(self, anycallable):
         self.__call__ = anycallable
 
-# Controls how sequences are uncoded. If true, elements may be given multiple values by
-#  assigning a sequence.
-doseq = 1
 
-class MultipartPostHandler(urllib2.BaseHandler):
+class MultipartPoster(urllib2.BaseHandler):
     handler_order = urllib2.HTTPHandler.handler_order - 10 # needs to run first
 
     def http_request(self, request):
         data = request.get_data()
         if data is not None and type(data) != str:
-            v_files = []
-            v_vars = []
+            req_files = []
+            req_vars = []
             try:
                  for(key, value) in data.items():
                      if type(value) == file:
-                         v_files.append((key, value))
+                        req_files.append((key, value))
                      else:
-                         v_vars.append((key, value))
+                         req_vars.append((key, value))
             except TypeError:
-                systype, value, traceback = sys.exc_info()
-                raise TypeError, "not a valid non-string sequence or mapping object", traceback
-
-            if len(v_files) == 0:
-                data = urllib.urlencode(v_vars, doseq)
-            else:
-                boundary, data = self.multipart_encode(v_vars, v_files)
-                contenttype = 'multipart/form-data; boundary=%s' % boundary
-                if(request.has_header('Content-Type')
-                   and request.get_header('Content-Type').find('multipart/form-data') != 0):
-                    print "Replacing %s with %s" % (request.get_header('content-type'), 'multipart/form-data')
-                request.add_unredirected_header('Content-Type', contenttype)
-
+                raise TypeError(
+                    "not a valid non-string sequence or mapping object"
+                    )
+            #if len(req_files) == 0:
+            #    # Below, True == do the encoding sequentially
+            #    data = urllib.urlencode(req_vars, True) 
+            #else:
+            boundary, data = self.multipart_encode(req_vars, req_files)
+            contenttype = 'multipart/form-data; boundary=%s' % boundary
+            request.add_unredirected_header('Content-Type', contenttype)
             request.add_data(data)
         return request
 
@@ -92,13 +65,13 @@ class MultipartPostHandler(urllib2.BaseHandler):
             buffer += 'Content-Disposition: form-data; name="%s"' % key
             buffer += '\r\n\r\n' + value + '\r\n'
         for(key, fd) in files:
-            file_size = os.fstat(fd.fileno())[stat.ST_SIZE]
             filename = fd.name.split('/')[-1]
-            contenttype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+            contenttype = mimetypes.guess_type(filename)[0] 
+            if not contenttype:
+                contenttype = 'application/octet-stream'
             buffer += '--%s\r\n' % boundary
             buffer += 'Content-Disposition: form-data; name="%s"; filename="%s"\r\n' % (key, filename)
             buffer += 'Content-Type: %s\r\n' % contenttype
-            # buffer += 'Content-Length: %s\r\n' % file_size
             fd.seek(0)
             buffer += '\r\n' + fd.read() + '\r\n'
         buffer += '--%s--\r\n\r\n' % boundary
@@ -107,11 +80,17 @@ class MultipartPostHandler(urllib2.BaseHandler):
 
     https_request = http_request
 
+
+
+
+
+
+
 def main():
     import tempfile, sys
 
     validatorURL = "http://validator.w3.org/check"
-    opener = urllib2.build_opener(MultipartPostHandler)
+    opener = urllib2.build_opener(MultipartPoster)
 
     def validateFile(url):
         temp = tempfile.mkstemp(suffix=".html")
