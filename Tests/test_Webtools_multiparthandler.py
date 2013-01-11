6b17c9a2d96e0338dbc147910460dc2ef068ec77
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-11 11:13:13 +1100

    pulled absence of bytes() in py2.5 fix from multiparthandler

--- Tests/test_Webtools_multiparthandler.py
+++ Tests/test_Webtools_multiparthandler.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2o12 Kevin Murray k.d.murray.91@gmail.com
+# Copyright (c) 2012 Kevin Murray k.d.murray.91@gmail.com
 # This test suite is adapted directly from multiparthandler
 # multiparthandler is licensed under the LGPL v3
 
@@ -26,8 +26,11 @@ class multiparthandler_t(unittest.TestCase):
         # Some inter-version portability here.
         # Python 2 returns a str from .read(), py3 returns bytes. if we convert
         # this to bytes, then we can reproducibly decode it to a string.
-        self.test_html = bytes(self.opener.open(self.test_url).read()).\
+        try:
+            self.test_html = bytes(self.opener.open(self.test_url).read()).\
                 decode("UTF-8")
+        except NameError:  # If the bytes() fn is not found, i.e. py2.5
+            self.test_html = self.opener.open(self.test_url).read()
 
     def test_post_as_file(self):
         tmp_fd, tmp_fn = tempfile.mkstemp(suffix=".html")
@@ -39,8 +42,12 @@ class multiparthandler_t(unittest.TestCase):
             "uploaded_file": open(tmp_fn, "r")
             }
         # Portablity hack as above
-        response_html = bytes(self.opener.open(self.validator_url, params).
-                read()).decode("UTF-8")
+        try:
+            response_html = bytes(self.opener.open(self.validator_url, params).
+                    read()).decode("UTF-8")
+        except NameError:
+            response_html = self.opener.open(self.validator_url, params)\
+                    .read()
         os.remove(tmp_fn)
         re_match = re.search(self.validator_result_re, response_html)
         self.assertTrue(re_match is not None)
@@ -52,8 +59,12 @@ class multiparthandler_t(unittest.TestCase):
             "fragment": self.test_html
             }
         # Portablity hack as above
-        response_html = bytes(self.opener.open(self.validator_url, params).
-                read()).decode("UTF-8")
+        try:
+            response_html = bytes(self.opener.open(self.validator_url, params).
+                    read()).decode("UTF-8")
+        except NameError:
+            response_html = self.opener.open(self.validator_url, params)\
+                    .read()
         re_match = re.search(self.validator_result_re, response_html)
         self.assertTrue(re_match is not None)
 
