2693033c6993bfaf1300983b356f4d7d10462579
Antony Lee <anntzer.lee@gmail.com>
2013-01-14 16:06:08 -0800

    Initial work on new restriction analysis library.

---
+++ Bio/Restriction2/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2013 Antony Lee
+# Inspired from Fredecic Sohm's Bio.Restriction.
+# All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+"""Restriction analysis library.
+"""
