95a63b1ee8e6beaa7402d998c20beada7e4dd945
bendmorris <ben@bendmorris.com>
2013-02-13 12:26:47 -0500

    Adding additional note about using Redland storage objects in CDAOIO.

--- Bio/Phylo/CDAOIO.py
+++ Bio/Phylo/CDAOIO.py
@@ -10,6 +10,11 @@
 
 This is an RDF format that conforms to the Comparative Data Analysis Ontology (CDAO).
 See: http://www.evolutionaryontology.org/cdao
+
+The CDAOIO.Parser, in addition to parsing text files, can also parse directly
+from a triple store that implements the Redland storage interface; similarly,
+the CDAOIO.Writer can store triples in a triple store instead of serializing
+them to a file.
 """
 
 __docformat__ = "restructuredtext en"
