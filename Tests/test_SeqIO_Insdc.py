07639dde32083f4f024616292a5c736e85770a4e
peterjc <p.j.a.cock@googlemail.com>
2013-03-12 14:37:23 +0000

    More EMBL testing, identified some more issues to look at...

--- Tests/test_SeqIO_Insdc.py
+++ Tests/test_SeqIO_Insdc.py
@@ -4,8 +4,11 @@
 # as part of this package.
 
 import unittest
+from StringIO import StringIO
 
 from Bio import SeqIO
+
+from seq_tests_common import compare_record
 
 class TestEmbl(unittest.TestCase):
     def test_annotation1(self):
@@ -24,6 +27,34 @@ class TestEmbl(unittest.TestCase):
                          ['JP 2005522996-A/12', 'test-data',
                           'lot and lots of keywords for this example',
                           'multi-line keywords'])
+
+class TestEmblRewrite(unittest.TestCase):
+    def check_rewrite(self, filename):
+        old = SeqIO.read(filename, "embl")
+
+        #TODO - Check these properties:
+        old.dbxrefs = []
+        old.annotations['accessions'] = old.annotations['accessions'][:1]
+        del old.annotations['references']
+
+        buffer = StringIO()
+        self.assertEqual(1, SeqIO.write(old, buffer, "embl"))
+        buffer.seek(0)
+        new = SeqIO.read(buffer, "embl")
+        
+        self.assertTrue(compare_record(old, new))
+
+    def test_annotation1(self):
+        """Check writing-and-parsing EMBL file (1)."""
+        self.check_rewrite("EMBL/TRBG361.embl")
+
+    def test_annotation2(self):
+        """Check writing-and-parsing EMBL file (2)."""
+        self.check_rewrite("EMBL/DD231055_edited.embl")
+
+    def test_annotation3(self):
+        """Check writing-and-parsing EMBL file (3)."""
+        self.check_rewrite("EMBL/AE017046.embl")
 
 
 if __name__ == "__main__":
