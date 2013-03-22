fff50874931434d54bdaaccd600e19a5c626c5c6
peterjc <p.j.a.cock@googlemail.com>
2013-03-22 14:07:47 +0000

    Make Jython DTD parsing check specific (in case they fix this)

--- Tests/test_Entrez_online.py
+++ Tests/test_Entrez_online.py
@@ -23,11 +23,13 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 if os.name == 'java':
-    from Bio import MissingExternalDependencyError
-    #This is a slight miss-use of MissingExternalDependencyError,
-    #but it will do in the short term to skip this unit test on Jython
-    raise MissingExternalDependencyError("The Bio.Entrez XML parser fails "
-        "on Jython, see http://bugs.jython.org/issue1447")
+    try:
+        from xml.parsers.expat import XML_PARAM_ENTITY_PARSING_ALWAYS
+        del XML_PARAM_ENTITY_PARSING_ALWAYS
+    except ImportError:
+        from Bio import MissingPythonDependencyError
+        raise MissingPythonDependencyError("The Bio.Entrez XML parser fails on "
+                                  "Jython, see http://bugs.jython.org/issue1447")
 
 
 #This lets us set the email address to be sent to NCBI Entrez:
