3f5d4c442424a7ca33ae0bafa60c840e80ae2fda
peterjc <p.j.a.cock@googlemail.com>
2013-03-28 17:49:40 +0000

    Test command line wrapper handling of stdout/stderr

---
+++ Tests/test_Application.py
@@ -0,0 +1,64 @@
+# Copyright 2013 by Peter Cock.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""Bio.Application related tests for command line application wrappers.
+
+This is intended to check generic things like argument parsing, and
+stdin/stdout/stderr handling.
+"""
+
+import os
+import unittest
+
+from Bio.Application import AbstractCommandline, _Argument
+
+class EchoApp(AbstractCommandline):
+    def __init__(self, cmd="echo", **kwargs):
+        self.parameters = [_Argument(["text"], "Text to echo")]
+        AbstractCommandline.__init__(self, cmd, **kwargs)
+
+class TestApp(unittest.TestCase):
+    def test_echo(self):
+        cline = EchoApp(text="Hello World")
+        stdout, stderr = cline()
+        self.assertEqual(stderr, "")
+        self.assertEqual(stdout, "Hello World\n")
+
+    def test_echo_capture_both(self):
+        cline = EchoApp(text="Hello World")
+        stdout, stderr = cline(stdout=True, stderr=True)
+        self.assertEqual(stderr, "")
+        self.assertEqual(stdout, "Hello World\n")
+
+    def test_echo_capture_stdout(self):
+        cline = EchoApp(text="Hello World")
+        stdout, stderr = cline(stdout=True, stderr=False)
+        self.assertEqual(stderr, None)
+        self.assertEqual(stdout, "Hello World\n")
+
+    #Same failure on Linux, Mac OS X:
+    # ApplicationError: Command 'echo Hello World' returned non-zero exit status 1,
+    # '/bin/sh: line 0: echo: write error: Bad file descriptor'
+    #And on Windows XP (with cygwin):
+    # ApplicationError: Command 'echo Hello World' returned non-zero exit status 1,
+    # 'echo: write error: Bad file descriptor' 
+    #def test_echo_capture_stderr(self):
+    #    cline = EchoApp(text="Hello World")
+    #    stdout, stderr = cline(stdout=False, stderr=True)
+    #    self.assertEqual(stderr, "")
+    #    self.assertEqual(stdout, None)
+
+    #Same failure on Linux, Mac OS X, and Windows XP (with cygwin):
+    # ApplicationError: Command 'echo Hello World' returned non-zero exit status 1
+    #def test_echo_capture_neither(self):
+    #    cline = EchoApp(text="Hello World")
+    #    stdout, stderr = cline(stdout=False, stderr=False)
+    #    self.assertEqual(stderr, None)
+    #    self.assertEqual(stdout, None)
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity = 2)
+    unittest.main(testRunner=runner)
