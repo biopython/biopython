54acb4079a75211906875f4a0c441930736bc86e
Iddo Friedberg <idoerg@gmail.com>
2013-05-07 13:03:32 -0400

    new file:   __init__.py

---
+++ Bio/UniProtGOA/__init__.py
@@ -0,0 +1,20 @@
+
+###########################################
+"""
+Parsers for the GAF, GPA and GPI formats
+from UniProt-GOA.
+
+Uniprot-GOA README:
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/README
+
+gp_association (GPA format) README:
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/gp_association_readme
+
+gp_information (GPI format) README:
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/gp_information_readme
+
+(c) 2013 Iddo Friedberg idoerg@gmail.com
+http://iddo-friedberg.net
+Distributed under Biopython license.
+"""
+###########################################
