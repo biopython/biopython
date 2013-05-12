dfe53ca907d700a27836a0fe98bf75377e52369f
Saket Choudhary <saketkc@gmail.com>
2013-05-12 23:29:38 +0530

    Adding samtools wrapper. Need to write unit tests

--- Bio/Sequencing/Applications/__init__.py
+++ Bio/Sequencing/Applications/__init__.py
@@ -3,10 +3,27 @@
 from _Novoalign import NovoalignCommandline
 from _bwa import BwaIndexCommandline, BwaAlignCommandline, BwaSamseCommandline
 from _bwa import BwaSampeCommandline, BwaBwaswCommandline
+from _samtools import SamtoolsViewCommandline,SamtoolsCalmdCommandline, SamtoolsCatCommandline, SamtoolsFaidxCommandline, SamtoolsFixmateCommandline
+from _samtools import SamtoolsIdxstatsCommandline, SamtoolsIndexCommandline, SamtoolsMergeCommandline, SamtoolsMpileupCommandline
+from _samtools import SamtoolsPhaseCommandline, SamtoolsReheaderCommandline, SamtoolsRmdupCommandline, SamtoolsSortCommandline, SamtoolsTargetcutCommandline
 #Make this explicit, then they show up in the API docs
 __all__ = ["BwaIndexCommandline",
            "BwaAlignCommandline",
            "BwaSamseCommandline",
            "BwaSampeCommandline",
            "BwaBwaswCommandline",
+           "SamtoolsViewCommandline",
+           "SamtoolsCalmdCommandline",
+           "SamtoolsCatCommandline",
+           "SamtoolsFaidxCommandline",
+           "SamtoolsFixmateCommandline",
+           "SamtoolsIdxstatsCommandline",
+           "SamtoolsIndexCommandline",
+           "SamtoolsMergeCommandline",
+           "SamtoolsMpileupCommandline",
+           "SamtoolsPhaseCommandline",
+           "SamtoolsReheaderCommandline",
+           "SamtoolsRmdupCommandline",
+           "SamtoolsSortCommandline",
+           "SamtoolsTargetcutCommandline"
            ]
