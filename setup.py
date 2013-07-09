0f821c5b69ab5f6b888d36fb29b1ba3b51ce8590
chapmanb <chapmanb@50mail.com>
2013-07-09 05:36:26 -0400

    Enable installation with pip using Python 3.3. Uses workarounds from numpy's setup.py. Thanks to @lgautier

--- setup.py
+++ setup.py
@@ -89,6 +89,20 @@ elif sys.version_info[0] == 3:
         if not os.path.isdir("build"):
             os.mkdir("build")
         do2to3.main(".", python3_source)
+    # Ugly hack to make pip work with Python 3, from 2to3 numpy setup:
+    # https://github.com/numpy/numpy/blob/bb726ca19f434f5055c0efceefe48d89469fcbbe/setup.py#L172
+    # Explanation: pip messes with __file__ which interacts badly with the
+    # change in directory due to the 2to3 conversion. Therefore we restore
+    # __file__ to what it would have been otherwise.
+    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    global __file__
+    __file__ = os.path.join(os.curdir, os.path.basename(__file__))
+    if '--egg-base' in sys.argv:
+        # Change pip-egg-info entry to absolute path, so pip can find it
+        # after changing directory.
+        idx = sys.argv.index('--egg-base')
+        if sys.argv[idx + 1] == 'pip-egg-info':
+            sys.argv[idx + 1] = os.path.join(local_path, 'pip-egg-info')
 
 # use setuptools, falling back on core modules if not found
 try:
