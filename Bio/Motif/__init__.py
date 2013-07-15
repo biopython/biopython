74fe3dd40c6f1f43032fa490a918abf052fd5c0e
peterjc <p.j.a.cock@googlemail.com>
2013-07-15 14:01:13 +0100

    Deprecated Bio.Motif as discussed on the mailing list

--- Bio/Motif/__init__.py
+++ Bio/Motif/__init__.py
@@ -2,11 +2,11 @@
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-"""Tools for sequence motif analysis (OBSOLETE, see Bio.motifs instead).
+"""Tools for sequence motif analysis (DEPRECATED, see Bio.motifs instead).
 
-This module (Bio.Motif) is now obsolete, and will be deprecated and
-removed in a future release of release of Biopython. Please use the
-new module Bio.motifs instead.
+This module (Bio.Motif) has been deprecated and will be removed in a
+future release of release of Biopython. Please use the new module
+Bio.motifs instead.
 
 This contains the core Motif class containing various I/O methods as
 well as methods for motif comparisons and motif searching in sequences.
@@ -14,14 +14,14 @@ It also inlcudes functionality for parsing AlignACE and MEME programs.
 """
 
 import warnings
-warnings.warn("The module Bio.Motif is now obsolete, and will be"
-              "deprecated and removed in a future release of"
-              "release of Biopython. As a replacement for Bio.Motif,"
-              "please use the new module Bio.motifs instead. Please"
-              "be aware that though the functionality of Bio.Motif"
-              "is retained (and extended) in Bio.motifs, usage may"
+from Bio import BiopythonDeprecationWarning
+warnings.warn("The module Bio.Motif has been depreacted and will be "
+              "removed in a future release of Biopython. Instead "
+              "please use the new module Bio.motifs instead. Please "
+              "be aware that though the functionality of Bio.Motif "
+              "is retained (and extended) in Bio.motifs, usage may "
               "be different.",
-              PendingDeprecationWarning)
+              BiopythonDeprecationWarning)
 
 
 from Bio.Motif._Motif import Motif
