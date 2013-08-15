771c1ed23bbb39dcf37805b4cb7bb23ffcb0c46a
Eric Talevich <eric.talevich@gmail.com>
2013-08-15 08:54:52 -0700

    test_Fasttree_tool: Don't use out= option (-out flag) in commands
    Older versions of FastTree don't have the -out option, but work with the wrapper
    otherwise. This change lets the test pass on more platforms where the wrapper is
    usable.
    
    Also cleaned up some extraneous whitespace and PEP8 issues.

--- Tests/test_Fasttree_tool.py
+++ Tests/test_Fasttree_tool.py
@@ -1,7 +1,7 @@
-# Copyright 2013 by Nate Sutton.  Based on test_Clustalw_tool.py by 
+# Copyright 2013 by Nate Sutton.  Based on test_Clustalw_tool.py by
 # Peter Cock.  Example code used from Biopython's Phylo cookbook by
-# Eric Talevich.  All rights reserved.  This code is part of the 
-# Biopython distribution and governed by its license.  Please see 
+# Eric Talevich.  All rights reserved.  This code is part of the
+# Biopython distribution and governed by its license.  Please see
 # the LICENSE file that should have been included as part of this package.
 
 from Bio import MissingExternalDependencyError
@@ -9,6 +9,8 @@ from Bio import MissingExternalDependencyError
 import sys
 import os
 import itertools
+from StringIO import StringIO
+
 from Bio import SeqIO
 from Bio import Phylo
 from Bio.Phylo.Applications import FastTreeCommandline
@@ -21,7 +23,7 @@ from Bio.Application import ApplicationError
 os.environ['LANG'] = 'C'
 
 fasttree_exe = None
-if sys.platform=="win32":
+if sys.platform == "win32":
     try:
         #This can vary depending on the Windows language.
         prog_files = os.environ["PROGRAMFILES"]
@@ -29,9 +31,9 @@ if sys.platform=="win32":
         prog_files = r"C:\Program Files (x86)"
 
     # A default fasttree file path of "C:\Program Files (x86)\Fasttree.exe"
-    # was chosen here but users can alter the path according to where 
+    # was chosen here but users can alter the path according to where
     # fasttree is located on their systems
-    
+
     likely_dirs = ["Fasttree", "fasttree", "FastTree"]
     likely_exes = ["Fasttree.exe", "fasttree.exe", "FastTree.exe"]
     for folder in likely_dirs:
@@ -46,17 +48,18 @@ else:
     import commands
     # Checking the -help argument
     output = commands.getoutput("fasttree -help")
-    #Since "is not recognized" may be in another language, try and be sure this is
-    #really the fasttree tool's output
+    # Since "is not recognized" may be in another language, try and be sure this
+    # is really the fasttree tool's output
     fasttree_found = False
     if "is not recognized" not in output and "protein_alignment" in output \
     and "nucleotide_alignment" in output:
         fasttree_exe = "fasttree"
 
 if not fasttree_exe:
-    raise MissingExternalDependencyError(\
-        "Install fasttree and correctly set the file path to the program if you want to use it from Biopython.")
-    
+    raise MissingExternalDependencyError(
+        "Install fasttree and correctly set the file path to the program "
+        "if you want to use it from Biopython.")
+
 #################################################################
 
 print "Checking error conditions"
@@ -64,9 +67,8 @@ print "========================="
 
 print "Empty file"
 input_file = "does_not_exist.fasta"
-output_file = "test.fasta"
 assert not os.path.isfile(input_file)
-cline = FastTreeCommandline(fasttree_exe, input=input_file, out=output_file)
+cline = FastTreeCommandline(fasttree_exe, input=input_file)
 try:
     stdout, stderr = cline()
     assert False, "Should have failed, returned:\n%s\n%s" % (stdout, stderr)
@@ -77,16 +79,16 @@ except ApplicationError, err:
     assert "Cannot open sequence file" in str(err) or \
            "Cannot open input file" in str(err) or \
            "non-zero exit status" in str(err), str(err)
-            
+
 print
 print "Single sequence"
 input_file = "Fasta/f001"
 assert os.path.isfile(input_file)
 assert len(list(SeqIO.parse(input_file,"fasta")))==1
-cline = FastTreeCommandline(fasttree_exe, input=input_file, out=output_file)
+cline = FastTreeCommandline(fasttree_exe, input=input_file)
 try:
     stdout, stderr = cline()
-    if "Unique: 1/1" in (stdout + stderr):
+    if "Unique: 1/1" in stderr:
         print "Failed (good)"
     else:
         assert False, "Should have failed, returned:\n%s\n%s" % (stdout, stderr)
@@ -98,7 +100,7 @@ print
 print "Invalid sequence"
 input_file = "Medline/pubmed_result1.txt"
 assert os.path.isfile(input_file)
-cline = FastTreeCommandline(fasttree_exe, input=input_file, out=output_file)
+cline = FastTreeCommandline(fasttree_exe, input=input_file)
 try:
     stdout, stderr = cline()
     assert False, "Should have failed, returned:\n%s\n%s" % (stdout, stderr)
@@ -114,7 +116,7 @@ except ApplicationError, err:
            or "not produced" in str(err) \
            or "No sequences in file" in str(err) \
            or "non-zero exit status " in str(err), str(err)
-           
+
 #################################################################
 print
 print "Checking normal situations"
@@ -123,37 +125,27 @@ print "=========================="
 #Create a temp fasta file with a space in the name
 temp_filename_with_spaces = "Clustalw/temp horses.fasta"
 handle = open(temp_filename_with_spaces, "w")
-SeqIO.write(SeqIO.parse("Phylip/hennigian.phy","phylip"),handle, "fasta")
+SeqIO.write(SeqIO.parse("Phylip/hennigian.phy","phylip"), handle, "fasta")
 handle.close()
 
-for input_file, output_file in [
-    ("Quality/example.fasta", "temp_test.tree"),
-    ("Clustalw/temp horses.fasta", "temp_test.tree"),
-    ]:
-    input_records = SeqIO.to_dict(SeqIO.parse(input_file,"fasta"),
-                                  lambda rec : rec.id)
-    if os.path.isfile(output_file):
-        os.remove(output_file)
+for input_file in ["Quality/example.fasta", "Clustalw/temp horses.fasta"]:
+    input_records = SeqIO.to_dict(SeqIO.parse(input_file,"fasta"))
     print
     print "Calling fasttree on %s (with %i records)" \
           % (repr(input_file), len(input_records))
-    print "using output file %s" % repr(output_file)
 
     #Any filesnames with spaces should get escaped with quotes automatically.
     #Using keyword arguments here.
-    cline = _Fasttree.FastTreeCommandline(fasttree_exe,
-                                input=input_file,
-                                out=output_file)        
-    assert str(eval(repr(cline)))==str(cline) 
-    
-    #print cline
+    cline = _Fasttree.FastTreeCommandline(fasttree_exe, input=input_file)
+    assert str(eval(repr(cline)))==str(cline)
+
     out, err = cline()
-    assert err.strip().startswith("FastTree")   
-            
+    assert err.strip().startswith("FastTree")
+
     print
     print "Checking generation of tree terminals"
-    tree = Phylo.read(output_file, 'newick')
-    
+    tree = Phylo.read(StringIO(out), 'newick')
+
     def lookup_by_names(tree):
         names = {}
         for clade in tree.find_clades():
@@ -164,10 +156,10 @@ for input_file, output_file in [
         return names
 
     names = lookup_by_names(tree)
-    
+
     assert len(names) > 0.0
-    print "Success"    
-    
+    print "Success"
+
     print
     print "Checking distances between tree terminals"
     def terminal_neighbor_dists(self):
@@ -178,13 +170,11 @@ for input_file, output_file in [
             return itertools.izip(pairs[0], pairs[1])
         return [self.distance(*i) for i in
                 generate_pairs(self.find_clades(terminal=True))]
-    
+
     for dist in terminal_neighbor_dists(tree):
         assert dist > 0.0
-        
+
     print "Success"
-    
-    os.remove(output_file)
 
 print
 print "Done"
