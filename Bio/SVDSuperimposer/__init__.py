c67e09848e05c6246b9ce3a43167f25bae444af8
peterjc <p.j.a.cock@googlemail.com>
2013-09-29 12:34:43 +0100

    Step 3, restore SVDSuperimposer module docstring

--- Bio/SVDSuperimposer/__init__.py
+++ Bio/SVDSuperimposer/__init__.py
@@ -2,6 +2,12 @@
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
+"""
+SVDSuperimposer finds the best rotation and translation to put
+two point sets on top of each other (minimizing the RMSD). This is
+eg. useful to superimpose crystal structures. SVD stands for singular
+value decomposition, which is used in the algorithm.
+"""
 
 from __future__ import print_function
 
