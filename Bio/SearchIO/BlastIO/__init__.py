c52151b9e5dce0e38d16724592fe3f8529343492
peterjc <p.j.a.cock@googlemail.com>
2013-09-29 10:49:59 +0100

    Fix an 'import *'
    Interestingly Jython 2.7 (beta1) regards this as a syntax
    error - although the rest of the buildbots seemed happy:
    
    SyntaxError: 'import *' not allowed with 'from .'

--- Bio/SearchIO/BlastIO/__init__.py
+++ Bio/SearchIO/BlastIO/__init__.py
@@ -395,9 +395,9 @@ The blast-text parser provides the following object attributes:
 
 """
 
-from .blast_tab import *
-from .blast_xml import *
-from .blast_text import *
+from .blast_tab import BlastTabParser, BlastTabIndexer, BlastTabWriter
+from .blast_xml import BlastTextParser
+from .blast_text import BlastXmlParser, BlastXmlIndexer, BlastXmlWriter
 
 
 # if not used as a module, run the doctest
