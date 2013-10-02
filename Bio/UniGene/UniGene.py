f1311157064a04d2dd0192c81e066e0fa32bb5f2
peterjc <p.j.a.cock@googlemail.com>
2013-10-02 17:56:00 +0100

    Remove licence duplication, add author copyright line

--- Bio/UniGene/UniGene.py
+++ Bio/UniGene/UniGene.py
@@ -1,26 +1,7 @@
+# Copyright 2001 by Katharine Lindner. All Rights Reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-#
-
-
-# Permission to use, copy, modify, and distribute this software and
-# its documentation with or without modifications and for any purpose
-# and without fee is hereby granted, provided that any copyright
-# notices appear in all copies and that both those copyright notices
-# and this permission notice appear in supporting documentation, and
-# that the names of the contributors or copyright holders not be used
-# in advertising or publicity pertaining to distribution of the software
-# without specific prior permission.
-#
-# THE CONTRIBUTORS AND COPYRIGHT HOLDERS OF THIS SOFTWARE DISCLAIM ALL
-# WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL THE
-# CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
-# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
-# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 from __future__ import print_function
 
