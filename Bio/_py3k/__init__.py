72e013502322ba39f6ed7fe6d0fc8cb08a223306
Peter Cock <p.j.a.cock@googlemail.com>
2013-10-05 21:16:34 +0100

    Handle some (changed) built in functions via Bio._py3k

--- Bio/_py3k/__init__.py
+++ Bio/_py3k/__init__.py
@@ -1,13 +1,45 @@
-# Copyright 2010 by Peter Cock.  All rights reserved.
+# Copyright 2010-2013 by Peter Cock.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-"""Python 3 compatibility tools (PRIVATE)."""
+"""Python 3 compatibility tools (PRIVATE).
 
+We currently have lines like this under Python 2 in order
+to use iterator based zip, map and filter:
+
+    from future_builtins import zip
+
+There is no similar option for range yet, other than:
+
+    range = xrange
+
+or:
+
+    from __builtin__ import xrange as range
+
+Under Python 3 this imports need to be removed. Also, deliberate
+importing of built in functions like open changes from Python 2:
+
+    from __builtin__ import open
+
+to this under Python 3:
+
+    from builtins import open
+
+Instead, we can do this under either Python 2 or 3:
+
+    from Bio._py3k import open
+    from Bio._py3k import zip
+
+Once we drop support for Python 2, the whole of Bio._py3k will
+go away.
+"""
 import sys
 
 if sys.version_info[0] >= 3:
-    #Python 3 code (which will be converted using 2to3 script)
+    #Code for Python 3
+    from builtins import open, zip, map, filter, range
+
     import codecs
 
     _bytes_to_string = lambda b: b.decode() # bytes to unicode string
@@ -84,6 +116,11 @@ if sys.version_info[0] >= 3:
 
 else:
     #Python 2 code
+    from __builtin__ import open
+
+    #Import Python3 like iterator functions:
+    from future_builtins import zip, map, filter
+    from __builtin__ import xrange as range
 
     _bytes_to_string = lambda b: b # bytes to string, i.e. do nothing
     _string_to_bytes = lambda s: str(s) # str (or unicode) to bytes string
