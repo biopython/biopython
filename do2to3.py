9b7472573b83642068919fc5544dc4f1f1bf158b
Peter Cock <p.j.a.cock@googlemail.com>
2013-10-06 13:25:04 +0100

    Don't need the 2to3 raw_input fixer anymore.
    $ 2to3 --no-diffs -n -w -f raw_input Bio BioSQL Tests Doc Scripts
    RefactoringTool: No files need to be modified.

--- do2to3.py
+++ do2to3.py
@@ -66,7 +66,7 @@ def run2to3(filenames):
                     "--fix=import",
                     "--fix=imports",
                     #"--fix=imports2",
-                    #"--fix=input", -- we avoid the input function
+                    #"--fix=input", -- we avoid the Python 2 input function
                     #"--fix=intern", -- we're not using the intern function
                     "--fix=isinstance",
                     "--fix=itertools",
@@ -83,7 +83,7 @@ def run2to3(filenames):
                     #"--fix=paren", -- already applied
                     #"--fix=print", -- we avoid the print statement
                     #"--fix=raise", -- we avoid old style raise exception
-                    "--fix=raw_input",
+                    #"--fix=raw_input", -- handled via Bio._py3k.builtins.input
                     #"--fix=reduce", -- already using 'from functools import reduce'
                     #"--fix=renames", -- already switched sys.maxint to sys.maxsize
                     #"--fix=repr", -- we avoid the old style back-ticks
