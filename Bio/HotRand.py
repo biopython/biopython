3cab931f351d2990052c6478da6831f2cf8926ab
Peter Cock <p.j.a.cock@googlemail.com>
2013-10-20 16:32:12 +0100

    PEP8 formatting in Bio.HotRand; Use increment operator

--- Bio/HotRand.py
+++ Bio/HotRand.py
@@ -16,13 +16,12 @@ import warnings
 warnings.warn("The HotRand module is deprecated and likely to be removed in a future release of Biopython. Please use an alternative RNG.", BiopythonDeprecationWarning)
 
 
-def byte_concat( text ):
+def byte_concat(text):
     val = 0
-    numbytes = len( text )
-    for i in range( 0, numbytes ):
+    numbytes = len(text)
+    for i in range(0, numbytes):
         val = val * 256
-        val = val + ord( text[ i ] )
-
+        val += ord(text[i])
     return val
 
 
@@ -31,46 +30,46 @@ class HotCache(object):
     def __init__( self  ):
 #        self.url = 'http://www.fourmilab.ch/cgi-bin/uncgi/Hotbits?num=5000&min=1&max=6&col=1'
         self.url = 'http://www.random.org/cgi-bin/randbyte?'
-        self.query = { 'nbytes': 128, 'fmt': 'h' }
+        self.query = {'nbytes': 128, 'fmt': 'h'}
         self.fill_hot_cache()
 
-    def fill_hot_cache( self ):
-        url = self.url + urllib.urlencode( self.query )
-        fh = urllib.urlopen( url )
+    def fill_hot_cache(self):
+        url = self.url + urllib.urlencode(self.query)
+        fh = urllib.urlopen(url)
         self.hot_cache = fh.read()
         fh.close()
 
-    def next_num( self, num_digits = 4 ):
+    def next_num(self, num_digits=4):
         cache = self.hot_cache
         numbytes = num_digits / 2
-        if( len( cache ) % numbytes != 0 ):
-            print('len_cache is %d' % len( cache ))
+        if len(cache) % numbytes != 0:
+            print('len_cache is %d' % len(cache))
             raise ValueError
-        if( cache == '' ):
+        if cache == '':
             self.fill_hot_cache()
             cache = self.hot_cache
-        hexdigits = cache[ :numbytes ]
-        self.hot_cache = cache[ numbytes: ]
-        return byte_concat( hexdigits )
+        hexdigits = cache[:numbytes]
+        self.hot_cache = cache[numbytes:]
+        return byte_concat(hexdigits)
 
 
 class HotRandom(object):
 
-    def __init__( self ):
+    def __init__(self):
         self.hot_cache = HotCache( )
 
-    def hot_rand( self, high, low = 0 ):
+    def hot_rand(self, high, low=0):
         span = high - low
         val = self.hot_cache.next_num()
-        val = ( span * val ) >> 16
-        val = val + low
+        val = (span * val) >> 16
+        val += low
         return val
 
 
-if( __name__ == '__main__' ):
+if __name__ == '__main__':
     hot_random = HotRandom()
-    for j in range( 0, 130 ):
-        print(hot_random.hot_rand( 25 ))
-    nums = [ '0000', 'abcd', '1234', '5555', '4321', 'aaaa', 'ffff' ]
+    for j in range(0, 130):
+        print(hot_random.hot_rand(25))
+    nums = ['0000', 'abcd', '1234', '5555', '4321', 'aaaa', 'ffff']
     for num in nums:
-        print(hex_convert( num ))
+        print(hex_convert(num))
