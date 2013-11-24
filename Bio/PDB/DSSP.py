biopython/biopython

commit 3e287df0dbf44fc51f5017c1414b23c23a3f98cf

Remove unnecessary some_dict.keys() calls.
For iteration and membership testing can use the
dictionary directly. This also avoids 2to3 adding
list(...) wrappers as list(some_dict.keys()) in
these cases where is it not needed.

Also fixed some white space and string concatenation.

Bio/PDB/DSSP.py
@@ -335,4 +335,4 @@ if __name__ == "__main__":
         print(d[('A', 1)])
         print(s[0]['A'][1].xtra)
     # Secondary structure
-    print(''.join(d[key][1] for key in d.keys()))
+    print(''.join(d[key][1] for key in d))

Bio/Align/AlignInfo.py
@@ -648,7 +648,7 @@ class PSSM(object):
 
     def __str__(self):
         out = " "
-        all_residues = sorted(self.pssm[0][1].keys())
+        all_residues = sorted(self.pssm[0][1])
 
         # first print out the top header
         for res in all_residues:
@@ -677,8 +677,7 @@ def print_info_content(summary_info,fout=None,rep_record=0):
     if not summary_info.ic_vector:
         summary_info.information_content()
     rep_sequence = summary_info.alignment._records[rep_record].seq
-    positions = sorted(summary_info.ic_vector.keys())
-    for pos in positions:
+    for pos in sorted(summary_info.ic_vector):
         fout.write("%d %s %.3f\n" % (pos, rep_sequence[pos],
                    summary_info.ic_vector[pos]))
 

Bio/Crystal/__init__.py
@@ -239,16 +239,14 @@ class Crystal(object):
 
     def __repr__(self):
         output = ''
-        keys = sorted(self.data.keys())
-        for key in keys:
-            output = output + '%s : %s\n' % (key, self.data[ key ])
+        for key in sorted(self.data):
+            output += '%s : %s\n' % (key, self.data[key])
         return output
 
     def __str__(self):
         output = ''
-        keys = sorted(self.data.keys())
-        for key in keys:
-            output = output + '%s : %s\n' % (key, self.data[ key ])
+        for key in sorted(self.data):
+            output += '%s : %s\n' % (key, self.data[key])
         return output
 
     def tostring(self):
@@ -264,7 +262,7 @@ class Crystal(object):
         if isinstance(item, Chain):
             self.data[key] = item
         elif isinstance(item, str):
-            self.data[ key ] = Chain(item)
+            self.data[key] = Chain(item)
         else:
             raise TypeError
 

Bio/FSSP/FSSPTools.py
@@ -28,10 +28,9 @@ def mult_align(sum_dict, align_dict):
         for j in align_dict.abs(i).pos_align_dict:
             # loop within a position
             mult_align_dict[j] += align_dict.abs(i).pos_align_dict[j].aa
-    seq_order = sorted(mult_align_dict.keys())
     fssp_align = Generic.Alignment(Alphabet.Gapped(
                                    Alphabet.IUPAC.extended_protein))
-    for i in seq_order:
+    for i in sorted(mult_align_dict):
         fssp_align.add_sequence(sum_dict[i].pdb2+sum_dict[i].chain2,
                                 mult_align_dict[i])
 #        fssp_align._add_numbering_table()
@@ -64,7 +63,7 @@ def filter(sum_dict, align_dict, filter_attribute, low_bound, high_bound):
         attr_value = getattr(sum_dict[prot_num], filter_attribute)
         if attr_value >= low_bound and attr_value <= high_bound:
             new_sum_dict[prot_num] = sum_dict[prot_num]
-    prot_numbers = sorted(new_sum_dict.keys())
+    prot_numbers = sorted(new_sum_dict)
     for pos_num in new_align_dict.abs_res_dict:
         new_align_dict.abs(pos_num).pos_align_dict = {}
         for prot_num in prot_numbers:
@@ -82,7 +81,7 @@ def name_filter(sum_dict, align_dict, name_list):
         for prot_num in sum_dict:
             if sum_dict[prot_num].pdb2+sum_dict[prot_num].chain2 == cur_pdb_name:
                 new_sum_dict[prot_num] = sum_dict[prot_num]
-    prot_numbers = sorted(new_sum_dict.keys())
+    prot_numbers = sorted(new_sum_dict)
     for pos_num in new_align_dict.abs_res_dict:
         new_align_dict.abs(pos_num).pos_align_dict = {}
         for prot_num in prot_numbers:

Bio/FSSP/__init__.py
@@ -184,8 +184,7 @@ class FSSPAlignDict(dict):
     # Returns a sequence string
     def sequence(self, num):
         s = ''
-        sorted_pos_nums = sorted(self.abs_res_dict.keys())
-        for i in sorted_pos_nums:
+        for i in sorted(self.abs_res_dict):
             s += self.abs(i).pos_align_dict[num].aa
         return s
 
@@ -196,9 +195,8 @@ class FSSPAlignDict(dict):
         for fssp_rec in self.itervalues():
             for j in fssp_rec.pos_align_dict:
                 mult_align_dict[j] += fssp_rec.pos_align_dict[j].aa
-        seq_order = sorted(mult_align_dict.keys())
         out_str = ''
-        for i in seq_order:
+        for i in sorted(mult_align_dict):
             out_str += '> %d\n' % i
             k = 0
             for j in mult_align_dict[i]:

Bio/GA/Selection/RouletteWheel.py
@@ -49,7 +49,7 @@ class RouletteWheelSelection(AbstractSelection):
         # set up the current probabilities for selecting organisms
         # from the population
         prob_wheel = self._set_up_wheel(population)
-        probs = sorted(prob_wheel.keys())
+        probs = sorted(prob_wheel)
 
         # now create the new population with the same size as the original
         new_population = []

Bio/Geo/Record.py
@@ -38,54 +38,54 @@ class Record(object):
 
     def __str__( self ):
         output = ''
-        output = output + 'GEO Type: %s\n' % self.entity_type
-        output = output + 'GEO Id: %s\n' % self.entity_id
-        att_keys = sorted(self.entity_attributes.keys())
+        output += 'GEO Type: %s\n' % self.entity_type
+        output += 'GEO Id: %s\n' % self.entity_id
+        att_keys = sorted(self.entity_attributes)
         for key in att_keys:
-            contents = self.entity_attributes[ key ]
+            contents = self.entity_attributes[key]
             if isinstance(contents, list):
                 for item in contents:
                     try:
-                        output = output + '%s: %s\n' % ( key, item[ :40 ] )
-                        output = output + out_block( item[ 40: ] )
+                        output += '%s: %s\n' % ( key, item[:40])
+                        output += out_block(item[40:])
                     except:
                         pass
             elif isinstance(contents, str):
-                output = output + '%s: %s\n' % ( key, contents[ :40 ] )
-                output = output + out_block( contents[ 40: ] )
+                output += '%s: %s\n' % (key, contents[:40])
+                output += out_block(contents[40:])
             else:
                 print(contents)
-                output = output + '%s: %s\n' % ( key, val[ :40 ] )
-                output = output + out_block( val[ 40: ] )
+                output += '%s: %s\n' % (key, val[:40])
+                output += out_block(val[40:])
         col_keys = self.col_defs.keys()
         col_keys.sort()
         output = output + 'Column Header Definitions\n'
         for key in col_keys:
-            val = self.col_defs[ key ]
-            output = output + '    %s: %s\n' % ( key, val[ :40 ] )
-            output = output + out_block( val[ 40: ], '    ' )
+            val = self.col_defs[key]
+            output += '    %s: %s\n' % (key, val[:40])
+            output += out_block(val[40:], '    ')
         #May have to display VERY large tables,
         #so only show the first 20 lines of data
-        MAX_ROWS = 20+1 # include header in count
+        MAX_ROWS = 20 + 1 # include header in count
         for row in self.table_rows[0:MAX_ROWS]:
-            output = output + '%s: ' % self.table_rows.index( row )
+            output += '%s: ' % self.table_rows.index(row)
             for col in row:
-                output = output + '%s\t' % col
-            output = output + '\n'
+                output += '%s\t' % col
+            output += '\n'
         if len(self.table_rows) > MAX_ROWS:
-            output = output + '...\n'
+            output += '...\n'
             row = self.table_rows[-1]
-            output = output + '%s: ' % self.table_rows.index( row )
+            output += '%s: ' % self.table_rows.index(row)
             for col in row:
-                output = output + '%s\t' % col
-            output = output + '\n'
+                output += '%s\t' % col
+            output += '\n'
 
         return output
 
 
 def out_block( text, prefix = '' ):
     output = ''
-    for j in range( 0, len( text ), 80 ):
-        output = output + '%s%s\n' % ( prefix, text[ j: j + 80 ] )
-    output = output + '\n'
+    for j in range(0, len(text), 80):
+        output += '%s%s\n' % (prefix, text[j:j+80])
+    output += '\n'
     return output

Bio/Graphics/GenomeDiagram/_Diagram.py
@@ -423,8 +423,7 @@ class Diagram(object):
 
             Return a sorted list of levels occupied by tracks in the diagram
         """
-        levels = sorted(self.tracks.keys())
-        return levels
+        return sorted(self.tracks)
 
     def get_drawn_levels(self):
         """ get_drawn_levels(self) -> [int, int, ...]
@@ -432,9 +431,7 @@ class Diagram(object):
             Return a sorted list of levels occupied by tracks that are not
             explicitly hidden
         """
-        drawn_levels = sorted(key for key in self.tracks.keys() if
-                              not self.tracks[key].hide) # get list of shown levels
-        return drawn_levels
+        return sorted(key for key in self.tracks if not self.tracks[key].hide)
 
     def range(self):
         """ range(self) -> (int, int)

commit 418178ae636cca4639e13d6128b6c65a78c28da9

DSSP: Fix ValueError: too many values to unpack in __main__
Commit 3e287df caused a minor issue since __iter__ and keys methods of DSSP class return different tuples. DSSP objects cannot be indexed by tuples returned by __iter__. 

Either [d[x][1] for x in d.keys()] or [x[1] for x in d] must be used to get the secondary structure.

Bio/PDB/DSSP.py
@@ -336,4 +336,4 @@ if __name__ == "__main__":
         print(d[('A', 1)])
         print(s[0]['A'][1].xtra)
     # Secondary structure
-    print(''.join(d[key][1] for key in d))
+    print(''.join(item[1] for item in d))