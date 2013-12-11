b453c78399fc6c9c487540fff3678e24b8de6e8b
lijax <yeyanbo289@gmail.com>
2013-12-11 22:13:51 +0800

    remove import *

--- Bio/Phylo/TreeConstruction.py
+++ Bio/Phylo/TreeConstruction.py
@@ -10,7 +10,7 @@ import itertools
 import copy
 from Bio.Phylo import BaseTree
 from Bio.Align import MultipleSeqAlignment
-from Bio.SubsMat.MatrixInfo import *
+from Bio.SubsMat import MatrixInfo
 
 class _Matrix(object):
     """A base class for distance matrix or scoring matrix that accepts
@@ -369,20 +369,10 @@ class DistanceCalculator(object):
 
     # matrices available
     dna_matrices = {'blastn': blastn, 'trans': trans}
-    protein_matrices = {'benner6': benner6, 'benner22': benner22, 'benner74': benner74, 'blosum100': blosum100,
-                      'blosum30': blosum30, 'blosum35': blosum35, 'blosum40': blosum40, 'blosum45': blosum45,
-                      'blosum50': blosum50, 'blosum55': blosum55, 'blosum60': blosum60, 'blosum62': blosum62,
-                      'blosum65': blosum65, 'blosum70': blosum70, 'blosum75': blosum75, 'blosum80': blosum80,
-                      'blosum85': blosum85, 'blosum90': blosum90, 'blosum95': blosum95, 'feng': feng,
-                      'fitch': fitch, 'genetic': genetic, 'gonnet': gonnet, 'grant': grant,
-                      'ident': ident, 'johnson': johnson, 'levin': levin, 'mclach': mclach,
-                      'miyata': miyata, 'nwsgappep': nwsgappep, 'pam120': pam120, 'pam180': pam180,
-                      'pam250': pam250, 'pam30': pam30, 'pam300': pam300, 'pam60': pam60,
-                      'pam90': pam90, 'rao': rao, 'risler': risler, 'structure': structure
-                     }
+    protein_models = MatrixInfo.available_matrices
+    protein_matrices = dict((name, getattr(MatrixInfo, name)) for name in protein_models)
 
     dna_models = dna_matrices.keys()
-    protein_models = protein_matrices.keys()
 
     models = ['identity'] + dna_models + protein_models
 
@@ -706,7 +696,7 @@ class Scorer(object):
 class TreeSearcher(object):
     """Base class for all tree searching methods"""
 
-    def search(self, starting_tree):
+    def search(self, starting_tree, alignment):
         """Caller to search the best tree with a starting tree.
         This should be implemented in subclass"""
         raise NotImplementedError("Method not implemented!")
