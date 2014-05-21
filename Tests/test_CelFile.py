1432a77eae85212296ca0655eda29451254f6945
Vincent DAvis <vincent@vincentdavis.net>
2014-05-21 11:59:41 -0600

    correct Add check for numpy

--- Tests/test_CelFile.py
+++ Tests/test_CelFile.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     from Bio import MissingPythonDependencyError
     raise MissingPythonDependencyError(
-        "Install NumPy if you want to use Bio.Cluster")
+        "Install NumPy if you want to use Bio.Affy.CelFile")
 
 class testCelFile(unittest.TestCase):
     """
