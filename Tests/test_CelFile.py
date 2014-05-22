b71d1e901475c022450566e2de04d1bfeb2676f7
peterjc <p.j.a.cock@googlemail.com>
2014-05-22 14:23:55 +0100

    Change import order; Include licence header

--- Tests/test_CelFile.py
+++ Tests/test_CelFile.py
@@ -1,6 +1,10 @@
+# Copyright 2014 by Vincent Davis. All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
 
 import unittest
-from Bio.Affy import CelFile
+
 try:
     import numpy
     from numpy.testing import assert_array_equal
@@ -8,6 +12,8 @@ except ImportError:
     from Bio import MissingPythonDependencyError
     raise MissingPythonDependencyError(
         "Install NumPy if you want to use Bio.Affy.CelFile")
+
+from Bio.Affy import CelFile
 
 class testCelFile(unittest.TestCase):
     """
