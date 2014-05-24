b1b234e62eefc21464204eef6d59d362078da0e4
Evan Parker <eparker05@gmail.com>
2014-05-24 00:59:55 -0700

    Stronger handling of lazy sequence prpoperty, unit tests are beginning to pass

--- Bio/SeqIO/_lazy.py
+++ Bio/SeqIO/_lazy.py
@@ -76,12 +76,15 @@ class SeqRecordProxyBase(SeqRecord):
 
 
     #subclassing to-do list
-    attributes that must be filled
-     -      self._seq_len    :::   int
+    attributes that must be filled 
+     -   self._seq         :::   sequence type, set by _read_seq
+     -   self._index_begin :::   defines begin index w/r/t global
+     -   self._index_end   :::   defines end index w/r/t global
      
 
     methods need implementation by derived class:
      -      self.__init__    ::: the init provided in the base class is for testing
+     -      self._read_seq    ::: gets the sequence from file and sets it
 
 
 
@@ -89,6 +92,24 @@ class SeqRecordProxyBase(SeqRecord):
     def __init__(self):
         raise NotImplementedError( \
             "__init__ must be implemented in the derived class")
+    
+    
+    def _return_seq(self):
+        """this simple function removes getter logic from _read_seq"""
+        if not self._seq:
+            self._read_seq()
+        return self._seq
+    
+    def _read_seq(self):
+        """this is implemented to handle file access for setting _seq"""
+        raise NotImplementedError( \
+            "_read_seq must be implemented in the derived class")
+
+    seq = property(fget=_return_seq,
+                   doc="The sequence itself, as a Seq or MutableSeq object.")
+
+    #TODO - letter annotations?
+    letter_annotations = None
 
     def __getitem__(self, index):
         """Returns a sub-sequence or an individual letter.
@@ -97,248 +118,56 @@ class SeqRecordProxyBase(SeqRecord):
         or a copy of itself with new marker indices.
         """
         if isinstance(index, int):
-            #NOTE - The sequence level annotation like the id, name, etc
-            #do not really apply to a single character.  However, should
-            #we try and expose any per-letter-annotation here?  If so how?
-            return self.seq[index]
+            #This mimics the behavior of the current SeqRecord
+            #The recursive call is required to prevent full parsing while
+            #only calling a single resiude
+            return self[index:index+1].seq[0]
+        
         elif isinstance(index, slice):
-            if self.seq is None:
-                raise ValueError("If the sequence is None, we cannot slice it.")
             parent_length = len(self)
-            answer = self.__class__(self.seq[index],
-                                    id=self.id,
-                                    name=self.name,
-                                    description=self.description)
-            #TODO - The desription may no longer apply.
-            #It would be safer to change it to something
-            #generic like "edited" or the default value.
-
-            #Don't copy the annotation dict and dbxefs list,
-            #they may not apply to a subsequence.
-            #answer.annotations = dict(self.annotations.items())
-            #answer.dbxrefs = self.dbxrefs[:]
-            #TODO - Review this in light of adding SeqRecord objects?
-
-            #TODO - Cope with strides by generating ambiguous locations?
+            if parent_length <= 0:
+                raise ValueError("If the sequence length is zero, we cannot slice it.")
+            
+            #this is what will be returned
+            seq_proxy_copy = copy(self)
+            #do some index math
             start, stop, step = index.indices(parent_length)
-            if step == 1:
-                #Select relevant features, add them with shifted locations
-                #assert str(self.seq)[index] == str(self.seq)[start:stop]
-                for f in self.features:
-                    if f.ref or f.ref_db:
-                        #TODO - Implement this (with lots of tests)?
-                        import warnings
-                        warnings.warn("When slicing SeqRecord objects, any "
-                              "SeqFeature referencing other sequences (e.g. "
-                              "from segmented GenBank records) are ignored.")
-                        continue
-                    if start <= f.location.nofuzzy_start \
-                    and f.location.nofuzzy_end <= stop:
-                        answer.features.append(f._shift(-start))
+            seq_proxy_copy._index_begin = self._index_begin + start
+            seq_proxy_copy._index_end = self._index_begin + stop
+            #fix _seq property when set
+            if self._seq:
+                seq_proxy_copy._seq = self._seq[start:stop]
 
-            #Slice all the values to match the sliced sequence
-            #(this should also work with strides, even negative strides):
-            for key, value in self.letter_annotations.items():
-                answer._per_letter_annotations[key] = value[index]
-
-            return answer
+            return seq_proxy_copy
         raise ValueError("Invalid index")
-
-    def __iter__(self):
-        """Iterate over the letters in the sequence.
-
-        For example, using Bio.SeqIO to read in a protein FASTA file:
-
-        >>> from Bio import SeqIO
-        >>> record = SeqIO.read("Fasta/loveliesbleeding.pro", "fasta")
-        >>> for amino in record:
-        ...     print(amino)
-        ...     if amino == "L": break
-        X
-        A
-        G
-        L
-        >>> print(record.seq[3])
-        L
-
-        This is just a shortcut for iterating over the sequence directly:
-
-        >>> for amino in record.seq:
-        ...     print(amino)
-        ...     if amino == "L": break
-        X
-        A
-        G
-        L
-        >>> print(record.seq[3])
-        L
-
-        Note that this does not facilitate iteration together with any
-        per-letter-annotation.  However, you can achieve that using the
-        python zip function on the record (or its sequence) and the relevant
-        per-letter-annotation:
-
-        >>> from Bio import SeqIO
-        >>> rec = SeqIO.read("Quality/solexa_faked.fastq", "fastq-solexa")
-        >>> print("%s %s" % (rec.id, rec.seq))
-        slxa_0001_1_0001_01 ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTNNNNNN
-        >>> print(list(rec.letter_annotations))
-        ['solexa_quality']
-        >>> for nuc, qual in zip(rec, rec.letter_annotations["solexa_quality"]):
-        ...     if qual > 35:
-        ...         print("%s %i" % (nuc, qual))
-        A 40
-        C 39
-        G 38
-        T 37
-        A 36
-
-        You may agree that using zip(rec.seq, ...) is more explicit than using
-        zip(rec, ...) as shown above.
-        """
-        return iter(self.seq)
-
-    def __contains__(self, char):
-        """Implements the 'in' keyword, searches the sequence.
-
-        e.g.
-
-        >>> from Bio import SeqIO
-        >>> record = SeqIO.read("Fasta/sweetpea.nu", "fasta")
-        >>> "GAATTC" in record
-        False
-        >>> "AAA" in record
-        True
-
-        This essentially acts as a proxy for using "in" on the sequence:
-
-        >>> "GAATTC" in record.seq
-        False
-        >>> "AAA" in record.seq
-        True
-
-        Note that you can also use Seq objects as the query,
-
-        >>> from Bio.Seq import Seq
-        >>> from Bio.Alphabet import generic_dna
-        >>> Seq("AAA") in record
-        True
-        >>> Seq("AAA", generic_dna) in record
-        True
-
-        See also the Seq object's __contains__ method.
-        """
-        return char in self.seq
-
-    def __str__(self):
-        """A human readable summary of the record and its annotation (string).
-
-        The python built in function str works by calling the object's ___str__
-        method.  e.g.
-
-        >>> from Bio.Seq import Seq
-        >>> from Bio.SeqRecord import SeqRecord
-        >>> from Bio.Alphabet import IUPAC
-        >>> record = SeqRecord(Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF",
-        ...                         IUPAC.protein),
-        ...                    id="YP_025292.1", name="HokC",
-        ...                    description="toxic membrane protein, small")
-        >>> print(str(record))
-        ID: YP_025292.1
-        Name: HokC
-        Description: toxic membrane protein, small
-        Number of features: 0
-        Seq('MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF', IUPACProtein())
-
-        In this example you don't actually need to call str explicity, as the
-        print command does this automatically:
-
-        >>> print(record)
-        ID: YP_025292.1
-        Name: HokC
-        Description: toxic membrane protein, small
-        Number of features: 0
-        Seq('MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF', IUPACProtein())
-
-        Note that long sequences are shown truncated.
-        """
-        lines = []
-        if self.id:
-            lines.append("ID: %s" % self.id)
-        if self.name:
-            lines.append("Name: %s" % self.name)
-        if self.description:
-            lines.append("Description: %s" % self.description)
-        if self.dbxrefs:
-            lines.append("Database cross-references: "
-                         + ", ".join(self.dbxrefs))
-        lines.append("Number of features: %i" % len(self.features))
-        for a in self.annotations:
-            lines.append("/%s=%s" % (a, str(self.annotations[a])))
-        if self.letter_annotations:
-            lines.append("Per letter annotation for: "
-                         + ", ".join(self.letter_annotations))
-        #Don't want to include the entire sequence,
-        #and showing the alphabet is useful:
-        lines.append(repr(self.seq))
-        return "\n".join(lines)
-
-    def __repr__(self):
-        """A concise summary of the record for debugging (string).
-
-        The python built in function repr works by calling the object's ___repr__
-        method.  e.g.
-
-        >>> from Bio.Seq import Seq
-        >>> from Bio.SeqRecord import SeqRecord
-        >>> from Bio.Alphabet import generic_protein
-        >>> rec = SeqRecord(Seq("MASRGVNKVILVGNLGQDPEVRYMPNGGAVANITLATSESWRDKAT"
-        ...                    +"GEMKEQTEWHRVVLFGKLAEVASEYLRKGSQVYIEGQLRTRKWTDQ"
-        ...                    +"SGQDRYTTEVVVNVGGTMQMLGGRQGGGAPAGGNIGGGQPQGGWGQ"
-        ...                    +"PQQPQGGNQFSGGAQSRPQQSAPAAPSNEPPMDFDDDIPF",
-        ...                    generic_protein),
-        ...                 id="NP_418483.1", name="b4059",
-        ...                 description="ssDNA-binding protein",
-        ...                 dbxrefs=["ASAP:13298", "GI:16131885", "GeneID:948570"])
-        >>> print(repr(rec))
-        SeqRecord(seq=Seq('MASRGVNKVILVGNLGQDPEVRYMPNGGAVANITLATSESWRDKATGEMKEQTE...IPF', ProteinAlphabet()), id='NP_418483.1', name='b4059', description='ssDNA-binding protein', dbxrefs=['ASAP:13298', 'GI:16131885', 'GeneID:948570'])
-
-        At the python prompt you can also use this shorthand:
-
-        >>> rec
-        SeqRecord(seq=Seq('MASRGVNKVILVGNLGQDPEVRYMPNGGAVANITLATSESWRDKATGEMKEQTE...IPF', ProteinAlphabet()), id='NP_418483.1', name='b4059', description='ssDNA-binding protein', dbxrefs=['ASAP:13298', 'GI:16131885', 'GeneID:948570'])
-
-        Note that long sequences are shown truncated. Also note that any
-        annotations, letter_annotations and features are not shown (as they
-        would lead to a very long string).
-        """
-        return self.__class__.__name__ \
-         + "(seq=%s, id=%s, name=%s, description=%s, dbxrefs=%s)" \
-         % tuple(map(repr, (self.seq, self.id, self.name,
-                            self.description, self.dbxrefs)))
-
+    
+    
     def __len__(self):
         """Returns the length of the sequence.
         
         The derived class must set attribute _seq_len
         """
-        return len(self._seq_len)
+        return self._index_end - self._index_begin
 
 
     def upper(self):
         """Returns a copy of the record with an upper case sequence.
-
-           proxy class needs a new implementation
         """
-        raise NotImplementedError(" this needs to be implemented")
+        if not self._seq:
+            self._read_seq()
+        newSelf = copy(self)
+        newSelf._seq = self._seq.upper()
+        return newSelf
 
 
     def lower(self):
         """Returns a copy of the record with a lower case sequence.
-
-        possibly do something clever
         """
-        raise NotImplementedError(" this needs to be implemented")
+        if not self._seq:
+            self._read_seq()
+        newSelf = copy(self)
+        newSelf._seq = self._seq.upper()
+        return newSelf
 
     # All methods tagged below are implemented in the base class
     #
@@ -352,14 +181,19 @@ class SeqRecordProxyBase(SeqRecord):
     #    returns non-proxy class where possible
     #def __radd__(self, other):
     #    returns non-proxy class where possible
+    #def __str__(self):
+    #def __repr__(self):
+    #def __contains__(self, char):
+    #def __iter__(self):
 
 class TestSeqRecordBaseClass(SeqRecordProxyBase):
-    """ this class implements simple forms of required methods and attributes
+    """ this class implements the minimum funcationality for a working proxy
     """
-    def __init__(self, seq, id = "<unknown id>", name = "<unknown name>",
+    def __init__(self, handle, id = "<unknown id>", name = "<unknown name>",
                  description = "<unknown description>", dbxrefs = None,
                  features = None, annotations = None,
-                 letter_annotations = None):
+                 letter_annotations = None, index_begin=0,
+                 index_end=None):
         """Create a SeqRecord.
 
         Arguments:
@@ -375,6 +209,10 @@ class TestSeqRecordBaseClass(SeqRecordProxyBase):
                                 length as the full sequence.
 
         """
+        self._index_begin = index_begin
+        self._index_end = len(handle)
+        self._seq = None
+
         if id is not None and not isinstance(id, basestring):
             #Lots of existing code uses id=None... this may be a bad idea.
             raise TypeError("id argument should be a string")
@@ -382,7 +220,7 @@ class TestSeqRecordBaseClass(SeqRecordProxyBase):
             raise TypeError("name argument should be a string")
         if not isinstance(description, basestring):
             raise TypeError("description argument should be a string")
-        self._seq = seq
+        self._handle = handle
         self.id = id
         self.name = name
         self.description = description
@@ -401,62 +239,25 @@ class TestSeqRecordBaseClass(SeqRecordProxyBase):
             raise TypeError("annotations argument should be a dict")
         self.annotations = annotations
 
-        if letter_annotations is None:
-            # annotations about each letter in the sequence
-            if seq is None:
-                #Should we allow this and use a normal unrestricted dict?
-                self._per_letter_annotations = _RestrictedDict(length=0)
-            else:
-                try:
-                    self._per_letter_annotations = \
-                                              _RestrictedDict(length=len(seq))
-                except:
-                    raise TypeError("seq argument should be a Seq object or similar")
-        else:
-            #This will be handled via the property set function, which will
-            #turn this into a _RestrictedDict and thus ensure all the values
-            #in the dict are the right length
-            self.letter_annotations = letter_annotations
+        #TODO handle letter annotations properly
+        self.letter_annotations = letter_annotations
 
-        # annotations about parts of the sequence
+        #TODO handle features annotations about parts of the sequence
         if features is None:
             features = []
         elif not isinstance(features, list):
             raise TypeError("features argument should be a list (of SeqFeature objects)")
         self.features = features
-
-    #TODO - Just make this a read only property?
-    def _set_per_letter_annotations(self, value):
-        if not isinstance(value, dict):
-            raise TypeError("The per-letter-annotations should be a "
-                            "(restricted) dictionary.")
-        #Turn this into a restricted-dictionary (and check the entries)
-        try:
-            self._per_letter_annotations = _RestrictedDict(length=len(self.seq))
-        except AttributeError:
-            #e.g. seq is None
-            self._per_letter_annotations = _RestrictedDict(length=0)
-        self._per_letter_annotations.update(value)
-    letter_annotations = property(
-        fget=lambda self: self._per_letter_annotations,
-        fset=_set_per_letter_annotations,
-        doc="""Dictionary of per-letter-annotation for the sequence.""")
     
-    def _set_seq(self, value):
-        #TODO - Add a deprecation warning that the seq should be write only?
-        if self._per_letter_annotations:
-            #TODO - Make this a warning? Silently empty the dictionary?
-            raise ValueError("You must empty the letter annotations first!")
-        self._seq = value
-        try:
-            self._per_letter_annotations = _RestrictedDict(length=len(self.seq))
-        except AttributeError:
-            #e.g. seq is None
-            self._per_letter_annotations = _RestrictedDict(length=0)
+    def _read_seq(self):
+        if self._seq:
+            pass
+        else:
+            i = self._index_begin
+            j = self._index_end 
+            self._seq = self._handle[i:j]
 
-    seq = property(fget=lambda self: self._seq,
-                   fset=_set_seq,
-                   doc="The sequence itself, as a Seq or MutableSeq object.")
+    
 
 
                    
@@ -475,7 +276,46 @@ if __name__ == "__main__":
             a = TestSeqRecordBaseClass("sequencefake", "fakeid")
             self.assertEqual(5, 5)
             self.assertRaises(NotImplementedError, SeqRecordProxyBase)
-            
+
+        def test_simple_tester(self):
+            """checks on basic definitions in tester class"""
+            a = TestSeqRecordBaseClass("sequencefake", "fakeid")
+            self.assertEqual(0, a._index_begin)
+            self.assertEqual(12, a._index_end)
+            self.assertEqual(None, a._seq)
+            self.assertEqual("sequencefake", a._handle)
+
+        def test_seq_reader(self):
+            """checks on basic worksing of _read_seq"""
+            a = TestSeqRecordBaseClass("sequencefake", "fakeid")
+            self.assertEqual(None, a._seq)
+            self.assertEqual("sequencefake", a.seq)
+            self.assertEqual("sequencefake", a._seq)
+
+        def test_simple_index_grab(self):
+            a = TestSeqRecordBaseClass("sequencefake", "fakeid")
+            b = a[1:9]
+            self.assertEqual(1, b._index_begin)
+            self.assertEqual(9, b._index_end)
+
+        def test_simple_index_grab_handle_identity(self):
+            a = TestSeqRecordBaseClass("sequencefake", "fakeid")
+            b = a[1:9]
+            self.assertTrue(a._handle is b._handle)
+            self.assertTrue(a is not b)
+
+        def test_two_level_index_grab(self):
+            a = TestSeqRecordBaseClass("sequencefake", "fakeid")
+            b = a[1:9]
+            c = b[1:8]
+            self.assertEqual(2, c._index_begin)
+            self.assertEqual(9, c._index_end)
+
+        def test_upper(self):
+            a = TestSeqRecordBaseClass("seQUencefake", "fakeid")
+            b = a.upper()
+            self.assertEqual("SEQUENCEFAKE", b._seq)
+
     unittest.main( exit=False )
     """if __name__ == "__main__":
         runner = unittest.TextTestRunner(verbosity = 2)
