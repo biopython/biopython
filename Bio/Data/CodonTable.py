fa64b8f6d4540856f9429ee40dec6158a54b87e5
carlosp420 <mycalesis@gmail.com>
2014-10-31 11:14:07 +0200

    Recipe for Codon Tables updated with PEP8 fixes
    Recipe code has been tested in python 2.7 and 3.3

--- Bio/Data/CodonTable.py
+++ Bio/Data/CodonTable.py
@@ -466,74 +466,78 @@ def register_ncbi_table(name, alt_name, id,
 # using the following:
 # import re
 # for line in open("gc.prt").readlines():
-#    if line[:2] == " {":
-#        names = []
-#        id = None
-#        aa = None
-#        start = None
-#        bases = []
-#    elif line[:6] == "  name":
-#        names.append(re.search('"([^"]*)"', line).group(1))
-#    elif line[:8] == "    name":
-#        names.append(re.search('"(.*)$', line).group(1))
-#    elif line == ' Mitochondrial; Mycoplasma; Spiroplasma" ,\n':
-#        names[-1] = names[-1] + " Mitochondrial; Mycoplasma; Spiroplasma"
-#    elif line[:4] == "  id":
-#        id = int(re.search('(\d+)', line).group(1))
-#    elif line[:10] == "  ncbieaa ":
-#        aa = line[12:12+64]
-#    elif line[:10] == "  sncbieaa":
-#        start = line[12:12+64]
-#    elif line[:9] == "  -- Base":
-#        bases.append(line[12:12+64])
-#    elif line[:2] == " }":
-#        assert names != [] and id is not None and aa is not None
-#        assert start is not None and bases != []
-#        if len(names) == 1:
-#            names.append(None)
-#        print "register_ncbi_table(name = %s," % repr(names[0])
-#        print "                    alt_name = %s, id = %d," % \
-#              (repr(names[1]), id)
-#        print "                    table = {"
-#        s = "    "
-#        for i in range(64):
-#            if aa[i] != "*":
-#                t = " '%s%s%s': '%s'," % (bases[0][i], bases[1][i],
-#                                          bases[2][i], aa[i])
-#                if len(s) + len(t) > 75:
-#                    print s
-#                    s = "    " + t
-#                else:
-#                    s = s + t
-#        print s, "},"
+#     if line[:2] == " {":
+#         names = []
+#         id = None
+#         aa = None
+#         start = None
+#         bases = []
+#     elif line[:6] == "  name":
+#         names.append(re.search('"([^"]*)"', line).group(1))
+#     elif line[:8] == "    name":
+#         names.append(re.search('"(.*)$', line).group(1))
+#     elif line == ' Mitochondrial; Mycoplasma; Spiroplasma" ,\n':
+#         names[-1] = names[-1] + " Mitochondrial; Mycoplasma; Spiroplasma"
+#     elif line[:4] == "  id":
+#         id = int(re.search('(\d+)', line).group(1))
+#     elif line[:10] == "  ncbieaa ":
+#         aa = line[12:12+64]
+#     elif line[:10] == "  sncbieaa":
+#         start = line[12:12+64]
+#     elif line[:9] == "  -- Base":
+#         bases.append(line[12:12+64])
+#     elif line[:2] == " }":
+#         assert names != [] and id is not None and aa is not None
+#         assert start is not None and bases != []
+#         if len(names) == 1:
+#             names.append(None)
+#         print("register_ncbi_table(name=%s," % repr(names[0]))
+#         print("                    alt_name=%s, id=%d," % \
+#               (repr(names[1]), id))
+#         print("                    table={")
+#         s = "    "
+#         for i in range(64):
+#             if aa[i] != "*":
+#                 t = " '%s%s%s': '%s'," % (bases[0][i], bases[1][i],
+#                                           bases[2][i], aa[i])
+#                 if len(s) + len(t) > 75:
+#                     print(s)
+#                     s = "    " + t
+#                 else:
+#                     s = s + t
+#         print("%s }," % s)
 
-#        s = "                    stop_codons = ["
-#        for i in range(64):
-#            if aa[i] == "*":
-#                t = " '%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
-#                if len(s) + len(t) > 75:
-#                    print s
-#                    s = "                                    " + t
-#                else:
-#                    s = s + t
-#        print s, "],"
+#         s = "                    stop_codons=["
+#         for i in range(64):
+#             if aa[i] == "*":
+#                 t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
+#                 if len(s) + len(t) > 75:
+#                     s_with_spaces = s.replace("','", "', '")
+#                     print(s_with_spaces)
+#                     s = "                                    " + t
+#                 else:
+#                     s = s + t
+#         s_with_spaces = s.replace("','", "', '")
+#         print("%s ]," % s_with_spaces)
 
-#        s = "                    start_codons = ["
-#        for i in range(64):
-#            if start[i] == "M":
-#                t = " '%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
-#                if len(s) + len(t) > 75:
-#                    print s
-#                    s = "                                    " + t
-#                else:
-#                    s = s + t
-#        print s, "]"
-#        print "                    )"
-#    elif line[:2] == "--" or line == "\n" or line == "}\n" or \
-#         line == 'Genetic-code-table ::= {\n':
-#        pass
-#    else:
-#        raise Exception("Unparsed: " + repr(line))
+#         s = "                    start_codons=["
+#         for i in range(64):
+#             if start[i] == "M":
+#                 t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
+#                 if len(s) + len(t) > 75:
+#                     s_with_spaces = s.replace("','", "', '")
+#                     print(s_with_spaces)
+#                     s = "                                    " + t
+#                 else:
+#                     s = s + t
+#         s_with_spaces = s.replace("','", "', '")
+#         print("%s ]" % s_with_spaces)
+#         print("                    )")
+#     elif line[:2] == "--" or line == "\n" or line == "}\n" or \
+#          line == 'Genetic-code-table ::= {\n':
+#         pass
+#     else:
+#         raise Exception("Unparsed: " + repr(line))
 
 register_ncbi_table(name='Standard',
                     alt_name='SGC0', id=1,
