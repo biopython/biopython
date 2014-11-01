6516068701ae62c68520988758d1090213398a90
Peter Cock <p.j.a.cock@googlemail.com>
2014-11-01 15:55:33 +0000

    Handle new options in BLAST+ 2.2.30 (issue #399)

--- Tests/test_NCBI_BLAST_tools.py
+++ Tests/test_NCBI_BLAST_tools.py
@@ -263,6 +263,12 @@ class CheckCompleteArgList(unittest.TestCase):
         if exe_name == "deltablast":
             # New in BLAST+ 2.2.29 so will look like extra args on BLAST+ 2.2.28
             extra = extra.difference(["-entrez_query", "-max_hsps", "-sum_statistics"])
+        if exe_name in ["blastx", "tblastn"]:
+            # New in BLAST+ 2.2.30 so will look like extra args on BLAST+ 2.2.29 etc
+            extra.remove("-task")
+        if exe_name == "psiblast":
+            # New in BLAST+ 2.2.30 so will look like extra args on BLAST+ 2.2.29 etc
+            extra = extra.difference(["-line_length", "-qcov_hsp_perc", "-sum_stats"])
 
         if extra or missing:
             import warnings
