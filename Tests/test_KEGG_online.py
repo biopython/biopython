3a4d27d19a364ec76a372b34bd5b38b1e4dd6f74
Peter Cock <p.j.a.cock@googlemail.com>
2014-11-11 17:35:27 +0900

    Python 2.6 URL handles (addinfourl) are not context managers
    You get this exception,
    
    AttributeError: addinfourl instance has no attribute '__enter__'
    
    See http://bugs.python.org/issue12487

--- Tests/test_KEGG_online.py
+++ Tests/test_KEGG_online.py
@@ -20,191 +20,230 @@ from Bio._py3k import _as_string
 from Bio.KEGG.KGML import KGML_parser
 from Bio.KEGG.REST import *
 
+# TODO - revert to using with statements once we drop
+# Python 2.6 and 2.7, see http://bugs.python.org/issue12487
+
 
 class KEGGTests(unittest.TestCase):
     """Tests for KEGG REST API."""
 
     def test_info_kegg(self):
-        with kegg_info("kegg") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/info/kegg")
+        h = kegg_info("kegg")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/info/kegg")
+        h.close()
 
     def test_info_pathway(self):
-        with kegg_info("pathway") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/info/pathway")
+        h = kegg_info("pathway")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/info/pathway")
+        h.close()
 
     def test_list_pathway(self):
-        with kegg_list("pathway") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/pathway")
+        h = kegg_list("pathway")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/pathway")
+        h.close()
 
     def test_pathway_hsa(self):
-        with kegg_list("pathway", "hsa") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/pathway/hsa")
+        h = kegg_list("pathway", "hsa")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/pathway/hsa")
+        h.close()
 
     def test_list_organism(self):
-        with kegg_list("organism") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/organism")
+        h = kegg_list("organism")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/organism")
+        h.close()
 
     def test_list_hsa(self):
-        with kegg_list("hsa") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa")
+        h = kegg_list("hsa")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa")
+        h.close()
 
     def test_list_T01001(self):
-        with kegg_list("T01001") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/T01001")
+        h = kegg_list("T01001")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/T01001")
+        h.close()
 
     def test_list_hsa_10458_plus_ece_Z5100(self):
-        with kegg_list("hsa:10458+ece:Z5100") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa:10458+ece:Z5100")
+        h = kegg_list("hsa:10458+ece:Z5100")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_list_hsa_10458_list_ece_Z5100(self):
-        with kegg_list(["hsa:10458", "ece:Z5100"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa:10458+ece:Z5100")
+        h = kegg_list(["hsa:10458", "ece:Z5100"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_list_cpd_C01290_plus_gl_G0009(self):
-        with kegg_list("cpd:C01290+gl:G00092") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/cpd:C01290+gl:G00092")
+        h = kegg_list("cpd:C01290+gl:G00092")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/cpd:C01290+gl:G00092")
+        h.close()
 
     def test_list_cpd_C01290_list_gl_G0009(self):
-        with kegg_list(["cpd:C01290", "gl:G00092"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/cpd:C01290+gl:G00092")
+        h = kegg_list(["cpd:C01290", "gl:G00092"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/cpd:C01290+gl:G00092")
+        h.close()
 
     def test_list_C01290_plus_G00092(self):
-        with kegg_list("C01290+G00092") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/C01290+G00092")
+        h = kegg_list("C01290+G00092")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/C01290+G00092")
+        h.close()
 
     def test_list_C01290_list_G00092(self):
-        with kegg_list(["C01290", "G00092"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/list/C01290+G00092")
+        h = kegg_list(["C01290", "G00092"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/list/C01290+G00092")
+        h.close()
 
     def test_find_genes_shiga_plus_toxin(self):
-        with kegg_find("genes", "shiga+toxin") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/genes/shiga+toxin")
+        h = kegg_find("genes", "shiga+toxin")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/genes/shiga+toxin")
+        h.close()
 
     def test_find_genes_shiga_list_toxin(self):
-        with kegg_find("genes", ["shiga", "toxin"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/genes/shiga+toxin")
+        h = kegg_find("genes", ["shiga", "toxin"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/genes/shiga+toxin")
+        h.close()
 
     def test_find_compound_C7H10O5_formula(self):
-        with kegg_find("compound", "C7H10O5", "formula") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/C7H10O5/formula")
+        h = kegg_find("compound", "C7H10O5", "formula")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/C7H10O5/formula")
+        h.close()
 
     def test_find_compound_O5C7_formula(self):
-        with kegg_find("compound", "O5C7", "formula") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/O5C7/formula")
+        h = kegg_find("compound", "O5C7", "formula")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/O5C7/formula")
+        h.close()
 
     def test_find_compound_exact_mass(self):
-        with kegg_find("compound", "174.05", "exact_mass") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/174.05/exact_mass")
+        h = kegg_find("compound", "174.05", "exact_mass")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/174.05/exact_mass")
+        h.close()
 
     def test_find_compound_weight(self):
-        with kegg_find("compound", "300-310", "mol_weight") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/300-310/mol_weight")
+        h = kegg_find("compound", "300-310", "mol_weight")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/find/compound/300-310/mol_weight")
+        h.close()
 
     def test_get_cpd_C01290_plus_gl_G00092(self):
-        with kegg_get("cpd:C01290+gl:G00092") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/cpd:C01290+gl:G00092")
+        h = kegg_get("cpd:C01290+gl:G00092")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/cpd:C01290+gl:G00092")
+        h.close()
 
     def test_get_cpd_C01290_list_gl_G00092(self):
-        with kegg_get(["cpd:C01290", "gl:G00092"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/cpd:C01290+gl:G00092")
+        h = kegg_get(["cpd:C01290", "gl:G00092"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/cpd:C01290+gl:G00092")
+        h.close()
 
     def test_get_C01290_plus_G00092(self):
-        with kegg_get(["C01290+G00092"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/C01290+G00092")
+        h = kegg_get(["C01290+G00092"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/C01290+G00092")
+        h.close()
 
     def test_get_C01290_list_G00092(self):
-        with kegg_get(["C01290", "G00092"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/C01290+G00092")
+        h = kegg_get(["C01290", "G00092"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/C01290+G00092")
+        h.close()
 
     def test_get_hsa_10458_plus_ece_Z5100(self):
-        with kegg_get("hsa:10458+ece:Z5100") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100")
+        h = kegg_get("hsa:10458+ece:Z5100")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_get_hsa_10458_list_ece_Z5100(self):
-        with kegg_get(["hsa:10458", "ece:Z5100"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100")
+        h = kegg_get(["hsa:10458", "ece:Z5100"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_get_hsa_10458_plus_ece_Z5100_as_aaseq(self):
-        with kegg_get("hsa:10458+ece:Z5100", "aaseq") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100/aaseq")
+        h = kegg_get("hsa:10458+ece:Z5100", "aaseq")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100/aaseq")
+        h.close()
 
     def test_get_hsa_10458_list_ece_Z5100_as_aaseq(self):
-        with kegg_get(["hsa:10458", "ece:Z5100"], "aaseq") as h:
-            data = _as_string(h.read())
-            self.assertEqual(data.count(">"), 2)
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100/aaseq")
+        h = kegg_get(["hsa:10458", "ece:Z5100"], "aaseq")
+        data = _as_string(h.read())
+        self.assertEqual(data.count(">"), 2)
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa:10458+ece:Z5100/aaseq")
+        h.close()
 
     def test_get_hsa05130_image(self):
-        with kegg_get("hsa05130", "image") as h:
-            data = h.read()
-            self.assertEqual(data[:4], b"\x89PNG")
-            self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa05130/image")
+        h = kegg_get("hsa05130", "image")
+        data = h.read()
+        self.assertEqual(data[:4], b"\x89PNG")
+        self.assertEqual(h.url, "http://rest.kegg.jp/get/hsa05130/image")
+        h.close()
 
     def test_conv_eco_ncbi_geneid(self):
-        with kegg_conv("eco", "ncbi-geneid") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/conv/eco/ncbi-geneid")
+        h = kegg_conv("eco", "ncbi-geneid")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/conv/eco/ncbi-geneid")
+        h.close()
 
     def test_conv_ncbi_geneid_eco(self):
-        with kegg_conv("ncbi-geneid", "eco") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-geneid/eco")
+        h = kegg_conv("ncbi-geneid", "eco")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-geneid/eco")
+        h.close()
 
     def test_conv_ncbi_gi_hsa_10458_plus_ece_Z5100(self):
-        with kegg_conv("ncbi-gi", "hsa:10458+ece:Z5100") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-gi/hsa:10458+ece:Z5100")
+        h = kegg_conv("ncbi-gi", "hsa:10458+ece:Z5100")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-gi/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_conv_ncbi_gi_hsa_10458_list_ece_Z5100(self):
-        with kegg_conv("ncbi-gi", ["hsa:10458", "ece:Z5100"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-gi/hsa:10458+ece:Z5100")
+        h = kegg_conv("ncbi-gi", ["hsa:10458", "ece:Z5100"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/conv/ncbi-gi/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_link_pathway_hsa(self):
-        with kegg_link("pathway", "hsa") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa")
+        h = kegg_link("pathway", "hsa")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa")
+        h.close()
 
     def test_link_hsa_pathway(self):
-        with kegg_link("hsa", "pathway") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/link/hsa/pathway")
+        h = kegg_link("hsa", "pathway")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/link/hsa/pathway")
+        h.close()
 
     def test_pathway_hsa_10458_plus_ece_Z5100(self):
-        with kegg_link("pathway", "hsa:10458+ece:Z5100") as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa:10458+ece:Z5100")
+        h = kegg_link("pathway", "hsa:10458+ece:Z5100")
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa:10458+ece:Z5100")
+        h.close()
 
     def test_pathway_hsa_10458_list_ece_Z5100(self):
-        with kegg_link("pathway", ["hsa:10458", "ece:Z5100"]) as h:
-            h.read()
-            self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa:10458+ece:Z5100")
+        h = kegg_link("pathway", ["hsa:10458", "ece:Z5100"])
+        h.read()
+        self.assertEqual(h.url, "http://rest.kegg.jp/link/pathway/hsa:10458+ece:Z5100")
+        h.close()
 
 
 class KGMLPathwayTests(unittest.TestCase):
@@ -213,9 +252,10 @@ class KGMLPathwayTests(unittest.TestCase):
     def test_parse_remote_pathway(self):
         """Download a KEGG pathway from the KEGG server and write KGML."""
         # Download the KEGG ko03070 pathway as a filehandle
-        with kegg_get("ko03070", "kgml") as h:
-            pathway = KGML_parser.read(h)
+        h = kegg_get("ko03070", "kgml")
+        pathway = KGML_parser.read(h)
         self.assertEqual(pathway.name, "path:ko03070")
+        h.close()
 
 
 if __name__ == '__main__':
