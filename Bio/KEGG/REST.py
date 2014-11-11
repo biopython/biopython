df377d95d7e9eef5dc86f45d7c99a2d14f09613b
Peter Cock <p.j.a.cock@googlemail.com>
2014-11-11 12:11:30 +0900

    Basic docstrings for KEGG REST API.

--- Bio/KEGG/REST.py
+++ Bio/KEGG/REST.py
@@ -1,17 +1,30 @@
 # Copyright 2014 by Kevin Wu.
+# Revisions copyright 2014 by Peter Cock.
 # All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-"""A wrapper for the online KEGG REST API.
+"""Provides code to access the REST-style KEGG online API.
+
+This module aims to make the KEGG online REST-style API easier to use. See:
+http://www.kegg.jp/kegg/rest/keggapi.html
+
+The KEGG REST-style API provides simple access to a range of KEGG databases.
+This works using simple URLs (which this module will construct for you),
+with any errors indicated via HTTP error levels.
+
+The functionality is somewhat similar to Biopython's Bio.TogoWS and Bio.Entrez
+modules.
+
+Currently KEGG does not provide any usage guidelines (unlike the NCBI whose
+requirements are reasonably clear). To avoid risking overloading the service,
+Biopython will only allow three calls per second.
 
 References:
 
 Kanehisa, M. and Goto, S.; KEGG: Kyoto Encyclopedia of Genes and Genomes.
 Nucleic Acids Res. 28, 29-34 (2000).
-
-API Docs: http://www.kegg.jp/kegg/docs/keggapi.html
 """
 
 from Bio._py3k import urlopen as _urlopen
@@ -30,6 +43,22 @@ def _q(op, arg1, arg2=None, arg3=None):
 
 # http://www.kegg.jp/kegg/rest/keggapi.html
 def kegg_info(database):
+    """KEGG info - Displays the current statistics of a given database.
+
+    db - database or organism (string)
+
+    The argument db can be a KEGG database name (e.g. 'pathway' or its
+    official abbreviation, 'path'), or a KEGG organism code or T number
+    (e.g. 'hsa' or 'T01001' for human).
+
+    A valid list of organism codes and their T numbers can be obtained
+    via kegg_info('organism') or http://rest.kegg.jp/list/organism
+
+    """
+    # TODO - return a string (rather than the handle?)
+    # TODO - chache and validate the organism code / T numbers?
+    # TODO - can we parse the somewhat formatted output?
+    #
     # http://rest.kegg.jp/info/<database>
     #
     # <database> = pathway | brite | module | disease | drug | environ |
@@ -40,6 +69,17 @@ def kegg_info(database):
 
 
 def kegg_list(database, org=None):
+    """KEGG list - Entry list for database, or specified database entries.
+
+    db - database or organism (string)
+    org - optional organism (string), see below.
+
+    For the pathway and module databases the optional organism can be
+    used to restrict the results.
+
+    """
+    # TODO - split into two functions (dbentries seems separate)?
+    #
     #  http://rest.kegg.jp/list/<database>/<org>
     #
     #  <database> = pathway | module
@@ -75,6 +115,25 @@ def kegg_list(database, org=None):
 
 
 def kegg_find(database, query, option=None):
+    """KEGG find - Data search.
+
+    Finds entries with matching query keywords or other query data in
+    a given database.
+
+    db - database or organism (string)
+    query - search terms (string)
+    option - search option (string), see below.
+
+    For the compound and drug database, set option to the string 'formula',
+    'exact_mass' or 'mol_weight' to search on that field only. The
+    chemical formula search is a partial match irrespective of the order
+    of atoms given. The exact mass (or molecular weight) is checked by
+    rounding off to the same decimal place as the query data. A range of
+    values may also be specified with the minus(-) sign.
+
+    """
+    # TODO - return list of tuples?
+    #
     # http://rest.kegg.jp/find/<database>/<query>/<option>
     #
     # <database> = compound | drug
@@ -100,6 +159,17 @@ def kegg_find(database, query, option=None):
 
 
 def kegg_get(dbentries, option=None):
+    """KEGG get - Data retrieval.
+
+    dbentries - Identifiers (single string, or list of strings), see below.
+    option - One of "aaseq", "ntseq", "mol", "kcf", "image", "kgml" (string)
+
+    The input is limited up to 10 entries.
+    The input is limited to one pathway entry with the image or kgml option.
+    The input is limited to one compound/glycan/drug entry with the image option.
+
+    Returns a handle.
+    """
     if isinstance(dbentries, list) and len(dbentries) <= 10:
         dbentries = "+".join(dbentries)
     elif isinstance(dbentries, list) and len(dbentries) > 10:
@@ -125,6 +195,12 @@ def kegg_get(dbentries, option=None):
 
 
 def kegg_conv(target_db, source_db, option=None):
+    """KEGG conv – convert KEGG identifiers to/from outside identifiers
+
+    target_db - Target database
+    source_db_or_dbentries - source database or database entries
+    option - Can be "turtle" or "n-triple" (string).
+    """
     # http://rest.kegg.jp/conv/<target_db>/<source_db>[/<option>]
     #
     # (<target_db> <source_db>) = (<kegg_db> <outside_db>) |
@@ -177,6 +253,12 @@ def kegg_conv(target_db, source_db, option=None):
 
 
 def kegg_link(target_db, source_db, option=None):
+    """KEGG link - find related entries by using database cross-references.
+
+    target_db - Target database
+    source_db_or_dbentries - source database
+    option - Can be "turtle" or "n-triple" (string).
+    """
     # http://rest.kegg.jp/link/<target_db>/<source_db>[/<option>]
     #
     # <target_db> = <database>
