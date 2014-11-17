95f0e0fea3a58f8366111d78b3225739cbea14cc
carlosp420 <mycalesis@gmail.com>
2014-11-17 16:07:32 +0200

    Convert test_NCBI_qblast.py to unittest
    I also added a html page in Tests/Blast to be used for testing the function
    `NCBIWWW._parse_qblast_ref_page`.
    I am not testing all cases of this function as I couldn't reproduce the
    necessary error pages in NCBI.
    
    Coverage of `NCBIWWW` went up to 78%.

--- Tests/test_NCBI_qblast.py
+++ Tests/test_NCBI_qblast.py
@@ -13,8 +13,10 @@ Goals:
     Make sure we can parse the latest XML format being used by the NCBI.
 """
 from __future__ import print_function
+import unittest
 
 from Bio._py3k import HTTPError
+from Bio._py3k import StringIO
 
 import requires_internet
 requires_internet.check()
@@ -34,96 +36,112 @@ from Bio.Blast import NCBIXML
 # - expectation value threshold
 # - Entrez filter string (or None)
 # - list of hit identifiers expected to be found (or None if expect 0)
-tests = [
-    # Simple protein blast filtered for rat only, using protein GI:160837788
-    # the actin related protein 2/3 complex, subunit 1B [Mus musculus]
-    ("blastp", "nr", "160837788", 0.001,
-     "rat [ORGN]", ['9506405', '13592137', '37589612', '149064087', '56912225']),
-    # This next example finds PCR primer matches in Chimpanzees, e.g. BRCA1:
-    ("blastn", "nr", "GTACCTTGATTTCGTATTC"+("N"*30)+"GACTCTACTACCTTTACCC",
-     10, "pan [ORGN]", ["37953274", "51104367", "51104367", "51104367"]),
-    # Try an orchid EST (nucleotide) sequence against NR using BLASTX
-    ("blastx", "nr", """>gi|116660609|gb|EG558220.1|EG558220 CR02019H04 Leaf CR02 cDNA library Catharanthus roseus cDNA clone CR02019H04 5', mRNA sequence
-CTCCATTCCCTCTCTATTTTCAGTCTAATCAAATTAGAGCTTAAAAGAATGAGATTTTTAACAAATAAAA
-AAACATAGGGGAGATTTCATAAAAGTTATATTAGTGATTTGAAGAATATTTTAGTCTATTTTTTTTTTTT
-TCTTTTTTTGATGAAGAAAGGGTATATAAAATCAAGAATCTGGGGTGTTTGTGTTGACTTGGGTCGGGTG
-TGTATAATTCTTGATTTTTTCAGGTAGTTGAAAAGGTAGGGAGAAAAGTGGAGAAGCCTAAGCTGATATT
-GAAATTCATATGGATGGAAAAGAACATTGGTTTAGGATTGGATCAAAAAATAGGTGGACATGGAACTGTA
-CCACTACGTCCTTACTATTTTTGGCCGAGGAAAGATGCTTGGGAAGAACTTAAAACAGTTTTAGAAAGCA
-AGCCATGGATTTCTCAGAAGAAAATGATTATACTTCTTAATCAGGCAACTGATATTATCAATTTATGGCA
-GCAGAGTGGTGGCTCCTTGTCCCAGCAGCAGTAATTACTTTTTTTTCTCTTTTTGTTTCCAAATTAAGAA
-ACATTAGTATCATATGGCTATTTGCTCAATTGCAGATTTCTTTCTTTTGTGAATG""",
-     0.0000001, None, ["21554275", "18409071", "296087288"]),
-]
 
 print("Checking Bio.Blast.NCBIWWW.qblast() with various queries")
-for program, database, query, e_value, entrez_filter, expected_hits in tests:
-    print("qblast('%s', '%s', %s, ...)" % (program, database, repr(query)))
-    try:
-        if program=="blastn":
-            # Check the megablast parameter is accepted
-            handle = NCBIWWW.qblast(program, database, query,
-                                    alignments=10, descriptions=10,
-                                    hitlist_size=10,
-                                    entrez_query=entrez_filter,
-                                    expect=e_value, megablast="FALSE")
-        else:
-            handle = NCBIWWW.qblast(program, database, query,
-                                    alignments=10, descriptions=10,
-                                    hitlist_size=10,
-                                    entrez_query=entrez_filter,
-                                    expect=e_value)
-    except HTTPError:
-        # e.g. a proxy error
-        raise MissingExternalDependencyError("internet connection failed")
-    record = NCBIXML.read(handle)
 
-    if record.query == "No definition line":
-        # We used a sequence as the query
-        assert len(query) == record.query_letters
-    elif query.startswith(">"):
-        # We used a FASTA record as the query
-        assert query[1:].split("\n", 1)[0] == (record.query)
-    else:
-        # We used an identifier as the query
-        assert query in record.query_id.split("|")
 
-    # Check the recorded input parameters agree with those requested
-    assert float(record.expect) == e_value
-    assert record.application.lower() == program
-    assert len(record.alignments) <= 10
-    assert len(record.descriptions) <= 10
+class TestQblast(unittest.TestCase):
+    def setUp(self):
+        self.tests = [
+            # Simple protein blast filtered for rat only, using protein GI:160837788
+            # the actin related protein 2/3 complex, subunit 1B [Mus musculus]
+            ("blastp", "nr", "160837788", 0.001,
+            "rat [ORGN]", ['9506405', '13592137', '37589612', '149064087', '56912225']),
+            # This next example finds PCR primer matches in Chimpanzees, e.g. BRCA1:
+            ("blastn", "nr", "GTACCTTGATTTCGTATTC"+("N"*30)+"GACTCTACTACCTTTACCC",
+             10, "pan [ORGN]", ["37953274", "51104367", "51104367", "51104367"]),
+            # Try an orchid EST (nucleotide) sequence against NR using BLASTX
+            ("blastx", "nr", """>gi|116660609|gb|EG558220.1|EG558220 CR02019H04 Leaf CR02 cDNA library Catharanthus roseus cDNA clone CR02019H04 5', mRNA sequence
+        CTCCATTCCCTCTCTATTTTCAGTCTAATCAAATTAGAGCTTAAAAGAATGAGATTTTTAACAAATAAAA
+        AAACATAGGGGAGATTTCATAAAAGTTATATTAGTGATTTGAAGAATATTTTAGTCTATTTTTTTTTTTT
+        TCTTTTTTTGATGAAGAAAGGGTATATAAAATCAAGAATCTGGGGTGTTTGTGTTGACTTGGGTCGGGTG
+        TGTATAATTCTTGATTTTTTCAGGTAGTTGAAAAGGTAGGGAGAAAAGTGGAGAAGCCTAAGCTGATATT
+        GAAATTCATATGGATGGAAAAGAACATTGGTTTAGGATTGGATCAAAAAATAGGTGGACATGGAACTGTA
+        CCACTACGTCCTTACTATTTTTGGCCGAGGAAAGATGCTTGGGAAGAACTTAAAACAGTTTTAGAAAGCA
+        AGCCATGGATTTCTCAGAAGAAAATGATTATACTTCTTAATCAGGCAACTGATATTATCAATTTATGGCA
+        GCAGAGTGGTGGCTCCTTGTCCCAGCAGCAGTAATTACTTTTTTTTCTCTTTTTGTTTCCAAATTAAGAA
+        ACATTAGTATCATATGGCTATTTGCTCAATTGCAGATTTCTTTCTTTTGTGAATG""",
+             0.0000001, None, ["21554275", "18409071", "296087288"]),
+        ]
 
-    # Check the expected result(s) are found in the alignments
-    if expected_hits is None:
-        assert len(record.alignments)==0, "Expected no alignments!"
-    else:
-        assert len(record.alignments) > 0, "Expected some alignments!"
-        found_result = False
-        for expected_hit in expected_hits:
-            for alignment in record.alignments:
-                if expected_hit in alignment.hit_id.split("|"):
-                    found_result = True
-                    break
-        if len(expected_hits)==1:
-            print("Update this test to have some redundancy...")
-            for alignment in record.alignments:
-                print(alignment.hit_id)
-        assert found_result, "Missing all of %s in alignments" \
-               % ", ".join(expected_hits)
+    def test_qblast(self):
+        for program, database, query, e_value, entrez_filter, expected_hits in self.tests:
+            try:
+                if program == "blastn":
+                    # Check the megablast parameter is accepted
+                    handle = NCBIWWW.qblast(program, database, query,
+                                            alignments=10, descriptions=10,
+                                            hitlist_size=10,
+                                            entrez_query=entrez_filter,
+                                            expect=e_value, megablast="FALSE")
+                else:
+                    handle = NCBIWWW.qblast(program, database, query,
+                                            alignments=10, descriptions=10,
+                                            hitlist_size=10,
+                                            entrez_query=entrez_filter,
+                                            expect=e_value)
+            except HTTPError:
+                # e.g. a proxy error
+                raise MissingExternalDependencyError("internet connection failed")
+            record = NCBIXML.read(handle)
 
-    # Check the expected result(s) are found in the descriptions
-    if expected_hits is None:
-        assert len(record.descriptions)==0, "Expected no descriptions!"
-    else:
-        assert len(record.descriptions) > 0, "Expected some descriptions!"
-        found_result = False
-        for expected_hit in expected_hits:
-            for descr in record.descriptions:
-                if expected_hit == descr.accession \
-                or expected_hit in descr.title.split(None, 1)[0].split("|"):
-                    found_result = True
-                    break
-        assert found_result, "Missing all of %s in descriptions" % expected_hit
+            if record.query == "No definition line":
+                # We used a sequence as the query
+                self.assertEqual(len(query), record.query_letters)
+            elif query.startswith(">"):
+                # We used a FASTA record as the query
+                expected = query[1:].split("\n", 1)[0]
+                self.assertEqual(expected, record.query)
+            else:
+                # We used an identifier as the query
+                self.assertTrue(query in record.query_id.split("|"))
 
-print("Done")
+            # Check the recorded input parameters agree with those requested
+            self.assertEqual(float(record.expect), e_value)
+            self.assertEqual(record.application.lower(), program)
+            self.assertLessEqual(len(record.alignments), 10)
+            self.assertLessEqual(len(record.descriptions), 10)
+
+            # Check the expected result(s) are found in the alignments
+            if expected_hits is None:
+                self.assertEqual(len(record.alignments), 0)  # Expected no alignments!
+            else:
+                self.assertGreater(len(record.alignments), 0)  # Expected some alignments!
+                found_result = False
+                for expected_hit in expected_hits:
+                    for alignment in record.alignments:
+                        if expected_hit in alignment.hit_id.split("|"):
+                            found_result = True
+                            break
+                if len(expected_hits) == 1:
+                    print("Update this test to have some redundancy...")
+                    for alignment in record.alignments:
+                        print(alignment.hit_id)
+                assert found_result, "Missing all of %s in alignments" \
+                       % ", ".join(expected_hits)
+                self.assertTrue(found_result)
+
+            # Check the expected result(s) are found in the descriptions
+            if expected_hits is None:
+                self.assertEqual(len(record.descriptions), 0)  # Expected no descriptions!
+            else:
+                self.assertGreater(len(record.descriptions), 0)  # Expected some descriptions!
+                found_result = False
+                for expected_hit in expected_hits:
+                    for descr in record.descriptions:
+                        if expected_hit == descr.accession \
+                        or expected_hit in descr.title.split(None, 1)[0].split("|"):
+                            found_result = True
+                            break
+                assert found_result, "Missing all of %s in descriptions" % expected_hit
+                self.assertTrue(found_result)
+
+    def test__parse_qblast_ref_page(self):
+        with open("Blast/html_msgid_29_blastx_001.html", "r") as f:
+            handle = StringIO(f.read())
+        self.assertRaises(ValueError, NCBIWWW._parse_qblast_ref_page, handle)
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
