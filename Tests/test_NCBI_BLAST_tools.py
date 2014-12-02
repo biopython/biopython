6d5a8593543a16a190c8d0f65c5b633423883b2f
peterjc <p.j.a.cock@googlemail.com>
2014-12-02 14:38:19 +0000

    BLAST+ 2.2.30 deliberately removed -word_size for RPS-BLAST
    According to the BLAST developers (by email), the word size
    is specified when the domain database is built, and so the
    option should never should have been in rpsblast and rpstblastn.
    
    TODO: Remove this from our rpsblast and rpstblastn wrappers.

--- Tests/test_NCBI_BLAST_tools.py
+++ Tests/test_NCBI_BLAST_tools.py
@@ -255,6 +255,7 @@ class CheckCompleteArgList(unittest.TestCase):
             # Removed in BLAST 2.2.29+ so will look like extra args on new BLAST
             extra = extra.difference(["-gilist", "-negative_gilist"])
             # Removed in BLAST 2.2.30 so will look like extra args on new BLAST
+            # Apparently -word_size should never have been added to these tools.
             extra = extra.difference(["-word_size"])
         if exe_name == "deltablast":
             # New in BLAST+ 2.2.29 so will look like extra args on BLAST+ 2.2.28
