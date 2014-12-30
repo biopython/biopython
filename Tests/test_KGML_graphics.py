77e8bb6acef0211a645506f827c81eafab63968f
widdowquinn <leightonpritchard@mac.com>
2014-12-30 18:42:41 +0000

    test_KGML_graphics_online.py: Split test with online requirements from main test.

--- Tests/test_KGML_graphics.py
+++ Tests/test_KGML_graphics.py
@@ -40,6 +40,8 @@ from Bio.Graphics.KGML_vis import KGMLCanvas
 __docformat__ = "restructuredtext en"
 
 
+# The PathwayData class is also imported by the online test module in
+# test_KGML_graphics_online.py
 class PathwayData(object):
     """Convenience structure for testing pathway data"""
     def __init__(self, name, element_counts, show_pathway_image=False):
@@ -101,22 +103,6 @@ class KGMLPathwayTest(unittest.TestCase):
                 kgml_map = KGMLCanvas(pathway)
                 kgml_map.import_imagemap = p.show_pathway_image
                 kgml_map.draw(p.output_stem + '_original.pdf')
-
-    def test_render_KGML_import_map(self):
-        """Basic rendering of KGML: use imported imagemap
-
-        Uses the URL indicated in the .xml file.
-
-        This test may fail if the imagemap is not available (e.g. if
-        there is not web connection), and may look odd if the remote
-        imagemap has changed since the local KGML file was downloaded.
-        """
-        # We test rendering of the original KEGG KGML using imported files
-        for p in self.data:
-            with open(p.infilename, 'rU') as f:
-                pathway = read(f)
-                kgml_map = KGMLCanvas(pathway, import_imagemap=True)
-                kgml_map.draw(p.output_stem + '_importmap.pdf')
 
     def test_render_KGML_modify(self):
         """Rendering of KGML to PDF, with modification."""
