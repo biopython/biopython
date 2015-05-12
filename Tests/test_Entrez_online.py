366ed69f3ad805a4fbb4fbafa2a543227815c034
David <danich1@umbc.edu>
2015-05-12 00:30:29 -0400

    Fixed the Version Discrepancy
    I made the necessary changes for the code to work in python2.6, python
    2.7 and python 3. I believe everything should be in order now.

--- Tests/test_Entrez_online.py
+++ Tests/test_Entrez_online.py
@@ -164,6 +164,15 @@ class EntrezOnlineCase(unittest.TestCase):
         expected_result = "proc natl acad sci u s a|1991|88|3248|mann bj|citation_1|2014248\n"
         self.assertEquals(result, expected_result)
 
+    def test_fetch_xml_schemas(self):
+        handle = Entrez.efetch("protein", id="783730874", rettype="ipg", retmode="xml")
+        records = list(Entrez.parse(handle))
+        handle.close()
+        self.assertEqual(len(records), 1)
+        self.assertTrue("'Product" in records[0])
+        self.assertTrue("Statistics" in records[0])
+        self.assertTrue("RedundantGiList" in records[0])
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
