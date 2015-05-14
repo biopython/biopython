11f2c46f5587a9f8a466bf85b95a8eab8a327f5f
Eric Rasche <esr@tamu.edu>
2015-05-14 15:40:50 -0500

    Initial version of Mauve xmfa parser

---
+++ Bio/AlignIO/MauveIO.py
@@ -0,0 +1,160 @@
+# Copyright 2015-2015 by Eric Rasche.  All rights reserved.
+#
+# This code is part of the Biopython distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+"""Bio.AlignIO support for "xmfa" output from Mauve/ProgressiveMauve.
+
+You are expected to use this module via the Bio.AlignIO functions (or the
+Bio.SeqIO functions if you want to work directly with the gapped sequences).
+
+For example, consider a progressiveMauve alignment file containing the following::
+
+    #FormatVersion Mauve1
+    #Sequence1File	three.fa
+    #Sequence1Entry	1
+    #Sequence1Format	FastA
+    #Sequence2File	three.fa
+    #Sequence2Entry	2
+    #Sequence2Format	FastA
+    #Sequence3File	three.fa
+    #Sequence3Entry	3
+    #Sequence3Format	FastA
+    #BackboneFile	three.xmfa.bbcols
+    > 1:0-0 + three.fa
+    --------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------
+    > 2:5417-5968 + three.fa
+    TTTAAACATCCCTCGGCCCGTCGCCCTTTTATAATAGCAGTACGTGAGAGGAGCGCCCTAAGCTTTGGGAAATTCAAGC-
+    --------------------------------------------------------------------------------
+    CTGGAACGTACTTGCTGGTTTCGCTACTATTTCAAACAAGTTAGAGGCCGTTACCTCGGGCGAACGTATAAACCATTCTG
+    > 3:9476-10076 - three.fa
+    TTTAAACACCTTTTTGGATG--GCCCAGTTCGTTCAGTTGTG-GGGAGGAGATCGCCCCAAACGTATGGTGAGTCGGGCG
+    TTTCCTATAGCTATAGGACCAATCCACTTACCATACGCCCGGCGTCGCCCAGTCCGGTTCGGTACCCTCCATGACCCACG
+    ---------------------------------------------------------AAATGAGGGCCCAGGGTATGCTT
+    =
+    > 2:5969-6015 + three.fa
+    -----------------------
+    GGGCGAACGTATAAACCATTCTG
+    > 3:9429-9476 - three.fa
+    TTCGGTACCCTCCATGACCCACG
+    AAATGAGGGCCCAGGGTATGCTT
+
+This is a multiple sequence alignment with multiple aligned sections, so you
+would probably load this using the Bio.AlignIO.parse() function:
+
+    >>> from Bio import AlignIO
+    >>> align = AlignIO.parse("Mauve/simple.xmfa", "mauve")
+    >>> alignments = list(align)
+    >>> for aln in alignments:
+    ...     print(align)
+    SingleLetterAlphabet() alignment with 3 rows and 240 columns
+    --------------------------------------------...--- 1
+    TTTAAACATCCCTCGGCCCGTCGCCCTTTTATAATAGCAGTACG...CTG 2
+    TTTAAACACCTTTTTGGATG--GCCCAGTTCGTTCAGTTGTG-G...CTT 3
+    SingleLetterAlphabet() alignment with 3 rows and 46 columns
+    ---------------------------------------------- 1
+    -----------------------GGGCGAACGTATAAACCATTCTG 2
+    TTCGGTACCCTCCATGACCCACGAAATGAGGGCCCAGGGTATGCTT 3
+
+Additional information is extracted from the XMFA file and available through
+the annotation attribute of each record::
+
+    >>> for record in alignments[0]:
+    ...   print record.id, len(record), record.annotations
+    1 240 {'start': 0, 'end': 0, 'strand': 1}
+    2 240 {'start': 5417, 'end': 5968, 'strand': 1}
+    3 240 {'start': 9476, 'end': 10076, 'strand': -1}
+
+"""
+
+from __future__ import print_function
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from .Interfaces import AlignmentIterator, SequentialAlignmentWriter
+
+__docformat__ = "restructuredtext en"
+
+
+class MauveIterator(AlignmentIterator):
+    """Mauve xmfa alignment iterator."""
+
+    _ids = []  # for caching IDs between __next__ calls
+
+    def __next__(self):
+        handle = self.handle
+        line = handle.readline()
+
+        if not line:
+            raise StopIteration
+
+        # Strip out header comments
+        while line and line.strip().startswith('#'):
+            line = handle.readline()
+
+        seqs = {}
+        seq_regions = {}
+        passed_end_alignment = False
+
+        latest_id = None
+        while True:
+            if not line:
+                break  # end of file
+            line = line.strip()
+
+            if line == '=':
+                # There may be more data, but we've reached the end of this
+                # alignment
+                break
+            elif line.startswith('>'):
+                parts = line.split()
+                id, start, end = self._identifier_split(parts[1])
+
+                if id not in self._ids:
+                    self._ids.append(id)
+
+                seq_regions[id] = (start, end, parts[2])
+
+                seqs.setdefault(id, '')
+                latest_id = id
+            else:
+                assert not passed_end_alignment
+                if latest_id is None:
+                    raise ValueError("Saw sequence before definition line")
+                seqs[latest_id] += line
+            line = handle.readline()
+
+        assert len(seqs) <= len(self._ids)
+
+        self.ids = self._ids
+        self.sequences = seqs
+
+        if self._ids and seqs:
+            alignment_length = len(list(seqs.values())[0])
+            records = []
+            for id in self._ids:
+                seq = seqs.get(id, '-' * alignment_length)
+                if alignment_length != len(seq):
+                    raise ValueError("Sequences have different lengths, or repeated identifier")
+                record = SeqRecord(Seq(seq, self.alphabet), id=id, name=id,
+                                   description=id)
+
+                if id in seq_regions:
+                    record.annotations["start"] = seq_regions[id][0]
+                    record.annotations["end"] = seq_regions[id][1]
+                    record.annotations["strand"] = 1 if seq_regions[id][2] == '+' else -1
+
+                records.append(record)
+            alignment = MultipleSeqAlignment(records, self.alphabet)
+            return alignment
+        else:
+            raise StopIteration
+
+    def _identifier_split(self, identifier):
+        """Returns (name, start, end) string tuple from an identifier"""
+        id, loc = identifier.split(':')
+        start, end = map(int, loc.split('-'))
+        return id, start, end
