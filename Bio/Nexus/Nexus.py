7aef682aabbbf51a613ae4c238f7c966cbb4b6d5
jcora <cora.1@osu.edu>
2015-06-17 17:31:17 -0400

    Added current year to copyright string

--- Bio/Nexus/Nexus.py
+++ Bio/Nexus/Nexus.py
@@ -1,5 +1,5 @@
 # Copyright 2005-2008 by Frank Kauff & Cymon J. Cox. All rights reserved.
-#                2014 by Joe Cora (standard data)
+#           2014-2015 by Joe Cora (standard data)
 # This code is part of the Biopython distribution and governed by its
 # license. Please see the LICENSE file that should have been included
 # as part of this package.
