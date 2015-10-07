80d634bd189626ba3e49b712f21e3617e8e625b1
John Bradley <johnbradley2008@gmail.com>
2015-10-07 17:59:38 -0400

    skip test if no numpy

--- Tests/test_pairwise2.py
+++ Tests/test_pairwise2.py
@@ -6,6 +6,13 @@
 
 import unittest
 
+try:
+    import numpy as np
+except ImportError:
+    from Bio import MissingPythonDependencyError
+    raise MissingPythonDependencyError(
+        "Install NumPy if you want to use Bio.SVDSuperimposer.")
+
 from Bio import pairwise2
 
 
