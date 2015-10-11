a5a07520fcab4769510c258ad9189fe68d72bb48
Peter Cock <p.j.a.cock@googlemail.com>
2015-10-11 13:11:43 +0100

    Skip test_seq.py on Python 2.6 if missing unittest2

--- Tests/test_seq.py
+++ Tests/test_seq.py
@@ -10,7 +10,11 @@ import warnings
 
 # Remove unittest2 import after dropping support for Python2.6
 if sys.version_info < (2, 7):
-    import unittest2 as unittest
+    try:
+        import unittest2 as unittest
+    except ImportError:
+        from Bio import MissingPythonDependencyError
+        raise MissingPythonDependencyError("Under Python 2.6 this test needs the unittest2 library")
 else:
     import unittest
 
