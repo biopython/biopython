2f2e7fb60c338be05c85d1c955deec340af6bbfe
carlosp420 <mycalesis@gmail.com>
2015-10-14 21:38:14 +0300

    increased coverage for SeqRecord

--- Tests/test_SeqRecord.py
+++ Tests/test_SeqRecord.py
@@ -11,7 +11,7 @@ and confirms they are consistent using our different parsers.
 import unittest
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna, generic_rna, generic_protein
-from Bio.Seq import Seq
+from Bio.Seq import Seq, MutableSeq
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature, FeatureLocation, ExactPosition
 from Bio.SeqFeature import WithinPosition, BeforePosition, AfterPosition, OneOfPosition
@@ -61,6 +61,30 @@ class SeqRecordCreation(unittest.TestCase):
         except (TypeError, ValueError) as e:
             pass
 
+    def test_valid_id(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), id=dict())
+
+    def test_valid_name(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), name=dict())
+
+    def test_valid_description(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), description=dict())
+
+    def test_valid_dbxrefs(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), dbxrefs=dict())
+
+    def test_valid_annotations(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), annotations=list())
+
+    def test_valid_features(self):
+        with self.assertRaises(TypeError):
+            SeqRecord(Seq("ACGT", generic_dna), features=dict())
+
 
 class SeqRecordMethods(unittest.TestCase):
     """Test SeqRecord methods."""
@@ -78,7 +102,48 @@ class SeqRecordMethods(unittest.TestCase):
                                 letter_annotations={"fake": "X" * 26},
                                 features=[f0, f1, f2, f3])
 
-    def test_slice_variantes(self):
+    def test_iter(self):
+        for amino in self.record:
+            self.assertEqual("A", amino)
+            break
+
+    def test_contains(self):
+        self.assertTrue(Seq("ABC", generic_protein) in self.record)
+
+    def test_str(self):
+        expected = """
+ID: TestID
+Name: TestName
+Description: TestDescr
+Database cross-references: TestXRef
+Number of features: 4
+/k=v
+Per letter annotation for: fake
+Seq('ABCDEFGHIJKLMNOPQRSTUVWZYX', ProteinAlphabet())"""
+        self.assertEqual(expected.lstrip(), str(self.record))
+
+    def test_repr(self):
+        expected = "SeqRecord(seq=Seq('ABCDEFGHIJKLMNOPQRSTUVWZYX', ProteinAlphabet()), " \
+                   "id='TestID', name='TestName', description='TestDescr', dbxrefs=['TestXRef'])"
+        self.assertEqual(expected, repr(self.record))
+
+    def test_format(self):
+        expected = ">TestID TestDescr\nABCDEFGHIJKLMNOPQRSTUVWZYX\n"
+        self.assertEqual(expected, self.record.format('fasta'))
+
+    def test_upper(self):
+        self.assertEqual("ABCDEFGHIJKLMNOPQRSTUVWZYX", str(self.record.lower().upper().seq))
+
+    def test_lower(self):
+        self.assertEqual("abcdefghijklmnopqrstuvwzyx", str(self.record.lower().seq))
+
+    def test_slicing(self):
+        self.assertEqual("B", self.record[1])
+        self.assertEqual("BC", self.record[1:3].seq)
+        with self.assertRaises(ValueError):
+            c = self.record['a'].seq
+
+    def test_slice_variants(self):
         """Simple slices using different start/end values"""
         for start in list(range(-30, 30)) + [None]:
             for end in list(range(-30, 30)) + [None]:
@@ -218,6 +283,47 @@ class SeqRecordMethods(unittest.TestCase):
             self.assertEqual(rec.annotations, {})  # May change this...
             self.assertEqual(rec.letter_annotations, {"fake": "X" * 26})
             self.assertTrue(len(rec.features) <= len(self.record.features))
+
+    def test_reverse_complement_seq(self):
+        s = SeqRecord(Seq("ACTG"), id="TestID", name="TestName",
+                      description="TestDescription", dbxrefs=["TestDbxrefs"],
+                      features=[SeqFeature(FeatureLocation(0, 3), type="Site")],
+                      annotations={'organism': 'bombyx'}, letter_annotations={'test': 'abcd'})
+        rc = s.reverse_complement(id=True, name=True, description=True,
+                                  dbxrefs=True, features=True, annotations=True, letter_annotations=True)
+
+        self.assertEqual("CAGT", str(rc.seq))
+        self.assertEqual("TestID", rc.id)
+        self.assertEqual("TestID", s.reverse_complement(id="TestID").id)
+
+        self.assertEqual("TestName", rc.name)
+        self.assertEqual("TestName", s.reverse_complement(name="TestName").name)
+
+        self.assertEqual("TestDescription", rc.description)
+        self.assertEqual("TestDescription",
+                         s.reverse_complement(description="TestDescription").description)
+
+        self.assertEqual(["TestDbxrefs"], rc.dbxrefs)
+        self.assertEqual(["TestDbxrefs"],
+                         s.reverse_complement(dbxrefs=["TestDbxrefs"]).dbxrefs)
+
+        self.assertEqual("[SeqFeature(FeatureLocation(ExactPosition(1), ExactPosition(4)), type='Site')]",
+                         repr(rc.features))
+        rc2 = s.reverse_complement(features=[SeqFeature(FeatureLocation(1, 4), type="Site")])
+        self.assertEqual("[SeqFeature(FeatureLocation(ExactPosition(1), ExactPosition(4)), type='Site')]",
+                         repr(rc2.features))
+
+        self.assertEqual({'organism': 'bombyx'}, rc.annotations)
+        self.assertEqual({'organism': 'bombyx'},
+                         s.reverse_complement(annotations={'organism': 'bombyx'}).annotations)
+
+        self.assertEqual({'test': 'dcba'}, rc.letter_annotations)
+        self.assertEqual({'test': 'abcd'},
+                         s.reverse_complement(letter_annotations={'test': 'abcd'}).letter_annotations)
+
+    def test_reverse_complement_mutable_seq(self):
+        s = SeqRecord(MutableSeq("ACTG"))
+        self.assertEqual("CAGT", str(s.reverse_complement().seq))
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
