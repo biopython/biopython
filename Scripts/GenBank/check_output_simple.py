03a33dc1135d26e1d0a17ed8cb30e169d33cbfe1
Vincent Davis <vincent@vincentdavis.net>
2015-12-04 16:35:17 -0700

    Use with open in Scripts/GenBank/check_output_simple.py

--- Scripts/GenBank/check_output_simple.py
+++ Scripts/GenBank/check_output_simple.py
@@ -23,32 +23,30 @@ if len(sys.argv) != 2:
 
 parser = GenBank.FeatureParser(debug_level=2)
 
-handle = open(sys.argv[1], 'r')
+with open(sys.argv[1], 'r') as handle:
 
-iterator = GenBank.Iterator(handle, parser)
+    iterator = GenBank.Iterator(handle, parser)
 
-while True:
-    cur_record = next(iterator)
+    while True:
+        cur_record = next(iterator)
 
-    if not(cur_record):
-        break
+        if not cur_record:
+            break
 
-    print("***Record")
-    print("Seq: %s" % cur_record.seq)
-    print("Id: %s" % cur_record.id)
-    print("Name: %s" % cur_record.name)
-    print("Description: %s" % cur_record.description)
-    print("Annotations****")
-    for annotation_key in cur_record.annotations:
-        if annotation_key != 'references':
-            print("Key: %s" % annotation_key)
-            print("Value: %s" % cur_record.annotations[annotation_key])
-        else:
-            print("References*")
-            for reference in cur_record.annotations[annotation_key]:
-                print(str(reference))
-    print("Feaures")
-    for feature in cur_record.features:
-        print(feature)
-
-handle.close()
+        print("***Record")
+        print("Seq: %s" % cur_record.seq)
+        print("Id: %s" % cur_record.id)
+        print("Name: %s" % cur_record.name)
+        print("Description: %s" % cur_record.description)
+        print("Annotations****")
+        for annotation_key in cur_record.annotations:
+            if annotation_key != 'references':
+                print("Key: %s" % annotation_key)
+                print("Value: %s" % cur_record.annotations[annotation_key])
+            else:
+                print("References*")
+                for reference in cur_record.annotations[annotation_key]:
+                    print(str(reference))
+        print("Feaures")
+        for feature in cur_record.features:
+            print(feature)
