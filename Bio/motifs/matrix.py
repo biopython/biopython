17552e2c66908bf2c67572383950d8c5c43f3969
Nader Morshed <morshed.nader@gmail.com>
2016-01-21 13:50:08 -0500

    Set Seq.alphabet correctly in degenerate_consensus

--- Bio/motifs/matrix.py
+++ Bio/motifs/matrix.py
@@ -13,6 +13,7 @@ from Bio._py3k import range
 
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+from Bio import Alphabet
 
 
 class GenericPositionMatrix(dict):
@@ -201,7 +202,15 @@ class GenericPositionMatrix(dict):
                 key = "ACGT"
             nucleotide = degenerate_nucleotide.get(key, key)
             sequence += nucleotide
-        return Seq(sequence, alphabet=IUPAC.ambiguous_dna)
+        if isinstance(self.alphabet, Alphabet.DNAAlphabet):
+            alpha = IUPAC.ambiguous_dna
+        elif isinstance(self.alphabet, Alphabet.RNAAlphabet):
+            alpha = IUPAC.amiguous_rna
+        elif isinstance(self.alphabet, Alphabet.ProteinAlphabet):
+            alpha = IUPAC.protein
+        else:
+            raise Exception("Unknown alphabet")
+        return Seq(sequence, alphabet=alpha)
 
     @property
     def gc_content(self):
