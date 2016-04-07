009d913ac2d8ed010db20aa43eab489e0031d9dd
peterjc <p.j.a.cock@googlemail.com>
2016-04-07 17:13:50 +0100

    Make import in doctest explicit

--- Bio/Align/__init__.py
+++ Bio/Align/__init__.py
@@ -128,6 +128,7 @@ class MultipleSeqAlignment(_Alignment):
         >>> from Bio.Alphabet import generic_dna
         >>> from Bio.Seq import Seq
         >>> from Bio.SeqRecord import SeqRecord
+        >>> from Bio.Align import MultipleSeqAlignment
         >>> a = SeqRecord(Seq("AAAACGT", generic_dna), id="Alpha")
         >>> b = SeqRecord(Seq("AAA-CGT", generic_dna), id="Beta")
         >>> c = SeqRecord(Seq("AAAAGGT", generic_dna), id="Gamma")
