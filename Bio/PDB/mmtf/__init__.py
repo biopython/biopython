28e7f23c2f2059471fd9e97d6549ed77c7838ece
abradle <anthony.richard.bradley@gmail.com>
2016-06-15 11:56:45 -0700

    Updates of the code, docs etc. to comply with the QuantifiedCode checks

--- Bio/PDB/mmtf/__init__.py
+++ Bio/PDB/mmtf/__init__.py
@@ -7,16 +7,20 @@ def get_from_decoded(decoder):
     decoder.pass_data_on(structure_decoder)
     return structure_decoder.structure_bulder.get_structure()
 
-class MMTFParser():
-
-    def get_structure_from_url(self, pdb_id):
+class MMTFParser(object):
+    """
+    Class with static methods to get a BioPython structure from a url or a file.
+    """
+    @staticmethod
+    def get_structure_from_url(pdb_id):
         """Get a structure from a URL - given a PDB id
         :param pdb_id the input PDB id
         :return the structure"""
         decoder = fetch(pdb_id)
         return get_from_decoded(decoder)
 
-    def get_structure(self, file_path):
+    @staticmethod
+    def get_structure(file_path):
         """Get a structrue from a file - given a file path
         :param file_path the input file path
         :return the structure"""
