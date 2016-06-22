20db3c28b7f9438b6a0b479bc6c40048ca3f2c1f
peterjc <p.j.a.cock@googlemail.com>
2016-06-22 10:37:55 +0100

    Move NCBI gc.prt code to Scripts/update_ncbi_codon_table.py
    No code changes, simply moved the (commented out) code in
    Bio/Data/CodonTable.py used to parse the NCBI genetic codes
    from ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt to
    update Bio/Data/CodonTable.py into a separate script as
    Scripts/update_ncbi_codon_table.py

--- Bio/Data/CodonTable.py
+++ Bio/Data/CodonTable.py
@@ -3,8 +3,9 @@
 # as part of this package.
 """Codon tables based on those from the NCBI.
 
-These tables are based on parsing the NCBI file:
+These tables are based on parsing the NCBI file
 ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt
+using Scripts/update_ncbi_codon_table.py
 
 Last updated at Version 4.0
 """
@@ -472,83 +473,10 @@ def register_ncbi_table(name, alt_name, id,
         ambiguous_generic_by_name[name] = ambig_generic
 
 
-# These tables created from the data file
-#  ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt
-# using the following:
-# import re
-# for line in open("gc.prt").readlines():
-#     if line[:2] == " {":
-#         names = []
-#         id = None
-#         aa = None
-#         start = None
-#         bases = []
-#     elif line[:6] == "  name":
-#         names.append(re.search('"([^"]*)"', line).group(1))
-#     elif line[:8] == "    name":
-#         names.append(re.search('"(.*)$', line).group(1))
-#     elif line == ' Mitochondrial; Mycoplasma; Spiroplasma" ,\n':
-#         names[-1] = names[-1] + " Mitochondrial; Mycoplasma; Spiroplasma"
-#     elif line[:4] == "  id":
-#         id = int(re.search('(\d+)', line).group(1))
-#     elif line[:10] == "  ncbieaa ":
-#         aa = line[12:12+64]
-#     elif line[:10] == "  sncbieaa":
-#         start = line[12:12+64]
-#     elif line[:9] == "  -- Base":
-#         bases.append(line[12:12+64])
-#     elif line[:2] == " }":
-#         assert names != [] and id is not None and aa is not None
-#         assert start is not None and bases != []
-#         if len(names) == 1:
-#             names.append(None)
-#         print("register_ncbi_table(name=%s," % repr(names[0]))
-#         print("                    alt_name=%s, id=%d," % \
-#               (repr(names[1]), id))
-#         print("                    table={")
-#         s = "    "
-#         for i in range(64):
-#             if aa[i] != "*":
-#                 t = " '%s%s%s': '%s'," % (bases[0][i], bases[1][i],
-#                                           bases[2][i], aa[i])
-#                 if len(s) + len(t) > 75:
-#                     print(s)
-#                     s = "    " + t
-#                 else:
-#                     s = s + t
-#         print("%s }," % s)
+##########################################################################
+# Start of auto-generated output from Scripts/update_ncbi_codon_table.py #
+##########################################################################
 
-#         s = "                    stop_codons=["
-#         for i in range(64):
-#             if aa[i] == "*":
-#                 t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
-#                 if len(s) + len(t) > 75:
-#                     s_with_spaces = s.replace("','", "', '")
-#                     print(s_with_spaces)
-#                     s = "                                    " + t
-#                 else:
-#                     s = s + t
-#         s_with_spaces = s.replace("','", "', '")
-#         print("%s ]," % s_with_spaces)
-
-#         s = "                    start_codons=["
-#         for i in range(64):
-#             if start[i] == "M":
-#                 t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
-#                 if len(s) + len(t) > 75:
-#                     s_with_spaces = s.replace("','", "', '")
-#                     print(s_with_spaces)
-#                     s = "                                    " + t
-#                 else:
-#                     s = s + t
-#         s_with_spaces = s.replace("','", "', '")
-#         print("%s ]" % s_with_spaces)
-#         print("                    )")
-#     elif line[:2] == "--" or line == "\n" or line == "}\n" or \
-#          line == 'Genetic-code-table ::= {\n':
-#         pass
-#     else:
-#         raise Exception("Unparsed: " + repr(line))
 
 register_ncbi_table(name='Standard',
                     alt_name='SGC0', id=1,
@@ -913,6 +841,12 @@ register_ncbi_table(name='Candidate Division SR1 and Gracilibacteria',
                     stop_codons=['TAA', 'TAG', ],
                     start_codons=['TTG', 'CTG', 'ATG', ]
                     )
+
+
+########################################################################
+# End of auto-generated output from Scripts/update_ncbi_codon_table.py #
+########################################################################
+
 
 # Basic sanity test,
 for key, val in generic_by_name.items():
---
+++ Scripts/update_ncbi_codon_table.py
@@ -0,0 +1,88 @@
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+"""Helper script to update Codon tables from the NCBI.
+
+These tables are based on parsing the NCBI file:
+ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt
+
+This script is used to update Bio/Data/CodonTable.py
+
+Note that the NCBI sometimes revise the older tables,
+so don't just add new tables - replace all of them
+and check for any differences in the old tables.
+"""
+
+import re
+
+for line in open("gc.prt").readlines():
+    if line[:2] == " {":
+        names = []
+        id = None
+        aa = None
+        start = None
+        bases = []
+    elif line[:6] == "  name":
+        names.append(re.search('"([^"]*)"', line).group(1))
+    elif line[:8] == "    name":
+        names.append(re.search('"(.*)$', line).group(1))
+    elif line == ' Mitochondrial; Mycoplasma; Spiroplasma" ,\n':
+        names[-1] = names[-1] + " Mitochondrial; Mycoplasma; Spiroplasma"
+    elif line[:4] == "  id":
+        id = int(re.search('(\d+)', line).group(1))
+    elif line[:10] == "  ncbieaa ":
+        aa = line[12:12+64]
+    elif line[:10] == "  sncbieaa":
+        start = line[12:12+64]
+    elif line[:9] == "  -- Base":
+        bases.append(line[12:12+64])
+    elif line[:2] == " }":
+        assert names != [] and id is not None and aa is not None
+        assert start is not None and bases != []
+        if len(names) == 1:
+            names.append(None)
+        print("register_ncbi_table(name=%s," % repr(names[0]))
+        print("                    alt_name=%s, id=%d," % \
+              (repr(names[1]), id))
+        print("                    table={")
+        s = "    "
+        for i in range(64):
+            if aa[i] != "*":
+                t = " '%s%s%s': '%s'," % (bases[0][i], bases[1][i],
+                                          bases[2][i], aa[i])
+                if len(s) + len(t) > 75:
+                    print(s)
+                    s = "    " + t
+                else:
+                    s = s + t
+        print("%s }," % s)
+        s = "                    stop_codons=["
+        for i in range(64):
+            if aa[i] == "*":
+                t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
+                if len(s) + len(t) > 75:
+                    s_with_spaces = s.replace("','", "', '")
+                    print(s_with_spaces)
+                    s = "                                    " + t
+                else:
+                    s = s + t
+        s_with_spaces = s.replace("','", "', '")
+        print("%s ]," % s_with_spaces)
+        s = "                    start_codons=["
+        for i in range(64):
+            if start[i] == "M":
+                t = "'%s%s%s'," % (bases[0][i], bases[1][i], bases[2][i])
+                if len(s) + len(t) > 75:
+                    s_with_spaces = s.replace("','", "', '")
+                    print(s_with_spaces)
+                    s = "                                    " + t
+                else:
+                    s = s + t
+        s_with_spaces = s.replace("','", "', '")
+        print("%s ]" % s_with_spaces)
+        print("                    )")
+    elif line[:2] == "--" or line == "\n" or line == "}\n" or \
+         line == 'Genetic-code-table ::= {\n':
+        pass
+    else:
+        raise Exception("Unparsed: " + repr(line))
