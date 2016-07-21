70df33c365bedd5ecdb5f620e284352c6116e274
peterjc <p.j.a.cock@googlemail.com>
2016-07-21 15:46:47 +0100

    Use .dev0 rather than .dev as the development version suffix.
    This avoids the "python setup.py build" warning:
    
    UserWarning: Normalizing '1.68.dev' to '1.68.dev0'
    
    See mailing list discussion where Eric Talevich suggested this.

--- Bio/__init__.py
+++ Bio/__init__.py
@@ -10,7 +10,7 @@ of freely available Python tools for computational molecular biology.
 http://biopython.org
 """
 
-__version__ = "1.68.dev"
+__version__ = "1.68.dev0"
 
 
 class MissingExternalDependencyError(Exception):
