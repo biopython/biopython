49fd432f1b9e73f9a29230f31c8f2c919d120b02
Peter Cock <p.j.a.cock@googlemail.com>
2016-11-18 17:07:30 +0000

    Remove old hack to record EMBL/GenBank topology

--- Bio/GenBank/__init__.py
+++ Bio/GenBank/__init__.py
@@ -1261,13 +1261,6 @@ class _FeatureConsumer(_BaseGenBankConsumer):
                 raise ValueError("Could not determine alphabet for seq_type %s"
                                  % self._seq_type)
 
-            # Also save the chomosome layout - now semi-obsolete,
-            # see topology consumer
-            if 'circular' in self._seq_type.lower():
-                self.data.annotations['topology'] = 'circular'
-            elif 'linear' in self._seq_type.lower():
-                self.data.annotations['topology'] = 'linear'
-
         if not sequence and self.__expected_size:
             self.data.seq = UnknownSeq(self._expected_size, seq_alphabet)
         else:
