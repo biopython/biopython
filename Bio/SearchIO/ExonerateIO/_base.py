08d4eb344a9427184b3dd9badf7c45bebe1c1732
bow <bow@bow.web.id>
2016-12-11 19:25:32 +0100

    Update internal _get_fragments_phase docs

--- Bio/SearchIO/ExonerateIO/_base.py
+++ Bio/SearchIO/ExonerateIO/_base.py
@@ -86,8 +86,24 @@ def _get_fragments_coord(frags):
 
 
 def _get_fragments_phase(frags):
-    """Given a list of three-letter amino acid fragments, return their
-    phase."""
+    """Returns the phases of the given list of 3-letter amino acid fragments.
+
+    This is an internal private function and is meant for parsing Exonerate's
+    three-letter amino acid output.
+
+    >>> from Bio.SearchIO.ExonerateIO._base import _get_fragments_phase
+    >>> _get_fragments_phase(['Thr', 'Ser', 'Ala'])
+    [0, 0, 0]
+    >>> _get_fragments_phase(['ThrSe', 'rAla'])
+    [0, 1]
+    >>> _get_fragments_phase(['ThrSe', 'rAlaLeu', 'ProCys'])
+    [0, 1, 0]
+    >>> _get_fragments_phase(['ThrSe', 'rAlaLeuP', 'roCys'])
+    [0, 1, 2]
+    >>> _get_fragments_phase(['ThrSe', 'rAlaLeuPr', 'oCys'])
+    [0, 1, 1]
+
+    """
     return [(3 - (x % 3)) % 3 for x in _get_fragments_coord(frags)]
 
 
