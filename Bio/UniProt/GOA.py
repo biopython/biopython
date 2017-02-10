e9cebed70bbebbcfacf573894fa2ce967c8aba4d
Peter Cock <p.j.a.cock@googlemail.com>
2017-02-10 14:36:30 +0000

    GPI v2 parsing not implemented.
    Spotted with flake8: F821 undefined name '_gpi20iterator'

--- Bio/UniProt/GOA.py
+++ Bio/UniProt/GOA.py
@@ -175,7 +175,8 @@ def gpi_iterator(handle):
         return _gpi10iterator(handle)
     elif inline.strip() == '!gpi-version: 2.1':
         # sys.stderr.write("gpi 2.1\n")
-        return _gpi20iterator(handle)
+        # return _gpi20iterator(handle)
+        raise NotImplementedError("Sorry, parsing GPI version 2 not implemented yet.")
     else:
         raise ValueError('Unknown GPI version {0}\n'.format(inline))
 
