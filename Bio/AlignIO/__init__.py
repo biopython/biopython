07395b30014daf86dbafddb145d8845b5909d3fa
peterjc <p.j.a.cock@googlemail.com>
2017-04-07 16:57:57 +0100

    List these alphabetically

--- Bio/AlignIO/__init__.py
+++ Bio/AlignIO/__init__.py
@@ -173,14 +173,14 @@ _FormatToIterator = {  # "fasta" is done via Bio.SeqIO
 
 _FormatToWriter = {  # "fasta" is done via Bio.SeqIO
                      # "emboss" : EmbossIO.EmbossWriter, (unfinished)
+                   "clustal": ClustalIO.ClustalWriter,
                    "maf": MafIO.MafWriter,
+                   "mauve": MauveIO.MauveWriter,
                    "nexus": NexusIO.NexusWriter,
                    "phylip": PhylipIO.PhylipWriter,
                    "phylip-sequential": PhylipIO.SequentialPhylipWriter,
                    "phylip-relaxed": PhylipIO.RelaxedPhylipWriter,
                    "stockholm": StockholmIO.StockholmWriter,
-                   "clustal": ClustalIO.ClustalWriter,
-                   "mauve": MauveIO.MauveWriter,
                    }
 
 
