c5b502593eb77f6b0cb724912d79b19949d04040
Zheng Ruan <zruan1991@gmail.com>
2017-04-21 04:36:34 -0400

    delete unused get_codon_alphabet method
    get_codon_alphabet is defined twice. Delete the one that is not used in the code.

--- Bio/codonalign/codonalphabet.py
+++ Bio/codonalign/codonalphabet.py
@@ -21,27 +21,6 @@ from Bio.Data.CodonTable import generic_by_id
 default_codon_table = copy.deepcopy(generic_by_id[1])
 
 
-def get_codon_alphabet(alphabet, gap="-", stop="*"):
-    """Gets alignment alphabet for codon alignment.
-
-    Only nucleotide alphabet is accepted. Raise an error when the type of
-    alphabet is incompatible.
-    """
-    from Bio.Alphabet import NucleotideAlphabet
-    if isinstance(alphabet, NucleotideAlphabet):
-        alpha = alphabet
-        if gap:
-            alpha = Gapped(alpha, gap_char=gap)
-        if stop:
-            alpha = HasStopCodon(alpha, stop_symbol=stop)
-    else:
-        raise TypeError("Only Nuclteotide Alphabet is accepted!")
-    return alpha
-
-
-default_alphabet = get_codon_alphabet(IUPAC.unambiguous_dna)
-
-
 class CodonAlphabet(Alphabet):
     """Generic Codon Alphabet with a size of three"""
     size = 3
