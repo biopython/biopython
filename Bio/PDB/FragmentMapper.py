4c2f1a0964ba278477aac910893613871adb11f9
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2017-06-01 07:20:20 -0700

    Fixed RST formatting

--- Bio/PDB/FragmentMapper.py
+++ Bio/PDB/FragmentMapper.py
@@ -18,7 +18,7 @@ J Mol Biol. 2002 323(2):297-307.
 
 The definition files of the fragments can be obtained from:
 
-U{http://github.com/csblab/fragments/}
+http://github.com/csblab/fragments/
 
 You need these files to use this module.
 
