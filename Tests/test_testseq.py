c8424b8e99f60a167833180065a3de9de37a878d
Adil Iqbal <aiqbal85@gmail.com>
2017-06-06 16:57:33 -0500

    Update test_testseq.py

--- Tests/test_testseq.py
+++ Tests/test_testseq.py
@@ -1,3 +1,7 @@
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
 import unittest
 from Scripts.testseq import testseq
 from Bio.Alphabet import IUPAC, NucleotideAlphabet
