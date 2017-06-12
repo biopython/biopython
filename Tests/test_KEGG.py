0c14a1797ce8c27ab94bb247ea2a49136f39cb55
Peter Cock <p.j.a.cock@googlemail.com>
2017-06-12 15:03:04 +0100

    Check some of KEGG parser output

--- Tests/test_KEGG.py
+++ Tests/test_KEGG.py
@@ -22,6 +22,31 @@ class EnzymeTests(unittest.TestCase):
             records = list(Enzyme.parse(handle))
         self.assertEqual(len(records), 8)
         self.assertEqual(records[0].entry, "1.1.1.1")
+        self.assertEqual(records[0].name,
+                         ['Alcohol dehydrogenase', 'Aldehyde reductase'])
+        self.assertEqual(records[0].pathway,
+                         [('PATH', 'MAP00010', 'Glycolysis / Gluconeogenesis'),
+                          ('PATH', 'MAP00071', 'Fatty acid metabolism'),
+                          ('PATH', 'MAP00120', 'Bile acid biosynthesis'),
+                          ('PATH', 'MAP00350', 'Tyrosine metabolism'),
+                          ('PATH', 'MAP00561', 'Glycerolipid metabolism')])
+        self.assertEqual(records[0].structures,
+                         [('PDB', ['1A4U', '1A71', '1A72', '1ADB', '1ADC',
+                                   '1ADF', '1ADG', '1AGN', '1AXE', '1AXG',
+                                   '1B14', '1B15', '1B16', '1B2L', '1BTO',
+                                   '1CDO', '1D1S', '1D1T', '1DDA', '1DEH',
+                                   '1E3E', '1E3I', '1E3L', '1EE2', '1HDX',
+                                   '1HDY', '1HDZ', '1HET', '1HEU', '1HF3',
+                                   '1HLD', '1HSO', '1HSZ', '1HT0', '1HTB',
+                                   '1LDE', '1LDY', '1QLH', '1QLJ', '1TEH',
+                                   '2OHX', '2OXI', '3BTO', '3HUD', '5ADH',
+                                   '6ADH', '7ADH'])])
+        self.assertEqual(records[0].dblinks,
+                         [('IUBMB Enzyme Nomenclature', ['1.1.1.1']),
+                          ('ExPASy - ENZYME nomenclature database', ['1.1.1.1']),
+                          ('WIT (What Is There) Metabolic Reconstruction', ['1.1.1.1']),
+                          ('BRENDA, the Enzyme Database', ['1.1.1.1']),
+                          ('SCOP (Structural Classification of Proteins)', ['1.1.1.1'])])
         self.assertEqual(records[-1].entry, "2.7.2.1")
 
     def test_irregular(self):
@@ -57,6 +82,15 @@ class CompoundTests(unittest.TestCase):
             records = list(Compound.parse(handle))
         self.assertEqual(len(records), 8)
         self.assertEqual(records[0].entry, "C00023")
+        self.assertEqual(records[0].mass, "")  # Why?
+        self.assertEqual(records[0].formula, "Fe")
+        self.assertEqual(records[0].name,
+                         ['Iron', 'Fe2+', 'Fe(II)', 'Fe3+', 'Fe(III)'])
+        self.assertEqual(records[0].pathway,
+                         [('PATH', 'MAP00860', 'Porphyrin and chlorophyll metabolism')])
+        self.assertEqual(records[0].enzyme[0], ('1.1.3.22', 'C'))
+        self.assertEqual(records[0].structures, [])
+        self.assertEqual(records[0].dblinks[0], ('CAS', ['7439-89-6']))
 
     def test_irregular(self):
         with open("KEGG/compound.irregular") as handle:
