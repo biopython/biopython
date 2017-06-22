0ee5d53c54250ee2d8e8c2ccf9a98e59b597053d
Peter Cock <p.j.a.cock@googlemail.com>
2017-06-22 17:24:20 +0100

    Deprecate Bio.NeuralNetwork
    See mailing list discussion threads:
    
    http://mailman.open-bio.org/pipermail/biopython-dev/2017-June/021719.html
    
    and:
    
    http://mailman.open-bio.org/pipermail/biopython-dev/2017-June/021728.html

--- Bio/NeuralNetwork/__init__.py
+++ Bio/NeuralNetwork/__init__.py
@@ -1,1 +1,10 @@
 # Allow files in this directory to be imported
+
+import warnings
+from Bio import BiopythonDeprecationWarning
+warnings.warn("Bio.NeuralNetwork has been deprecated, and we intend to remove"
+              " it in a future release of Biopython. Please consider using"
+              " scikit-learn or TensorFlow instead.  If you would like to"
+              " continue using Bio.SomeModule, please contact the Biopython"
+              " developers via the mailing list or GitHub.",
+              BiopythonDeprecationWarning)
