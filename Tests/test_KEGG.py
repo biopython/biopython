3bbfb24b9c33fff568d2f63a80cea6341fa42617
Gasta88 <gasta88@hotmail.com>
2017-08-14 08:20:47 +0100

    new tests for test_KEGG.py

--- Tests/test_KEGG.py
+++ Tests/test_KEGG.py
@@ -119,6 +119,11 @@ class CompoundTests(unittest.TestCase):
         self.assertEqual(records[1].enzyme[0], ('2.3.2.6'))
         self.assertEqual(records[1].structures, [])
         self.assertEqual(records[1].dblinks[0], ('PubChem', ['3319']))
+        self.assertEqual(records[-1].__str__().replace(" ","").split("\n")[:10],
+                         ['ENTRYC01386', 'NAMENH2Mec', '7-Amino-4-methylcoumarin',
+                          'FORMULAC10H9NO2', 'DBLINKSCAS:26093-31-2',
+                          'PubChem:4580', 'ChEBI:51771', 'ChEMBL:CHEMBL270672',
+                          'KNApSAcK:C00048593', 'PDB-CCD:MCM'])
 
     def test_irregular(self):
         with open("KEGG/compound.irregular") as handle:
