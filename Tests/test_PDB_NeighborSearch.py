344762fa1fa0e277075c09060da454696cba49be
Sourav Singh <souravsingh@users.noreply.github.com>
2017-08-17 00:47:13 +0530

    Create test_PDB_NeighborSearch.py

---
+++ Tests/test_PDB_NeighborSearch.py
@@ -0,0 +1,24 @@
+
+fro Bio.PDB import NeighborSearch
+
+from numpy.random import random
+
+import unittest
+
+
+class Atom(object):
+    def __init__(self):
+        self.coord = (100 * random(3))
+
+    def get_coord(self):
+        return self.coord
+class KDTreeTest(unittest.TestCase):
+    
+    def test_NeighborSearch():
+    
+        for i in range(0, 20):
+            # Make a list of 100 atoms
+            al = [Atom() for j in range(100)]
+            ns = NeighborSearch(al)
+            for i in range(0, 6):
+                self.assertEqual(i, len(ns.search_all(5.0)))
