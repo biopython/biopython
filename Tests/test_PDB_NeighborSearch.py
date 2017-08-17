ecdc101aaeedaff41866c96baeee5f294715ef1c
Sourav Singh <souravsingh@users.noreply.github.com>
2017-08-17 20:09:12 +0530

    Fix the loop

--- Tests/test_PDB_NeighborSearch.py
+++ Tests/test_PDB_NeighborSearch.py
@@ -23,5 +23,5 @@ class NeighborSearchTest(unittest.TestCase):
         for i in range(0, 20):
             al = [Atom() for j in range(100)]
             ns = NeighborSearch(al)
-            for i in range(0, 6):
+            for i in [0, 1, 2, 3, 4, 5, 6]:
                 self.assertEqual(i, len(ns.search_all(5.0)))
