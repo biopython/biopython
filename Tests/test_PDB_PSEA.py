b00c59250986f6f6111e3658b5bc8508eddcc831
Peter Cock <p.j.a.cock@googlemail.com>
2017-08-22 10:44:01 +0100

    Fix variable name, import error, white space

--- Tests/test_PDB_PSEA.py
+++ Tests/test_PDB_PSEA.py
@@ -2,21 +2,22 @@ import os
 import sys
 import unittest
 
+from Bio import MissingExternalDependencyError
 from Bio.PDB import PDBParser, PSEA
 
 
 os.environ['LANG'] = 'C'
 
-p-sea = None
+psea = None
 from Bio._py3k import getoutput
 try:
     output = getoutput("p-sea -h")
     if output.startswith("displays this help"):
-        p-sea = "p-sea"
+        psea = "p-sea"
 except OSError:
     pass
 
-if not clustalo_exe:
+if not psea:
     raise MissingExternalDependencyError(
         "Install p-sea if you want to use Bio.PSEA from Biopython.")
 
@@ -29,7 +30,8 @@ class PSEATests(unittest.TestCase):
         p = PDBParser()
         s = p.get_structure('X', pdb1)
         self.assertEqual(PSEA.PSEA(s[0], pdb1))
-        
+
+
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
     unittest.main(testRunner=runner)
