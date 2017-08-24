53371d902fe4cd138ec837b5ec1030113b3ca17d
Sourav Singh <souravsingh@users.noreply.github.com>
2017-08-24 20:45:57 +0530

    Fix assert statement

--- Tests/test_PDB_NeighborSearch.py
+++ Tests/test_PDB_NeighborSearch.py
@@ -23,5 +23,4 @@ class NeighborSearchTest(unittest.TestCase):
         for i in range(0, 20):
             al = [Atom() for j in range(100)]
             ns = NeighborSearch(al)
-            for i in range(7):
-                self.assertEqual(i, len(ns.search_all(5.0)))
+            self.assertTrue(0 <= len(ns.search_all(5.0)) <= 6)
