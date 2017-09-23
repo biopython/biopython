7df15debdd74b8a1f09703401563701ea8a40e6f
mdehoon <mjldehoon@yahoo.com>
2017-09-23 21:13:22 +0900

    C Clustering Library version 1.54. Fixes kmedoids. (#1395)

--- Bio/Cluster/cluster.h
+++ Bio/Cluster/cluster.h
@@ -38,7 +38,7 @@
 #  include <windows.h>
 #endif
 
-#define CLUSTERVERSION "1.53"
+#define CLUSTERVERSION "1.54"
 
 /* Chapter 2 */
 double clusterdistance (int nrows, int ncolumns, double** data, int** mask,
