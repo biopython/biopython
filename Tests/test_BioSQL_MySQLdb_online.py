fd66d0054ef887140db2adbe2943edc7991e3321
Adhemar Zerlotini <azneto@gmail.com>
2017-10-27 11:11:39 -0200

    Tests do not require postgresql CREATE DATABASE permissions
    Test framework changes so as not to require a postgres user with createdb privileges.
    
    Under TravisCI / AppVeyor a dedicated postgres user is setup.
    
    Squashed commit of pull request #1420.

--- Tests/test_BioSQL_MySQLdb_online.py
+++ Tests/test_BioSQL_MySQLdb_online.py
@@ -3,7 +3,7 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-"""Run BioSQL tests using SQLite"""
+"""Run BioSQL tests using MySQL"""
 
 import unittest
 
--- Tests/test_BioSQL_psycopg2.py
+++ Tests/test_BioSQL_psycopg2.py
@@ -16,10 +16,6 @@ DBHOST, DBUSER, DBPASSWD, TESTDB = load_biosql_ini(DBTYPE)
 # This will abort if driver not installed etc:
 check_config(DBDRIVER, DBTYPE, DBHOST, DBUSER, DBPASSWD, TESTDB)
 
-# Some of the unit tests don't create their own database,
-# so just in case there is no database already:
-TESTDB = create_database()
-
 if __name__ == "__main__":
     # Run the test cases
     runner = unittest.TextTestRunner(verbosity=2)
