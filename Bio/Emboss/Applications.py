8604baadf294d201f04bb1b4bcda0976f88f04aa
qifei <qifei9@gmail.com>
2017-11-14 00:49:07 +0100

    Fix Fuzznuc para and add Fuzzpro wrapper

--- Bio/Emboss/Applications.py
+++ Bio/Emboss/Applications.py
@@ -988,11 +988,30 @@ class FuzznucCommandline(_EmbossCommandLine):
             _Option(["-pattern", "pattern"],
                     "Search pattern, using standard IUPAC one-letter codes",
                     is_required=True),
-            _Option(["-mismatch", "mismatch"],
-                    "Number of mismatches",
-                    is_required=True),
+            _Option(["-pmismatch", "pmismatch"],
+                    "Number of mismatches"),
             _Option(["-complement", "complement"],
                     "Search complementary strand"),
+            _Option(["-rformat", "rformat"],
+                    "Specify the report format to output in."),
+            ]
+        _EmbossCommandLine.__init__(self, cmd, **kwargs)
+
+
+class FuzzproCommandline(_EmbossCommandLine):
+    """Commandline object for the fuzzpro program from EMBOSS."""
+
+    def __init__(self, cmd="fuzzpro", **kwargs):
+        """Initialize the class."""
+        self.parameters = [
+            _Option(["-sequence", "sequence"],
+                    "Sequence database USA",
+                    is_required=True),
+            _Option(["-pattern", "pattern"],
+                    "Search pattern, using standard IUPAC one-letter codes",
+                    is_required=True),
+            _Option(["-pmismatch", "pmismatch"],
+                    "Number of mismatches"),
             _Option(["-rformat", "rformat"],
                     "Specify the report format to output in."),
             ]
