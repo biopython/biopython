a9e37d6df6633bd67fd7c4abbc6d91a227dff65e
mpeters <mpeters@intrexon.com>
2017-11-17 20:18:42 -0800

    changed atom coordinates to numpy array to allow distance calculation

--- Bio/PDB/mmtf/DefaultParser.py
+++ Bio/PDB/mmtf/DefaultParser.py
@@ -1,5 +1,5 @@
 from Bio.PDB.StructureBuilder import StructureBuilder
-
+import numpy
 
 class StructureDecoder(object):
     """Class to pass the data from mmtf-python into a Biopython data structure."""
@@ -50,7 +50,7 @@ class StructureDecoder(object):
             alternative_location_id = " "
 
         # Atom_name is in twice - the full_name is with spaces
-        self.structure_bulder.init_atom(str(atom_name), [x, y, z],
+        self.structure_bulder.init_atom(str(atom_name), numpy.array((x, y, z), "f"),
                                         temperature_factor, occupancy,
                                         alternative_location_id, str(atom_name),
                                         serial_number=serial_number,
