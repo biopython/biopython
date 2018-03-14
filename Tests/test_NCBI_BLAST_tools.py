cb80747142d889664cc620f8e4b208685103a8aa
Peter Cock <p.j.a.cock@googlemail.com>
2018-03-14 12:02:05 +0000

    Implement negative_seqidlist in BLAST+ wrappers.
    New in BLAST+ 2.7.1 (or maybe in 2.7.0)

--- Tests/test_NCBI_BLAST_tools.py
+++ Tests/test_NCBI_BLAST_tools.py
@@ -281,6 +281,10 @@ class CheckCompleteArgList(unittest.TestCase):
         if exe_name in ["deltablast", "psiblast"]:
             # New in BLAST+ 2.3.0 so will look like extra args on older verions
             extra = extra.difference(["-save_each_pssm", "-save_pssm_after_last_round"])
+        # This was added in BLAST+ 2.7.1 (or maybe 2.7.0) to most/all the tools,
+        # so will be seen as an extra argument on older versions:
+        if "-negative_seqidlist" in extra:
+            extra.remove("-negative_seqidlist")
 
         if extra or missing:
             import warnings
