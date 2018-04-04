68fedf11d640a40d3920bd3f00ce6bd1c39553ed
Oscar GG <profesor.oscar.gomez@gmail.com>
2018-04-04 18:59:36 +0200

    Solves https://github.com/biopython/biopython/issues/1539 by ignoring HTML format tags. Tests include an example of a file with HTML tags in the ArticleTitle element

--- Bio/Entrez/Parser.py
+++ Bio/Entrez/Parser.py
@@ -342,7 +342,8 @@ class DataHandler(object):
                 else:
                     self.parse_xsd(ET.fromstring(handle.read()))
                     handle.close()
-        self.content = ""
+        if name not in ["i", "u", "b", "sup", "sub"]:
+            self.content = ""
         if name in self.lists:
             object = ListElement()
         elif name in self.dictionaries:
