2f6c104dbee7545b2cbe367e1d063fb955176e0a
peterjc <p.j.a.cock@googlemail.com>
2018-04-10 16:32:23 +0100

    Dual license Bio/AlignIO/__init__.py
    Contributors from the git log have all agreed on #898,
    
     - @brandoninvergo Brandon Invergo
     - @cbrueffer Christian Brueffer
     - @cmccoy Connor McCoy
     - @erasche Eric Rasche
     - @lennax Lenna Peterson
     - @odoublewen Owen Solberg
     - @twrightsman Travis Wrightsman
     - @bow Wibowo Arindrarto
     - @carlosp420 Carlos Peña
     - @fomightez Wayne Decatur
     - @polyatail Andrew Sczesnak

--- Bio/AlignIO/__init__.py
+++ Bio/AlignIO/__init__.py
@@ -1,7 +1,9 @@
-# Copyright 2008-2017 by Peter Cock.  All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# Copyright 2008-2018 by Peter Cock.  All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """Multiple sequence alignment input/output as alignment objects.
 
