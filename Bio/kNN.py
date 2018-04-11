6e831d9c18cb7a47bfa205efa679807baddf0cad
peterjc <p.j.a.cock@googlemail.com>
2018-04-11 11:50:40 +0100

    These files were by Jeff Chang (2002) [ci skip]

--- Bio/kNN.py
+++ Bio/kNN.py
@@ -1,3 +1,5 @@
+# Copyright 2002 by Jeffrey Chang.
+# All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
