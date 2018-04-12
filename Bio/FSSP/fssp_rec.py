2e07e8d09461bbfa3c12a42403603885b1ad8cdc
peterjc <p.j.a.cock@googlemail.com>
2018-04-12 13:43:22 +0100

    Dual license Bio/FSSP/fssp_rec.py [ci skip]
    All the contributors to this file according to git have
    agreed to the licensing change, see #898.
    
     - Brian Osborne (@bosborne)
     - Christian Brueffer (@cbrueffer)
     - Connor McCoy (@cmccoy)
     - Peter Cock (@peterjc)
     - Adhemar Zerlotini (@azneto)
     - Carlos Peña (@carlosp420)
     - Iddo Friedberg (@idoerg)
     - @morrme

--- Bio/FSSP/fssp_rec.py
+++ Bio/FSSP/fssp_rec.py
@@ -1,3 +1,7 @@
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """A superclass for reading [f]ixed-column type [f]lat-[f]ile records."""
 
 
