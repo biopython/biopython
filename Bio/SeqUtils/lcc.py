fda7c5b037300efde4b82160014247f7df7306f5
peterjc <p.j.a.cock@googlemail.com>
2018-04-12 14:30:11 +0100

    Dual licence Bio/SeqUtils/lcc.py [ci skip]
    Original author Sebastian Bassi and contributors since
    according to the git logs for Bio/SeqUtils/lcc.py and
    the original filename Bio/lcc.py have all agreed, see
    GitHub issue #898.
    
     - Christian Brueffer (@cbrueffer)
     - Markus Piotrowski (@MarkusPiotrowski)
     - Peter Cock (@peterhc)
     - Sebastian Bassi (@sbassi)
     - Wibowo Arindrarto (@bow)
     - Carlos Peña (@carlosp420)
     - Michiel de Hoon (@mdehoon)
     - @morrme

--- Bio/SeqUtils/lcc.py
+++ Bio/SeqUtils/lcc.py
@@ -1,8 +1,10 @@
 # Copyright 2003, 2007 by Sebastian Bassi. sbassi@genesdigitales.com
-# All rights reserved.  This code is part of the Biopython
-# distribution and governed by its license.
-# Please see the LICENSE file that should have been included as part
-# of this package.
+# All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Local Composition Complexity."""
 
 import math
