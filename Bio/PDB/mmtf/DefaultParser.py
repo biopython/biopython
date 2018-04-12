26bdf94f4f6bc7437306e9f039048db88ec5d418
peterjc <p.j.a.cock@googlemail.com>
2018-04-12 16:39:34 +0100

    Fill in copyright and dual license Bio.PDB.mmtf [ci skip]
    These files were missing a copyright and license statement,
    see GitHub issue #1609.
    
    Both the original author Anthony Bradley (@abradle) and the
    contributors since as recorded in git have agreed to the
    dual licensing, see GitHub issue #898.
    
     - Adhemar Zerlotini (@azneto)
     - Christian Brueffer (@cbrueffer)
     - Francesco Gastaldello (@gasta88)
     - Joe Greener (@jgreener64)
     - Maximilian Peters (@Ashafix)
     - Peter Cock (@peterjc)
     - Stefans Mezulis (@StefansM)
     - Anthony Bradly (@abradle)
     - Adhemar Zerlotini (@azneto)
     - @morrme

--- Bio/PDB/mmtf/DefaultParser.py
+++ Bio/PDB/mmtf/DefaultParser.py
@@ -1,3 +1,11 @@
+# Copyright 2016 Anthony Bradley.  All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
+"""Code handle loading mmtf-python into Biopython's structures."""
+
 from Bio.PDB.StructureBuilder import StructureBuilder
 import numpy
 
