b83e86ebd9d1bceb7db3800c63a699c0cc8f4872
peterjc <p.j.a.cock@googlemail.com>
2018-04-12 13:51:37 +0100

    Dual license Bio/Statistics/__init__.py
    The original author Michiel de Hoon (@mdehoon), Brian Osborn
    (@bosborne), myself and Adhemar Zerlotini (@azneto) are the
    only contributors to this file according to the git log.
    We have all agreed to the dual licensing, see #898.

--- Bio/Statistics/__init__.py
+++ Bio/Statistics/__init__.py
@@ -1,2 +1,6 @@
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Basic statistics module."""
 # TODO: Remove empty __init__.py once we drop Python 2 support
