0d500141220fae7c688f1a4a46aec8e4e649cc24
peterjc <p.j.a.cock@googlemail.com>
2018-04-13 16:38:15 +0100

    Dual license Bio.Alphabet.Reduced [ci skip]
    The original author Iddo Friedberg and contributors since
    according to the git log have agreed, see GitHub issue #898.
    
     - Christian Brueffer (@cbrueffer)
     - Iddo Friedberg (@idoerg)
     - Markus Piotrowski (@MarkusPiotrowski)
     - Peter Cock (@peterjc)

--- Bio/Alphabet/Reduced.py
+++ Bio/Alphabet/Reduced.py
@@ -1,9 +1,10 @@
 # Copyright 2004 by Iddo Friedberg.
 # All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
-
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Reduced alphabets which lump together several amino-acids into one letter.
 
 Reduced (redundant or simplified) alphabets are used to represent protein
