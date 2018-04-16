c9053cb83935a7b674808abfb3239899c60dcbb2
peterjc <p.j.a.cock@googlemail.com>
2018-04-16 15:10:53 +0100

    Dual license Bio.Application [ci skip]
    Original author Brad Chapman (@chapmanb) and myself (the
    named copyright holders) and all the other contributors
    from the git log have agreed, see GitHub issue #898.
    
     - Carlos Peña (@carlosp420)
     - Christian Brueffer (@cbrueffer)
     - Iddo Friedberg (@idoerg)
     - Michiel de Hoon (@mdehoon)
     - Saket Choudhary (@saketkc)
     - Suresh Panneerselvam (@Amrithasuresh)
     - Tiago Antao (@tiagoantao)
     - Travis Wrightsman (@twrightsman)
     - @morrme

--- Bio/Application/__init__.py
+++ Bio/Application/__init__.py
@@ -1,9 +1,11 @@
 # Copyright 2001-2004 Brad Chapman.
 # Revisions copyright 2009-2013 by Peter Cock.
 # All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """General mechanisms to access applications in Biopython.
 
 This module is not intended for direct use. It provides the basic objects which
