0c60613560abba31a5b5c9ac429a6b5358f19761
peterjc <p.j.a.cock@googlemail.com>
2018-04-17 10:25:02 +0100

    Add copyright and dual license MarkovModel.py
    Original author Jeff Chang (@jefftc) and the other contributors
    since record in the git log have all agreed, see GitHub issue #898.
    
     - Brad Chapman (@chapmanb)
     - Brian Osborne (@bosborne)
     - Carlos Peña (@carlosp420)
     - Christian Brueffer (@cbrueffer)
     - Connor McCoy (@cmccoy)
     - Francesco Gastaldello (@gasta88)
     - Michiel de Hoon (@mdehoon)
     - Peter Cock (@peterjc)

--- Bio/MarkovModel.py
+++ Bio/MarkovModel.py
@@ -1,8 +1,10 @@
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# Copyright 2002 by Jeffrey Chang.
+# All rights reserved.
 #
-
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package
 """A state-emitting MarkovModel.
 
 Note terminology similar to Manning and Schutze is used.
