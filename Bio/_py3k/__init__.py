8cec0501443b85902cc65e26cd764267d57e7550
peterjc <p.j.a.cock@googlemail.com>
2018-04-17 15:07:17 +0100

    Dual licese Bio/_py3k/__init__.py
    I was the original author, and the other contributors according
    to the git log have also agreed, see GitHub issue #898:
    
     - Brian Osborne (@bosborne)
     - Carlos Peña (@carlosp420)
     - Christian Brueffer (@cbrueffer)
     - Edward Miller (@EdwardBlakeMiller)
     - Evan Parker (@eparker05)
     - Jacek Śmietański (@dadoskawina)
     - Jeroen Van Goey (@BioGeek)
     - Wibowo Arindrarto (@bow)
     - @morrme
    
    Author: Brian Osborne <briano@bioteam.net>
    Author: Edward Miller <ebm2134@columbia.edu>
    Author: Evan Parker <eparker05@gmail.com>
    Author: Jacek Śmietański <jacek.smietanski@ii.uj.edu.pl>
    Author: bow <bow@bow.web.id>

--- Bio/_py3k/__init__.py
+++ Bio/_py3k/__init__.py
@@ -1,7 +1,9 @@
-# Copyright 2010-2013 by Peter Cock.  All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# Copyright 2010-2018 by Peter Cock.  All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Python 3 compatibility tools (PRIVATE).
 
 We used to have lines like this under Python 2 in order to use
