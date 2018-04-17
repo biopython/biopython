360e790aeb67f2031bd8d617e99fb6dd5d9fe757
peterjc <p.j.a.cock@googlemail.com>
2018-04-17 10:43:08 +0100

    Dual license Bio/pairwise2.py [ci skip]
    Named copyright holders Jeffrey Chang (@jefftc) and
    Markus Piotrowski (@MarkusPiotrowski), and the other
    contributors recorded in the git log have all agreed,
    see GitHub issue #898.
    
     - Bryan Lunt (@bryan-lunt)
     - Carlos Peña (@carlosp420)
     - Chris Mitchell (@chrismit aka @Chris7)
     - Christian Brueffer (@cbrueffer)
     - Chunlei Wu (@newgene)
     - Connor McCoy (@cmccoy)
     - Don Kirkby (@donkirkby) - see #1560
     - Francesco Gastaldello (@gasta88)
     - Jeroen Van Goey (@BioGeek)
     - João Rodrigues (@JoaoRodrigues)
     - Kuan-Yi Li (@kuanyili)
     - Michiel de Hoon (@mdehoon)
     - Peter Cock (@peterjc)
     - Sergei Lebedev (@superbobry)
     - Travis Wrightsman (@twrightsman)
     - @morrme

--- Bio/pairwise2.py
+++ Bio/pairwise2.py
@@ -1,10 +1,11 @@
 # Copyright 2002 by Jeffrey Chang.
 # Copyright 2016 by Markus Piotrowski.
 # All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
-
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Pairwise sequence alignment using a dynamic programming algorithm.
 
 This provides functions to get global and local alignments between two
