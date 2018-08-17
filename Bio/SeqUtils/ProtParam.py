67c4e4e18e79397b42bc27c00d0382989997b055
peterjc <p.j.a.cock@googlemail.com>
2018-08-17 14:31:44 +0100

    Standard license header on Yair Benita's SeqUtils files.
    Note the comment "Biopython (http://biopython.org) license applies"
    can likely be used to switch this file to dual-licensing if-and-when
    we manage to dual-license the rest of Biopython, even if we don't
    have all the individual contributor's explicit agreement.
    
    See issue #898.

--- Bio/SeqUtils/ProtParam.py
+++ Bio/SeqUtils/ProtParam.py
@@ -1,5 +1,8 @@
-# Copyright Yair Benita Y.Benita@pharm.uu.nl
-# Biopython (http://biopython.org) license applies
+# Copyright 2003 by Yair Benita <Y.Benita@pharm.uu.nl>
+#
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
 
 """Simple protein analysis.
 
