4f65856e349da7e4dc87ba743f25fe7090df329c
peterjc <p.j.a.cock@googlemail.com>
2018-08-21 16:12:34 +0100

    Dual license Bio/Emboss/__init__.py
    Original author Brad Chapman and contributors since
    (myself and Brian Osborne) have all agreed. See
    GitHub issue #898.

--- Bio/Emboss/__init__.py
+++ Bio/Emboss/__init__.py
@@ -1,6 +1,8 @@
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# Copyright 2001 Brad Chapman. All rights reserved.
 #
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """Code to interact with the ever-so-useful EMBOSS programs."""
