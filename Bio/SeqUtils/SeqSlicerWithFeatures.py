d619cfaf5e6e562c0f36e47dd4e915ae4312a86c
Marek Schwarz <schwarz.marek@outlook.com>
2018-08-23 17:21:22 +0200

    few fixes + unit test for SeqSlicer v.01

--- Bio/SeqUtils/SeqSlicerWithFeatures.py
+++ Bio/SeqUtils/SeqSlicerWithFeatures.py
@@ -1,7 +1,6 @@
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature, FeatureLocation, ExactPosition, BeforePosition, AfterPosition, BetweenPosition,\
     WithinPosition, CompoundLocation
-# from Bio import SeqIO
 
 
 def move_position(pos, move: int, keep_all: bool, slice_len: int):
@@ -15,28 +14,33 @@ def move_position(pos, move: int, keep_all: bool, slice_len: int):
             return tt(pos + move)
     elif isinstance(pos, (BetweenPosition, WithinPosition)):
         # This would need update if BetweenPosition or WithinPosition gets obsolete or changed
-        # attributes accessed because they are not exposed in any other way
+        #  attributes accessed because they are not exposed in any other way
         diff_left = pos._left + move
         diff_right = pos._right + move
 
         if diff_left < 0 and diff_right <= 0:
             return BeforePosition(0)
         elif diff_left < 0 < diff_right:
+            # ----....=========------
+            #      |-----|
+            # ----....=========------
+            #   |---|
             if keep_all:
-                tt(0, 0, pos._right)
+                return tt(0, 0, diff_right)
+            else:
+                return None
+        elif diff_left < slice_len < diff_right:
+            # ----=========....------
+            #          |-----|
+            # ----=========....------
+            #               |---|
+            if keep_all:
+                return tt(slice_len, diff_left, slice_len)
             else:
                 return None
         elif diff_left > 0 and diff_right > 0:
-            if diff_left < slice_len and diff_right < slice_len:
-                return AfterPosition(slice_len)
-            elif diff_left < slice_len < diff_right:
-                if keep_all:
-                    return tt(slice_len, diff_left, slice_len)
-                else:
-                    return None
-            else:
-                # This should not be reached
-                raise ValueError
+            # the pos is fully within slice
+            return tt(pos.position + move, diff_left, diff_right)
         else:
             # This should not be reached
             raise ValueError
@@ -53,28 +57,46 @@ def end_violation_pos(x, sl: slice):
     return x.start.position < sl.stop < x.end.position
 
 
+def outside(x, sl: slice):
+    return sl.start > x.end.position or sl.stop < x.start.position
+
+
 def slice_feature(feature: FeatureLocation, sl: slice, keep_all_features: bool, seq_len: int):
-    if isinstance(feature, CompoundLocation):
-        of = []
-        for ff in feature.parts:
-            # this must be without location boundaries checking
-            #  - only location objects adjacent to slice boundaries can cross it
+    # the boundary checking (whether the slicing would be applied) uses feature 'start' and 'end' position
+    #  if they are not equal to min("all feature positions") and max("all feature positions") it is possible
+    #  that feature will not be detected
+    #  this is by design
+    #  =================
+    #  we rely on the start/end position being defined as important pointers (and checking would also be complicated)
+    if start_violation_pos(feature, sl) or end_violation_pos(feature, sl):
+        if isinstance(feature, CompoundLocation):
+            of = []
+            for ff in feature.parts:
+                # this must be without location boundaries checking
+                #  - only location objects adjacent to slice boundaries can cross it
 
-            # However we need to check whether the location object is fully inside the slice
-            #  (and discard those which are not)
+                # However we need to check whether the location object is not fully outside the slice
+                #  (and discard those which are outside)
+                if outside(ff, sl):
+                    continue
 
-            mps = move_position(ff.start, - sl.start, keep_all_features, seq_len)
-            mpe = move_position(ff.end, - sl.start, keep_all_features, seq_len)
-            if mps is not None and mpe is not None:
-                fl = FeatureLocation(mps, mpe, ff.strand)
+                mps = move_position(ff.start, - sl.start, keep_all_features, seq_len)
+                mpe = move_position(ff.end, - sl.start, keep_all_features, seq_len)
+                if mps is not None and mpe is not None:
+                    fl = FeatureLocation(mps, mpe, ff.strand)
 
-            # Feature locations outside of slice gets discarded
-            if fl is not None:
-                of.append(fl)
+                # Feature locations outside of slice gets discarded
+                if fl is not None:
+                    of.append(fl)
 
-        return CompoundLocation(of, operator=feature.operator)
-    else:
-        if start_violation_pos(feature, sl) or end_violation_pos(feature, sl):
+            if len(of) == 0:
+                return None
+            elif len(of) == 1:
+                # deal with slices when only one feature from compound location remain
+                return of[0]
+            else:
+                return CompoundLocation(of, operator=feature.operator)
+        else:
 
             mps = move_position(feature.start, - sl.start, keep_all_features, seq_len)
             mpe = move_position(feature.end, - sl.start, keep_all_features, seq_len)
@@ -85,8 +107,8 @@ def slice_feature(feature: FeatureLocation, sl: slice, keep_all_features: bool, 
                 return fl
             else:
                 return None
-        else:
-            return None
+    else:
+        return None
 
 
 def slice_sequence_with_features(seq: SeqRecord, sl: slice, keep_all_features: bool = False) -> SeqRecord:
@@ -95,15 +117,17 @@ def slice_sequence_with_features(seq: SeqRecord, sl: slice, keep_all_features: b
       it is intended for visualization purposes where part of a feature also provides valuable information
 
     # location variants:
-    - CompoundLocation - Collection of FeatureLocation objects (for joins etc).             ? -> solved
+    - CompoundLocation - Collection of FeatureLocation objects (for joins etc).             -> solved
     - ExactPosition - Specify the position as being exact.                                  OK
-    - WithinPosition - Specify a position occurring within some range.                      ? -> solved
-    - BetweenPosition - Specify a position occurring between a range (OBSOLETE?).           ? -> solved
+    - WithinPosition - Specify a position occurring within some range.                      -> solved
+    - BetweenPosition - Specify a position occurring between a range (OBSOLETE?).           -> solved
     - BeforePosition - Specify the position as being found before some base.                OK
     - AfterPosition - Specify the position as being found after some base.                  OK
-    - OneOfPosition - Specify a position where the location can be multiple positions.      ? Not is spec ? -> raise
+    - OneOfPosition - Specify a position where the location can be multiple positions.      ? Not is spec -> raise
     - UnknownPosition - Represents missing information like '?' in UniProt.                 raise
 
+    Biopython raises on feature with UnknownPosition
+    OneOfPosition - do not implement.
     """
     ns = seq[sl.start:sl.stop]
 
@@ -122,34 +146,3 @@ def slice_sequence_with_features(seq: SeqRecord, sl: slice, keep_all_features: b
             ns.features.append(nf)
 
     return ns
-
-
-if __name__ == '__main__':
-
-    # to my understanding
-    # the BetweenPosition should be OK (not DEPRECATED)
-    #    - as the between position is only for (n, n+1) - slicing should only retain full feature
-    # the within position (usage restricted)
-    #    - the within position must be inspected
-    # One-of does not exist in specification
-    #    - I do not expect it to see it
-    # Unknown position not considered (maybe do some basic testing that presence would not break the function)
-
-    # this fails for unknown reason
-    # from Bio import SeqIO
-    # import os
-    # if os.path.exists('../../Tests/GenBank/NC_005816.gb'):
-    #     print('ok')
-
-    # using example record from Biopython Tests
-    # a = SeqIO.read('../../Tests/GenBank/NC_005816.gb', format='gb')
-    # b = slice_sequence_with_features(a, slice(5933, 5935))
-    # b = slice_sequence_with_features(a, slice(5932, 5934))
-    # b = slice_sequence_with_features(a, slice(5933, 5934))
-    # b = slice_sequence_with_features(a, slice(1370, 1670))
-    # b = slice_sequence_with_features(a, slice(1430, 1460))
-    # b = slice_sequence_with_features(a, slice(1438, 1619))
-    # for f in b.features:
-    #     print(f)
-
-    print('a')
