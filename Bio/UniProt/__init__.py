f2a402dfea98e498803ace7dd753ce8fd17be0f8
JureFabjan <41614295+JureFabjan@users.noreply.github.com>
2018-09-28 12:04:45 +0200

    Documentation
    Added documentation for output_format and columns.

--- Bio/UniProt/__init__.py
+++ Bio/UniProt/__init__.py
@@ -20,7 +20,14 @@ from Bio._py3k import _binary_to_string_handle
 def search(text, output_format="tab", sort="score", oragnism="", columns=(),
            isoform=False, compress=False, offset=0, limit=0):
     """Perform a query over the UniProt API.
-
+    
+    output_format: options:
+           html, tab, xls, fasta, gff, txt, xml, rdf, list, rss
+    columns: list/tuple of strings from the following options: 
+           citation, clusters, comments, domains, domain, ec, id, entry name,
+           existence, families, features, genes, go, go-id, interactor,7
+           keywords, last-modified, length, organism, organism-id, pathway,
+           protein names, reviewed, sequence, 3d, version, virus hosts
     More at: https://www.uniprot.org/help/api_queries
     """
     cgi = "https://www.uniprot.org/uniprot/?"
