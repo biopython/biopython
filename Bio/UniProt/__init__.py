1857f984ad8ce3bcf99e1293eb487e84faf45bd8
peterjc <p.j.a.cock@googlemail.com>
2018-12-20 10:16:52 +0000

    Dual license Bio/UniProt/__init__.py
    Original author Iddo and contributors since have agreed:
    
     - Brian Osborne (@bosborne)
       https://github.com/biopython/biopython/issues/898#issuecomment-241429879
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Iddo Friedberg (@idoerg)
       https://github.com/biopython/biopython/issues/898#issuecomment-380437378
     - Michiel de Hoon (@mdehoon)
       https://github.com/biopython/biopython/issues/898#issuecomment-237122630
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613

--- Bio/UniProt/__init__.py
+++ Bio/UniProt/__init__.py
@@ -1,8 +1,9 @@
 # Copyright 2013 by Iddo Friedberg idoerg@gmail.com
 # Revision copyright 2013 by Peter Cock.  All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Code for dealing with assorted UniProt file formats.
 
 This currently include parsers for the GAF, GPA and GPI formats
