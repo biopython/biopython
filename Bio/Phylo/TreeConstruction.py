f3c721253e312ecbfc644fd5de29c355f3643e8f
Marco Vincenzi <vmarco.vince@gmail.com>
2019-01-11 17:36:05 +0100

    Fixed wrong indentation

--- Bio/Phylo/TreeConstruction.py
+++ Bio/Phylo/TreeConstruction.py
@@ -756,18 +756,18 @@ class DistanceTreeConstructor(TreeConstructor):
             dm.names[min_j] = "Inner" + str(inner_count)
             del dm[min_i]
 
-            # set the last clade as one of the child of the inner_clade
-            root = None
-            if clades[0] == inner_clade:
-                clades[0].branch_length = 0
-                clades[1].branch_length = dm[1, 0]
-                clades[0].clades.append(clades[1])
-                root = clades[0]
-            else:
-                clades[0].branch_length = dm[1, 0]
-                clades[1].branch_length = 0
-                clades[1].clades.append(clades[0])
-                root = clades[1]
+        # set the last clade as one of the child of the inner_clade
+        root = None
+        if clades[0] == inner_clade:
+            clades[0].branch_length = 0
+            clades[1].branch_length = dm[1, 0]
+            clades[0].clades.append(clades[1])
+            root = clades[0]
+        else:
+            clades[0].branch_length = dm[1, 0]
+            clades[1].branch_length = 0
+            clades[1].clades.append(clades[0])
+            root = clades[1]
 
         return BaseTree.Tree(root, rooted=False)
 
