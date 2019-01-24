ea2108795c94ba1504d352f04dfa5adeade47d20
svalqui <svalqui@acslink.net.au>
2019-01-24 13:21:52 +1100

    Added SV to NEWS.rts

--- Tests/test_PDB_Dice.py
+++ Tests/test_PDB_Dice.py
@@ -3,6 +3,7 @@
 # choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
 # Please see the LICENSE file that should have been included as part of this
 # package.
+"""Unit tests for the Bio.PDB.Dice Module."""
 
 import unittest
 import tempfile
