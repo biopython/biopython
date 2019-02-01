326056cee48b68e327bd20edcab33ae0f719a355
Hom <platinhom@users.noreply.github.com>
2019-02-02 02:23:55 +0800

    Vector division broken in python 3.x #1914
    Support division operator in Python 3. If users use `Vector.__div__(x)` to perform division,  they should modify their codes.

--- Bio/PDB/vectors.py
+++ Bio/PDB/vectors.py
@@ -6,6 +6,7 @@
 """Vector class, including rotation-related functions."""
 
 from __future__ import print_function
+from __future__ import division
 
 import numpy
 
@@ -276,11 +277,11 @@ class Vector(object):
         """Return Vector.Vector (dot product)."""
         return sum(self._ar * other._ar)
 
-    def __div__(self, x):
+    def __truediv__(self, x):
         """Return Vector(coords/a)."""
         a = self._ar / numpy.array(x)
         return Vector(a)
-
+    
     def __pow__(self, other):
         """Return VectorxVector (cross product) or Vectorxscalar."""
         if isinstance(other, Vector):
