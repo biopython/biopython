531aa033a618b97f8311739a1979e8a314c926ce
platinhom <zhaozxcpu@hotmail.com>
2019-02-02 15:16:25 +0800

    Vector division broken in python 3.x #1914

--- Tests/test_PDB_vectors.py
+++ Tests/test_PDB_vectors.py
@@ -36,6 +36,16 @@ class LegacyImportTests(unittest.TestCase):
             del vector_to_axis, Vector
 
 
+class VectorTests(unittest.TestCase):
+    """Tests for the Vector class."""
+
+    def test_division(self):
+        """Confirm division works."""
+        from Bio.PDB.Vector import Vector
+        v = Vector(2, 2, 2) / 2
+        self.assertEqual(repr(v), "<Vector 1.00, 1.00, 1.00>")
+
+
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
     unittest.main(testRunner=runner)
