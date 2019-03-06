3aa4f3f478fbc48042d7eb593197f0e7a09c5724
Michiel de Hoon <mjldehoon@yahoo.com>
2019-03-06 17:00:00 +0900

    making flake happy

--- Tests/test_SeqIO_NibIO.py
+++ Tests/test_SeqIO_NibIO.py
@@ -5,6 +5,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
+
 class TestNibReaderWriter(unittest.TestCase):
 
     nucleotides = 'ACGTAAACCGTACCCGTANANCANNNNACNANNANCN'
