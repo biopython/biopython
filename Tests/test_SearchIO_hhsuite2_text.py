3eed98615f2b52655182a3ced5ce552fbacad075
jmht <linucks42@gmail.com>
2019-03-15 16:26:37 +0000

    Fixes for flake8

--- Tests/test_SearchIO_hhsuite2_text.py
+++ Tests/test_SearchIO_hhsuite2_text.py
@@ -41,9 +41,9 @@ class HhsuiteCases(unittest.TestCase):
 
         hit = qresult[0]
         self.assertEqual('2uvo_A_1', hit.id)
-        self.assertEqual('Agglutinin isolectin 1; carbohydrate-binding protein, hevein domain, chitin-binding,' \
-                         ' GERM agglutinin, chitin-binding protein; HET: NDG NAG GOL; 1.40A {Triticum aestivum}' \
-                         ' PDB: 1wgc_A* 2cwg_A* 2x3t_A* 4aml_A* 7wga_A 9wga_A 2wgc_A 1wgt_A 1k7t_A* 1k7v_A* 1k7u_A' \
+        self.assertEqual('Agglutinin isolectin 1; carbohydrate-binding protein, hevein domain, chitin-binding,'
+                         ' GERM agglutinin, chitin-binding protein; HET: NDG NAG GOL; 1.40A {Triticum aestivum}'
+                         ' PDB: 1wgc_A* 2cwg_A* 2x3t_A* 4aml_A* 7wga_A 9wga_A 2wgc_A 1wgt_A 1k7t_A* 1k7v_A* 1k7u_A'
                          ' 2x52_A* 1t0w_A*', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(3.7e-34, hit.evalue)
@@ -52,7 +52,7 @@ class HhsuiteCases(unittest.TestCase):
 #         self.assertEqual(1.5, hit.domain_exp_num)
 #         self.assertEqual(1, hit.domain_obs_num)
         self.assertEqual(1, len(hit))
-# 
+
         hsp = hit.hsps[0]
 #         self.assertEqual(1, hsp.domain_index)
         self.assertTrue(hsp.is_included)
@@ -70,14 +70,14 @@ class HhsuiteCases(unittest.TestCase):
 #         self.assertEqual(205, hsp.env_end)
 #         self.assertEqual('..', hsp.env_endtype)
 #         self.assertEqual(0.97, hsp.acc_avg)
-        self.assertEqual('ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYC' \
-                         'GAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCS' \
+        self.assertEqual('ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYC'
+                         'GAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCS'
                          'KWGSCGIGPGYCGAGCQSGGCDG',
                          str(hsp.hit.seq))
-        self.assertEqual('ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYC' \
-                         'GAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCS' \
-                        'KWGSCGIGPGYCGAGCQSGGCDG',
-                        str(hsp.query.seq))
+        self.assertEqual('ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYC'
+                         'GAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCS'
+                         'KWGSCGIGPGYCGAGCQSGGCDG',
+                         str(hsp.query.seq))
 #         self.assertEqual('67899******************************************************************96',
 #                          hsp.aln_annotation['PP'])
 
@@ -98,10 +98,10 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(116, hsp.hit_end)
         self.assertEqual(54, hsp.query_start)
         self.assertEqual(163, hsp.query_end)
-        self.assertEqual('XCXXXXCCXXXXXCXXXXXXCXXXCXXXXCXXXXXCXXX--XXXCXXXXCCXXXXXCXXXXXXCXXXCXXXXCXXXXXCX' \
-                        'XX--XXXCXXXXCCXXXXXCXXXXXXCXXX',
-                        str(hsp.hit.seq))
-        self.assertEqual('TCTNNQCCSQYGYCGFGAEYCGAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCG' \
+        self.assertEqual('XCXXXXCCXXXXXCXXXXXXCXXXCXXXXCXXXXXCXXX--XXXCXXXXCCXXXXXCXXXXXXCXXXCXXXXCXXXXXCX'
+                         'XX--XXXCXXXXCCXXXXXCXXXXXXCXXX',
+                         str(hsp.hit.seq))
+        self.assertEqual('TCTNNQCCSQYGYCGFGAEYCGAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCG'
                          'KDAGGRVCTNNYCCSKWGSCGIGPGYCGAG',
                          str(hsp.query.seq))
 
@@ -140,7 +140,7 @@ class HhsuiteCases(unittest.TestCase):
 
         hit = qresult[0]
         self.assertEqual('1klr_A_1', hit.id)
-        self.assertEqual('Zinc finger Y-chromosomal protein; transcription; NMR {Synthetic} SCOP: g.37.1.1 PDB: ' \
+        self.assertEqual('Zinc finger Y-chromosomal protein; transcription; NMR {Synthetic} SCOP: g.37.1.1 PDB: '
                          '5znf_A 1kls_A 1xrz_A* 7znf_A', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(3.4E+04, hit.evalue)
@@ -161,7 +161,7 @@ class HhsuiteCases(unittest.TestCase):
         # Check last hit
         hit = qresult[nhits - 1]
         self.assertEqual('1zfd_A_1', hit.id)
-        self.assertEqual('SWI5; DNA binding motif, zinc finger DNA binding domain; NMR {Saccharomyces cerevisiae}' \
+        self.assertEqual('SWI5; DNA binding motif, zinc finger DNA binding domain; NMR {Saccharomyces cerevisiae}'
                          ' SCOP: g.37.1.1', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(3.6e+04, hit.evalue)
@@ -196,9 +196,9 @@ class HhsuiteCases(unittest.TestCase):
 
         hit = qresult[0]
         self.assertEqual('5ZIM_A_1', hit.id)
-        self.assertEqual('Bacteriorhodopsin; proton pump, membrane protein, PROTON; HET: L2P, RET; 1.25A {Halobacterium' \
-                         ' salinarum}; Related PDB entries: 1R84_A 1KG8_A 1KME_B 1KGB_A 1KG9_A 1KME_A 4X31_A 5ZIL_A 1E0P_A '\
-                         '4X32_A 5ZIN_A 1S53_B 1S51_B 1S53_A 1S54_A 1F50_A 1S54_B 1S51_A 1F4Z_A 5J7A_A 1S52_B 1S52_A 4Y9H_A ' \
+        self.assertEqual('Bacteriorhodopsin; proton pump, membrane protein, PROTON; HET: L2P, RET; 1.25A {Halobacterium'
+                         ' salinarum}; Related PDB entries: 1R84_A 1KG8_A 1KME_B 1KGB_A 1KG9_A 1KME_A 4X31_A 5ZIL_A 1E0P_A '
+                         '4X32_A 5ZIN_A 1S53_B 1S51_B 1S53_A 1S54_A 1F50_A 1S54_B 1S51_A 1F4Z_A 5J7A_A 1S52_B 1S52_A 4Y9H_A '
                          '3T45_A 3T45_C 3T45_B 1C3W_A 1L0M_A', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(2.1e-48, hit.evalue)
@@ -213,11 +213,11 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(227, hsp.hit_end)
         self.assertEqual(1, hsp.query_start)
         self.assertEqual(226, hsp.query_end)
-        self.assertEqual('GRPEWIWLALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGYGLTMVPFGGEQNPIYWARYAD' \
-                         'WLFTTPLLLLDLALLVDADQGTILALVGADGIMIGTGLVGALTKVYSYRFVWWAISTAAMLYILYVLFFGFTSKAESMRP' \
+        self.assertEqual('GRPEWIWLALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGYGLTMVPFGGEQNPIYWARYAD'
+                         'WLFTTPLLLLDLALLVDADQGTILALVGADGIMIGTGLVGALTKVYSYRFVWWAISTAAMLYILYVLFFGFTSKAESMRP'
                          'EVASTFKVLRNVTVVLWSAYPVVWLIGSEGAGIVPLNIETLLFMVLDVSAKVGFGLILLRSRAIFG', str(hsp.hit.seq))
-        self.assertEqual('GRPEWIWLALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGYGLTMVPFGGEQNPIYWARYAD' \
-                         'WLFTTPLLLLDLALLVDADQGTILALVGADGIMIGTGLVGALTKVYSYRFVWWAISTAAMLYILYVLFFGFTSKAESMRP' \
+        self.assertEqual('GRPEWIWLALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGYGLTMVPFGGEQNPIYWARYAD'
+                         'WLFTTPLLLLDLALLVDADQGTILALVGADGIMIGTGLVGALTKVYSYRFVWWAISTAAMLYILYVLFFGFTSKAESMRP'
                          'EVASTFKVLRNVTVVLWSAYPVVWLIGSEGAGIVPLNIETLLFMVLDVSAKVGFGLILLRSRAIFG', str(hsp.query.seq))
 
         # Check last hit
@@ -241,7 +241,6 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual('FWLVTAALLASTVFFFVERDRVS-AKWKTSLTVSGLVTGIAFWHYMYMRGVW', str(hsp.hit.seq))
         self.assertEqual('LALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGY', str(hsp.query.seq))
 
-
     def test_q9bsu1(self):
         "Test parsing hhsearch_q9bsu1_uniclust_w_ss_pfamA_30.hhr"
 
@@ -253,7 +252,8 @@ class HhsuiteCases(unittest.TestCase):
 
         nhits = 16
         self.assertEqual('HHSUITE', qresult.program)
-        self.assertEqual('sp|Q9BSU1|CP070_HUMAN UPF0183 protein C16orf70 OS=Homo sapiens OX=9606 GN=C16orf70 PE=1 SV=1', qresult.id)
+        self.assertEqual('sp|Q9BSU1|CP070_HUMAN UPF0183 protein C16orf70 OS=Homo sapiens OX=9606 GN=C16orf70'
+                         ' PE=1 SV=1', qresult.id)
         self.assertEqual(422, qresult.seq_len)
         self.assertEqual(nhits, len(qresult))
 
@@ -273,17 +273,17 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(395, hsp.hit_end)
         self.assertEqual(11, hsp.query_start)
         self.assertEqual(407, hsp.query_end)
-        self.assertEqual('SLGNEQWEFTLGMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLK' \
-                         'YCGVHFNSQAIAPTIEQIDQSFGATHPGVYNSAEQLFHLNFRGLSFSFQLDSWTEAPKYEPNFAHGLASLQIPHGATVKR' \
-                         'MYIYSGNSLQDTKAPMMPLSCFLGNVYAESVDVLRDGTGPAGLRLRLLAAGCGPGLLADAKMRVFERSVYFGDSCQDVLS' \
-                         'MLGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYFTLGVDILFDANTHKVKKFVLHTNYPGHYNFNIYHRCEFKIP' \
-                         'LAIKKENADGQTE--TCTTYSKWDNIQELLGHPVEKPVVLHRSSSPNNTNPFGSTFCFGLQRMIFEVMQNNHIASVTLY', \
+        self.assertEqual('SLGNEQWEFTLGMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLK'
+                         'YCGVHFNSQAIAPTIEQIDQSFGATHPGVYNSAEQLFHLNFRGLSFSFQLDSWTEAPKYEPNFAHGLASLQIPHGATVKR'
+                         'MYIYSGNSLQDTKAPMMPLSCFLGNVYAESVDVLRDGTGPAGLRLRLLAAGCGPGLLADAKMRVFERSVYFGDSCQDVLS'
+                         'MLGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYFTLGVDILFDANTHKVKKFVLHTNYPGHYNFNIYHRCEFKIP'
+                         'LAIKKENADGQTE--TCTTYSKWDNIQELLGHPVEKPVVLHRSSSPNNTNPFGSTFCFGLQRMIFEVMQNNHIASVTLY',
                          str(hsp.query.seq))
-        self.assertEqual('EQWE----FALGMPLAQAISILQKHCRIIKNVQVLYSEQMPLSHDLILNLTQDGIKLLFDACNQRLKVIEVYDLTKVKLK' \
-                         'YCGVHFNSQAIAPTIEQIDQSFGATHPGVYNAAEQLFHLNFRGLSFSFQLDSWSEAPKYEPNFAHGLASLQIPHGATVKR' \
-                         'MYIYSGNNLQETKAPAMPLACFLGNVYAECVEVLRDGAGPLGLKLRLLTAGCGPGVLADTKVRAVERSIYFGDSCQDVLS' \
-                         'ALGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYYILGVDILFDSTTHLVKKFVLHTNFPGHYNFNIYHRCDFKIP' \
-                         'LIIKKDGADAHSEDCILTTYSKWDQIQELLGHPMEKPVVLHRSSSANNTNPFGSTFCFGLQRMIFEVMQNNHIASVTLY', \
+        self.assertEqual('EQWE----FALGMPLAQAISILQKHCRIIKNVQVLYSEQMPLSHDLILNLTQDGIKLLFDACNQRLKVIEVYDLTKVKLK'
+                         'YCGVHFNSQAIAPTIEQIDQSFGATHPGVYNAAEQLFHLNFRGLSFSFQLDSWSEAPKYEPNFAHGLASLQIPHGATVKR'
+                         'MYIYSGNNLQETKAPAMPLACFLGNVYAECVEVLRDGAGPLGLKLRLLTAGCGPGVLADTKVRAVERSIYFGDSCQDVLS'
+                         'ALGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYYILGVDILFDSTTHLVKKFVLHTNFPGHYNFNIYHRCDFKIP'
+                         'LIIKKDGADAHSEDCILTTYSKWDQIQELLGHPMEKPVVLHRSSSANNTNPFGSTFCFGLQRMIFEVMQNNHIASVTLY',
                          str(hsp.hit.seq))
 
         # Check last hit
@@ -323,7 +323,8 @@ class HhsuiteCases(unittest.TestCase):
 
         hit = qresult[0]
         self.assertEqual('4P79_A_1', hit.id)
-        self.assertEqual('cell adhesion protein; cell adhesion, tight junction, membrane; HET: OLC, MSE; 2.4A {Mus musculus}', hit.description)
+        self.assertEqual('cell adhesion protein; cell adhesion, tight junction, membrane; HET: OLC'
+                         ', MSE; 2.4A {Mus musculus}', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(6.8e-32, hit.evalue)
         self.assertEqual(194.63, hit.score)
@@ -337,19 +338,19 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(198, hsp.hit_end)
         self.assertEqual(1, hsp.query_start)
         self.assertEqual(198, hsp.query_end)
-        self.assertEqual('GSEFMSVAVETFGFFMSALGLLMLGLTLSNSYWRVSTVHGNVITTNTIFENLWYSCATDSLGVSNCWDFPSMLALSGYVQ' \
-                         'GCRALMITAILLGFLGLFLGMVGLRATNVGNMDLSKKAKLLAIAGTLHILAGACGMVAISWYAVNITTDFFNPLYAGTKY' \
+        self.assertEqual('GSEFMSVAVETFGFFMSALGLLMLGLTLSNSYWRVSTVHGNVITTNTIFENLWYSCATDSLGVSNCWDFPSMLALSGYVQ'
+                         'GCRALMITAILLGFLGLFLGMVGLRATNVGNMDLSKKAKLLAIAGTLHILAGACGMVAISWYAVNITTDFFNPLYAGTKY'
                          'ELGPALYLGWSASLLSILGGICVFSTAAASSKEEPATR', str(hsp.query.seq))
-        self.assertEqual('GSEFMSVAVETFGFFMSALGLLMLGLTLSNSYWRVSTVHGNVITTNTIFENLWYSCATDSLGVSNCWDFPSMLALSGYVQ' \
-                         'GCRALMITAILLGFLGLFLGMVGLRATNVGNMDLSKKAKLLAIAGTLHILAGACGMVAISWYAVNITTDFFNPLYAGTKY' \
+        self.assertEqual('GSEFMSVAVETFGFFMSALGLLMLGLTLSNSYWRVSTVHGNVITTNTIFENLWYSCATDSLGVSNCWDFPSMLALSGYVQ'
+                         'GCRALMITAILLGFLGLFLGMVGLRATNVGNMDLSKKAKLLAIAGTLHILAGACGMVAISWYAVNITTDFFNPLYAGTKY'
                          'ELGPALYLGWSASLLSILGGICVFSTAAASSKEEPATR', str(hsp.hit.seq))
 
         # Check last hit
         hit = qresult[nhits - 1]
         self.assertEqual('5YQ7_F_1', hit.id)
-        self.assertEqual('Beta subunit of light-harvesting 1; Photosynthetic core complex, PHOTOSYNTHESIS; ' \
-                         'HET: MQE, BCL, HEM, KGD, BPH;{Roseiflexus castenholzii}; Related PDB entries: 5YQ7_V' \
-                         ' 5YQ7_3 5YQ7_T 5YQ7_J 5YQ7_9 5YQ7_N 5YQ7_A 5YQ7_P 5YQ7_H 5YQ7_D 5YQ7_5 5YQ7_7 5YQ7_1 ' \
+        self.assertEqual('Beta subunit of light-harvesting 1; Photosynthetic core complex, PHOTOSYNTHESIS; '
+                         'HET: MQE, BCL, HEM, KGD, BPH;{Roseiflexus castenholzii}; Related PDB entries: 5YQ7_V'
+                         ' 5YQ7_3 5YQ7_T 5YQ7_J 5YQ7_9 5YQ7_N 5YQ7_A 5YQ7_P 5YQ7_H 5YQ7_D 5YQ7_5 5YQ7_7 5YQ7_1 '
                          '5YQ7_R', hit.description)
         self.assertTrue(hit.is_included)
         self.assertEqual(6.7, hit.evalue)
@@ -366,7 +367,6 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(37, hsp.query_end)
         self.assertEqual('RTSVVVSTLLGLVMALLIHFVVLSSGAFNWLRAP', str(hsp.hit.seq))
         self.assertEqual('SVAVETFGFFMSALGLLMLGLTLSNS--YWRVST', str(hsp.query.seq))
-        
 
     def test_9590198(self):
         "Test parsing hhpred_9590198.hhr"
@@ -379,7 +379,8 @@ class HhsuiteCases(unittest.TestCase):
 
         nhits = 34
         self.assertEqual('HHSUITE', qresult.program)
-        self.assertEqual('sp|Q9BSU1|CP070_HUMAN UPF0183 protein C16orf70 OS=Homo sapiens OX=9606 GN=C16orf70 PE=1 SV=1',
+        self.assertEqual('sp|Q9BSU1|CP070_HUMAN UPF0183 protein C16orf70 OS=Homo sapiens OX=9606 GN=C16orf70'
+                         ' PE=1 SV=1',
                          qresult.id)
         self.assertEqual(422, qresult.seq_len)
         self.assertEqual(nhits, len(qresult))
@@ -400,17 +401,17 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(394, hsp.hit_end)
         self.assertEqual(22, hsp.query_start)
         self.assertEqual(407, hsp.query_end)
-        self.assertEqual('GMHFSQSVAIIQSQVGTIRGVQVLYSDQNPLSVDLVINMPQDGMRLIFDPVAQRLKIIEIYNMKLVKLRYSGMCFNSPEI' \
-                         'TPSIEQVEHCFGATHPGLYDSQRHLFALNFRGLSFYFPVDS-----KFEPGYAHGLGSLQFPNGGSPVVSRTTIYYGSQH' \
-                         'QLSSNTSSRVSGVPLPDLPLSCYRQQLHLRRCDVLRNTTSTMGLRLHMFTEGT--SRALEPSQVALVRVVRFGDSCQGVA' \
-                         'RALGAPARLYYKADDKMRIHRPTARRR-PPPASDYLFNYFTLGLDVLFDARTNQVKKFVLHTNYPGHYNFNMYHRCEFEL' \
-                         'TVQPD-KSEAHSLVESGGGVAVTAYSKWEVVSRAL-RVCERPVVLNRASSTNTTNPFGSTFCYGYQDIIFEVMSNNYIAS' \
+        self.assertEqual('GMHFSQSVAIIQSQVGTIRGVQVLYSDQNPLSVDLVINMPQDGMRLIFDPVAQRLKIIEIYNMKLVKLRYSGMCFNSPEI'
+                         'TPSIEQVEHCFGATHPGLYDSQRHLFALNFRGLSFYFPVDS-----KFEPGYAHGLGSLQFPNGGSPVVSRTTIYYGSQH'
+                         'QLSSNTSSRVSGVPLPDLPLSCYRQQLHLRRCDVLRNTTSTMGLRLHMFTEGT--SRALEPSQVALVRVVRFGDSCQGVA'
+                         'RALGAPARLYYKADDKMRIHRPTARRR-PPPASDYLFNYFTLGLDVLFDARTNQVKKFVLHTNYPGHYNFNMYHRCEFEL'
+                         'TVQPD-KSEAHSLVESGGGVAVTAYSKWEVVSRAL-RVCERPVVLNRASSTNTTNPFGSTFCYGYQDIIFEVMSNNYIAS'
                          'ITLY', str(hsp.hit.seq))
-        self.assertEqual('GMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLKYCGVHFNSQAI' \
-                         'APTIEQIDQSFGATHPGVYNSAEQLFHLNFRGLSFSFQLDSWTEAPKYEPNFAHGLASLQIPHGA--TVKRMYIYSGNSL' \
-                         'Q---------DTKA-PMMPLSCFLGNVYAESVDVLRDGTGPAGLRLRLLAAGCGPGLLADAKMRVFERSVYFGDSCQDVL' \
-                         'SMLGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYFTLGVDILFDANTHKVKKFVLHTNYPGHYNFNIYHRCEFKI' \
-                         'PLAIKKENADG------QTETCTTYSKWDNIQELLGHPVEKPVVLHRSSSPNNTNPFGSTFCFGLQRMIFEVMQNNHIAS' \
+        self.assertEqual('GMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLKYCGVHFNSQAI'
+                         'APTIEQIDQSFGATHPGVYNSAEQLFHLNFRGLSFSFQLDSWTEAPKYEPNFAHGLASLQIPHGA--TVKRMYIYSGNSL'
+                         'Q---------DTKA-PMMPLSCFLGNVYAESVDVLRDGTGPAGLRLRLLAAGCGPGLLADAKMRVFERSVYFGDSCQDVL'
+                         'SMLGSPHKVFYKSEDKMKIHSPSPHKQVPSKCNDYFFNYFTLGVDILFDANTHKVKKFVLHTNYPGHYNFNIYHRCEFKI'
+                         'PLAIKKENADG------QTETCTTYSKWDNIQELLGHPVEKPVVLHRSSSPNNTNPFGSTFCFGLQRMIFEVMQNNHIAS'
                          'VTLY', str(hsp.query.seq))
 
         # Check last hit
@@ -430,10 +431,11 @@ class HhsuiteCases(unittest.TestCase):
         self.assertEqual(96, hsp.hit_end)
         self.assertEqual(19, hsp.query_start)
         self.assertEqual(114, hsp.query_end)
-        self.assertEqual('FTLGMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLKYCGVH-FN' \
+        self.assertEqual('FTLGMPLAQAVAILQKHCRIIKNVQVLYSEQSPLSHDLILNLTQDGIKLMFDAFNQRLKVIEVCDLTKVKLKYCGVH-FN'
                          'SQAIAPTIEQIDQSFGA', str(hsp.query.seq))
-        self.assertEqual('IQFGMDRTLVWQLAGADQSCSDQVERIICYNNPDH-------YGPQGHFFFNA-ADKLIHKRQMELFPAPKPTMRLATYN' \
+        self.assertEqual('IQFGMDRTLVWQLAGADQSCSDQVERIICYNNPDH-------YGPQGHFFFNA-ADKLIHKRQMELFPAPKPTMRLATYN'
                          'KTQTGMTEAQFWAAVPS', str(hsp.hit.seq))
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
