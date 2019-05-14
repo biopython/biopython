d5395cd8a3b1d7ec8b1f2a1f9d6fb13e2af88a9d
Markus.Piotrowski@ruhr-uni-bochum.de <Markus.Piotrowski@ruhr-uni-bochum.de>
2019-05-14 23:06:41 +0200

    Restructure pairwise2 test suite

--- Tests/test_pairwise2_no_C.py
+++ Tests/test_pairwise2_no_C.py
@@ -4,7 +4,7 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-"""Tests for pairwise2 module using the default C functions.
+"""Tests for pairwise2 module using the pure Python fallback functions.
 
 This test file imports the TestCases from ``pairwise2_testCases.py``.
 If you want to add more tests, do this over there.
@@ -16,13 +16,11 @@ import unittest
 # Import all test classes deliberately:
 from pairwise2_testCases import *  # noqa: F401, F403
 
-# Implicitly using functions from C extension:
 from Bio import pairwise2
 
-if pairwise2.rint == pairwise2._python_rint:
-    from Bio import MissingExternalDependencyError
-    raise MissingExternalDependencyError("Missing or non-compiled file: "
-                                         "'cpairwise2'")
+# Explicitly using pure Python fallback functions:
+pairwise2._make_score_matrix_fast = pairwise2._python_make_score_matrix_fast
+pairwise2.rint = pairwise2._python_rint
 
 
 if __name__ == '__main__':
