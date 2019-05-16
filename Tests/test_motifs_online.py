1d7304de8e3efff5278e205fba43a5229f76ba77
peterjc <p.j.a.cock@googlemail.com>
2019-05-16 12:08:38 +0100

    Dual license Tests/test_motifs_online.py
    Original authors Antony Lee and myself, and contributors since as recorded
    with git have agreed:
    
     - Antony Lee (@anntzer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236969803
     - Nader Morshed (@naderm)
       https://github.com/biopython/biopython/issues/898#issuecomment-238632934
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613

--- Tests/test_motifs_online.py
+++ Tests/test_motifs_online.py
@@ -1,8 +1,9 @@
 # Copyright 2015 by Peter Cock.  All rights reserved.
 # Copyright 2015 by Antony Lee. All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """Testing online code for Bio.motifs (weblogo etc)."""
 import os
