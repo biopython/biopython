a66f4954a16ae8fa47129f1b01bea820c406bcb1
Markus Piotrowski <Markus.Piotrowski@ruhr-uni-bochum.de>
2019-05-24 18:09:29 +0200

    Introduce charge_at_pH functionality

--- Bio/SeqUtils/IsoelectricPoint.py
+++ Bio/SeqUtils/IsoelectricPoint.py
@@ -31,48 +31,112 @@ charged_aas = ("K", "R", "H", "D", "E", "C", "Y")
 
 
 class IsoelectricPoint(object):
-    """A class with one public method to calculate the pI of a protein.
+    """A class for calculating the IEP or charge at given pH of a protein.
 
-    Access this class through ProtParam.ProteinAnalysis class.
-    First make a ProteinAnalysis object and then call its isoelectric_point
-    method.
+    Parameters
+    ----------
+    :protein_sequence: A ``Bio.Seq`` or string object containing a protein
+                       sequence.
+    :aa_content: A dictionary with amino acid letters as keys and it's
+                 occurences as integers, e.g. ``{'A': 3, 'C': 0, ...}``.
+                 Default: ``None``. If ``None``, the dic will be calculated
+                 from the given sequence.
+
+    Methods
+    -------
+    :charge_at_pH(pH):  Calculates the charge of the protein for a given pH
+    :pi():              Calculates the isoelectric point
+
+
+    Examples
+    --------
+    The methods of this class can either be accessed from the class itself
+    or from a ``ProtParam.ProteinAnalysis`` object (with partially different
+    names):
+
+    >>> from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint as IP
+    >>> protein = IP('INGAR')
+    >>> print('IEP of peptide {} is {:.2f}'
+    ...       .format(protein.sequence, protein.pi()))
+    IEP of peptide INGAR is 9.75
+    >>> print("It's charge at pH 7 is {:.2f}"
+    ...       .format(protein.charge_at_pH(7.0)))
+    It's charge at pH 7 is 0.76
+
+
+    >>> from Bio.SeqUtils.ProtParam import ProteinAnalysis as PA
+    >>> protein = PA('PETER')
+    >>> print('IEP of {}: {:.2f}'.format(protein.sequence,
+    ...                                  protein.isoelectric_point()))
+    IEP of PETER: 4.53
+    >>> print('Charge at pH 4.53: {:.2f}'
+    ...       .format(protein.charge_at_pH(4.53)))
+    Charge at pH 4.53: 0.00
+
     """
 
-    def __init__(self, ProteinSequence, AminoAcidsContent):
+    def __init__(self, protein_sequence, aa_content=None):
         """Initialize the class."""
-        self.sequence = ProteinSequence
-        self.charged_aas_content = self._select_charged(AminoAcidsContent)
+        self.sequence = str(protein_sequence).upper()
+        if not aa_content:
+            from Bio.SeqUtils.ProtParam import ProteinAnalysis as _PA
+            aa_content = _PA(self.sequence).count_amino_acids()
+        self.charged_aas_content = self._select_charged(aa_content)
+
+        self.pos_pKs, self.neg_pKs = self._update_pKs_tables()
 
     # This function creates a dictionary with the contents of each charged aa,
     # plus Cterm and Nterm.
-    def _select_charged(self, AminoAcidsContent):
+    def _select_charged(self, aa_content):
         charged = {}
         for aa in charged_aas:
+<<<<<<< HEAD
             charged[aa] = float(AminoAcidsContent[aa])
         charged["Nterm"] = 1.0
         charged["Cterm"] = 1.0
+=======
+            charged[aa] = float(aa_content[aa])
+        charged['Nterm'] = 1.0
+        charged['Cterm'] = 1.0
+>>>>>>> f0e502f8d... Introduce charge_at_pH functionality
         return charged
 
+    def _update_pKs_tables(self):
+        """Update pKs tables with seq specific values for N- and C-termini."""
+        pos_pKs = positive_pKs.copy()
+        neg_pKs = negative_pKs.copy()
+        nterm, cterm = self.sequence[0], self.sequence[-1]
+        if nterm in pKnterminal:
+            pos_pKs['Nterm'] = pKnterminal[nterm]
+        if cterm in pKcterminal:
+            neg_pKs['Cterm'] = pKcterminal[cterm]
+        return pos_pKs, neg_pKs
+
     # This function calculates the total charge of the protein at a given pH.
-    def _chargeR(self, pH, pos_pKs, neg_pKs):
-        PositiveCharge = 0.0
-        for aa, pK in pos_pKs.items():
+    def _chargeR(self, pH):
+        positive_charge = 0.0
+        for aa, pK in self.pos_pKs.items():
             CR = 10 ** (pK - pH)
             partial_charge = CR / (CR + 1.0)
-            PositiveCharge += self.charged_aas_content[aa] * partial_charge
+            positive_charge += self.charged_aas_content[aa] * partial_charge
 
-        NegativeCharge = 0.0
-        for aa, pK in neg_pKs.items():
+        negative_charge = 0.0
+        for aa, pK in self.neg_pKs.items():
             CR = 10 ** (pH - pK)
             partial_charge = CR / (CR + 1.0)
-            NegativeCharge += self.charged_aas_content[aa] * partial_charge
+            negative_charge += self.charged_aas_content[aa] * partial_charge
 
-        return PositiveCharge - NegativeCharge
+        return positive_charge - negative_charge
+
+    def charge_at_pH(self, pH):
+        """Calculate the charge of a protein at given pH."""
+        return self._chargeR(pH)
 
     # This is the action function, it tries different pH until the charge of
     # the protein is 0 (or close).
     def pi(self):
         """Calculate and return the isoelectric point as float."""
+<<<<<<< HEAD
         pos_pKs = dict(positive_pKs)
         neg_pKs = dict(negative_pKs)
         nterm = self.sequence[0]
@@ -82,45 +146,47 @@ class IsoelectricPoint(object):
         if cterm in pKcterminal:
             neg_pKs["Cterm"] = pKcterminal[cterm]
 
+=======
+>>>>>>> f0e502f8d... Introduce charge_at_pH functionality
         # Bracket between pH1 and pH2
-        pH = 7.0
-        Charge = self._chargeR(pH, pos_pKs, neg_pKs)
-        if Charge > 0.0:
+        pH = 7
+        charge = self._chargeR(pH)
+        if charge > 0.0:
             pH1 = pH
-            Charge1 = Charge
-            while Charge1 > 0.0:
+            charge1 = charge
+            while charge1 > 0.0:
                 pH = pH1 + 1.0
-                Charge = self._chargeR(pH, pos_pKs, neg_pKs)
-                if Charge > 0.0:
+                charge = self._chargeR(pH)
+                if charge > 0.0:
                     pH1 = pH
-                    Charge1 = Charge
+                    charge1 = charge
                 else:
                     pH2 = pH
-                    Charge2 = Charge
+                    charge2 = charge
                     break
         else:
             pH2 = pH
-            Charge2 = Charge
-            while Charge2 < 0.0:
+            charge2 = charge
+            while charge2 < 0.0:
                 pH = pH2 - 1.0
-                Charge = self._chargeR(pH, pos_pKs, neg_pKs)
-                if Charge < 0.0:
+                charge = self._chargeR(pH)
+                if charge < 0.0:
                     pH2 = pH
-                    Charge2 = Charge
+                    charge2 = charge
                 else:
                     pH1 = pH
-                    Charge1 = Charge
+                    charge1 = charge
                     break
 
         # Bisection
-        while pH2 - pH1 > 0.0001 and Charge != 0.0:
+        while pH2 - pH1 > 0.0001 and charge != 0.0:
             pH = (pH1 + pH2) / 2.0
-            Charge = self._chargeR(pH, pos_pKs, neg_pKs)
-            if Charge > 0.0:
+            charge = self._chargeR(pH)
+            if charge > 0.0:
                 pH1 = pH
-                Charge1 = Charge
+                charge1 = charge
             else:
                 pH2 = pH
-                Charge2 = Charge
+                charge2 = charge
 
         return pH
