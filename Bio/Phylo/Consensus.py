f2ef7374eed918d10efa81e251f835af162a0f88
Chris Rands <c_rands100@hotmail.com>
2019-05-29 15:35:22 +0200

    bug fixes

--- Bio/Phylo/Consensus.py
+++ Bio/Phylo/Consensus.py
@@ -633,5 +633,5 @@ def _equal_topology(tree1, tree2):
     """
     term_names1 = {term.name for term in tree1.find_clades(terminal=True)}
     term_names2 = {term.name for term in tree2.find_clades(terminal=True)}
-    return (term_names1 == term_names2) and
-            (_bitstring_topology(tree1) == _bitstring_topology(tree2))
+    return ((term_names1 == term_names2) and
+            (_bitstring_topology(tree1) == _bitstring_topology(tree2)))
