63684483c2cf78faa23ca55db724d3c79c1a6d75
Peter Cock <p.j.a.cock@googlemail.com>
2019-06-03 13:59:21 +0100

    flake8 noqa, workaround for pydocstyle crash in flake8 on Python2.7
    Still don't understand why these changes should trigger anything
    in pydocstyle (under Python 2 but not 3), but that is known to
    sometimes crash flake8:
    
    https://gitlab.com/pycqa/flake8-docstrings/issues/24

--- Scripts/Restriction/ranacompiler.py
+++ Scripts/Restriction/ranacompiler.py
@@ -407,7 +407,7 @@ class DictionaryBuilder(object):
             #   Now select the right type for the enzyme.
             #
             bases = cls.bases
-            clsbases = tuple(eval(x) for x in bases)
+            clsbases = tuple([eval(x) for x in bases])  # noqa: C407
             typestuff = ''
             for t in tdct.values():
                 #
@@ -434,7 +434,8 @@ class DictionaryBuilder(object):
             else:
                 enzlst = []
                 tydct = dict(typestuff.__dict__)
-                tydct = {k: v for k, v in tydct.items() if k in commonattr}
+                tydct = dict([(k, v) for k, v in tydct.items()  # noqa: C404
+                              if k in commonattr])
                 enzlst.append(name)
                 typedict[typename] = (bases, enzlst)
             for letter in cls.__dict__['suppl']:
