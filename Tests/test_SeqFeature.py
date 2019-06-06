ae87425f386034bc24839d09dee634519e7a0bf2
mdehoon <mjldehoon@yahoo.com>
2019-06-06 21:31:50 +0900

    converted to unittest (#2110)
    * converted to unittest
    
    * merge into test_SeqFeature.py

--- Tests/test_SeqFeature.py
+++ Tests/test_SeqFeature.py
@@ -1,17 +1,21 @@
+# Copyright 2001 by Brad Chapman.  All rights reserved.
+# Revisions copyright 2011-2013 by Peter Cock. All rights reserved.
 # Copyright 2015-2017 by Kai Blin.  All rights reserved.
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-
 """Tests Bio.SeqFeature."""
 
 import unittest
+
 from os import path
+
 from Bio import Seq, SeqIO
 from Bio.Alphabet import generic_dna
 from Bio.Data.CodonTable import TranslationError
 from Bio.SeqFeature import FeatureLocation, AfterPosition, BeforePosition
 from Bio.SeqFeature import CompoundLocation, UnknownPosition, SeqFeature
+from Bio.SeqFeature import ExactPosition, WithinPosition, BetweenPosition
 
 
 class TestReference(unittest.TestCase):
@@ -147,3 +151,49 @@ class TestSeqFeature(unittest.TestCase):
         f.qualifiers['transl_table'] = [11]
         with self.assertRaises(TranslationError):
             f.translate(seq)
+
+
+class TestLocations(unittest.TestCase):
+
+    def test_fuzzy(self):
+        """Test fuzzy representations."""
+        # check the positions alone
+        exact_pos = ExactPosition(5)
+        within_pos_s = WithinPosition(10, left=10, right=13)
+        within_pos_e = WithinPosition(13, left=10, right=13)
+        between_pos_e = BetweenPosition(24, left=20, right=24)
+        before_pos = BeforePosition(15)
+        after_pos = AfterPosition(40)
+        self.assertEqual(int(within_pos_s), 10)
+        self.assertEqual(str(within_pos_s), "(10.13)")
+        self.assertEqual(int(within_pos_e), 13)
+        self.assertEqual(str(within_pos_e), "(10.13)")
+        self.assertEqual(int(between_pos_e), 24)
+        self.assertEqual(str(between_pos_e), "(20^24)")
+        self.assertEqual(str(before_pos), "<15")
+        self.assertEqual(str(after_pos), ">40")
+        # put these into Locations
+        location1 = FeatureLocation(exact_pos, within_pos_e)
+        location2 = FeatureLocation(before_pos, between_pos_e)
+        location3 = FeatureLocation(within_pos_s, after_pos)
+        self.assertEqual(str(location1), "[5:(10.13)]")
+        self.assertEqual(str(location1.start), "5")
+        self.assertEqual(str(location1.end), "(10.13)")
+        self.assertEqual(str(location2), "[<15:(20^24)]")
+        self.assertEqual(str(location2.start), "<15")
+        self.assertEqual(str(location2.end), "(20^24)")
+        self.assertEqual(str(location3), "[(10.13):>40]")
+        self.assertEqual(str(location3.start), "(10.13)")
+        self.assertEqual(str(location3.end), ">40")
+        # --- test non-fuzzy representations
+        self.assertEqual(location1.nofuzzy_start, 5)
+        self.assertEqual(location1.nofuzzy_end, 13)
+        self.assertEqual(location2.nofuzzy_start, 15)
+        self.assertEqual(location2.nofuzzy_end, 24)
+        self.assertEqual(location3.nofuzzy_start, 10)
+        self.assertEqual(location3.nofuzzy_end, 40)
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
