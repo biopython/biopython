5c2ba0bdb339171f8b00658a96bf5955a29a7528
Michiel de Hoon <mjldehoon@yahoo.com>
2019-06-07 19:13:34 +0900

    converted to unittest

--- Tests/test_SearchIO_legacy.py
+++ Tests/test_SearchIO_legacy.py
@@ -5,11 +5,13 @@
 
 """Tests for SearchIO legacy module."""
 
-from __future__ import print_function
 
 import string
+import unittest
+
 from Bio._py3k import StringIO
 from Bio import File
+
 
 import warnings
 from Bio import BiopythonWarning
@@ -17,103 +19,65 @@ with warnings.catch_warnings():
     warnings.simplefilter('ignore', BiopythonWarning)
     from Bio.SearchIO._legacy import ParserSupport
 
-# pyUnit
 
+class TestParserSupport(unittest.TestCase):
 
-def pb(b):
-    if b:
-        return 1
-    return 0
+    def test_TaggingConsumer(self):
 
-# TaggingConsumer
+        h = StringIO()
+        tc = ParserSupport.TaggingConsumer(handle=h, colwidth=5)
+        tc.start_section()
+        self.assertEqual(h.getvalue(), '***** start_section\n')
+        h.seek(0)
+        h.truncate(0)
+        tc.test1('myline')
+        self.assertEqual(h.getvalue(), 'test1: myline\n')
+        h.seek(0)
+        h.truncate(0)
+        tc.end_section()
+        self.assertEqual(h.getvalue(), '***** end_section\n')
 
+    def test_is_blank_line(self):
+        is_blank_line = ParserSupport.is_blank_line
+        self.assertTrue(is_blank_line('\n'))
+        self.assertTrue(is_blank_line('\r\n'))
+        self.assertTrue(is_blank_line('\r'))
+        self.assertTrue(is_blank_line(''))
+        self.assertTrue(is_blank_line('', allow_spaces=1))
+        self.assertTrue(is_blank_line('', allow_spaces=0))
+        self.assertTrue(is_blank_line(string.whitespace, allow_spaces=1))
+        self.assertFalse(is_blank_line('hello'))
+        self.assertFalse(is_blank_line('hello', allow_spaces=1))
+        self.assertFalse(is_blank_line('hello', allow_spaces=0))
+        self.assertFalse(is_blank_line(string.whitespace, allow_spaces=0))
 
-print("Running tests on TaggingConsumer")
-
-
-class TestHandle(object):
-    def write(self, s):
-        print(s)
-
-
-h = TestHandle()
-tc = ParserSupport.TaggingConsumer(handle=h, colwidth=5)
-tc.start_section()  # '***** start_section\n'
-tc.test1('myline')  # 'test1: myline\n'
-tc.end_section()    # '***** end_section\n'
-
-
-# is_blank_line
-
-print("Running tests on is_blank_line")
-
-
-def is_blank_line(*args, **keywds):
-    return pb(ParserSupport.is_blank_line(*args, **keywds))
-
-
-print(is_blank_line('\n'))                              # 1
-print(is_blank_line('\r\n'))                            # 1
-print(is_blank_line('\r'))                              # 1
-print(is_blank_line(''))                                # 1
-print(is_blank_line('', allow_spaces=1))                # 1
-print(is_blank_line('', allow_spaces=0))                # 1
-print(is_blank_line(string.whitespace, allow_spaces=1))  # 1
-print(is_blank_line('hello'))                           # 0
-print(is_blank_line('hello', allow_spaces=1))           # 0
-print(is_blank_line('hello', allow_spaces=0))           # 0
-print(is_blank_line(string.whitespace, allow_spaces=0))  # 0
-
-
-# safe_readline
-
-print("Running tests on safe_readline")
-
-data = """This
+    def test_safe_readline(self):
+        data = """\
+This
 file"""
+        h = File.UndoHandle(StringIO(data))
+        safe_readline = ParserSupport.safe_readline
+        self.assertEqual(safe_readline(h), "This\n")
+        self.assertEqual(safe_readline(h), "file")
+        self.assertRaises(ValueError, safe_readline, h)
 
-h = File.UndoHandle(StringIO(data))
-
-safe_readline = ParserSupport.safe_readline
-print(safe_readline(h))    # "This"
-print(safe_readline(h))    # "file"
-try:
-    safe_readline(h)
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-
-# safe_peekline
-
-print("Running tests on safe_peekline")
-safe_peekline = ParserSupport.safe_peekline
-
-data = """This
+    def test_safe_peekline(self):
+        safe_peekline = ParserSupport.safe_peekline
+        data = """\
+This
 file"""
+        h = File.UndoHandle(StringIO(data))
+        self.assertEqual(safe_peekline(h), "This\n")
+        h.readline()
+        self.assertEqual(safe_peekline(h), "file")
+        h.readline()
+        self.assertRaises(ValueError, safe_peekline, h)
+        h.saveline('hello')
+        self.assertEqual(safe_peekline(h), 'hello')
 
-h = File.UndoHandle(StringIO(data))
-
-print(safe_peekline(h))  # "This"
-h.readline()
-print(safe_peekline(h))  # "file"
-h.readline()
-try:
-    safe_peekline(h)
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-h.saveline('hello')
-print(safe_peekline(h))  # 'hello'
-
-
-# read_and_call
-
-print("Running tests on read_and_call")
-
-data = """>gi|132871|sp|P19947|RL30_BACSU 50S RIBOSOMAL PROTEIN L30 (BL27)
+    def test_read_and_call(self):
+        data = """\
+>gi|132871|sp|P19947|RL30_BACSU 50S RIBOSOMAL PROTEIN L30 (BL27)
 MAKLEITLKRSVIGRPEDQRVTVRTLGLKKTNQTVVHEDNAAIRGMINKVSHLVSVKEQ
 >gi|132679|sp|P19946|RL15_BACSU 50S RIBOSOMAL PROTEIN L15
 MKLHELKPSEGSRKTRNRVGRGIGSGNGKTAGKGHKGQNARSGGGVRPGFEGGQMPLFQRLPKRGFTNIN
@@ -122,83 +86,35 @@ GTAEVI
 
 
 """
+        h = File.UndoHandle(StringIO(data))
+        lines = []
+        rac = ParserSupport.read_and_call
+        rac(h, lines.append)
+        self.assertEqual(lines[-1][:10], '>gi|132871')
+        rac(h, lines.append, start='MAKLE', end='KEQ', contains='SVIG')
+        self.assertRaises(ValueError, rac, h, lines.append, blank=1)
+        self.assertRaises(ValueError, rac, h, lines.append, start='foobar')
+        self.assertRaises(ValueError, rac, h, lines.append, end='foobar')
+        self.assertRaises(ValueError, rac, h, lines.append, contains='foobar')
+        self.assertRaises(ValueError, rac, h, lines.append, blank=0)
 
-h = File.UndoHandle(StringIO(data))
-
-rac = ParserSupport.read_and_call
-lines = []
-
-
-def m(line):
-    lines.append(line)
-
-
-rac(h, m)
-print(lines[-1][:10])   # '>gi|132871'
-rac(h, m, start='MAKLE', end='KEQ', contains='SVIG')
-rac(h, m, blank=0)
-
-# These should be errors.  If they're not, then complain.
-try:
-    rac(h, m, blank=1)
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-try:
-    rac(h, m, start='foobar')
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-try:
-    rac(h, m, end='foobar')
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-try:
-    rac(h, m, contains='foobar')
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-try:
-    rac(h, m, blank=0)
-except ValueError:
-    print("correctly failed")
-else:
-    print("ERROR, should have failed")
-
-
-# attempt_read_and_call
-
-print("Running tests on attempt_read_and_call")
-
-
-def arac(*args, **keywds):
-    return pb(ParserSupport.attempt_read_and_call(*args, **keywds))
-
-
-def m(line):
-    lines.append(line)
-
-
-data = """>gi|132871|sp|P19947|RL30_BACSU 50S RIBOSOMAL PROTEIN L30 (BL27)
+    def test_attempt_read_and_call(self):
+        data = """\
+>gi|132871|sp|P19947|RL30_BACSU 50S RIBOSOMAL PROTEIN L30 (BL27)
 MAKLEITLKRSVIGRPEDQRVTVRTLGLKKTNQTVVHEDNAAIRGMINKVSHLVSVKEQ
 >gi|132679|sp|P19946|RL15_BACSU 50S RIBOSOMAL PROTEIN L15
 MKLHELKPSEGSRKTRNRVGRGIGSGNGKTAGKGHKGQNARSGGGVRPGFEGGQMPLFQRLPKRGFTNIN
 RKEYAVVNLDKLNGFAEGTEVTPELLLETGVISKLNAGVKILGNGKLEKKLTVKANKFSASAKEAVEAAG
 GTAEVI"""
+        h = File.UndoHandle(StringIO(data))
+        lines = []
+        arac = ParserSupport.attempt_read_and_call
+        self.assertTrue(arac(h, lines.append, contains="RIBOSOMAL PROTEIN"))
+        self.assertFalse(arac(h, lines.append, start="foobar"))
+        self.assertFalse(arac(h, lines.append, blank=1))
+        self.assertTrue(arac(h, lines.append, end="LVSVKEQ"))
 
-h = File.UndoHandle(StringIO(data))
-lines = []
 
-print(arac(h, m, contains="RIBOSOMAL PROTEIN"))   # 1
-print(arac(h, m, start="foobar"))                 # 0
-print(arac(h, m, blank=1))                        # 0
-print(arac(h, m, end="LVSVKEQ"))                  # 1
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
