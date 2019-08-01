b10b0aed3d1e6e7ea98ba077aa7e5c4ffe188c18
peterjc <p.j.a.cock@googlemail.com>
2019-08-01 13:21:16 +0100

    Document use of tilde and dots in MSF
    Based on http://rothlab.ucdavis.edu/genhelp/pileup.html
    and http://rothlab.ucdavis.edu/genhelp/appendix_iii.html

--- Bio/AlignIO/MsfIO.py
+++ Bio/AlignIO/MsfIO.py
@@ -10,6 +10,11 @@ The file format was produced by the GCG PileUp and and LocalPileUp tools,
 and later tools such as T-COFFEE and MUSCLE support it as an optional
 output format.
 
+The original GCG toola would write gaps at ends of each sequence which could
+be missing data as tildes (``~``), whereas internal gaps were periods (``.``)
+instead. This parser replaces both with minus signs (``-``) for consistency
+with the rest of ``Bio.AlignIO``.
+
 You are expected to use this module via the Bio.AlignIO functions (or the
 Bio.SeqIO functions if you want to work directly with the gapped sequences).
 """
@@ -282,8 +287,7 @@ class MsfIterator(AlignmentIterator):
 
         records = (
             SeqRecord(
-                # Early files used ~ for gaps, MUSCLE uses . for a gap
-                # Convert to Biopython convention of - for gaps:
+                # Convert tilde or dots to Biopython convention of - for gaps:
                 Seq("".join(s).replace("~", "-").replace(".", "-"), self.alphabet),
                 id=i,
                 name=i,
