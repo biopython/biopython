731078220bd39f28467494d69ee794a2918c4938
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2019-08-27 15:23:46 +0200

    Fix path to test files.
    Paths phenotype/Plate.json were changed to Tests/phenotype/Plate.json

--- Bio/phenotype/phen_micro.py
+++ Bio/phenotype/phen_micro.py
@@ -64,7 +64,7 @@ class PlateRecord(object):
     accessed calling their id as an index or iterating on the PlateRecord:
 
     >>> from Bio import phenotype
-    >>> plate = phenotype.read("phenotype/Plate.json", "pm-json")
+    >>> plate = phenotype.read("Tests/phenotype/Plate.json", "pm-json")
     >>> well = plate['A05']
     >>> for well in plate:
     ...    print(well.id)
@@ -128,7 +128,7 @@ class PlateRecord(object):
     Two PlateRecord objects can be compared: if all their wells are equal the
     two plates are considered equal:
 
-    >>> plate2 = phenotype.read("phenotype/Plate.json", "pm-json")
+    >>> plate2 = phenotype.read("Tests/phenotype/Plate.json", "pm-json")
     >>> plate == plate2
     True
 
@@ -207,7 +207,7 @@ class PlateRecord(object):
         plate[0:2,1:3] uses only rows 0 & 1 and only cols 1 & 2
 
         >>> from Bio import phenotype
-        >>> plate = phenotype.read("phenotype/Plate.json", "pm-json")
+        >>> plate = phenotype.read("Tests/phenotype/Plate.json", "pm-json")
 
         You can access a well of the plate, using its id.
 
@@ -570,7 +570,7 @@ class WellRecord(object):
     WellRecord or using lists indexes or slices:
 
     >>> from Bio import phenotype
-    >>> plate = phenotype.read("phenotype/Plate.json", "pm-json")
+    >>> plate = phenotype.read("Tests/phenotype/Plate.json", "pm-json")
     >>> well = plate['A05']
     >>> for time, signal in well:
     ...    print("Time: %f, Signal: %f" % (time, signal)) # doctest:+ELLIPSIS
@@ -793,7 +793,7 @@ class WellRecord(object):
         method.  e.g.
 
         >>> from Bio import phenotype
-        >>> plate = phenotype.read("phenotype/Plate.json", "pm-json")
+        >>> plate = phenotype.read("Tests/phenotype/Plate.json", "pm-json")
         >>> record = plate['A05']
         >>> print(record)
         Plate ID: PM01
