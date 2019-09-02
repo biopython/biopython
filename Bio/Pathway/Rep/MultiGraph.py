c8e0c144429e24a18b7ffbb53a8df02f59090a40
peterjc <p.j.a.cock@googlemail.com>
2019-09-02 11:44:10 +0100

    Dual license Bio/Pathway/Rep/MultiGraph.py
    Original author Tarjei Mikkelsen and all the contributors since as tracked
    by version control have agreed:
    
     - Brian Osborne (@bosborne)
       https://github.com/biopython/biopython/issues/898#issuecomment-241429879
     - Carlos Pena (@carlosp420)
       https://github.com/biopython/biopython/issues/898#issuecomment-236925650
     - Chris Rands (@chris-rands)
       https://github.com/biopython/biopython/pull/1301
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Connor McCoy (@cmccoy)
       https://github.com/biopython/biopython/issues/898#issuecomment-380478091
     - Francesco Gastaldello (@gasta88)
       https://github.com/biopython/biopython/pull/1063
     - Maximilian Greil (@MaxGreil)
       https://github.com/biopython/biopython/pull/1110
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Tarjei Mikkelsen (@tmikkelsen)
       https://github.com/biopython/biopython/issues/2243#issuecomment-526818174

--- Bio/Pathway/Rep/MultiGraph.py
+++ Bio/Pathway/Rep/MultiGraph.py
@@ -1,8 +1,10 @@
 # Copyright 2001 by Tarjei Mikkelsen.  All rights reserved.
 # Revisions copyright 2018 by Maximilian Greil. All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """get/set abstraction for multi-graph representation."""
 
