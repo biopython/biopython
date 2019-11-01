a7892a7aaa96a1737a7e99739886255c39ad26c0
Peter Cock <p.j.a.cock@googlemail.com>
2019-11-01 15:03:55 +0000

    Dual license Bio/Phylo/PAML/baseml.py
    Original author Brandon Invergo and contributors since as
    tracked with version control have all agreed:
    
     - Brandon Invergo (@brandoninvergo)
       https://github.com/biopython/biopython/issues/898#issuecomment-238558638
     - Brian Osborne (@bosborne)
       https://github.com/biopython/biopython/issues/898#issuecomment-241429879
     - Carlos Pena (@carlosp420)
       https://github.com/biopython/biopython/issues/898#issuecomment-236925650
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Lenna Peterson (@lennax)
       https://github.com/biopython/biopython/issues/898#issuecomment-237023021
     - @morrme
       https://github.com/biopython/biopython/pull/1172#issuecomment-295430028
     - Nick Negretti (@nimne)
       https://github.com/biopython/biopython/pull/1768#issue-210631196
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Rona Costello (@RonaCostello)
       https://github.com/biopython/biopython/pull/1793
     - Sergio Valqui (@svalqui)
       https://github.com/biopython/biopython/pull/1749
     - Wayne Decatur (@fomightez)
       https://github.com/biopython/biopython/issues/898#issuecomment-380149442

--- Bio/Phylo/PAML/baseml.py
+++ Bio/Phylo/PAML/baseml.py
@@ -1,7 +1,9 @@
 # Copyright (C) 2011, 2018 by Brandon Invergo (b.invergo@gmail.com)
-# This code is part of the Biopython distribution and governed by its
-# license. Please see the LICENSE file that should have been included
-# as part of this package.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """Classes for the support of baseml.
 
