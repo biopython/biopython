db53cf427176b5bffd7bc3153dcaa6eb7013c9f8
Peter Cock <p.j.a.cock@googlemail.com>
2019-11-05 09:29:53 +0000

    Test DOA_prot.msf raises expected exception

--- Tests/test_AlignIO.py
+++ Tests/test_AlignIO.py
@@ -7,7 +7,6 @@
 
 
 import unittest
-import warnings
 
 
 from Bio._py3k import StringIO
@@ -405,29 +404,12 @@ class TestAlignIO_reading(unittest.TestCase):
 
     def test_reading_alignments_msf1(self):
         path = "msf/DOA_prot.msf"
-        with warnings.catch_warnings(record=True) as w:
-            self.check_iterator_for_loop_handle(path, "msf", 1, 12)
-            self.check_iterator_for_loop_filename(path, "msf", 1)
-            self.check_iterator_next(path, "msf", 1)
-            self.check_iterator_next_and_list(path, "msf", 1)
-            self.check_iterator_next_for_loop(path, "msf", 1)
-            alignment = self.check_read(path, "msf", 12, 250)
-        warning_msgs = {str(_.message) for _ in w}
-        self.assertIn(
-            "GCG MSF header said alignment length 62, but 11 of 12 sequences said Len: 250 - attempting to continue",
-            warning_msgs)
-        self.assertIn(
-            "One of more alignment sequences were truncated and have been gap padded",
-            warning_msgs)
-        self.check_alignment_columns(
-            alignment, ["MMMM-M-MMMMM",
-                        "AAAA-A-AAAAA",
-                        "LLLL-L-LLLLL",
-                        "RRRR-R-RRRRR",
-                        "AAAA-A-AAAAA",
-                        # ...
-                        "RRRRRRRRRRR-"])
-        self.check_summary_simple(alignment)
+        with self.assertRaisesRegexp(
+                ValueError,
+                "GCG MSF header said alignment length 62, "
+                "but 11 of 12 sequences said Len: 250"
+        ):
+            AlignIO.read(path, "msf")
 
     def test_reading_alignments_stockholm1(self):
         path = "Stockholm/simple.sth"
