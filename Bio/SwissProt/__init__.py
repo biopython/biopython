38e843b5470e0cb3e8aec6a4fc31a8b77c75b349
Michiel de Hoon <mjldehoon@yahoo.com>
2019-12-17 19:35:27 +0900

    travis

--- Bio/SwissProt/__init__.py
+++ Bio/SwissProt/__init__.py
@@ -25,6 +25,7 @@ class SwissProtParserError(ValueError):
     """An error occurred while parsing a SwissProt file."""
 
     def __init__(self, *args, line=None):
+        """Create a SwissProtParserError object with the offending line."""
         super().__init__(*args)
         self.line = line
 
