85aa21c3ebe0d70c220d6dca7ab15fbb948a73c6
Amrithasuresh <sureshcbt@gmail.com>
2019-12-21 12:30:11 -0500

    Dual license BSD dual license Bio.KDTreemodule Original author Michiel de Hoon and contributors since as tracked with version control have all agree:
    -- Michiel de Hoon (@mdehoon)
       [#2374 (comment)](https://github.com/biopython/biopython/issues/2374#issuecomment-566825939)
    -- Christian Brueffer (@cbrueffer)
       [#898 (comment)](https://github.com/biopython/biopython/issues/898#issuecomment-236922857)
    -- She Zhang (@SHZ66)
       [#1617 (comment)](https://github.com/biopython/biopython/pull/1617#issue-182351202)

--- Bio/KDTree/KDTreemodule.c
+++ Bio/KDTree/KDTreemodule.c
@@ -1,3 +1,11 @@
+/* Copyright 2008-2018 by Michiel de Hoon.  All rights reserved.
+ *
+ * This file is part of the Biopython distribution and governed by your
+ * choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+ * Please see the LICENSE file that should have been included as part of this
+ * package.
+ */
+
 #include <Python.h>
 #include "KDTree.h"
 
