ca4b3bcce8d60c0b7302c6491fa192b96c45fb7d
Chris Rands <c_rands100@hotmail.com>
2020-01-01 16:28:56 +0100

    pyupgrade again

--- Bio/Entrez/Parser.py
+++ Bio/Entrez/Parser.py
@@ -879,14 +879,14 @@ class DataHandler:
         path = os.path.join(self.local_dtd_dir, filename)
         try:
             handle = open(path, "rb")
-        except IOError:
+        except FileNotFoundError:
             pass
         else:
             return handle
         path = os.path.join(self.global_dtd_dir, filename)
         try:
             handle = open(path, "rb")
-        except IOError:
+        except FileNotFoundError:
             pass
         else:
             return handle
@@ -898,14 +898,14 @@ class DataHandler:
         path = os.path.join(self.local_xsd_dir, filename)
         try:
             handle = open(path, "rb")
-        except IOError:
+        except FileNotFoundError:
             pass
         else:
             return handle
         path = os.path.join(self.global_xsd_dir, filename)
         try:
             handle = open(path, "rb")
-        except IOError:
+        except FileNotFoundError:
             pass
         else:
             return handle
@@ -917,7 +917,7 @@ class DataHandler:
         path = os.path.join(self.local_dtd_dir, filename)
         try:
             handle = open(path, "wb")
-        except IOError:
+        except OSError:
             warnings.warn("Failed to save %s at %s" % (filename, path))
         else:
             handle.write(text)
@@ -929,7 +929,7 @@ class DataHandler:
         path = os.path.join(self.local_xsd_dir, filename)
         try:
             handle = open(path, "wb")
-        except IOError:
+        except OSError:
             warnings.warn("Failed to save %s at %s" % (filename, path))
         else:
             handle.write(text)
@@ -974,7 +974,7 @@ class DataHandler:
             # the internet instead.
             try:
                 handle = urlopen(url)
-            except IOError:
+            except OSError:
                 raise RuntimeError("Failed to access %s at %s" % (filename, url)) from None
             text = handle.read()
             handle.close()
