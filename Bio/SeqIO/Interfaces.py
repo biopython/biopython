2300abf84b23b858875fa3b85c1d716573faa1f3
Peter Cock <p.j.a.cock@googlemail.com>
2020-01-09 13:01:08 +0000

    Remove unused import

--- Bio/SeqIO/Interfaces.py
+++ Bio/SeqIO/Interfaces.py
@@ -10,9 +10,6 @@ Unless you are writing a new parser or writer for Bio.SeqIO, you should not
 use this module.  It provides base classes to try and simplify things.
 """
 
-
-import sys  # for checking if Python 2
-
 from Bio.Alphabet import generic_alphabet
 from Bio.Seq import Seq, MutableSeq
 from Bio.SeqRecord import SeqRecord
