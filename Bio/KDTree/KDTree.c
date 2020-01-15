c96c48d83d1ea1c2cb3ecccc53feab2906291409
Peter Cock <p.j.a.cock@googlemail.com>
2020-01-15 22:19:11 +0000

    Dual license Bio/KDTree/KDTree.c
    Original author Michiel de Hoon and contributors
    since as tracked by version control have all
    agreed:
    
     - Bartosz Telenczuk (@btel)
       https://github.com/biopython/biopython/commit/285c085d9f0476eaefcc1049e36285f2d3b9b54f
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Joanna and Dominik kasprzak (by email to Peter Cock on 9 September 2016)
       https://github.com/biopython/biopython/issues/898#issuecomment-236945515
     - Kristian Rother (@krother)
       https://github.com/biopython/biopython/issues/898#issuecomment-236926827
     - Michiel de Hoon (@mdehoon)
       https://github.com/biopython/biopython/issues/898#issuecomment-237122630
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613

--- Bio/KDTree/KDTree.c
+++ Bio/KDTree/KDTree.c
@@ -1,3 +1,11 @@
+/* Copyright 2008 by Michiel de Hoon.  All rights reserved.
+ *
+ * This file is part of the Biopython distribution and governed by your
+ * choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+ * Please see the LICENSE file that should have been included as part of this
+ * package.
+ */
+
 #include <math.h>
 #include <stdlib.h>
 #include "KDTree.h"
