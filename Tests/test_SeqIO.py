0934934d0a02adacd8d3cd64b16e3a22ce91d517
Michiel de Hoon <mjldehoon@yahoo.com>
2020-01-31 13:26:35 +0900

    updated Bio.SeqIO.InsdcIO for python3

--- Tests/test_SeqIO.py
+++ Tests/test_SeqIO.py
@@ -156,7 +156,7 @@ class TestZipped(unittest.TestCase):
             self.assertEqual(6, len(list(SeqIO.parse(handle, "gb"))))
         with gzip.open("GenBank/cor6_6.gb.bgz") as handle:
             with self.assertRaisesRegex(
-                ValueError, "Is this handle in binary mode not text mode"
+                ValueError, "GenBank files must be opened in text mode."
             ):
                 list(SeqIO.parse(handle, "gb"))
 
