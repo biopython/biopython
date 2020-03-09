abb2d15584ae47e8048e405b7ff93e94d750212a
svalqui <svalqui@acslink.net.au>
2020-03-10 09:12:39 +1100

    Apply black style to test_AlignIO* files, version 19.10b0. test_AlignIO_ClustalIO.py test_AlignIO_convert.py test_AlignIO_EmbossIO.py test_AlignIO_FastaIO.py test_AlignIO_MauveIO.py test_AlignIO_PhylipIO.py

--- Tests/test_AlignIO_convert.py
+++ Tests/test_AlignIO_convert.py
@@ -57,18 +57,23 @@ tests = [
     ("Fasta/output001.m10", "fasta-m10", None),
     ("IntelliGenetics/VIF_mase-pro.txt", "ig", generic_protein),
     ("NBRF/clustalw.pir", "pir", None),
-    ]
+]
 output_formats = ["fasta"] + sorted(AlignIO._FormatToWriter)
 
 for filename, in_format, alphabet in tests:
     for out_format in output_formats:
+
         def funct(fn, fmt1, fmt2, alpha):
             f = lambda x: x.simple_check(fn, fmt1, fmt2, alpha)  # noqa: E731
             f.__doc__ = "Convert %s from %s to %s" % (fn, fmt1, fmt2)
             return f
-        setattr(ConvertTests, "test_%s_%s_to_%s"
-                % (filename.replace("/", "_").replace(".", "_"), in_format, out_format),
-                funct(filename, in_format, out_format, alphabet))
+
+        setattr(
+            ConvertTests,
+            "test_%s_%s_to_%s"
+            % (filename.replace("/", "_").replace(".", "_"), in_format, out_format),
+            funct(filename, in_format, out_format, alphabet),
+        )
     del funct
 
 if __name__ == "__main__":
