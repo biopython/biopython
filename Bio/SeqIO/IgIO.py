34fd67b2dc7159831f2338e0f323685b7dd0fc08
Michiel de Hoon <mjldehoon@yahoo.com>
2020-04-22 22:29:44 +0900

    Ig

--- Bio/SeqIO/IgIO.py
+++ Bio/SeqIO/IgIO.py
@@ -17,23 +17,70 @@ You are expected to use this module via the Bio.SeqIO functions.
 from Bio.Alphabet import single_letter_alphabet
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio import StreamModeError
+from . import Interfaces
 
 
-def _parse(handle, alphabet):
-    # Skip any file header text before the first record (;; lines)
-    for line in handle:
-        if not line.startswith(";;"):
-            break
-    else:
-        # Empty file, or header only
-        return
 
-    if line[0] != ";":
-        raise ValueError("Records should start with ';' and not:\n%r" % line)
+class IgIterator(Interfaces.SequenceIterator):
 
-    while line:
-        # Now iterate over the records
+    def __init__(self, source, alphabet=single_letter_alphabet):
+        """Iterate over IntelliGenetics records (as SeqRecord objects).
+
+        source - file-like object opened in text mode, or a path to a file
+        alphabet - optional alphabet
+
+        The optional free format file header lines (which start with two
+        semi-colons) are ignored.
+
+        The free format commentary lines at the start of each record (which
+        start with a semi-colon) are recorded as a single string with embedded
+        new line characters in the SeqRecord's annotations dictionary under the
+        key 'comment'.
+
+        Examples
+        --------
+        >>> with open("IntelliGenetics/TAT_mase_nuc.txt") as handle:
+        ...     for record in IgIterator(handle):
+        ...         print("%s length %i" % (record.id, len(record)))
+        ...
+        A_U455 length 303
+        B_HXB2R length 306
+        C_UG268A length 267
+        D_ELI length 309
+        F_BZ163A length 309
+        O_ANT70 length 342
+        O_MVP5180 length 348
+        CPZGAB length 309
+        CPZANT length 309
+        A_ROD length 390
+        B_EHOA length 420
+        D_MM251 length 390
+        STM_STM length 387
+        VER_AGM3 length 354
+        GRI_AGM677 length 264
+        SAB_SAB1C length 219
+        SYK_SYK length 330
+
+        """
+        super().__init__(source, alphabet=alphabet, mode="t", fmt="IntelliGenetics")
+
+    def read_header(self, handle):
+        # Skip any file header text before the first record (;; lines)
+        for line in handle:
+            if not line.startswith(";;"):
+                self.line = line
+                break
+        else:
+            # Empty file, or header only
+            self.line = None
+            return
+
+    def parse(self, handle):
+        line = self.line
+        if line is None:
+            raise StopIteration
+        if line[0] != ";":
+            raise ValueError("Records should start with ';' and not:\n%r" % line)
 
         # Try and agree with SeqRecord convention from the GenBank parser,
         # (and followed in the SwissProt parser) which stores the comments
@@ -63,67 +110,11 @@ def _parse(handle, alphabet):
             raise ValueError("Potential terminator digit one found within sequence.")
 
         # Return the record and then continue...
+        alphabet = self.alphabet
         record = SeqRecord(Seq(seq_str, alphabet), id=title, name=title)
         record.annotations["comment"] = "\n".join(comment_lines)
-        yield record
-
-    # We should be at the end of the file now
-    assert not line
-
-
-def IgIterator(source, alphabet=single_letter_alphabet):
-    """Iterate over IntelliGenetics records (as SeqRecord objects).
-
-    source - file-like object opened in text mode, or a path to a file
-    alphabet - optional alphabet
-
-    The optional free format file header lines (which start with two
-    semi-colons) are ignored.
-
-    The free format commentary lines at the start of each record (which
-    start with a semi-colon) are recorded as a single string with embedded
-    new line characters in the SeqRecord's annotations dictionary under the
-    key 'comment'.
-
-    Examples
-    --------
-    >>> with open("IntelliGenetics/TAT_mase_nuc.txt") as handle:
-    ...     for record in IgIterator(handle):
-    ...         print("%s length %i" % (record.id, len(record)))
-    ...
-    A_U455 length 303
-    B_HXB2R length 306
-    C_UG268A length 267
-    D_ELI length 309
-    F_BZ163A length 309
-    O_ANT70 length 342
-    O_MVP5180 length 348
-    CPZGAB length 309
-    CPZANT length 309
-    A_ROD length 390
-    B_EHOA length 420
-    D_MM251 length 390
-    STM_STM length 387
-    VER_AGM3 length 354
-    GRI_AGM677 length 264
-    SAB_SAB1C length 219
-    SYK_SYK length 330
-
-    """
-    try:
-        handle = open(source)
-    except TypeError:
-        handle = source
-        if handle.read(0) != "":
-            raise StreamModeError(
-                "IntelliGenetics files must be opened in text mode."
-            ) from None
-
-    try:
-        yield from _parse(handle, alphabet)
-    finally:
-        if handle is not source:
-            handle.close()
+        self.line = line
+        return record
 
 
 if __name__ == "__main__":
