1e422106ac58bc36f7c7cf940572a56135c715fe
Troy <troyalekson@gmail.com>
2020-05-08 20:15:44 +0300

    deleted my local venv

--- venv/lib/python3.8/site-packages/pydocstyle/__init__.py
+++
@@ -1,7 +0,0 @@
-from .checker import check
-from .violations import Error, conventions
-from .utils import __version__
-
-# Temporary hotfix for flake8-docstrings
-from .checker import ConventionChecker
-from .parser import AllError
