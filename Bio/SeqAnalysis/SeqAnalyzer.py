8e2fd1d2afb137c131849c9d3e5e75437cd55544
Kacper Domzal <kd386308@students.mimuw.edu.pl>
2020-05-12 21:43:44 +0200

    added new functionalities

--- Bio/SeqAnalysis/SeqAnalyzer.py
+++ Bio/SeqAnalysis/SeqAnalyzer.py
@@ -1,10 +1,16 @@
 import pandas as pd
-#from Bio import SeqAnalysis
+from Bio import SeqAnalysis
+import numpy as np
 
 seq_db = SeqAnalysis.database(['YP_025292.1', '1JOY'])
 records = seq_db.get()
 
+
 class analyzer(records):
+    """
+    A class that takes in records from SeqAnalysis.database and performs
+    multiple operations on it and returns pd.DataFrame with values
+    """
 
     def makedict(records: list[tuple]) -> dict :
         """
@@ -19,13 +25,22 @@ class analyzer(records):
 
 
     def standard_deviaton(self):
-        pass
-
+        """
+        Calculates standard deviation of lenths of given sequences
+        :return:
+        changes self.stdev values to standard value calculate from sequences
+        """
+        list_of_seq = dict.values()
+        list_of_lenths = []
+        for _ in list_of_seq:
+            list_of_lenths.append(len(_))
+        stdev = np.std(list_of_lenths)
+        self.stdev = stdev
 
 
     def __init__(self):
         self.dict = analyzer.makedict(records) #creates a dict with seq ids and seqeances itself
-        self.stdev = 
+        self.stdev = 0
 
 
 
