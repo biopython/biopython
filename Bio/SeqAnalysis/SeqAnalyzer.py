06da8808d6a2e8e9a8b0b0dc2efe377b1f3b425c
Kacper Domzal <kd386308@students.mimuw.edu.pl>
2020-05-12 22:47:05 +0200

    draft of functionalities

--- Bio/SeqAnalysis/SeqAnalyzer.py
+++ Bio/SeqAnalysis/SeqAnalyzer.py
@@ -2,14 +2,8 @@ import pandas as pd
 from Bio import SeqAnalysis
 import numpy as np
 
-seq_db = SeqAnalysis.database(['YP_025292.1', '1JOY'])
+#seq_db = SeqAnalysis.database(['YP_025292.1', '1JOY'])
 records = seq_db.get()
-
-
-
-
-
-
 
 
 class analyzer(records):
@@ -17,7 +11,19 @@ class analyzer(records):
     A class that takes in records from SeqAnalysis.database and performs
     multiple operations on it and returns pd.DataFrame with values
     """
-    def standard_deviaton(self):
+
+    def makedict(self) -> dict:
+        """
+        Makes a dictionary from records, where ids are keys and sequences are values
+        :return:
+        returns a created dictionary
+        """
+        slownik = {}
+        for _ in records:
+            slownik[_[0]] = _[1][0]
+        return slownik
+
+    def median_stdev_avg(self):
         """
         Calculates standard deviation of lenths of given sequences
         :return:
@@ -28,34 +34,34 @@ class analyzer(records):
         for _ in list_of_seq:
             list_of_lenths.append(len(_))
         stdev = np.std(list_of_lenths)
-        self.stdev = stdev
-
+        avg = np.average(list_of_lenths)
+        median = np.median(list_of_lenths)
+        return stdev, avg, median
 
     def __init__(self):
-        self.dict = analyzer.makedict(records) #creates a dict with seq ids and seqeances itself
-        self.stdev = 0
-        self.median = 0
-        self.average = 0
+        self.dict = analyzer.makedict(self)  # creates a dict with seq ids and seqeances itself
+        self.tuples = [(id, seq) for id, seq in self.dict.values()]
+        self.stdev, self.average, self.median = analyzer.median_stdev_avg(self)
 
     def results(self):
-        data = {
-            'id': [],
-            'sequence_length': [],
+        data = pd.DataFrame(data={
+            'id': [id[0] for id in self.tuples].append(['median', 'standard_deviation', 'average_lenth']),
+            'sequence_length': [len(seq[1]) for seq in self.tuples].append([self.median, self.stdev, self.average]),
             'molecular_weight': [],
-            'theoretical_pl': [1.4, 24.5],
-            'amino_acid_comp': [{'A': 12, 'G': 24}, {'L': 124, 'A': 52}],
-            'atomic_comp': [{'C': 42, 'H': 335, 'N': 55}, {'C': 55, 'H': 523, 'N': 13, 'O': 53}],
-            'extinction_coefficient': [1],
+            'theoretical_pl': [],
+            'extinction_coefficient': [],
             'instability_index': [],
             'gravy': [],
             'est_half_life': [],
             'c_term_freq': [{'A': 0.2, 'G': 0.8}, {'A': 0.3, 'L': 0.7}],
             'n_term_freq': [{'A': 0.8, 'G': 0.2}, {'A': 0.7, 'L': 0.3}],
-            'standard_deviation': self.stdev,
-            'median':
-        }
+            'amino_acid_comp': [{'A': 12, 'G': 24}, {'L': 124, 'A': 52}],
+            'atomic_comp': [{'C': 42, 'H': 335, 'N': 55}, {'C': 55, 'H': 523, 'N': 13, 'O': 53}]
+        })
+
         df = pd.DataFrame(data)
         return df
+
 
 seq_analyzer = SeqAnalysis.analyzer(records)
 results = seq_analyzer.results()
