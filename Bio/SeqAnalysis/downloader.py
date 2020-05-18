32ba278b06c0b4032d364df00e5cd83eb4c4632e
s0lczi <solak977@gmail.com>
2020-05-18 19:22:19 +0200

    Dockstrings for classes and most of the class methods. Typing for methods parameters. Added few exceptions for wrong parameter type.

--- Bio/SeqAnalysis/downloader.py
+++ Bio/SeqAnalysis/downloader.py
@@ -1,21 +1,55 @@
 import os
 import re
-import urllib
 from multiprocessing.pool import ThreadPool
 from os.path import isfile, join
 from time import time as timer
+from typing import Union, List, Tuple
+from urllib import request, error
 
 from Bio import Entrez
 from Bio import SeqIO
 
 
 class SeqDownloader:
-    def __init__(self, full_list, directory):
-        if type(full_list) == str:
+    """
+    SeqDownloader class is responsible for downloading fasta files for Uniprot/NCBI IDs
+    corresponding to given list or single ID ensuring their uniqueness.
+    """
+
+    def __init__(self, full_list: Union[str, List], directory: str):
+        """
+        In several steps, __init__ method is downloading and saving multiple fasta files in a short period of time:
+        1) Checks if the parameters are of the correct type, if not, raises exceptions
+        2) Checks if directories needed for proper file organization exists, if not, creates them
+        3) Prepares list of urls based on the list of IDs from which files will be obtained
+        4) Downloads and saves files using multiprocessing
+        5) Saves database in one fasta file ready for parse by SeqIO module. Also saves additional outputs which are
+           text files that contains entries not downloaded either because of being obsolete or due to http errors
+
+        :param full_list: Either List of IDs or single str.
+        :param directory: str given by user to organize files properly.
+
+        :raises: Exception when variables are not of the expected type.
+
+        """
+        if isinstance(full_list, str):
             clear_ids = [full_list]
-        elif type(full_list) == list:
+        elif isinstance(full_list, List):
             clear_ids = full_list
-        analysis_dir = directory
+        else:
+            raise Exception("Given variable is not either str or List.")
+        if isinstance(directory, str):
+            analysis_dir = directory
+        else:
+            raise Exception("Your directory name is not str.")
+
+        clear_ids = list(dict.fromkeys(clear_ids))
+
+        for list_element in clear_ids:
+            if isinstance(list_element, str):
+                pass
+            else:
+                raise Exception("At least one element of your IDs list is not str.")
 
         print("Number of unique IDs: {0}".format(len(clear_ids)))
         print("Name of the current batch of files: {0}".format(analysis_dir))
@@ -49,10 +83,10 @@ class SeqDownloader:
         obsolete = list(dict.fromkeys(obsolete))
 
         mypath = ('./Downloads/{0}'.format(analysis_dir))
-        onlyfiles = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+        only_files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
         files_no_ext = [".".join(f.split(".")[:-1]) for f in os.listdir(mypath) if os.path.isfile(join(mypath, f))]
 
-        total_number = len(onlyfiles)
+        total_number = len(only_files)
         print("Download of " + str(total_number) + f" took {timer() - start} seconds")
 
         print("-------------------------------------Saving outputs------------------------------------------")
@@ -71,7 +105,7 @@ class SeqDownloader:
                 fail.write(item + '\n')
 
         print("Creating database. Please wait...")
-        for file in onlyfiles:
+        for file in only_files:
             with open('./Downloads/{0}/{1}'.format(analysis_dir, file)) as big_fasta:
                 one_fasta = big_fasta.read()
                 with open('./Database/{0}.fasta'.format(analysis_dir), 'a+') as data:
@@ -81,12 +115,22 @@ class SeqDownloader:
         print("Done.")
         self.downloaded_files = files_no_ext
 
-    def read_list_of_ids(self, file) -> list:
+    @staticmethod
+    def read_list_of_ids(file: str) -> List:
+        """
+        Reads given file and creates List with unique IDs.
+
+        :param file: str, name of file.
+        :raise: Exception when can't find a file.
+        :return: List with IDs.
+        """
         try:
             data = ''
             with open(file, 'r') as f:
                 for line in f.readlines():
                     line = re.sub('[!@/#$|:]', ' ', line).strip()
+                    # Substitutes above signs with space ensuring your
+                    # file with IDs will be always read correctly.
                     data = data + ' ' + line
                 data = data.split()
                 data = list(dict.fromkeys(data))
@@ -98,26 +142,36 @@ class SeqDownloader:
                             "If not please specify path to the ID file.\n"
                             "E.g. dir1/subdir1/list.txt")
 
-    def mk_dirs(self, dirName):
-        if not os.path.exists(dirName):
-            os.mkdir(dirName)
-            print("Directory {0} status: created".format(dirName))
+    @staticmethod
+    def mk_dirs(dir_name: str):
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+            print("Directory {0} status: created".format(dir_name))
         else:
-            print("Directory {0} status: already exists".format(dirName))
+            print("Directory {0} status: already exists".format(dir_name))
 
-    def mk_subdirs(self, dirName):
-        if not os.path.exists('./Downloads/{0}/'.format(dirName)):
-            os.mkdir('./Downloads/{0}/'.format(dirName))
-            print("Directory Downloads/{0} status: created".format(dirName))
+    @staticmethod
+    def mk_subdirs(dir_name: str):
+        if not os.path.exists('./Downloads/{0}/'.format(dir_name)):
+            os.mkdir('./Downloads/{0}/'.format(dir_name))
+            print("Directory Downloads/{0} status: created".format(dir_name))
         else:
-            print("Directory Downloads/{0} status: already exists".format(dirName))
-        if not os.path.exists('./Failed/{0}/'.format(dirName)):
-            os.mkdir('./Failed/{0}/'.format(dirName))
-            print("Directory Failed/{0} status: created".format(dirName))
+            print("Directory Downloads/{0} status: already exists".format(dir_name))
+        if not os.path.exists('./Failed/{0}/'.format(dir_name)):
+            os.mkdir('./Failed/{0}/'.format(dir_name))
+            print("Directory Failed/{0} status: created".format(dir_name))
         else:
-            print("Directory Failed/{0} status: already exists".format(dirName))
+            print("Directory Failed/{0} status: already exists".format(dir_name))
 
-    def paths_and_urls(self, ids, cdir) -> list:
+    @staticmethod
+    def paths_and_urls(ids: List, cdir: str) -> List:
+        """
+        Creates List of tuples with the path where the file will be saved and url from which it will be fetched.
+
+        :param ids: List with IDs, given by user.
+        :param cdir: str, name of the directory where the analysis is currently being carried out.
+        :return: List of tuples.
+        """
         pu = []
         for idx in ids:
             if re.match(r'([A-Z]){1}\d+', idx):
@@ -127,12 +181,22 @@ class SeqDownloader:
 
         return pu
 
-    def fetch_url(self, entry, obs, http) -> str:
+    @staticmethod
+    def fetch_url(entry: Tuple, obs: List, http: List) -> str:
+        """
+        Fetching files from given urls, then saving in the indicated directory. In case of http error
+        or after confirming that entry is obsolete, appends currently handled ID to the appropriate list.
+
+        :param entry: Tuple from paths_and_urls method.
+        :param obs: List of obsolete entries.
+        :param http: List of entries failed to download due to http errors.
+        :return: str, path were the file will be saved.
+        """
         path, url = entry
         uniprot_id = re.sub('[/.]', ' ', path).strip().split()
         if not os.path.exists(path):
             try:
-                fasta = urllib.request.urlopen(url)
+                fasta = request.urlopen(url)
                 fasta = fasta.read().decode()
 
                 if fasta == '':
@@ -141,11 +205,21 @@ class SeqDownloader:
                     with open(path, 'w+') as f:
                         f.write(fasta)
 
-            except urllib.error.HTTPError or TimeoutError or urllib.error.URLError:
+            except error.HTTPError or TimeoutError or error.URLError:
                 http.append(uniprot_id[2])
         return path
 
-    def download_ncbi(self, ncbi_id, cdir, obs, http):
+    @staticmethod
+    def download_ncbi(ncbi_id: str, cdir: str, obs: List, http: List):
+        """
+        Fetching files using NCBI IDs in Entrez module and saving them in the indicated directory. In case of http error
+        or after confirming that entry is obsolete, appends currently handled ID to the appropriate list.
+
+        :param ncbi_id: str, ID confirmed to be from the NCBI database.
+        :param cdir: str, name of the directory where the analysis is currently being carried out.
+        :param obs: List of obsolete entries.
+        :param http: List of entries failed to download due to http errors.
+        """
         try:
             handle = Entrez.efetch(db="protein", id=ncbi_id, rettype="fasta", retmode="text")
             record = handle.read()
@@ -156,27 +230,48 @@ class SeqDownloader:
                 with open('./Downloads/{0}/{1}.fasta'.format(cdir, ncbi_id), 'w+') as f:
                     f.write(record)
 
-        except urllib.error.HTTPError or TimeoutError or urllib.error.URLError:
+        except error.HTTPError or TimeoutError or error.URLError:
             http.append(ncbi_id)
 
 
-class SequenceDatabase:
-    def __init__(self, full_list, directory):
+class SeqDatabase:
+    """
+    SeqDatabase class is responsible for creating and giving you access to Dict containing IDs and SeqRecord objects.
+    """
+
+    def __init__(self, full_list: Union[str, List], directory: str):
+        """
+        Uses SeqDownloader class to download desired fasta files and then SeqIO module to create Dict with ID of fasta
+        file as key and SeqRecord object as vaule.
+
+        :param full_list: Either List of IDs or single str.
+        :param directory: str given by user to organize files properly.
+
+        """
         download = SeqDownloader(full_list, directory)
-        seqData = {}
+        seq_data = {}
         for index, record in zip(download.downloaded_files,
                                  SeqIO.parse("./Database/{0}.fasta".format(directory), "fasta")):
-            if index not in seqData.keys():
-                seqData[index] = record
-        self.database = seqData
+            if index not in seq_data.keys():
+                seq_data[index] = record
+        self.database = seq_data
 
-    def get(self, indexes=None) -> list:
+    def get(self, indexes: Union[str, List] = None) -> List:
+        """
+        Allows user to get desired record or list of records data.
+
+        :param indexes: str or List with IDs corresponding to records that user would like to get
+        :raises: Exception when variables are not of the expected type.
+        :return: List of SeqRecord objects
+        """
         if indexes is None:
             records = list(self.database.values())
         else:
-            if type(indexes) == str:
+            if isinstance(indexes, str):
                 keys = [indexes]
-            elif type(indexes) == list:
+            elif isinstance(indexes, List):
                 keys = indexes
+            else:
+                raise Exception("Given variable is not either str or List.")
             records = [self.database.get(key) for key in keys]
         return records
--- Bio/SeqAnalysis/downloader_tests.py
+++ Bio/SeqAnalysis/downloader_tests.py
@@ -1,6 +1,7 @@
-from Bio.SeqAnalysis.downloader import SequenceDatabase
+from Bio.SeqAnalysis.downloader import SeqDatabase
 
-s = SequenceDatabase(["F5GWV9", "A0AVL1", "A0PK19", "A1KZ92", "A2A2A0", "B5ME49", "P98088", "Q7Z5P9"], "directory")
+s = SeqDatabase(["F5GWV9", "A0AVL1", "A0PK19", "A1KZ92", "A2A2A0", "B5ME49", "P98088", "Q7Z5P9", "A0AVL1", "A2A2A0"]
+                , "test")
 record = s.get()[0:2]
 same_record = s.get(["A0AVL1", "A0PK19"])
 print(record == same_record)
