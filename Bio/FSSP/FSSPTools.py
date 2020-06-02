87a784eecb5042263a4282335ea9222e8ab9f8d1
Peter Cock <p.j.a.cock@googlemail.com>
2020-06-02 11:52:45 +0100

    Drop Gapped alphabet in FSSP

--- Bio/FSSP/FSSPTools.py
+++ Bio/FSSP/FSSPTools.py
@@ -15,7 +15,7 @@ new_sum, new_align = filter(sum, align, 'zscore', 4, 7.5)
 from Bio import FSSP
 import copy
 from Bio.Align import MultipleSeqAlignment
-from Bio import Alphabet
+from Bio.Alphabet import generic_protein
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
@@ -49,12 +49,12 @@ def mult_align(sum_dict, align_dict):
         for j in align_dict.abs(i).pos_align_dict:
             # loop within a position
             mult_align_dict[j] += align_dict.abs(i).pos_align_dict[j].aa
-    alpha = Alphabet.Gapped(Alphabet.IUPAC.extended_protein)
-    fssp_align = MultipleSeqAlignment([], alphabet=alpha)
+    fssp_align = MultipleSeqAlignment([], alphabet=generic_protein)
     for i in sorted(mult_align_dict):
         fssp_align.append(
             SeqRecord(
-                Seq(mult_align_dict[i], alpha), sum_dict[i].pdb2 + sum_dict[i].chain2
+                Seq(mult_align_dict[i], generic_protein),
+                sum_dict[i].pdb2 + sum_dict[i].chain2,
             )
         )
     return fssp_align
