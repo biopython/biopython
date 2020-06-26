5b6c3d29a102f5c2c21f4ee2c97a85c9d726bb0f
Peter Cock <p.j.a.cock@googlemail.com>
2020-06-26 19:10:55 +0100

    BSD dual license Bio.Align.substitution_matrices
    Original author Michiel de Hoon and myself as
    the only other contributor as tracked by version
    control have agreed:
    
    https://github.com/biopython/biopython/pull/2297
    https://github.com/biopython/biopython/pull/2427

--- Bio/Align/substitution_matrices/__init__.py
+++ Bio/Align/substitution_matrices/__init__.py
@@ -1,3 +1,10 @@
+# Copyright 2019 by Michiel de Hoon.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
+
 """Substitution matrices."""
 
 import os
