7d0736a4b84070c7506d662139524f7ed7fc2644
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-06-26 17:52:50 -0700

    Fixed copyright notice - Should be attributed to Rob Miller

--- Tests/test_PDB_internal_coords.py
+++ Tests/test_PDB_internal_coords.py
@@ -1,4 +1,4 @@
-# Copyright 2017 by Bernhard Thiel.  All rights reserved.
+# Copyright 2020 by Rob Miller.  All rights reserved.
 # This file is part of the Biopython distribution and governed by your
 # choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
 # Please see the LICENSE file that should have been included as part of this
