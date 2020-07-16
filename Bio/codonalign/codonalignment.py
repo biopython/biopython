d10adc38a88cfc65dd9d4ba5ea2cccd9d58b2285
Michiel de Hoon <mjldehoon@yahoo.com>
2020-07-16 14:40:52 +0900

    missing import

--- Bio/codonalign/codonalignment.py
+++ Bio/codonalign/codonalignment.py
@@ -14,6 +14,8 @@ import warnings
 from Bio.Align import MultipleSeqAlignment
 from Bio.SeqRecord import SeqRecord
 from Bio.Data import CodonTable
+from Bio import BiopythonWarning
+
 
 from Bio.codonalign.codonseq import _get_codon_list, CodonSeq, cal_dn_ds
 from Bio.codonalign.chisq import chisqprob
