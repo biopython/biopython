8f3754d2e6a0cdf0d2254246f873951cd72158a6
mdehoon <mjldehoon@yahoo.com>
2020-08-01 02:02:58 +0900

    fix three typos (#3191)
    * fix three typos
    
    * Update test_Seq_objs.py

--- Tests/test_Seq_objs.py
+++ Tests/test_Seq_objs.py
@@ -472,26 +472,26 @@ class StringMethodTests(unittest.TestCase):
 
     def test_str_split(self):
         """Check matches the python string rstrip method."""
-        # Calling (r)split should return a list of Seq-like objects, we'll
+        # Calling split should return a list of Seq-like objects, we'll
         # just apply str() to each of them so it matches the string method
         self._test_method(
-            "rstrip", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
+            "split", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
         )
 
     def test_str_rsplit(self):
         """Check matches the python string rstrip method."""
-        # Calling (r)split should return a list of Seq-like objects, we'll
+        # Calling rsplit should return a list of Seq-like objects, we'll
         # just apply str() to each of them so it matches the string method
         self._test_method(
-            "rstrip", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
+            "rsplit", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
         )
 
     def test_str_lsplit(self):
         """Check matches the python string rstrip method."""
-        # Calling (r)split should return a list of Seq-like objects, we'll
+        # Calling lsplit should return a list of Seq-like objects, we'll
         # just apply str() to each of them so it matches the string method
         self._test_method(
-            "rstrip", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
+            "lsplit", pre_comp_function=lambda x: [str(y) for y in x]  # noqa: E731
         )
 
     def test_str_length(self):
