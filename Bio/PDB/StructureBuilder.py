a90ffe1279301bfdbb577bbf7ead98827a718b91
Suyash Gupta <sgalpha01@gmail.com>
2020-09-14 01:56:34 +0530

    Used str.strip method instead of replace()

--- Bio/PDB/StructureBuilder.py
+++ Bio/PDB/StructureBuilder.py
@@ -118,7 +118,7 @@ class StructureBuilder:
         if field != " ":
             if field == "H":
                 # The hetero field consists of H_ + the residue name (e.g. H_FUC)
-                field = ("H_" + resname).replace(" ", "")
+                field = "H_" + resname.strip()
         res_id = (field, resseq, icode)
         if field == " ":
             if self.chain.has_id(res_id):
