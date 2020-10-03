2d07831866b6b4a4a9bd0cfb87d478d7e5ac0aa7
Peter Cock <p.j.a.cock@googlemail.com>
2020-10-03 21:56:36 +0100

    Avoid unreachable code warning
    SonarCloud complained: Delete this unreachable code or refactor the code to make it reachable.

--- Bio/Nexus/Nexus.py
+++ Bio/Nexus/Nexus.py
@@ -869,8 +869,8 @@ class Nexus:
         if "labels" in options:
             self.labels = options["labels"]
         if "transpose" in options:
+            # self.transpose = True
             raise NexusError("TRANSPOSE is not supported!")
-            self.transpose = True
         if "interleave" in options:
             if options["interleave"] is None or options["interleave"].lower() == "yes":
                 self.interleave = True
