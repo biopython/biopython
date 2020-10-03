f2a2548f151dcdce19f9d107674e21fed95c2f88
Peter Cock <p.j.a.cock@googlemail.com>
2020-10-03 21:58:18 +0100

    Remove useless if/else
    SonarCloud complained: Remove this if statement or edit its code blocks so that they're not all the same.

--- Bio/Nexus/Trees.py
+++ Bio/Nexus/Trees.py
@@ -482,10 +482,7 @@ class Tree(Nodes.Chain):
 
         result = is_monophyletic(self,taxon_list)
         """
-        if isinstance(taxon_list, str):
-            taxon_set = set(taxon_list)
-        else:
-            taxon_set = set(taxon_list)
+        taxon_set = set(taxon_list)
         node_id = self.root
         while True:
             subclade_taxa = set(self.get_taxa(node_id))
