402112cbfca498c911d599a98fbff8f0ea366fa9
Michiel de Hoon <mjldehoon@yahoo.com>
2020-10-17 11:25:10 +0900

    I fail to see how this is clearer than what we had

--- Tests/test_SeqIO_SeqXML.py
+++ Tests/test_SeqIO_SeqXML.py
@@ -210,7 +210,10 @@ class TestReadAndWrite(unittest.TestCase):
         text = output.decode("UTF-8")
         self.assertIn("Homo sapiens (Human)", text)
         self.assertIn("9606", text)
-        if '<species name="Homo sapiens (Human)" ncbiTaxID="9606"></species>' not in text:
+        if (
+            '<species name="Homo sapiens (Human)" ncbiTaxID="9606"></species>'
+            not in text
+        ):
             raise ValueError("Missing expected <species> tag: %r" % text)
 
 
