f155971e28dc709849959eb98f720f8af90f42c0
Peter Cock <p.j.a.cock@googlemail.com>
2020-11-14 13:25:49 +0000

    Dual license Bio/PopGen/GenePop/LargeFileParser.py
    Original author Tiago Antao and other contributors
    since as tracked with version control have agreed:
    
     - Adhemar Zerlotini (@azneto)
       https://github.com/biopython/biopython/pull/1412#issue-147372599
     - Brian Osborne (@bosborne)
       https://github.com/biopython/biopython/issues/898#issuecomment-241429879
     - Carlos Pena (@carlosp420)
       https://github.com/biopython/biopython/issues/898#issuecomment-236925650
     - Chris Rands (@chris-rands)
       https://github.com/biopython/biopython/pull/1301
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Connor McCoy (@cmccoy)
       https://github.com/biopython/biopython/issues/898#issuecomment-380478091
     - Francesco Gastaldello (@gasta88)
       https://github.com/biopython/biopython/pull/1063
     - @morrme
       https://github.com/biopython/biopython/pull/1172#issuecomment-295430028
     - Nick Negretti (@nimne)
       https://github.com/biopython/biopython/pull/1768#issue-210631196
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Sergio Valqui (@svalqui)
       https://github.com/biopython/biopython/pull/1749
     - Tiago Antao (@tiagoantao)
       https://github.com/biopython/biopython/issues/898#issuecomment-278067002

--- Bio/PopGen/GenePop/LargeFileParser.py
+++ Bio/PopGen/GenePop/LargeFileParser.py
@@ -1,7 +1,9 @@
 # Copyright 2010 by Tiago Antao.  All rights reserved.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 
 """Large file parsing of Genepop files.
 
