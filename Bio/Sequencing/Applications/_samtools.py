8fa0be81516d487d860cc0ad4526299c0c38b40b
Peter Cock <p.j.a.cock@googlemail.com>
2020-11-15 16:59:34 +0000

    Dual license Bio/Sequencing/Applications/_samtools.py
    Original author Saket Choudhary and other contributors
    since as tracked by version control have agreed:
    
     - Adhemar Zerlotini (@azneto)
       https://github.com/biopython/biopython/pull/1412#issue-147372599
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - @morrme
       https://github.com/biopython/biopython/pull/1172#issuecomment-295430028
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Saket Choudhary (@saketkc)
       https://github.com/biopython/biopython/issues/898#issuecomment-236924941
     - Sergio Valqui (@svalqui)
       https://github.com/biopython/biopython/pull/1749
     - Sujan Dulal (@Dsujan)
       https://github.com/biopython/biopython/pull/2430
     - Travis Wrightsman (@twrightsman)
       https://github.com/biopython/biopython/issues/898#issuecomment-237625470
     - Veronika Berman (@NikiB)
       https://github.com/biopython/biopython/pull/927#issuecomment-287738278

--- Bio/Sequencing/Applications/_samtools.py
+++ Bio/Sequencing/Applications/_samtools.py
@@ -1,7 +1,9 @@
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
-
+# Copyright 2014 Saket Choudhary. All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Command line wrapper for samtools."""
 # Last Checked with samtools [0.1.20 and 1.2]
 # TODO samtools 1.x has additional options over 0.x which
