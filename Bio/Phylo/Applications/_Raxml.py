03206c5fc0b3bd8647fd1788124382bf232f20e9
Peter Cock <p.j.a.cock@googlemail.com>
2020-11-15 15:50:47 +0000

    Dual license Bio/Phylo/Applications/_Raxml.py
    Original author Eric Talevich and contributors since
    tracked with version control (with one exception noted
    below) have all agreed:
    
     - Adhemar Zerlotini (@azneto)
       https://github.com/biopython/biopython/pull/1412#issue-147372599
     - Andrey Raspopov (@Andrey-Raspopov)
       https://github.com/biopython/biopython/pull/2444
     - Chris Daley (@chebizarro)
       https://github.com/biopython/biopython/pull/2349
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - Eric Talevich (@etal)
       https://github.com/biopython/biopython/issues/898#issuecomment-236949433
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Sergio Valqui (@svalqui)
       https://github.com/biopython/biopython/pull/1749
     - Sujan Dulal (@Dsujan)
       https://github.com/biopython/biopython/pull/2430
    
    We have not had explicit permission from Jingping Li
    (@Jingping) for the one line change applied in
    https://github.com/biopython/biopython/pull/73 and
    credited to them. The change was suggested by me, but
    the recorded authorship was more to recognise their
    work in identifying the problem and testing the solution.

--- Bio/Phylo/Applications/_Raxml.py
+++ Bio/Phylo/Applications/_Raxml.py
@@ -1,8 +1,9 @@
 # Copyright 2012 by Eric Talevich.  All rights reserved.
 #
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Command-line wrapper for the tree inference program RAxML.
 
 Derived from the help page for RAxML version 7.3 by Alexandros Stamatakis, but
