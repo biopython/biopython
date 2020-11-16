559d462460472d330c8817bd939ec4e44a1c3153
Peter Cock <p.j.a.cock@googlemail.com>
2020-11-16 10:21:18 +0000

    Dual license Bio/Sequencing/Applications/__init__.py
    Original author Osvaldo Zagordi and the contributors
    since as tracked by version control have agreed:
    
     - Brad Chapman (@chapmanb)
       http://mailman.open-bio.org/pipermail/biopython-dev/2013-July/019903.html
     - Brian Osborne (@bosborne)
       https://github.com/biopython/biopython/issues/898#issuecomment-241429879
     - Carlos Pena (@carlosp420)
       https://github.com/biopython/biopython/issues/898#issuecomment-236925650
     - Christian Brueffer (@cbrueffer)
       https://github.com/biopython/biopython/issues/898#issuecomment-236922857
     - John Ma (@JohnMCMa)
       https://github.com/biopython/biopython/pull/1790
     - Osvaldo Zagordi (@ozagordi)
       https://github.com/biopython/biopython/issues/3365
     - Peter Cock (@peterjc)
       https://github.com/biopython/biopython/issues/898#issue-167420613
     - Saket Choudhary (@saketkc)
       https://github.com/biopython/biopython/issues/898#issuecomment-236924941
     - Sergio Valqui (@svalqui)
       https://github.com/biopython/biopython/pull/1749
     - Veronika Berman (@NikiB)
       https://github.com/biopython/biopython/pull/927#issuecomment-287738278

--- Bio/Sequencing/Applications/__init__.py
+++ Bio/Sequencing/Applications/__init__.py
@@ -1,8 +1,9 @@
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# Copyright 2009 by Osvaldo Zagordi.  All rights reserved.
 #
-
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
 """Main entry point for sequencing related command line application wrappers."""
 from ._Novoalign import NovoalignCommandline
 from ._bwa import BwaIndexCommandline, BwaAlignCommandline, BwaSamseCommandline
