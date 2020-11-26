bfe3bf6fed762105287a6c3fbaf0ba1eab70edd5
Erundil95 <cristianferrari95@gmail.com>
2020-11-26 20:25:44 +0100

    Update _IQtree.py

--- Bio/Phylo/Applications/_IQtree.py
+++ Bio/Phylo/Applications/_IQtree.py
@@ -1,3 +1,4 @@
+
 # Based on code in _Phyml.py by Eric Talevich.
 # All rights reserved.
 #
@@ -11,7 +12,7 @@ from Bio.Application import _Option, _Switch, _Argument, AbstractCommandline
 class IQTreeCommandline(AbstractCommandline):
 	r"""Command-line wrapper for IQTree.
 
-	Input and Output parameters are mandatory.
+	Specify command is mandatory, used to specify input alignment file.
 
 	From the terminal command line use ``iqtree -h`` or ``iqtree -?``
 	for more explanation of usage options and commands
@@ -20,6 +21,17 @@ class IQTreeCommandline(AbstractCommandline):
 
 	References:
 
+
+	Example on Windows:
+
+		import _IQtree
+
+		iqtree_exe = r"C:\iqtree-Windows\bin\iqtree.exe"
+		input = r"C:\Documents\example.phy"
+
+		cmd = _IQtree.IQTreeCommandline(iqtree_exe, Specify = input)
+		print(cmd)
+		cmd()
 
 	"""
 
@@ -31,9 +43,9 @@ class IQTreeCommandline(AbstractCommandline):
 				["-s", "Specify"],   
 				"""Specify input alignment file in PHYLIP, FASTA, NEXUS
 				   CLUSTAL or MSF format""",
-				   is_required = True,
-				   equate = False,
-				   filename=True,
+				is_required = True,
+				equate = False,
+				filename=True,
 				),  
 			_Option(
 				["-st", "SequenceType"],
@@ -55,15 +67,17 @@ class IQTreeCommandline(AbstractCommandline):
 					RANDOM starts tree search from completely random tree
 					
 					Default: 100 parsimony trees + BIONJ tree""",
-					#TODO: checker function with filename if in it
-				filename=True,
+				equate = False,
+				filename = (lambda x: not(x in ("BIONJ", "RANDOM","PARS", "PLLPARS"))),                            #Not sure if this works
+				checker_function= lambda x: True if filename else (x in ("BIONJ", "RANDOM","PARS", "PLLPARS")),     #Need tree file to test
 				),
 			_Option(
 				["-te", "UserTree"],
 				"""Like -t but fixing user tree, no tree search is performed
 				   and the program computes the log-likelihood of the fixed user tree""",
-				   filename = True,
-				   #TODO: checker function with filename if in it like -t
+				equate = False,
+				filename = (lambda x: not(x in ("BIONJ", "RANDOM","PARS", "PLLPARS"))),                            #Not sure if this works
+				checker_function= lambda x: True if filename else (x in ("BIONJ", "RANDOM","PARS", "PLLPARS")),     #Need tree file to test
 				),
 			_Option(
 				["-o", "outgroup"],
@@ -86,14 +100,14 @@ class IQTreeCommandline(AbstractCommandline):
 				""" Specify the number of CPU cores for the multicore version.
 					A special option -nt AUTO will tell IQTree to automatically determine
 					the best number of cores given the current data and computer""",
-				checker_function = (lambda x: isinstance(x, int) or x.isdigit()),
+				checker_function = (lambda x: isinstance(x, int) or x.isdigit() or x in ("AUTO")),
 				equate = False,
 				),
 			_Option(
 				["-ntmax", "ntmax"],
 				"""Specify the maximal number of CPU cores.
 					Default: # CPU cores on the current machine""",
-				checker_function = (lambda x: isinstance(x, int) or x.isdigit()),
+				checker_function = (lambda x: isinstance(x, int) or x.isdigit() or x in ("AUTO")),
 				equate= False,
 				),
 			_Switch(
@@ -129,7 +143,52 @@ class IQTreeCommandline(AbstractCommandline):
 				   Default: IQTree will not exceed the computer RAM size""",
 				equate= False,
 				),
+			_Switch(
+				["-redo", "redo"],
+				"""Redo the entire analysis no matter if it was stopped or successful
 
+				   Note: This option will overwrite all existing output files""",
+				),
+			_Option(
+				["-cptime", "checkpoint_interval"],
+				"""Specify the minimum checkpoint time interval in seconds
+
+				   Default 20 seconds""",
+				equate = False,
+				checker_function = lambda x: isinstance(x, int) or x.isdigit(),
+				),
+
+			#Likelihood mapping analysis comands
+
+			_Option(
+				["-lmap", "lmap"],
+				"""Specify the number of quartets to be randomly drawn.
+				   If you specify -lmap ALL, all unique quartets will be drawn, instead.""",
+				equate = False,
+				checker_function = lambda x: isinstance(x, int) or x.isdigit() or x in ("ALL"),
+				),
+			_Option(
+				["-lmclust", "taxon_clusters"],
+				"""Specify a NEXUS file containing taxon clusters for quartet mapping analysis""",
+				filename = True,
+				equate = False,
+				),
+			_Switch(
+				["-wql", "wql"],
+				"""Write quartet log-likelihoods into '.lmap.quartelh' file""",
+				),
+			_Option(
+				["-n", "n"],
+				"""Fix number of iterations to stop
+				   
+				   0 - To skip subsequent tree search, useful when you want to assess the
+				       phylogenetic information of the alignment
+
+				   Default: auto""",
+
+				equate = False,
+				checker_function = lambda x: isinstance(x, int) or x.isdigit(),
+				)
 		]
 
 		AbstractCommandline.__init__(self, cmd, **kwargs)
