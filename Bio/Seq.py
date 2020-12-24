ab6b5f2025fc4c21e7e3828092c4bd511fa6cb53
Michiel de Hoon <mjldehoon@yahoo.com>
2020-12-24 14:26:36 +0900

    travis

--- Bio/Seq.py
+++ Bio/Seq.py
@@ -269,11 +269,14 @@ class SequenceDataAbstractBaseClass(ABC):
         """
         return bytes(self).replace(old, new)
 
-    def translate(self, table):
+    def translate(self, table, delete=b""):
         """Return a copy with each character mapped by the given translation table.
 
           table
             Translation table, which must be a bytes object of length 256.
+
+        All characters occurring in the optional argument delete are removed.
+        The remaining characters are mapped through the given translation table.
         """
         return bytes(self).translate(table)
 
