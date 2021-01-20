bea0bce6d740eb561b2be30755f587ea10910360
Erundil95 <cristianferrari95@gmail.com>
2021-01-20 21:36:52 +0100

    Add files via upload

--- Tests/test_IQTree_tool.py
+++ Tests/test_IQTree_tool.py
@@ -2,9 +2,8 @@
 # Based on test_phyml_tool.py by Eric Talevich.
 #
 # This code is part of the Biopython distribution and governed by its
-# license. Please see the LICENSE file that should have been included
+# license.  Please see the LICENSE file that should have been included
 # as part of this package.
-
 """Test for IQTree tool."""
 
 from Bio import MissingExternalDependencyError
@@ -32,13 +31,11 @@ if sys.platform == "Win32":
         prog_files = r"C:\Program Files"
 
 
-    likely_dirs = [
-        "",
+    likely_dirs = ["",
         "iqtree-1.6.12-Windows",
         r"iqtree-1.6.12-Windows\bin",
         "iqtree2-1.6.12-Windows",
-        r"iqtree2-1.6.12-Windows\bin",
-        ]
+        r"iqtree2-1.6.12-Windows\bin",]
     likely_exes = ["iqtree.exe", "iqtree2.exe"]
 
     for folder in likely_dirs:
@@ -65,9 +62,8 @@ else:
 
 if not iqtree_exe:
     raise MissingExternalDependencyError(
-        "Install IQTree or IQTree2 if you want to use the "
-        "Bio.Phylo.Applications wrapper."
-        )
+       "Install IQTree or IQTree2 if you want to use the Bio.Phylo.Applications wrapper."
+    )
 
 #Example Phylip file provided on IQ-Tree website
 Example_Phy = "Phylip/example_iqtree.phy"
@@ -86,21 +82,19 @@ class IQTreeTestCase(unittest.TestCase):
              outfname = Example_Phy + ".iqtree"
              print(outfname)
              self.assertEqual(os.path.isfile(outfname), True)
-    
+
         except Exception as exc:
              self.fail("IQTree wrapper error: %s" % exc)
 
         finally:
           # Clean up generated files
-          for suffix in [
-              ".bionj",
+          for suffix in [".bionj",
               ".ckp.gz",
               ".iqtree",
               ".log",
               ".mldist",
               ".model.gz",
-              ".treefile",
-              ]:
+              ".treefile",]:
               fname = Example_Phy + suffix
               if os.path.isfile(fname):
                   os.remove(fname)
