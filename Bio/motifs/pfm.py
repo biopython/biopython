ca9b4749ed4320a9cd6c34e22cb84ad6f5dba007
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2021-03-12 20:53:17 +0100

    Add support for pfm-four-rows PWMS with scientific numbers.
    Add support for pfm-four-rows PWMS with scientific numbers.

--- Bio/motifs/pfm.py
+++ Bio/motifs/pfm.py
@@ -335,9 +335,9 @@ def _read_pfm_four_rows(handle):
 
     name_pattern = re.compile(r"^>\s*(.+)\s*")
     row_pattern_with_nucleotide_letter = re.compile(
-        r"\s*([ACGT])\s*[[]*[|]*\s*([0-9.\s]+)\s*[]]*\s*"
+        r"\s*([ACGT])\s*[[]*[|]*\s*([0-9.\-eE\s]+)\s*[]]*\s*"
     )
-    row_pattern_without_nucleotide_letter = re.compile(r"\s*([0-9.\s]+)\s*")
+    row_pattern_without_nucleotide_letter = re.compile(r"\s*([0-9.\-eE\s]+)\s*")
 
     motif_name = None
     nucleotide_counts = {}
