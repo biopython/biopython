4eb4aef9e7a9548c16252717971194271722731c
Michiel de Hoon <mjldehoon@yahoo.com>
2021-03-21 19:17:02 +0900

    make sure the pairwise2 tests are run correctly

--- Tests/test_pairwise2_no_C.py
+++ Tests/test_pairwise2_no_C.py
@@ -16,11 +16,18 @@ from pairwise2_testCases import *  # noqa: F401, F403
 
 from Bio import pairwise2
 
-# Explicitly using pure Python fallback functions:
-pairwise2._make_score_matrix_fast = pairwise2._python_make_score_matrix_fast
-pairwise2.rint = pairwise2._python_rint
-
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
-    unittest.main(testRunner=runner)
+    # Explicitly using pure Python fallback functions:
+    orig_make_score_matrix_fast = pairwise2._make_score_matrix_fast
+    orig_rint = pairwise2.rint
+    try:
+        pairwise2._make_score_matrix_fast = pairwise2._python_make_score_matrix_fast
+        pairwise2.rint = pairwise2._python_rint
+        unittest.main(testRunner=runner)
+    finally:
+        # To avoid interfering with the remaining pairwise2 tests,
+        # restore the original functions
+        pairwise2._make_score_matrix_fast = orig_make_score_matrix_fast
+        pairwise2.rint = orig_python_rint
