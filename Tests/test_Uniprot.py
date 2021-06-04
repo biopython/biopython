ed75270f5bd9e76f693d0c2d9050abfb29994c26
eglif <fabian.egli@hest.ethz.ch>
2021-06-04 08:00:28 +0200

    test 'status' and 'evidence' attributes of SeqFeature

--- Tests/test_Uniprot.py
+++ Tests/test_Uniprot.py
@@ -416,6 +416,11 @@ class TestUniprot(SeqRecordTestBaseClass):
             ],
         )
 
+        self.assertEqual(
+            seq_record.features[1],
+            "SeqFeature(FeatureLocation(ExactPosition(17), ExactPosition(43)), type='propeptide', id='PRO_0000009556', evidence='7', status='potential')",
+        )
+
     def test_sp016(self):
         """Parsing SwissProt file sp016."""
         filename = "sp016"
