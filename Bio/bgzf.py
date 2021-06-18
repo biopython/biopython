4e39f44807bac12688eef4c3826bf4c265f11f84
Vincent <vincent@heteroskedastic.com>
2021-06-17 21:50:00 -0600

    Change http to https

--- Bio/bgzf.py
+++ Bio/bgzf.py
@@ -13,7 +13,7 @@ format (SAM), and a compressed binary format (BAM). The latter uses a
 modified form of gzip compression called BGZF (Blocked GNU Zip Format),
 which can be applied to any file format to provide compression with
 efficient random access. BGZF is described together with the SAM/BAM
-file format at http://samtools.sourceforge.net/SAM1.pdf
+file format at https://samtools.sourceforge.net/SAM1.pdf
 
 Please read the text below about 'virtual offsets' before using BGZF
 files for random access.
