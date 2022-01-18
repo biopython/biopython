22e4a8131d0bbe086a9c12e8bbb5e9c7f22aa427
Peter Cock <p.j.a.cock@googlemail.com>
2022-01-18 13:32:05 +0000

    Support Python 3.10

--- setup.py
+++ setup.py
@@ -245,6 +245,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Software Development :: Libraries :: Python Modules",
