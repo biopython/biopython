aa7e7cf1c7df1fb79dc1fabb5a1c6651da71fd0b
Michiel de Hoon <mjldehoon@yahoo.com>
2022-01-21 19:12:23 +0900

    black

--- Bio/Align/psl.py
+++ Bio/Align/psl.py
@@ -72,7 +72,6 @@ class AlignmentWriter(interfaces.AlignmentWriter):
         self.wildcard = wildcard
         self.mask = mask
 
-    # fmt: off
     def write_header(self, alignments):
         """Write the PSL header."""
         if not self.header:
@@ -83,6 +82,7 @@ class AlignmentWriter(interfaces.AlignmentWriter):
             version = "3"
         else:
             version = metadata.get("version", "3")
+        # fmt: off
         self.stream.write(
             f"""\
 psLayout version {version}
@@ -92,7 +92,7 @@ match	mis- 	rep. 	N's	Q gap	Q gap	T gap	T gap	strand	Q        	Q   	Q    	Q  	T 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 """
         )  # noqa: W191, E101
-    # fmt: on
+        # fmt: on
 
     def write_alignment(self, alignment):
         """Write a complete alignment as one PSL line."""
