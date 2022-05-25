ba2b30e8b6d202f4b41ad0b11e527cc73d3e8b38
valentin994 <valentin.vareskic@outlook.com>
2022-05-25 15:37:21 +0200

    created test file, and added todos

---
+++ Tests/test_PDB_PSEA.py
@@ -0,0 +1,12 @@
+# Copyright 2017 by Valentin Vareskic (valentin.vareskic@gmail.com).
+# All rights reserved. This code is part of the Biopython distribution
+# and governed by its license.  Please see the LICENSE file that should
+# have been included as part of this package.
+
+"""Tests for PDB PSEA."""
+
+# TODO Create unittest for run_psea(fname)
+# TODO Create unittest psea(pname)
+# TODO Create unittest psea2HEC(pseq)
+# TODO Create unittest annotate(m, ss_seq)
+# TODO Create unittest class PSEA
