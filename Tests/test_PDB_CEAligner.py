Fix bug in rotation matrix transpose when using QCP methods (#3957)

Tests/test_PDB_CEAligner.py
@@ -9,7 +9,7 @@
 import unittest
 
 try:
-    import numpy
+    import numpy as np
 except ImportError:
     from Bio import MissingPythonDependencyError
 
@@ -24,24 +24,37 @@ from Bio.PDB import MMCIFParser
 class CEAlignerTests(unittest.TestCase):
     """Test CEAligner class."""
 
+    @staticmethod
+    def _get_ca_coords_as_array(structure):
+        xyz_list = [a.coord for a in structure.get_atoms() if a.name == "CA"]
+        return np.asarray(xyz_list, dtype=np.float64)
+
     def test_cealigner(self):
         """Test aligning 7CFN on 6WQA."""
         ref = "PDB/6WQA.cif"
         mob = "PDB/7CFN.cif"
+        result = "PDB/7CFN_aligned.cif"
 
         parser = MMCIFParser(QUIET=1)
         s1 = parser.get_structure("6wqa", ref)
         s2 = parser.get_structure("7cfn", mob)
 
-        s2_original_coords = [list(a.coord) for a in s2.get_atoms()]
-
         aligner = CEAligner()
         aligner.set_reference(s1)
         aligner.align(s2)
-        s2_coords_final = [list(a.coord) for a in s2.get_atoms()]
 
         self.assertAlmostEqual(aligner.rms, 3.83, places=2)
-        self.assertNotEqual(s2_original_coords, s2_coords_final)
+
+        # Assert the transformation was done right by comparing
+        # the moved coordinates to a 'ground truth' reference.
+        # Reference obtained with Pymol's CEAlign code.
+        refe = parser.get_structure("7cfn_aligned", result)
+        refe_coords = self._get_ca_coords_as_array(refe)
+        s2_f_coords = self._get_ca_coords_as_array(s2)
+
+        diff = refe_coords - s2_f_coords
+        rmsd = np.sqrt((diff * diff).sum() / len(refe_coords))
+        self.assertAlmostEqual(rmsd, 0.0, places=2)  # < 0.01
 
     def test_cealigner_no_transform(self):
         """Test aligning 7CFN on 6WQA without transforming 7CFN."""