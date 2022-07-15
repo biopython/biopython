d3fedbc57ff314ab2ce8e332862108253cd6b0d2
Michiel de Hoon <mjldehoon@yahoo.com>
2022-07-15 16:13:37 +0900

    update

--- Tests/test_Align_Alignments.py
+++ Tests/test_Align_Alignments.py
@@ -7,8 +7,6 @@
 import unittest
 import warnings
 
-from io import StringIO
-
 from Bio import BiopythonExperimentalWarning
 
 with warnings.catch_warnings():
@@ -22,7 +20,7 @@ except ImportError:
     from Bio import MissingPythonDependencyError
 
     raise MissingPythonDependencyError(
-        "Install numpy if you want to use Bio.Align.emboss."
+        "Install numpy if you want to use Bio.Align.interfaces."
     ) from None
 
 
@@ -641,221 +639,53 @@ class TestAlignments(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(self.alignments)
 
-    def test_needle(self):
-        alignments = self.alignments
-        self.assertEqual(alignments.metadata["Program"], "needle")
-        self.assertEqual(alignments.metadata["Rundate"], "Sun 27 Apr 2007 17:20:35")
-        self.assertEqual(
-            alignments.metadata["Command line"],
-            "needle [-asequence] Spo0F.faa [-bsequence] paired_r.faa -sformat2 pearson",
-        )
-        self.assertEqual(alignments.metadata["Align_format"], "srspair")
-        self.assertEqual(alignments.metadata["Report_file"], "ref_rec .needle")
-        alignment = next(alignments)
-        self.assertEqual(alignment.annotations["Matrix"], "EBLOSUM62")
-        self.assertAlmostEqual(alignment.annotations["Gap_penalty"], 10.0)
-        self.assertAlmostEqual(alignment.annotations["Extend_penalty"], 0.5)
-        self.assertEqual(alignment.annotations["Identity"], 32)
-        self.assertEqual(alignment.annotations["Similarity"], 64)
-        self.assertEqual(alignment.annotations["Gaps"], 17)
-        self.assertAlmostEqual(alignment.score, 112.0)
-        self.assertEqual(len(alignment), 2)
-        self.assertEqual(alignment.shape, (2, 124))
-        self.assertEqual(alignment.sequences[0].id, "ref_rec")
-        self.assertEqual(alignment.sequences[1].id, "gi|94968718|receiver")
-        self.assertEqual(
-            alignment.sequences[0].seq,
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV",
-        )
-        self.assertEqual(
-            alignment.sequences[1].seq,
-            "VLLADDHALVRRGFRLMLEDDPEIEIVAEAGDGAQAVKLAGELHPRVVVMDCAMPGMSGMDATKQIRTQWPDIAVLMLTMHSEDTWVRLALEAGANGYILKSAIDLDLIQAVRRVANGET",
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                alignment.coordinates,
-                numpy.array(
-                    [
-                        [0, 1, 7, 7, 17, 19, 100, 100, 108, 109, 111, 111],
-                        [0, 0, 6, 10, 20, 20, 101, 102, 110, 110, 112, 120],
-                    ]
-                ),
-            )
-        )
-        self.assertEqual(
-            alignment[0],
-            "KILIVDD----QYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAK-PFDIDEIRDAV--------",
-        )
-        self.assertEqual(
-            alignment[1],
-            "-VLLADDHALVRRGFRLMLED--DPEIEIVAEAGDGAQAVKLAGELHPRVVVMDCAMPGMSGMDATKQIRTQWPDIAVLMLTMHSEDTWVRLALEAGANGYILKSAIDLDLIQ-AVRRVANGET",
-        )
-        self.assertEqual(
-            alignment.column_annotations["emboss_consensus"],
-            " :|:.||    :.|.|::|.:  :.|.....:|.:|.||:.:..:..|.:|::|..:|||.|::..|:::....:|.|:::|.:.|...::.:.|.||..:..| ..|:|.|: ||        ",
-        )
-        alignment = next(alignments)
-        self.assertEqual(alignment.annotations["Matrix"], "EBLOSUM62")
-        self.assertAlmostEqual(alignment.annotations["Gap_penalty"], 10.0)
-        self.assertAlmostEqual(alignment.annotations["Extend_penalty"], 0.5)
-        self.assertEqual(alignment.annotations["Identity"], 34)
-        self.assertEqual(alignment.annotations["Similarity"], 58)
-        self.assertEqual(alignment.annotations["Gaps"], 9)
-        self.assertAlmostEqual(alignment.score, 154.0)
-        self.assertEqual(len(alignment), 2)
-        self.assertEqual(alignment.shape, (2, 119))
-        self.assertEqual(alignment.sequences[0].id, "ref_rec")
-        self.assertEqual(alignment.sequences[1].id, "gi|94968761|receiver")
-        self.assertEqual(
-            alignment.sequences[0].seq,
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV",
-        )
-        self.assertEqual(
-            alignment.sequences[1].seq,
-            "ILIVDDEANTLASLSRAFRLAGHEATVCDNAVRALEIAKSKPFDLILSDVVMPGRDGLTLLEDLKTAGVQAPVVMMSGQAHIEMAVKATRLGALDFLEKPLSTDKLLLTVENALKLKR",
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                alignment.coordinates, numpy.array([[0, 1, 111, 111], [0, 0, 110, 118]])
-            )
-        )
-        self.assertEqual(
-            alignment[0],
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV--------",
-        )
-        self.assertEqual(
-            alignment[1],
-            "-ILIVDDEANTLASLSRAFRLAGHEATVCDNAVRALEIAKSKPFDLILSDVVMPGRDGLTLLEDLKTAGVQAPVVMMSGQAHIEMAVKATRLGALDFLEKPLSTDKLLLTVENALKLKR",
-        )
-        self.assertEqual(
-            alignment.column_annotations["emboss_consensus"],
-            " ||||||:......|:..|...|::.....|.::||:|...:..||:|.|:.:||.||:.:|:.:|.......|::|:....::|..::..||||....||...|::...|        ",
-        )
-        alignment = next(alignments)
-        self.assertEqual(alignment.annotations["Matrix"], "EBLOSUM62")
-        self.assertAlmostEqual(alignment.annotations["Gap_penalty"], 10.0)
-        self.assertAlmostEqual(alignment.annotations["Extend_penalty"], 0.5)
-        self.assertEqual(alignment.annotations["Identity"], 29)
-        self.assertEqual(alignment.annotations["Similarity"], 53)
-        self.assertEqual(alignment.annotations["Gaps"], 9)
-        self.assertAlmostEqual(alignment.score, 121.0)
-        self.assertEqual(len(alignment), 2)
-        self.assertEqual(alignment.shape, (2, 120))
-        self.assertEqual(alignment.sequences[0].id, "ref_rec")
-        self.assertEqual(alignment.sequences[1].id, "gi|94967506|receiver")
-        self.assertEqual(
-            alignment.sequences[0].seq,
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV",
-        )
-        self.assertEqual(
-            alignment.sequences[1].seq,
-            "LHIVVVDDDPGTCVYIESVFAELGHTCKSFVRPEAAEEYILTHPVDLAIVDVYLGSTTGVEVLRRCRVHRPKLYAVIITGQISLEMAARSIAEGAVDYIQKPIDIDALLNIAERALEHKE",
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                alignment.coordinates, numpy.array([[0, 0, 111, 111], [0, 1, 112, 120]])
-            )
-        )
-        self.assertEqual(
-            alignment[0],
-            "-KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV--------",
-        )
-        self.assertEqual(
-            alignment[1],
-            "LHIVVVDDDPGTCVYIESVFAELGHTCKSFVRPEAAEEYILTHPVDLAIVDVYLGSTTGVEVLRRCRVHRPKLYAVIITGQISLEMAARSIAEGAVDYIQKPIDIDALLNIAERALEHKE",
-        )
-        self.assertEqual(
-            alignment.column_annotations["emboss_consensus"],
-            " .|::|||..|..:.:..||.:.|:..........|.:.:.....||.::|:.:....|:|:|:|.:|....:..:|:|....|:|...|...||:.:..||.|||.:.:..        ",
-        )
-        alignment = next(alignments)
-        self.assertEqual(alignment.annotations["Matrix"], "EBLOSUM62")
-        self.assertAlmostEqual(alignment.annotations["Gap_penalty"], 10.0)
-        self.assertAlmostEqual(alignment.annotations["Extend_penalty"], 0.5)
-        self.assertEqual(alignment.annotations["Identity"], 30)
-        self.assertEqual(alignment.annotations["Similarity"], 64)
-        self.assertEqual(alignment.annotations["Gaps"], 9)
-        self.assertAlmostEqual(alignment.score, 126.0)
-        self.assertEqual(len(alignment), 2)
-        self.assertEqual(alignment.shape, (2, 118))
-        self.assertEqual(alignment.sequences[0].id, "ref_rec")
-        self.assertEqual(alignment.sequences[1].id, "gi|94970045|receiver")
-        self.assertEqual(
-            alignment.sequences[0].seq,
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV",
-        )
-        self.assertEqual(
-            alignment.sequences[1].seq,
-            "VLLVEDEEALRAAAGDFLETRGYKIMTARDGTEALSMASKFAERIDVLITDLVMPGISGRVLAQELVKIHPETKVMYMSGYDDETVMVNGEIDSSSAFLRKPFRMDALSAKIREVL",
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                alignment.coordinates,
-                numpy.array(
-                    [
-                        [0, 1, 41, 41, 82, 83, 98, 98, 105, 105, 111],
-                        [0, 0, 40, 42, 83, 83, 98, 99, 106, 110, 116],
-                    ]
-                ),
-            )
-        )
-        self.assertEqual(
-            alignment[0],
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTK--ERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHF-AKPFDID----EIRDAV",
-        )
-        self.assertEqual(
-            alignment[1],
-            "-VLLVEDEEALRAAAGDFLETRGYKIMTARDGTEALSMASKFAERIDVLITDLVMPGISGRVLAQELVKIHPETKVMYMSGYDD-ETVMVNGEIDSSSAFLRKPFRMDALSAKIREVL",
-        )
-        self.assertEqual(
-            alignment.column_annotations["emboss_consensus"],
-            " :|:|:|:..:|....:.....||:...|.:|.:||.:.:|  ||.|:::.|:.:||:.|..:.:.:..|....:|:.|:.|.: :.:..:.|:.:.:.| .|||.:|    :||:.:",
-        )
-        alignment = next(alignments)
-        self.assertEqual(alignment.annotations["Matrix"], "EBLOSUM62")
-        self.assertAlmostEqual(alignment.annotations["Gap_penalty"], 10.0)
-        self.assertAlmostEqual(alignment.annotations["Extend_penalty"], 0.5)
-        self.assertEqual(alignment.annotations["Identity"], 35)
-        self.assertEqual(alignment.annotations["Similarity"], 70)
-        self.assertEqual(alignment.annotations["Gaps"], 18)
-        self.assertAlmostEqual(alignment.score, 156.5)
-        self.assertEqual(len(alignment), 2)
-        self.assertEqual(alignment.shape, (2, 125))
-        self.assertEqual(alignment.sequences[0].id, "ref_rec")
-        self.assertEqual(alignment.sequences[1].id, "gi|94970041|receiver")
-        self.assertEqual(
-            alignment.sequences[0].seq,
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIVTKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFAKPFDIDEIRDAV",
-        )
-        self.assertEqual(
-            alignment.sequences[1].seq,
-            "TVLLVEDEEGVRKLVRGILSRQGYHVLEATSGEEALEIVRESTQKIDMLLSDVVLVGMSGRELSERLRIQMPSLKVIYMSGYTDDAIVRHGVLTESAEFLQKPFTSDSLLRKVRAVLQKRQ",
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                alignment.coordinates,
-                numpy.array(
-                    [
-                        [0, 39, 39, 88, 92, 99, 99, 111, 111],
-                        [0, 39, 41, 90, 90, 97, 101, 113, 121],
-                    ]
-                ),
-            )
-        )
-        self.assertEqual(
-            alignment[0],
-            "KILIVDDQYGIRILLNEVFNKEGYQTFQAANGLQALDIV--TKERPDLVLLDMKIPGMDGIEILKRMKVIDENIRVIIMTAYGELDMIQESKELGALTHFA----KPFDIDEIRDAV--------",
-        )
-        self.assertEqual(
-            alignment[1],
-            "TVLLVEDEEGVRKLVRGILSRQGYHVLEATSGEEALEIVRESTQKIDMLLSDVVLVGMSGRELSERLRIQMPSLKVIYMSGYTDDAIVRH----GVLTESAEFLQKPFTSDSLLRKVRAVLQKRQ",
-        )
-        self.assertEqual(
-            alignment.column_annotations["emboss_consensus"],
-            ".:|:|:|:.|:|.|:..:.:::||...:|.:|.:||:||  :.::.|::|.|:.:.||.|.|:.:|:::...:::||.|:.|.:..:::.    |.||..|    |||..|.:...|        ",
-        )
-        with self.assertRaises(StopIteration):
-            next(alignments)
+    def test_extend(self):
+        same_repr = ("<Bio.Align.Alignment object (2 rows x 124 columns) at ",
+                     "<Bio.Align.Alignment object (2 rows x 119 columns) at ",
+                     "<Bio.Align.Alignment object (2 rows x 120 columns) at ",
+                     "<Bio.Align.Alignment object (2 rows x 118 columns) at ",
+                     "<Bio.Align.Alignment object (2 rows x 125 columns) at ",
+                    )
+        other_repr = ("<Bio.Align.Alignment object (2 rows x 131 columns) at ",
+                     )
+        same_alignments = AlignmentIterator(self.path)
+        self.assertEqual(self.alignments.metadata['Align_format'], 'srspair')
+        self.assertEqual(self.alignments.metadata['Program'], 'needle')
+        self.assertEqual(self.alignments.metadata['Rundate'], 'Sun 27 Apr 2007 17:20:35')
+        self.assertEqual(self.alignments.metadata['Command line'], 'needle [-asequence] Spo0F.faa [-bsequence] paired_r.faa -sformat2 pearson')
+        self.assertEqual(self.alignments.metadata['Report_file'], 'ref_rec .needle')
+        self.assertEqual(len(self.alignments.metadata), 5)
+        self.assertFalse(self.alignments._loaded)
+        self.assertFalse(same_alignments._loaded)
+        self.alignments.extend(same_alignments)
+        self.assertTrue(self.alignments._loaded)
+        self.assertFalse(same_alignments._loaded)
+        for alignment, representation in zip(self.alignments, 2 * same_repr):
+            self.assertTrue(repr(alignment).startswith(representation))
+        self.assertEqual(len(self.alignments), 10)
+        self.assertEqual(self.alignments.metadata['Align_format'], 'srspair')
+        self.assertEqual(self.alignments.metadata['Program'], 'needle')
+        self.assertEqual(self.alignments.metadata['Rundate'], 'Sun 27 Apr 2007 17:20:35')
+        self.assertEqual(self.alignments.metadata['Command line'], 'needle [-asequence] Spo0F.faa [-bsequence] paired_r.faa -sformat2 pearson')
+        self.assertEqual(self.alignments.metadata['Report_file'], 'ref_rec .needle')
+        other_alignments = AlignmentIterator("Emboss/water.txt")
+        self.assertEqual(other_alignments.metadata['Align_format'], 'srspair')
+        self.assertEqual(other_alignments.metadata['Program'], 'water')
+        self.assertEqual(other_alignments.metadata['Rundate'], 'Wed Jan 16 17:23:19 2002')
+        self.assertEqual(other_alignments.metadata['Report_file'], 'stdout')
+        self.assertEqual(len(other_alignments.metadata), 4)
+
+        self.assertFalse(other_alignments._loaded)
+        self.alignments.extend(other_alignments)
+        self.assertFalse(other_alignments._loaded)
+        for alignment, representation in zip(self.alignments, 2 * same_repr + other_repr):
+            self.assertTrue(repr(alignment).startswith(representation))
+        self.assertEqual(len(self.alignments), 11)
+        self.assertEqual(self.alignments.metadata['Align_format'], 'srspair')
+        self.assertEqual(self.alignments.metadata['Program'], 'needle')
+        self.assertEqual(self.alignments.metadata['Rundate'], 'Sun 27 Apr 2007 17:20:35')
+        self.assertEqual(self.alignments.metadata['Command line'], 'needle [-asequence] Spo0F.faa [-bsequence] paired_r.faa -sformat2 pearson')
+        self.assertEqual(self.alignments.metadata['Report_file'], 'ref_rec .needle')
 
 
 if __name__ == "__main__":
