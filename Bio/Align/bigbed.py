58048bd292f947077537afc2f47e04cdb39fe2df
Michiel de Hoon <mjldehoon@yahoo.com>
2023-05-04 21:05:27 +0900

    update

--- Bio/Align/bigbed.py
+++ Bio/Align/bigbed.py
@@ -329,40 +329,40 @@ class _ZoomLevels(list):
             res *= _ZoomLevels.bbiResIncrement
         return reductions[:resTry]
 
-    def reduce(self, rezoomedList, initialReduction, buffer, blockSize, itemsPerSlot):
+    def reduce(self, summaries, initialReduction, buffer, blockSize, itemsPerSlot):
         zoomCount = initialReduction["size"]
         reduction = initialReduction["scale"] * _ZoomLevels.bbiResIncrement
         output = buffer.output
         formatter = _RTreeFormatter()
         for zoomLevels in range(1, _ZoomLevels.bbiMaxZoomLevels):
-            rezoomCount = len(rezoomedList)
+            rezoomCount = len(summaries)
             if rezoomCount >= zoomCount:
                 break
             zoomCount = rezoomCount
             self[zoomLevels].dataOffset = output.tell()
             data = zoomCount.to_bytes(4, sys.byteorder)
             output.write(data)
-            for summary in rezoomedList:
+            for summary in summaries:
                 buffer.write(summary)
             buffer.flush()
             indexOffset = output.tell()
-            formatter.write(rezoomedList, blockSize, itemsPerSlot, indexOffset, output)
+            formatter.write(summaries, blockSize, itemsPerSlot, indexOffset, output)
             self[zoomLevels].indexOffset = indexOffset
             self[zoomLevels].amount = reduction
             reduction *= _ZoomLevels.bbiResIncrement
             i = 0
             chromId = None
-            for summary in rezoomedList:
+            for summary in summaries:
                 if summary.chromId != chromId or summary.end > end:  # noqa: F821
                     end = summary.start + reduction
                     chromId = summary.chromId
                     currentSummary = summary
-                    rezoomedList[i] = currentSummary
+                    summaries[i] = currentSummary
                     i += 1
                 else:
                     currentSummary += summary
-            rezoomedList = rezoomedList[:i]
-        self[:] = self[:zoomLevels]
+            del summaries[i:]
+        del self[zoomLevels:]
 
 
 class _Summary:
