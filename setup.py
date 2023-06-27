5e14d722b6b4fdd64750da767dbd9c2f3f5de851
Joel Ibaceta <mail@joelibaceta.com>
2023-06-27 16:17:06 -0500

    Style fix

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ EXTENSIONS = [
 
 
 def get_version():
-    """Get version number from __init__.py"""
+    """Get version number from __init__.py."""
     for line in open("Bio/__init__.py"):
         if line.startswith("__version__ = "):
             return ast.literal_eval(line.split("=")[1].strip())
