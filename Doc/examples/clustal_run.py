57fa79761462523b2763cfc6f8f5a6e0651d9614
Michiel de Hoon <mdehoon@MichielMacBook.local>
2023-12-08 20:53:05 +0900

    update

--- Doc/examples/clustal_run.py
+++ Doc/examples/clustal_run.py
@@ -17,7 +17,6 @@ import sys
 import subprocess
 
 # biopython
-from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
 from Bio.Align import AlignInfo
 from Bio.motifs import Motif
@@ -25,40 +24,33 @@ from Bio.motifs import Motif
 # create the command line to run clustalw
 # this assumes you've got clustalw somewhere on your path, otherwise
 # you need to pass the full path of the executable to this via cmd="..."
-cline = ClustalwCommandline(infile="opuntia.fasta", outfile="test.aln")
+command = "clustalw -infile=opuntia.fasta -outfile=test.aln"
 
 # actually perform the alignment
-return_code = subprocess.call(str(cline), shell=(sys.platform != "win32"))
+return_code = subprocess.call(command, shell=(sys.platform != "win32"))
 assert return_code == 0, "Calling ClustalW failed"
 
 # Parse the output
 msa = AlignIO.read("test.aln", "clustal")
+alignment = msa.alignment
 
-print(msa)
+print(alignment)
 
-print("first description: %s" % msa[0].description)
-print("first sequence: %s" % msa[0].seq)
+print("first description: %s" % alignment.sequences[0].description)
+print("first sequence: %s" % alignment.sequences[0].seq)
 
 # get the length of the alignment
-print("length %i" % msa.get_alignment_length())
-
-print(msa)
+print("length %i" % alignment.length)
 
 # print out interesting information about the alignment
 summary_align = AlignInfo.SummaryInfo(msa)
-
 consensus = summary_align.dumb_consensus()
 print("consensus %s" % consensus)
 
-alignment = msa.alignment
 motif = Motif("ACGT", alignment)
 counts = motif.counts
 print(counts)
 
-expect_freq = {"A": 0.3, "G": 0.2, "T": 0.3, "C": 0.2}
-
-info_content = summary_align.information_content(
-    5, 30, chars_to_ignore=["N"], e_freq_table=expect_freq
-)
-
-print("relative info content: %f" % info_content)
+motif.background = {"A": 0.3, "G": 0.2, "T": 0.3, "C": 0.2}
+relative_entropy = motif.relative_entropy
+print("relative entropy for columns [5:30]: %f" % sum(relative_entropy[5:30]))
