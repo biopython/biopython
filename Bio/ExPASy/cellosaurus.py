8051e8a87b08116f823f46d91b9ff196e03a2acb
Judith Bernett <bernett@in.tum.de>
2024-02-02 14:30:16 +0100

    Changed rst table

--- Bio/ExPASy/cellosaurus.py
+++ Bio/ExPASy/cellosaurus.py
@@ -87,27 +87,28 @@ class Record(dict):
 
     Each record contains the following keys:
 
-     ---------  ------------------------------  -----------------------
-     Line code  Content                         Occurrence in an entry
-     ---------  ------------------------------  -----------------------
-     ID         Identifier (cell line name)     Once; starts an entry
-     AC         Accession (CVCL_xxxx)           Once
-     AS         Secondary accession number(s)   Optional; once
-     SY         Synonyms                        Optional; once
-     DR         Cross-references                Optional; once or more
-     RX         References identifiers          Optional: once or more
-     WW         Web pages                       Optional; once or more
-     CC         Comments                        Optional; once or more
-     ST         STR profile data                Optional; twice or more
-     DI         Diseases                        Optional; once or more
-     OX         Species of origin               Once or more
-     HI         Hierarchy                       Optional; once or more
-     OI         Originate from same individual  Optional; once or more
-     SX         Sex of cell                     Optional; once
-     AG         Age of donor at sampling        Optional; once
-     CA         Category                        Once
-     DT         Date (entry history)            Once
-     //         Terminator                      Once; ends an entry
+    =========  ==============================  =======================
+    Line code  Content                         Occurrence in an entry
+    =========  ==============================  =======================
+    ID         Identifier (cell line name)     Once; starts an entry
+    AC         Accession (CVCL_xxxx)           Once
+    AS         Secondary accession number(s)   Optional; once
+    SY         Synonyms                        Optional; once
+    DR         Cross-references                Optional; once or more
+    RX         References identifiers          Optional: once or more
+    WW         Web pages                       Optional; once or more
+    CC         Comments                        Optional; once or more
+    ST         STR profile data                Optional; twice or more
+    DI         Diseases                        Optional; once or more
+    OX         Species of origin               Once or more
+    HI         Hierarchy                       Optional; once or more
+    OI         Originate from same individual  Optional; once or more
+    SX         Sex of cell                     Optional; once
+    AG         Age of donor at sampling        Optional; once
+    CA         Category                        Once
+    DT         Date (entry history)            Once
+    //         Terminator                      Once; ends an entry
+    =========  ==============================  =======================
 
     """
 
