248f94473ae88927ce2afed7e50f6dc662543db5
Peter Cock <p.j.a.cock@googlemail.com>
2024-03-15 09:34:28 +0000

    raise MissingPythonDependencyError(...) from None
    Cherry-picked from a PR

--- Tests/test_PDB_internal_coords.py
+++ Tests/test_PDB_internal_coords.py
@@ -16,7 +16,9 @@ try:
 except ImportError:
     from Bio import MissingPythonDependencyError
 
-    raise MissingPythonDependencyError("Install NumPy if you want to use Bio.PDB.")
+    raise MissingPythonDependencyError(
+        "Install NumPy if you want to use Bio.PDB."
+    ) from None
 
 from Bio.PDB.ic_rebuild import (
     structure_rebuild_test,
