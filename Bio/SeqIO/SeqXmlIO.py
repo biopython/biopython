bfe57658a5c47e5eb1e47e89bc5b30a6ff97e198
mdehoon <mjldehoon@yahoo.com>
2024-03-31 08:49:53 +0900

    using original block size

--- Bio/SeqIO/SeqXmlIO.py
+++ Bio/SeqIO/SeqXmlIO.py
@@ -442,7 +442,7 @@ class SeqXmlIterator(SequenceIterator):
     """
 
     # Small block size can be a problem with libexpat 2.6.0 onwards:
-    BLOCK = 4096
+    BLOCK = 1024
 
     def __init__(self, stream_or_path, namespace=None):
         """Create the object and initialize the XML parser."""
