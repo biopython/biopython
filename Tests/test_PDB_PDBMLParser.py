797dcbfe2f41553de52e49e3f6e4901467aac26e
Peter Cock <p.j.a.cock@googlemail.com>
2024-05-13 11:25:42 +0100

    Silence warning 'Residue (...) redefined at line ...' in test

--- Tests/test_PDB_PDBMLParser.py
+++ Tests/test_PDB_PDBMLParser.py
@@ -6,9 +6,11 @@ returned by the mmCIF parser for any PDB structure.
 """
 
 import unittest
+import warnings
 
 from Bio.PDB import MMCIFParser
 from Bio.PDB import PDBMLParser
+from Bio.PDB.PDBExceptions import PDBConstructionWarning
 
 
 class TestPDBMLParser(unittest.TestCase):
@@ -16,21 +18,25 @@ class TestPDBMLParser(unittest.TestCase):
         mmcif_parser = MMCIFParser()
         pdbml_parser = PDBMLParser()
 
-        for entry in ["1GBT", "6WG6", "3JQH"]:
-            mmcif_structure = mmcif_parser.get_structure(entry, f"PDB/{entry}.cif")
-            pdbml_structure = pdbml_parser.get_structure(f"PDB/{entry}.xml")
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", PDBConstructionWarning)
+            for entry in ["1GBT", "6WG6", "3JQH"]:
+                mmcif_structure = mmcif_parser.get_structure(entry, f"PDB/{entry}.cif")
+                pdbml_structure = pdbml_parser.get_structure(f"PDB/{entry}.xml")
             self.assertEqual(mmcif_structure, pdbml_structure)
 
     def test_get_structure_filehandle(self):
         mmcif_parser = MMCIFParser()
         pdbml_parser = PDBMLParser()
 
-        for entry in ["1GBT"]:
-            with open(f"PDB/{entry}.cif") as mmcif_file, open(
-                f"PDB/{entry}.xml"
-            ) as pdbml_file:
-                mmcif_structure = mmcif_parser.get_structure(entry, mmcif_file)
-                pdbml_structure = pdbml_parser.get_structure(pdbml_file)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", PDBConstructionWarning)
+            for entry in ["1GBT"]:
+                with open(f"PDB/{entry}.cif") as mmcif_file, open(
+                    f"PDB/{entry}.xml"
+                ) as pdbml_file:
+                    mmcif_structure = mmcif_parser.get_structure(entry, mmcif_file)
+                    pdbml_structure = pdbml_parser.get_structure(pdbml_file)
                 self.assertEqual(mmcif_structure, pdbml_structure)
 
 
