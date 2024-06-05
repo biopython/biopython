d6b25bf888973146c4fb7c1d72b7e88929b97d03
mdehoon <mjldehoon@yahoo.com>
2024-06-05 10:47:59 +0900

    test

--- Bio/Align/__init__.py
+++ Bio/Align/__init__.py
@@ -1037,10 +1037,10 @@ class Alignment:
         >>> sequences
         ['TAGGCATACGTG', 'AACGTACGT', 'ACGCATACTTG']
         >>> coordinates = Alignment.infer_coordinates(lines)
-        >>> print(coordinates)
-        [[ 0  1  4  6 11 12]
-         [ 0  1  4  4  9  9]
-         [ 0  0  3  5 10 11]]
+        >>> coordinates
+        array([[ 0,  1,  4,  6, 11, 12],
+               [ 0,  1,  4,  4,  9,  9],
+               [ 0,  0,  3,  5, 10, 11]])
         >>> alignment = Alignment(sequences, coordinates)
         """
         warnings.warn(
