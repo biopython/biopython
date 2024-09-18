b28c438e78267469525a94e54a80e4eb189b5da8
Samuel Prince <samuel.prince-drouin@umontreal.ca>
2024-09-18 12:53:16 -0400

    Changed tests files names

--- Tests/test_SearchIO_infernal_tab_index.py
+++ Tests/test_SearchIO_infernal_tab_index.py
@@ -21,16 +21,16 @@ class InfernalTabRawCases(CheckRaw):
 
 
     def test_infernal_tab_single(self):
-        """Test infernal-tab raw string retrieval, cmsearch, single query."""
-        filename = os.path.join("Infernal", "U2_Yeast-threshold.tbl")
+        """Test infernal-tab raw string retrieval, cmsearch, single query (U2_Yeast)."""
+        filename = os.path.join("Infernal", "cmsearch_114_U2_Yeast.tbl")
         raw = """ENA|BK006936|BK006936.2 -         U2                   RF00004    cm        1      193   681858   681747      -    no    1 0.33   0.1   98.7   5.9e-20 !   TPA_inf: Saccharomyces cerevisiae S288C chromosome II, complete sequence.
 """
         self.check_raw(filename, "U2", raw)
 
     
     def test_infernal_tab_multiple(self):
-        """Test infernal-tab raw string retrieval, cmsearch, single query, multiple non-consecutive hits."""
-        filename = os.path.join("Infernal", "U2_Yeast-shuf.tbl")
+        """Test infernal-tab raw string retrieval, cmsearch, single query, multiple non-consecutive hits (U2_Yeast_full_shuffled)."""
+        filename = os.path.join("Infernal", "cmsearch_114_U2_Yeast_full_shuffled.tbl")
         raw = """ENA|BK006936|BK006936.2 -         U2                   RF00004    cm        1      193   681858   681747      -    no    1 0.33   0.1   98.7   1.3e-20 !   -
 ENA|BK006948|BK006948.2 -         U2                   RF00004    cm        1      193   737498   737324      -    no    1 0.39   0.0   19.8      0.11 ?   -
 ENA|BK006936|BK006936.2 -         U2                   RF00004    cm        1      193  1370418  1370563      +    no    1 0.34   0.1   15.6       1.1 ?   -
@@ -43,7 +43,7 @@ ENA|BK006948|BK006948.2 -         U2                   RF00004    cm        1   
 
     def test_infernal_tab_single_first(self):
         """Test infernal-tab raw string retrieval, cmsearch, multiple queries, first."""
-        filename = os.path.join("Infernal", "IRES_5S_U2_Yeast-cmscan.tbl")
+        filename = os.path.join("Infernal", "cmscan_115_IRES_5S_U2_Yeast.tbl")
         raw = """U2                   RF00004   ENA|BK006935|BK006935.2 -          cm        1      193    52929    53083      +    no    1 0.44   0.0   13.5      0.91 ?   U2 spliceosomal RNA
 U2                   RF00004   ENA|BK006935|BK006935.2 -          cm        1      193   196571   196389      -    no    1 0.33   5.3   12.8       1.3 ?   U2 spliceosomal RNA
 """
@@ -52,7 +52,7 @@ U2                   RF00004   ENA|BK006935|BK006935.2 -          cm        1   
     
     def test_infernal_tab_multiple_middle(self):
         """Test infernal-tab raw string retrieval, cmsearch, multiple queries, middle."""
-        filename = os.path.join("Infernal", "IRES_5S_U2_Yeast-cmscan.tbl")
+        filename = os.path.join("Infernal", "cmscan_115_IRES_5S_U2_Yeast.tbl")
         raw = """U2                   RF00004   ENA|BK006936|BK006936.2 -          cm        1      193   681858   681747      -    no    1 0.33   0.1   98.7   1.2e-20 !   U2 spliceosomal RNA
 """
         self.check_raw(filename, "ENA|BK006936|BK006936.2", raw)
@@ -60,7 +60,7 @@ U2                   RF00004   ENA|BK006935|BK006935.2 -          cm        1   
 
     def test_infernal_tab_multiple_last(self):
         """Test infernal-tab raw string retrieval, cmsearch, multiple queries, last."""
-        filename = os.path.join("Infernal", "IRES_5S_U2_Yeast-cmscan.tbl")
+        filename = os.path.join("Infernal", "cmscan_115_IRES_5S_U2_Yeast.tbl")
         raw = """5S_rRNA              RF00001   ENA|BK006937|BK006937.2 -          cm        1      119      761      644      -    no    1 0.41   0.3   14.1       2.4 ?   5S ribosomal RNA
 U2                   RF00004   ENA|BK006937|BK006937.2 -          cm        1      193   229986   229885      -    no    1 0.32   0.1   11.1       4.7 ?   U2 spliceosomal RNA
 """
@@ -69,7 +69,7 @@ U2                   RF00004   ENA|BK006937|BK006937.2 -          cm        1   
     
     def test_infernal_tab_multiple_fmt_2(self):
         """Test infernal-tab raw string retrieval, cmsearch, multiple queries, fmt 2."""
-        filename = os.path.join("Infernal", "IRES_5S_U2_Yeast-cmscan-fmt_2.tbl")
+        filename = os.path.join("Infernal", "cmscan_115_IRES_5S_U2_Yeast_fmt_2.tbl")
         raw = """1    U2                   RF00004   ENA|BK006936|BK006936.2 -         -          cm        1      193   681858   681747      -    no    1 0.33   0.1   98.7   1.2e-20  !   *       -      -      -      -      -      -     193  813184 U2 spliceosomal RNA
 """
         self.check_raw(filename, "ENA|BK006936|BK006936.2", raw)
@@ -81,25 +81,25 @@ class InfernalTabIndexCases(CheckIndex):
 
     def test_infernal_tab_single(self):
         """Test infernal-tab indexing, cmsearch, single query."""
-        filename = os.path.join("Infernal", "U2_Yeast-threshold.tbl")
+        filename = os.path.join("Infernal", "cmsearch_114_U2_Yeast.tbl")
         self.check_index(filename, self.fmt)
 
     
     def test_infernal_tab_single_no_hit(self):
         """Test infernal-tab indexing, cmsearch, single query."""
-        filename = os.path.join("Infernal", "IRES_Yeast.tbl")
+        filename = os.path.join("Infernal", "cmsearch_114_IRES_Yeast.tbl")
         self.check_index(filename, self.fmt)
 
 
     def test_infernal_tab_single_multiple_hit(self):
         """Test infernal-tab indexing, cmsearch, single query."""
-        filename = os.path.join("Infernal", "5S_Yeast.tbl")
+        filename = os.path.join("Infernal", "cmsearch_114_5S_Yeast.tbl")
         self.check_index(filename, self.fmt)
     
 
     def test_infernal_tab_multiple_multiple_hit(self):
         """Test infernal-tab indexing, cmscan, multiple query."""
-        filename = os.path.join("Infernal", "IRES_5S_U2_Yeast-cmscan.tbl")
+        filename = os.path.join("Infernal", "cmscan_115_IRES_5S_U2_Yeast.tbl")
         self.check_index(filename, self.fmt)
 
 
