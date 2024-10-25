05bc05321a70316d41f52b6d4f3b256201f83952
Samuel Prince <samuel.prince-drouin@umontreal.ca>
2024-10-25 11:57:15 -0400

    Add InfernalIO module to setup.py

--- setup.py
+++ setup.py
@@ -168,6 +168,7 @@ PACKAGES = [
     "Bio.SearchIO.BlastIO",
     "Bio.SearchIO.HHsuiteIO",
     "Bio.SearchIO.HmmerIO",
+    "Bio.SearchIO.InfernalIO",
     "Bio.SearchIO.ExonerateIO",
     "Bio.SearchIO.InterproscanIO",
     "Bio.SeqIO",
