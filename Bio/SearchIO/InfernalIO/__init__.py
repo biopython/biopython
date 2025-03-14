biopython/biopython

commit e477fd89c9815a996fe406912d436b2da00315aa

Remove reference to the fmt override in the documentation

Bio/SearchIO/InfernalIO/__init__.py
@@ -132,16 +132,17 @@ infernal-tab
 ============
 
 The Infernal plain text parser supports the standard cmsearch tabular output and
-cmscan tabular output files formats 1, 2 and 3.
+cmscan tabular output files formats 1, 2 and 3 (formats are inferred 
+automatically). 
 
     >>> # set a non-default cmscan tabular output format
     >>> from Bio import SearchIO
-    >>> file = 'Infernal/cmscan_115_IRES_5S_U2_Yeast_fmt_2.tbl'
+    >>> file = 'Infernal/cmscan_115_IRES_5S_U2_Yeast.tbl'
     >>> qresult = SearchIO.read(file, 'infernal-tab')
     >>> qresult
     QueryResult(id='ENA|BK006936|BK006936.2', 1 hits)
 
-Rows marked with '*' denotes attributes not available in the default format (`--fmt 1`).
+Rows marked with '*' denotes attributes not available in the default format.
 
 +-----------------+-------------------------+----------------------------------+
 | Object          | Attribute               | Value                            |

commit 3454c6e8030cd9d38b9191221f6546c48a173441

Fixed issues in docstring introduced in e477fd8

Bio/SearchIO/InfernalIO/__init__.py
@@ -137,10 +137,10 @@ automatically).
 
     >>> # set a non-default cmscan tabular output format
     >>> from Bio import SearchIO
-    >>> file = 'Infernal/cmscan_115_IRES_5S_U2_Yeast.tbl'
-    >>> qresult = SearchIO.read(file, 'infernal-tab')
-    >>> qresult
-    QueryResult(id='ENA|BK006936|BK006936.2', 1 hits)
+    >>> file = 'Infernal/cmscan_115_IRES_5S_U2_Yeast_fmt_2.tbl'
+    >>> qresult = SearchIO.parse(file, 'infernal-tab')
+    >>> next(qresult)
+    QueryResult(id='ENA|BK006935|BK006935.2', 1 hits)
 
 Rows marked with '*' denotes attributes not available in the default format.
 