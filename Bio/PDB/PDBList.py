41fa46f50e01fe2ffc30bba4a604736d13e6f353
lee-t <tyrone_lee@hms.harvard.edu>
2025-05-18 21:25:44 -0400

    format with black

--- Bio/PDB/PDBList.py
+++ Bio/PDB/PDBList.py
@@ -341,16 +341,20 @@ class PDBList:
         except HTTPError as e:
             print(
                 f"Failed to download PDB structure: '{pdb_code}':"
-                f" HTTP Error {e.code} - {e.reason}")
+                f" HTTP Error {e.code} - {e.reason}"
+            )
             return None
         except URLError as e:
             print(
                 f"Failed to download PDB structure: '{pdb_code}':"
-                f" URL Error {e.reason}")
+                f" URL Error {e.reason}"
+            )
             return None
         except OSError as e:
-            print(f"Desired structure not found or doesn't exist:"
-                  f" '{pdb_code}': {str(e)}")
+            print(
+                f"Desired structure not found or doesn't exist:"
+                f" '{pdb_code}': {str(e)}"
+            )
             return None
         else:
             with gzip.open(filename, "rb") as gz:
