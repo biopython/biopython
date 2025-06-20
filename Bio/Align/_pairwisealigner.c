9f4305a39ee83449f5843bd6325a2381a3bb7c55
Michiel Jan Laurens de Hoon <mdehoon@Michiels-MacBook-Air.local>
2025-06-20 18:43:04 +0900

    update

--- Bio/Align/_pairwisealigner.c
+++ Bio/Align/_pairwisealigner.c
@@ -1,4 +1,4 @@
-/* Copyright 2018-2019 by Michiel de Hoon.  All rights reserved.
+/* Copyright 2018-2025 by Michiel de Hoon.  All rights reserved.
  * This file is part of the Biopython distribution and governed by your
  * choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
  * Please see the LICENSE file that should have been included as part of this
