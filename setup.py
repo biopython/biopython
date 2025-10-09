37f6c5f81d64c35aa00c5c37c66367bcbc15c68a
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-10-09 09:35:17 +0100

    Test on Python 3.10 ~ 3.14

--- setup.py
+++ setup.py
@@ -261,6 +261,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Software Development :: Libraries :: Python Modules",
